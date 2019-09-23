98ad88f4d537665b9de851663a9a224c409329cb
Endless <loli.con@qq.com>
2019-09-23 19:50:33 +0800

    Add 981D with test.

---
+++ main/981D.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+func found(bits uint64, sum []uint64, k int) bool {
+	n := len(sum) - 1
+	var dp [51][51]bool
+	for r := 1; r <= n; r++ {
+		dp[1][r] = bits&sum[r] == bits
+	}
+	for i := 2; i <= k; i++ {
+		for l := i - 1; l < n; l++ {
+			if dp[i-1][l] {
+				for r := l + 1; r <= n; r++ {
+					dp[i][r] = dp[i][r] || bits&(sum[r]-sum[l]) == bits
+				}
+			}
+		}
+	}
+	return dp[k][n]
+}
+
+func Sol981D(reader io.Reader, writer io.Writer) {
+	in := bufio.NewReader(reader)
+	out := bufio.NewWriter(writer)
+	defer out.Flush()
+
+	var n, k int
+	Fscan(in, &n, &k)
+	sum := make([]uint64, n+1)
+	for i := 1; i <= n; i++ {
+		var a uint64
+		Fscan(in, &a)
+		sum[i] = sum[i-1] + a
+	}
+
+	ans := uint64(0)
+	for i := 55; i >= 0; i-- {
+		pow2 := uint64(1) << uint(i)
+		if found(ans|pow2, sum, k) {
+			ans |= pow2
+		}
+	}
+
+	Fprintln(out, ans)
+}
+
+func main() {
+	Sol981D(os.Stdin, os.Stdout)
+}
---
+++ main/981D_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"testing"
+)
+
+func TestSol981D(t *testing.T) {
+	// just copy from website
+	rawText := `10 4
+9 14 28 1 7 13 15 29 2 31
+outputCopy
+24
+inputCopy
+7 3
+3 14 15 92 65 35 89
+outputCopy
+64`
+	testutil.AssertEqual(t, rawText, Sol981D)
+}
