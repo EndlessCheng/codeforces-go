7d00dca4e092555aebdf5c7ecc446ad0dcc5cb2c
Endless <loli.con@qq.com>
2019-09-24 11:44:38 +0800

    Add quickPow

--- copypasta/common.go
+++ copypasta/common.go
@@ -14,4 +14,16 @@ func commonFunc() {
 		}
 		return b
 	}
+
+	const mod = int64(1e9 + 7)
+	quickPow := func(x, n int64) int64 {
+		res := int64(1)
+		for ; n > 0; n >>= 1 {
+			if n&1 == 1 {
+				res = res * x % mod
+			}
+			x = x * x % mod
+		}
+		return res
+	}
 }
