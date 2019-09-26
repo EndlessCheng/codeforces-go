4e68f98a3851c5399810850f33bb67ce0fb5fff4
Endless <loli.con@qq.com>
2019-09-26 20:13:47 +0800

    Add 118D with test

---
+++ main/118D_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"testing"
+)
+
+func TestSol118D(t *testing.T) {
+	// just copy from website
+	rawText := `
+2 1 1 10
+outputCopy
+1
+inputCopy
+2 3 1 2
+outputCopy
+5
+inputCopy
+2 4 1 1
+outputCopy
+0`
+	testutil.AssertEqual(t, rawText, Sol118D)
+}
