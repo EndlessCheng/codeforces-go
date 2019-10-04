bdb19063dc37c736c67ad54a884aa8f0fc6c8295
Endless <loli.con@qq.com>
2019-10-04 21:15:47 +0800

    Add lowbit and update maxPow2

--- copypasta/bits.go
+++ copypasta/bits.go
@@ -4,15 +4,17 @@ package copypasta
 注意 11100001 的情况
 */
 func bitsCollection() {
+	// ^n+1 = (-1-n)+1 = -n
+	lowbit := func(n int64) int64 { return n & -n }
+
 	maxPow2 := func(n int64) int64 {
 		if n == 0 {
 			return 0
 		}
-		msb := uint(0)
-		for n >>= 1; n != 0; n >>= 1 {
-			msb++
+		for lb := lowbit(n); n != lb; lb = lowbit(n) {
+			n -= lb
 		}
-		return int64(1) << msb
+		return n
 	}
 
 	_ = []interface{}{maxPow2}
