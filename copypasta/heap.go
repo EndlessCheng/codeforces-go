3d2a7fa51958591b398e88ab94ef881af3a4f869
Endless <loli.con@qq.com>
2019-10-04 15:32:18 +0800

    Add intHeap and pairHeap

--- copypasta/heap.go
+++ copypasta/heap.go
@@ -1,5 +1,33 @@
 package copypasta
 
+import "sort"
+
+type intHeap struct {
+	sort.IntSlice
+}
+
+func (h *intHeap) Push(v interface{}) { h.IntSlice = append(h.IntSlice, v.(int)) }
+func (h *intHeap) Pop() (v interface{}) {
+	n := len(h.IntSlice)
+	h.IntSlice, v = h.IntSlice[:n-1], h.IntSlice[n-1]
+	return
+}
+
+//
+
+type pair struct {
+	x, y int
+}
+type pairHeap []pair
+
+func (h pairHeap) Len() int              { return len(h) }
+func (h pairHeap) Less(i, j int) bool    { return h[i].x < h[j].x || h[i].x == h[j].x && h[i].y < h[j].y }
+func (h pairHeap) Swap(i, j int)         { h[i], h[j] = h[j], h[i] }
+func (h *pairHeap) Push(v interface{})   { *h = append(*h, v.(pair)) }
+func (h *pairHeap) Pop() (v interface{}) { n := len(*h); *h, v = (*h)[:n-1], (*h)[n-1]; return }
+
+//
+
 type prioQueue struct {
 	heap  []int // vertices in heap order
 	index []int // index of each vertex in the heap
