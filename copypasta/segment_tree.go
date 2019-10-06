03a768abe6c743356b08c6ed0cdae6fac4f41a2a
Endless <loli.con@qq.com>
2019-10-06 23:49:21 +0800

    Add lazySegmentTree

--- copypasta/segment_tree.go
+++ copypasta/segment_tree.go
@@ -1,6 +1,6 @@
 package copypasta
 
-func max(a, b int) int {
+func max(a, b int64) int64 {
 	if a >= b {
 		return a
 	}
@@ -10,18 +10,18 @@ func max(a, b int) int {
 //
 
 type node struct {
-	l, r int
-	val  int
+	l, r int // 也可以写到方法参数上，实测二者在执行效率上无异。考虑到 debug 和 bug free 上的优点，写到结构体参数中。
+	val  int64
 }
 type segmentTree []node // t := make(segmentTree, 4*n)
 
 func (t segmentTree) _pushUp(o int) {
-	lo, ro := t[o<<1], t[o<<1|1]
+	lo, ro := t[o<<1], t[o<<1|1] // 必要时用指针
 	// *custom*
 	t[o].val = max(lo.val, ro.val)
 }
 
-func (t segmentTree) _build(arr []int, o, l, r int) {
+func (t segmentTree) _build(arr []int64, o, l, r int) {
 	t[o].l, t[o].r = l, r
 	if l == r {
 		t[o].val = arr[l]
@@ -37,7 +37,7 @@ func (t segmentTree) _build(arr []int, o, l, r int) {
 	t._pushUp(o)
 }
 
-func (t segmentTree) _update(o, idx int, val int) {
+func (t segmentTree) _update(o, idx int, val int64) {
 	if t[o].l == t[o].r {
 		t[o].val = val
 		return
@@ -50,7 +50,7 @@ func (t segmentTree) _update(o, idx int, val int) {
 	t._pushUp(o)
 }
 
-func (t segmentTree) _query(o, l, r int) (res int) {
+func (t segmentTree) _query(o, l, r int) (res int64) {
 	if l <= t[o].l && t[o].r <= r {
 		return t[o].val
 	}
@@ -67,15 +67,86 @@ func (t segmentTree) _query(o, l, r int) (res int) {
 }
 
 // NOTE: arr must start at 1
-func (t segmentTree) init(arr []int)          { t._build(arr, 1, 1, len(arr)-1) }
-func (t segmentTree) update(idx int, val int) { t._update(1, idx, val) }
-func (t segmentTree) query(l, r int)          { t._query(1, l, r) } // [l,r]
+func (t segmentTree) init(arr []int64)          { t._build(arr, 1, 1, len(arr)-1) }
+func (t segmentTree) update(idx int, val int64) { t._update(1, idx, val) }
+func (t segmentTree) query(l, r int) int64      { return t._query(1, l, r) } // [l,r]
 
 //
 
-type node2 struct {
-	l, r int
-	val  int
-	// TODO
+type lazyNode struct {
+	l, r        int
+	sum         int64
+	addChildren int64 // 子节点待更新
 }
-type lazySegmentTree []node2 // t := make(lazySegmentTree, 4*n)
+type lazySegmentTree []lazyNode // t := make(lazySegmentTree, 4*n)
+
+func (t lazySegmentTree) _pushUp(o int) {
+	lo, ro := t[o<<1], t[o<<1|1] // 必要时用指针
+	// *custom*
+	t[o].sum = lo.sum + ro.sum
+}
+
+func (t lazySegmentTree) _build(arr []int64, o, l, r int) {
+	t[o].l, t[o].r = l, r
+	if l == r {
+		t[o].sum = arr[l]
+		// *custom*
+		return
+	}
+	mid := (l + r) >> 1
+	t._build(arr, o<<1, l, mid)
+	t._build(arr, o<<1|1, mid+1, r)
+
+	// *custom* after built children
+
+	t._pushUp(o)
+}
+
+func (t lazySegmentTree) _spread(o int) {
+	if add := t[o].addChildren; add != 0 {
+		lo, ro := &t[o<<1], &t[o<<1|1]
+		lo.sum += add * int64(lo.r-lo.l+1)
+		ro.sum += add * int64(ro.r-ro.l+1)
+		lo.addChildren += add
+		ro.addChildren += add
+		t[o].addChildren = 0
+	}
+}
+
+func (t lazySegmentTree) _update(o, l, r int, add int64) {
+	if l <= t[o].l && t[o].r <= r {
+		t[o].sum += add * int64(t[o].r-t[o].l+1)
+		t[o].addChildren += add
+		return
+	}
+	t._spread(o)
+	mid := (t[o].l + t[o].r) >> 1
+	if l <= mid {
+		t._update(o<<1, l, r, add)
+	}
+	if mid < r {
+		t._update(o<<1|1, l, r, add)
+	}
+	t._pushUp(o)
+}
+
+func (t lazySegmentTree) _query(o, l, r int) (res int64) {
+	if l <= t[o].l && t[o].r <= r {
+		return t[o].sum
+	}
+	t._spread(o)
+	mid := (t[o].l + t[o].r) >> 1
+	// *custom*
+	if l <= mid {
+		res += t._query(o<<1, l, r)
+	}
+	if mid < r {
+		res += t._query(o<<1|1, l, r)
+	}
+	return
+}
+
+// NOTE: arr must start at 1
+func (t lazySegmentTree) init(arr []int64)           { t._build(arr, 1, 1, len(arr)-1) }
+func (t lazySegmentTree) update(l, r int, val int64) { t._update(1, l, r, val) }
+func (t lazySegmentTree) query(l, r int) int64       { return t._query(1, l, r) } // [l,r]
---
+++ copypasta/segment_tree_test.go
@@ -0,0 +1,20 @@
+package copypasta
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_lazySegmentTree(t *testing.T) {
+	assert := assert.New(t)
+
+	n := 10
+	st := make(lazySegmentTree, 4*n)
+	st.init(make([]int64, n+1))
+	st.update(1, 10, 100)
+	st.update(2, 4, 100)
+	assert.EqualValues(100, st.query(1,1))
+	assert.EqualValues(200, st.query(3, 3))
+	assert.EqualValues(400, st.query(3, 4))
+	assert.EqualValues(1300, st.query(1, 10))
+}
