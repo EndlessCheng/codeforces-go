76153b6740e2bd83e43e1223d134c40675e546ba
Endless <loli.con@qq.com>
2019-10-06 21:51:57 +0800

    Add 1221D with test

--- main/1221D.go
+++ main/1221D.go
@@ -28,7 +28,7 @@ func Sol1221D(reader io.Reader, writer io.Writer) {
 	out := bufio.NewWriter(writer)
 	defer out.Flush()
 
-	var dp [int(3e5)][3]int64
+	var dp [2][3]int64
 	var t int
 	for Fscan(in, &t); t > 0; t-- {
 		var n int
@@ -39,19 +39,23 @@ func Sol1221D(reader io.Reader, writer io.Writer) {
 			Fscan(in, &h[i], &costs[i])
 		}
 
+		row := 0
+		dp[0][0] = 0
 		dp[0][1] = int64(costs[0])
 		dp[0][2] = int64(2 * costs[0])
 		for i := 1; i < n; i++ {
+			row ^= 1
 			for d0 := 0; d0 < 3; d0++ {
-				dp[i][d0] = inf
+				ans := inf
 				for d1 := 0; d1 < 3; d1++ {
 					if h[i]+d0 != h[i-1]+d1 {
-						dp[i][d0] = min(dp[i][d0], dp[i-1][d1]+int64(d0*costs[i]))
+						ans = min(ans, dp[row^1][d1]+int64(d0*costs[i]))
 					}
 				}
+				dp[row][d0] = ans
 			}
 		}
-		Fprintln(out, mins(dp[n-1][:]...))
+		Fprintln(out, mins(dp[row][:]...))
 	}
 }
 
