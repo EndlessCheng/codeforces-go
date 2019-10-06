f75840a7ff9b0230d8e784e36abcc4c8c39d344a
Endless <loli.con@qq.com>
2019-10-06 19:28:07 +0800

    Add 126B with test

---
+++ main/126B.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"strings"
+)
+
+func Sol126B(reader io.Reader, writer io.Writer) {
+	calcMaxMatchLengths := func(pattern string) []int {
+		n := len(pattern)
+		maxMatchLengths := make([]int, n)
+		maxLength := 0
+		for i := 1; i < n; i++ {
+			c := pattern[i]
+			for maxLength > 0 && pattern[maxLength] != c {
+				maxLength = maxMatchLengths[maxLength-1]
+			}
+			if pattern[maxLength] == c {
+				maxLength++
+			}
+			maxMatchLengths[i] = maxLength
+		}
+		return maxMatchLengths
+	}
+
+	in := bufio.NewReader(reader)
+	out := bufio.NewWriter(writer)
+	defer out.Flush()
+
+	const fail = "Just a legend"
+	var s string
+	Fscan(in, &s)
+	if len(s) < 3 {
+		Fprint(out, fail)
+		return
+	}
+	inS := s[1 : len(s)-1]
+
+	lens := calcMaxMatchLengths(s)
+	for maxLen := lens[len(s)-1]; maxLen > 0; maxLen = lens[maxLen-1] {
+		if strings.Contains(inS, s[:maxLen]) {
+			Fprint(out, s[:maxLen])
+			return
+		}
+	}
+	Fprint(out, fail)
+}
+
+//func main() {
+//	Sol126B(os.Stdin, os.Stdout)
+//}
