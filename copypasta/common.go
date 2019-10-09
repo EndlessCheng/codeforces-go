8647f9bb543c6abdb63e3493f4fc2c11e04ef451
Endless <loli.con@qq.com>
2019-10-09 16:40:27 +0800

    Add pow10 and unique

--- copypasta/common.go
+++ copypasta/common.go
@@ -5,6 +5,7 @@ func commonCollection() {
 	const inf int = 0x3f3f3f3f
 	const inf64 int64 = 0x3f3f3f3f3f3f3f3f
 	pow2 := [...]int{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144}
+	pow10 := [...]int{1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9}
 
 	min := func(a, b int) int {
 		if a <= b {
@@ -36,6 +37,17 @@ func commonCollection() {
 			}
 		}
 		return ans
+	}
+
+	// arr need to be sorted
+	unique := func(arr []int) (newArr []int) {
+		newArr = append(newArr, arr[0])
+		for i := 1; i < len(arr); i++ {
+			if arr[i] != arr[i-1] {
+				newArr = append(newArr, arr[i])
+			}
+		}
+		return
 	}
 
 	ifElse := func(cond bool, r1, r2 string) string {
@@ -106,7 +118,7 @@ func commonCollection() {
 	dirOffset4 := [4][2]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
 	dirOffset8 := [8][2]int{{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}}
 
-	_ = []interface{}{pow2, min, mins, max, maxs, ifElse, abs, quickPow, reverse, binarySearchF, ternarySearch, dirOffset4, dirOffset8}
+	_ = []interface{}{pow2, pow10, min, mins, max, maxs, ifElse, unique, abs, quickPow, reverse, binarySearchF, ternarySearch, dirOffset4, dirOffset8}
 }
 
 // Permute the values at index i to len(arr)-1.
