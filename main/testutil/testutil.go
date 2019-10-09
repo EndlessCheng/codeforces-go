0f055512be6dcd1691e4f3a41578182ca5620106
Endless <loli.con@qq.com>
2019-10-09 21:48:48 +0800

    Update testutil

--- main/testutil/testutil.go
+++ main/testutil/testutil.go
@@ -20,6 +20,8 @@ func AssertEqualCase(t *testing.T, rawText string, useCase int, solFunc func(io.
 		outputs = append(outputs, splits[1])
 	}
 
+	// TODO: time costs
+	var ok bool
 	for i, input := range inputs {
 		if useCase >= 0 && i+1 != useCase {
 			continue
@@ -31,7 +33,10 @@ func AssertEqualCase(t *testing.T, rawText string, useCase int, solFunc func(io.
 		if actualOutput != "" && actualOutput[len(actualOutput)-1] == '\n' {
 			actualOutput = actualOutput[:len(actualOutput)-1]
 		}
-		assert.Equal(t, outputs[i], actualOutput, "Please check test case %d", i+1)
+		ok = assert.Equal(t, outputs[i], actualOutput, "Please check test case %d", i+1)
+	}
+	if useCase >= 0 && ok {
+		t.Skip("OK, NOW TRY TO TEST ALL CASES!")
 	}
 }
 
