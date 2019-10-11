dd88ef1f690dfe6614c5f197a610087a081828c3
Endless <loli.con@qq.com>
2019-10-12 00:52:51 +0800

    Add sieve divisors primeFactors

--- copypasta/math.go
+++ copypasta/math.go
@@ -11,16 +11,60 @@ func mathCollection() {
 		return a / calcGCD(a, b) * b
 	}
 
-	isPrime := func(n int) bool {
-		if n < 2 {
-			return false
-		}
-		for i := 2; i*i <= n; i++ {
+	isPrime := func(n int64) bool {
+		for i := int64(2); i*i <= n; i++ {
 			if n%i == 0 {
 				return false
 			}
 		}
-		return true
+		return n >= 2
+	}
+
+	sieve := func(n int) (prime []int, isPrime []bool) {
+		isPrime = make([]bool, n+1)
+		for i := range isPrime {
+			isPrime[i] = true
+		}
+		isPrime[0], isPrime[1] = false, false
+		for i := 2; i <= n; i++ {
+			if isPrime[i] {
+				prime = append(prime, i)
+				for j := 2 * i; j <= n; j += i {
+					isPrime[j] = false
+				}
+			}
+		}
+		return
+	}
+
+	divisors := func(n int64) (res []int64) {
+		for i := int64(1); i*i <= n; i++ {
+			if n%i == 0 {
+				res = append(res, i)
+				if d := n / i; d != i {
+					res = append(res, d)
+				}
+			}
+		}
+		return
+	}
+
+	primeFactors := func(n int64) (factors []int64, exps []int) {
+		for i := int64(2); i*i <= n; i++ {
+			cnt := 0
+			for ; n%i == 0; n /= i {
+				cnt++
+			}
+			if cnt > 0 {
+				factors = append(factors, i)
+				exps = append(exps, cnt)
+			}
+		}
+		if n != 1 {
+			factors = append(factors, n)
+			exps = append(exps, 1)
+		}
+		return
 	}
 
 	// ax ≡ 1 (mod m)
@@ -29,7 +73,7 @@ func mathCollection() {
 		return (x%m + m) % m
 	}
 
-	_ = []interface{}{calcLCM, isPrime, modInverse}
+	_ = []interface{}{calcLCM, isPrime, sieve, divisors, primeFactors, modInverse}
 }
 
 // exgcd solve equation ax+by=gcd(a,b)
