46abfc3e1451e5c4e8f8478782516bc4bf69bac2
Endless <loli.con@qq.com>
2019-10-15 17:17:31 +0800

    Add 1168A with test (二分最小值)

---
+++ main/1168A_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"testing"
+)
+
+func TestSol1168A(t *testing.T) {
+	// just copy from website
+	rawText := `
+10 10
+5 0 5 9 4 6 4 5 0 0
+outputCopy
+6
+inputCopy
+5 3
+0 0 0 1 2
+outputCopy
+0
+inputCopy
+5 7
+0 6 1 3 2
+outputCopy
+1`
+	testutil.AssertEqualCase(t, rawText, -1, Sol1168A)
+}
