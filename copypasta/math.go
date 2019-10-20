b139820eb2474fbbf8520fe1336f4c58ad93f243
Endless <loli.con@qq.com>
2019-10-20 22:58:36 +0800

    Add Adaptive Simpson's Rule

--- copypasta/math.go
+++ copypasta/math.go
@@ -1,5 +1,7 @@
 package copypasta
 
+import "math"
+
 func mathCollection() {
 	calcGCD := func(a, b int64) int64 {
 		for b > 0 {
@@ -86,3 +88,29 @@ func exgcd(a, b int64) (gcd, x, y int64) {
 	y -= a / b * x
 	return
 }
+
+type mathF func(x float64) float64
+
+// Simpson's 1/3 rule
+// https://en.wikipedia.org/wiki/Simpson%27s_rule
+// 证明过程 https://phqghume.github.io/2018/05/19/%E8%87%AA%E9%80%82%E5%BA%94%E8%BE%9B%E6%99%AE%E6%A3%AE%E6%B3%95/
+func simpson(l, r float64, f mathF) float64 {
+	h := (r - l) / 2
+	return h * (f(l) + 4*f(l+h) + f(r)) / 3
+}
+
+// 不放心的话还可以设置一个最大递归深度 maxDeep
+// 15eps 的证明过程 http://www2.math.umd.edu/~mariakc/teaching/adaptive.pdf
+func _asr(l, r, eps, A float64, f mathF) float64 {
+	mid := l + (r-l)/2
+	L := simpson(l, mid, f)
+	R := simpson(mid, r, f)
+	if math.Abs(L+R-A) <= 15*eps {
+		return L + R + (L+R-A)/15
+	}
+	return _asr(l, mid, eps/2, L, f) + _asr(mid, r, eps/2, R, f)
+}
+
+// Adaptive Simpson's Rule
+// https://en.wikipedia.org/wiki/Adaptive_Simpson%27s_method
+func asr(a, b, eps float64, f mathF) float64 { return _asr(a, b, eps, simpson(a, b, f), f) }
