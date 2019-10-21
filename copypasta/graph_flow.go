334b5fd310f01ea122d062b2135d98326bcf41d9
Endless <loli.con@qq.com>
2019-10-21 23:41:56 +0800

    update

--- copypasta/graph_flow.go
+++ copypasta/graph_flow.go
@@ -11,6 +11,7 @@ type flowGraph struct {
 	edges [][]edge
 	level []int // 顶点到源点的距离标号
 	iter  []int // 当前弧，在其之前的边已经没有用了
+	//used  []bool
 }
 
 func newFlowGraph(size int) *flowGraph {
@@ -19,12 +20,20 @@ func newFlowGraph(size int) *flowGraph {
 		edges: make([][]edge, size+1),
 		level: make([]int, size+1),
 		iter:  make([]int, size+1),
+		//used:  make([]bool, size+1),
 	}
 }
 
-func (g *flowGraph) addEdge(from, to int, cap int) {
+func (g *flowGraph) add(from, to int, cap int) {
 	g.edges[from] = append(g.edges[from], edge{to, cap, len(g.edges[to])})
 	g.edges[to] = append(g.edges[to], edge{from, 0, len(g.edges[from]) - 1})
+}
+
+func (g *flowGraph) addBoth(from, to int, cap int) {
+	g.add(from, to, cap)
+	if from != to {
+		g.add(to, from, cap)
+	}
 }
 
 // BFS 地计算从源点出发的距离标号
@@ -60,14 +69,14 @@ func (g *flowGraph) dfs(v, t int, f int) int {
 	}
 	for i := g.iter[v]; i < len(g.edges[v]); i++ {
 		e := &g.edges[v][i]
-		if e.cap > 0 && g.level[v] < g.level[e.to] {
+		if e.cap > 0 && g.level[e.to] > g.level[v] {
 			if d := g.dfs(e.to, t, g.min(f, e.cap)); d > 0 {
 				e.cap -= d
 				g.edges[e.to][e.rev].cap += d
 				return d
 			}
 		}
-		g.iter[v]++
+		g.iter[v]++ // 当前弧优化（避免对没有用的边进行多次检查）
 	}
 	return 0
 }
@@ -90,3 +99,36 @@ func (g *flowGraph) maxFlow(s, t int) (flow int) {
 		}
 	}
 }
+
+//// DFS 地寻找增广路
+//func (g *flowGraph) dfs2(v, t int, f int) int {
+//	if v == t {
+//		return f
+//	}
+//	g.used[v] = true
+//	for i := range g.edges[v] {
+//		e := &g.edges[v][i]
+//		if !g.used[e.to] && e.cap > 0 {
+//			if d := g.dfs2(e.to, t, g.min(f, e.cap)); d > 0 {
+//				e.cap -= d
+//				g.edges[e.to][e.rev].cap += d
+//				return d
+//			}
+//		}
+//	}
+//	return 0
+//}
+//
+//// Ford–Fulkerson algorithm (FFA)
+//// 耗时大约是 Dinic x2.4
+//func (g *flowGraph) maxFlow2(s, t int) (flow int) {
+//	const inf int = 1e8
+//	for {
+//		g.used = make([]bool, g.size+1)
+//		if f := g.dfs2(s, t, inf); f > 0 {
+//			flow += f
+//		} else {
+//			return
+//		}
+//	}
+//}
