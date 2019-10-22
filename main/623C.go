983452ccf0d05ce972bbd8dda40d655128c6d492
Endless <loli.con@qq.com>
2019-10-22 13:55:24 +0800

    [1800][!] Add 623C with test (比较的本质)

---
+++ main/623C.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"sort"
+)
+
+// github.com/EndlessCheng/codeforces-go
+func Sol623C(reader io.Reader, writer io.Writer) {
+	in := bufio.NewReader(reader)
+	out := bufio.NewWriter(writer)
+	defer out.Flush()
+
+	var n int
+	Fscan(in, &n)
+	s := make([]string, n)
+	for i := range s {
+		Fscan(in, &s[i])
+	}
+	sort.Slice(s, func(i, j int) (less bool) { return s[i]+s[j] < s[j]+s[i] })
+	for _, val := range s {
+		Fprint(out, val)
+	}
+}
+
+//func main() {
+//	Sol623C(os.Stdin, os.Stdout)
+//}
---
+++ main/623C_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"testing"
+)
+
+func TestSol623C(t *testing.T) {
+	// just copy from website
+	rawText := `
+2
+bbz
+b
+outputCopy
+bbbz
+inputCopy
+2
+b
+bbz
+outputCopy
+bbbz
+inputCopy
+2
+b
+bba
+outputCopy
+bbab
+inputCopy
+2
+b
+bza
+outputCopy
+bbza
+inputCopy
+2
+b
+baa
+outputCopy
+baab
+inputCopy
+2
+a
+az
+outputCopy
+aaz
+inputCopy
+4
+abba
+abacaba
+bcd
+er
+outputCopy
+abacabaabbabcder
+inputCopy
+5
+x
+xx
+xxa
+xxaa
+xxaaa
+outputCopy
+xxaaaxxaaxxaxxx
+inputCopy
+3
+c
+cb
+cba
+outputCopy
+cbacbc`
+	testutil.AssertEqualCase(t, rawText, 0, Sol623C)
+}
