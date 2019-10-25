dbafe433911d9c7152ac875de0b2c4482537812d
Endless <loli.con@qq.com>
2019-10-25 10:13:36 +0800

    update

--- copypasta/template/generator_test.go
+++ copypasta/template/generator_test.go
@@ -9,6 +9,11 @@ import (
 	"testing"
 )
 
+const (
+	contestID = 1249
+	overwrite = false
+)
+
 func copyFile(dst, src string) error {
 	in, err := os.Open(src)
 	if err != nil {
@@ -30,8 +35,6 @@ func copyFile(dst, src string) error {
 }
 
 func Test(t *testing.T) {
-	const contestID = 1244
-
 	rootPath := fmt.Sprintf("../../dash/%d/", contestID)
 	if err := filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -42,8 +45,10 @@ func Test(t *testing.T) {
 		}
 		for _, fileName := range [...]string{"main.go", "main_test.go"} {
 			goFilePath := filepath.Join(path, fileName)
-			if _, err := os.Stat(goFilePath); !os.IsNotExist(err) {
-				continue
+			if !overwrite {
+				if _, err := os.Stat(goFilePath); !os.IsNotExist(err) {
+					continue
+				}
 			}
 			if err := copyFile(goFilePath, fileName); err != nil {
 				return err
@@ -54,7 +59,7 @@ func Test(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	tips := fmt.Sprintf("cd %[1]d\ncf submit %[1]d a a/main.go\n", contestID)
+	tips := fmt.Sprintf("cd %[1]d\ncf submit %[1]d d d/main.go\n", contestID)
 	if err := ioutil.WriteFile(rootPath+"tips.txt", []byte(tips), 0644); err != nil {
 		t.Fatal(err)
 	}
