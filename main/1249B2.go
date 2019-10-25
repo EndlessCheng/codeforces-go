24187b4532beca0ef40a74beafcdf84db579b111
Endless <loli.con@qq.com>
2019-10-25 11:44:01 +0800

    update

---
+++ main/1249B2.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+// github.com/EndlessCheng/codeforces-go
+func Sol1249B2(reader io.Reader, writer io.Writer) {
+	in := bufio.NewReader(reader)
+	out := bufio.NewWriter(writer)
+	defer out.Flush()
+
+	var q int
+	for Fscan(in, &q); q > 0; q-- {
+		var n int
+		Fscan(in, &n)
+		p := make([]int, n)
+		for i := range p {
+			Fscan(in, &p[i])
+			p[i]--
+		}
+		ans := make([]interface{}, n)
+		vis := make([]bool, n)
+		for _, pi := range p {
+			if vis[pi] {
+				continue
+			}
+			cycle := 0
+			vals := []int{}
+			for j := pi; !vis[j]; j = p[j] {
+				vis[j] = true
+				cycle++
+				vals = append(vals, j)
+			}
+			for _, val := range vals {
+				ans[val] = cycle
+			}
+		}
+		Fprintln(out, ans...)
+	}
+}
+
+func main() {
+	Sol1249B2(os.Stdin, os.Stdout)
+}
---
+++ main/1249C2.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+// github.com/EndlessCheng/codeforces-go
+func Sol1249C2(reader io.Reader, writer io.Writer) {
+	in := bufio.NewReader(reader)
+	out := bufio.NewWriter(writer)
+	defer out.Flush()
+
+	var q int
+	for Fscan(in, &q); q > 0; q-- {
+		var n int64
+		Fscan(in, &n)
+		coef := []int64{}
+		for ; n > 0; n /= 3 {
+			coef = append(coef, n%3)
+		}
+		coef = append(coef, 0)
+		for i := len(coef) - 1; i >= 0; i-- {
+			if coef[i] == 2 {
+				for j := range coef[:i] {
+					coef[j] = 0
+				}
+				for j := i; j < len(coef); j++ {
+					if coef[j] == 2 {
+						coef[j] = 0
+						coef[j+1]++
+					}
+				}
+			}
+
+		}
+		ans := int64(0)
+		pow3 := int64(1)
+		for _, ci := range coef {
+			ans += pow3 * ci
+			pow3 *= 3
+		}
+		Fprintln(out, ans)
+	}
+}
+
+func main() {
+	Sol1249C2(os.Stdin, os.Stdout)
+}
