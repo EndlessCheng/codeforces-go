7c865f2937930f7bfd45dca065bd05f63b9533cd
Endless <loli.con@qq.com>
2019-10-30 17:00:23 +0800

    update

--- copypasta/odt_bst.go
+++ copypasta/odt_bst.go
@@ -5,8 +5,8 @@ type odtNode struct {
 	l, r int
 }
 
-func (t *treap) floor(key int) (floor *odtNode) { return }
-func (t *treap) next(key int) (next *odtNode)   { return }
+func (t *treap) floor(l int) (floor *odtNode) { return }
+func (t *treap) next(l int) (next *odtNode)   { return }
 
 func (t *treap) split(mid int) {
 	if o := t.floor(mid); o.l < mid && mid <= o.r {
@@ -22,7 +22,7 @@ func (t *treap) prepare(l, r int) {
 }
 
 func (t *treap) merge(l, r int, value tpValueType) {
-	//t.prepare(l, r)
+	t.prepare(l, r)
 	for o := t.next(l); o != nil && o.l <= r; o = t.next(o.l) {
 		//t.delete(o.l)
 	}
