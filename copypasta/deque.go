3f8cb29d1af963ffdf7ce68153145626db56ad96
Endless <loli.con@qq.com>
2019-11-01 17:27:58 +0800

    update

--- copypasta/deque.go
+++ copypasta/deque.go
@@ -7,25 +7,22 @@ type deque struct {
 	stackL, stackR []int
 }
 
-func (q *deque) len() int {
-	return len(q.stackL) + len(q.stackR)
-}
-
+func (q *deque) len() int    { return len(q.stackL) + len(q.stackR) }
+func (q *deque) empty() bool { return q.len() == 0 }
 func (q *deque) topL() int {
-	return q.stackL[len(q.stackL)-1]
+	if len(q.stackL) > 0 {
+		return q.stackL[len(q.stackL)-1]
+	}
+	return q.stackR[0]
 }
-
 func (q *deque) topR() int {
-	return q.stackR[len(q.stackR)-1]
+	if len(q.stackR) > 0 {
+		return q.stackR[len(q.stackR)-1]
+	}
+	return q.stackL[0]
 }
-
-func (q *deque) pushL(v int) {
-	q.stackL = append(q.stackL, v)
-}
-
-func (q *deque) pushR(v int) {
-	q.stackR = append(q.stackR, v)
-}
+func (q *deque) pushL(v int) { q.stackL = append(q.stackL, v) }
+func (q *deque) pushR(v int) { q.stackR = append(q.stackR, v) }
 
 // panic if empty
 func (q *deque) popL() (v int) {
