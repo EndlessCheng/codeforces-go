868c71f2574dca600f2531c16b811ae0b8cdd398
Endless <loli.con@qq.com>
2019-11-02 09:10:29 +0800

    [1900][NEW!] Add 220B with test (离线+分块=>小化指针移动次数)

---
+++ main/220B_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"testing"
+)
+
+func TestSol220B(t *testing.T) {
+	// just copy from website
+	rawText := `
+7 2
+3 1 2 2 3 3 7
+1 7
+3 4
+outputCopy
+3
+1`
+	testutil.AssertEqualCase(t, rawText, 0, Sol220B)
+}
