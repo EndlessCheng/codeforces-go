33698299693b9e3ccdbab45ba1b8deee29efc6fa
Endless <loli.con@qq.com>
2019-11-23 20:59:25 +0800

    [1900][1A] Add 1243D (最小生成树，考虑 1-边最小的点)

--- main/1243D.go
+++ main/1243D.go
@@ -3,43 +3,48 @@ package main
 import (
 	"bufio"
 	. "fmt"
-	"os"
+	"io"
 )
 
-var fa []int
-
-func initFa(n int) {
-	fa = make([]int, n)
-	for i := range fa {
-		fa[i] = i
-	}
-}
-func find(x int) int {
-	if fa[x] != x {
-		fa[x] = find(fa[x])
-	}
-	return fa[x]
-}
-func merge(from, to int) { fa[find(from)] = find(to) }
-func same(x, y int) bool { return find(x) == find(y) }
-
+// github.com/EndlessCheng/codeforces-go
 // 题解 https://www.luogu.org/blog/endlesscheng/solution-cf1242b
-func main() {
-	in := bufio.NewReader(os.Stdin)
+func Sol1243D(reader io.Reader, writer io.Writer) {
+	in := bufio.NewScanner(reader)
+	in.Split(bufio.ScanWords)
+	out := bufio.NewWriter(writer)
+	defer out.Flush()
+	read := func() (x int) {
+		in.Scan()
+		for _, b := range in.Bytes() {
+			x = x*10 + int(b-'0')
+		}
+		return
+	}
+	var fa []int
+	initFa := func(n int) {
+		fa = make([]int, n)
+		for i := range fa {
+			fa[i] = i
+		}
+	}
+	var find func(int) int
+	find = func(x int) int {
+		if fa[x] != x {
+			fa[x] = find(fa[x])
+		}
+		return fa[x]
+	}
+	merge := func(from, to int) { fa[find(from)] = find(to) }
+	same := func(x, y int) bool { return find(x) == find(y) }
 
-	var n, m int
-	Fscan(in, &n, &m)
+	n, m := read(), read()
 	g := make([][]int, n)
 	for ; m > 0; m-- {
-		var v, w int
-		Fscan(in, &v, &w)
-		v--
-		w--
+		v, w := read()-1, read()-1
 		g[v] = append(g[v], w)
 		g[w] = append(g[w], v)
 	}
 
-	// 寻找 0-边最大的点 maxDeg0V
 	maxDeg0, maxDeg0V := 0, 0
 	for v, edges := range g {
 		if deg0 := n - 1 - len(edges); deg0 > maxDeg0 {
@@ -47,40 +52,39 @@ func main() {
 			maxDeg0V = v
 		}
 	}
-
-	// 若图中没有 0-边，答案就是点的个数-1
 	if maxDeg0 == 0 {
-		Print(n - 1)
+		Fprint(out, n-1)
 		return
 	}
 
-	mergeEdge0 := func(v int, edges []int) {
-		// 将与点 v 以 0-边相连的点，合并到点 v 所属的连通分量上
+	merge0 := func(v int, edges []int) {
 		vs := map[int]bool{v: true}
 		for _, w := range edges {
 			vs[w] = true
 		}
 		for i := 0; i < n; i++ {
-			if !vs[i] { // i-v 是 0-边
+			if !vs[i] {
 				merge(i, v)
 			}
 		}
 	}
 	initFa(n)
-	mergeEdge0(maxDeg0V, g[maxDeg0V])
+	merge0(maxDeg0V, g[maxDeg0V])
 	for v, edges := range g {
 		if !same(v, maxDeg0V) {
-			// 暴力遍历剩余的点
-			mergeEdge0(v, edges)
+			merge0(v, edges)
 		}
 	}
 
-	// 计算联通分量个数-1
 	ans := -1
-	for i, faI := range fa {
-		if i == faI {
+	for i, v := range fa {
+		if i == v {
 			ans++
 		}
 	}
-	Print(ans)
+	Fprint(out, ans)
 }
+
+//func main() {
+//	Sol1243D(os.Stdin, os.Stdout)
+//}
