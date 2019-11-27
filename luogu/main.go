a385679443a8f42244aaea7c83d42f72af36dc5c
Endless <loli.con@qq.com>
2019-11-27 17:54:49 +0800

    update

--- luogu/main.go
+++ luogu/main.go
@@ -4,70 +4,88 @@ import (
 	"bufio"
 	. "fmt"
 	"io"
-	"math/bits"
+	"math"
 	"os"
-	"sort"
 )
 
-type pstNode struct {
-	l, r   int
-	lo, ro *pstNode // 由于使用了指针，pstNode 必须存放于一个分配了足够空间的 slice 中，避免扩容时改变了内存位置
-	sum    int
-}
-type pst struct {
-	nodes        []pstNode
-	versionRoots []*pstNode
+// Fast Fourier transform
+// https://zhuanlan.zhihu.com/p/31584464
+type fft struct {
+	n               int
+	omega, omegaInv []complex128
 }
 
-func (t *pst) _build(l, r int) *pstNode {
-	t.nodes = append(t.nodes, pstNode{l: l, r: r})
-	o := &t.nodes[len(t.nodes)-1]
-	if l == r {
-		return o
+func newFFT(n int) *fft {
+	omega := make([]complex128, n)
+	omegaInv := make([]complex128, n)
+	for i := range omega {
+		sin, cos := math.Sincos(2 * math.Pi * float64(i) / float64(n))
+		omega[i] = complex(cos, sin)
+		omegaInv[i] = complex(cos, -sin)
 	}
-	mid := (l + r) >> 1
-	o.lo = t._build(l, mid)
-	o.ro = t._build(mid+1, r)
-	return o
+	return &fft{n, omega, omegaInv}
 }
 
-func (t *pst) _update(o *pstNode, idx int) *pstNode {
-	t.nodes = append(t.nodes, *o)
-	o = &t.nodes[len(t.nodes)-1]
-	if o.l == o.r {
-		o.sum++
-		return o
+func (f *fft) transform(a, omega []complex128) {
+	for i, j := 0, 0; i < f.n; i++ {
+		if i > j {
+			a[i], a[j] = a[j], a[i]
+		}
+		for l := f.n >> 1; ; l >>= 1 {
+			j ^= l
+			if j >= l {
+				break
+			}
+		}
 	}
-	if mid := o.lo.r; idx <= mid {
-		o.lo = t._update(o.lo, idx)
-	} else {
-		o.ro = t._update(o.ro, idx)
-	}
-	o.sum = o.lo.sum + o.ro.sum
-	return o
-}
-
-func (t *pst) _queryKth(o1, o2 *pstNode, k int) (allKth int) {
-	if o1.l == o1.r {
-		return o1.l
-	}
-	if d := o2.lo.sum - o1.lo.sum; d >= k {
-		return t._queryKth(o1.lo, o2.lo, k)
-	} else {
-		return t._queryKth(o1.ro, o2.ro, k-d)
+	for l := 2; l <= f.n; l <<= 1 {
+		m := l >> 1
+		for st := 0; st < f.n; st += l {
+			p := a[st:]
+			for i := 0; i < m; i++ {
+				t := omega[f.n/l*i] * p[m+i]
+				p[m+i] = p[i] - t
+				p[i] += t
+			}
+		}
 	}
 }
 
-func (t *pst) init(n int) {
-	t.versionRoots[0] = t._build(1, n)
+func (f *fft) dft(a []complex128) {
+	f.transform(a, f.omega)
 }
 
-func (t *pst) update(dstVersion, srcVersion int, idx int) {
-	t.versionRoots[dstVersion] = t._update(t.versionRoots[srcVersion], idx)
+func (f *fft) idft(a []complex128) {
+	f.transform(a, f.omegaInv)
+	for i := range a {
+		a[i] /= complex(float64(f.n), 0)
+	}
 }
 
-func (t *pst) queryKth(l, r int, k int) (allKth int) {
-	return t._queryKth(t.versionRoots[l-1], t.versionRoots[r], k)
+// 入参出参都是从低到高的系数
+func convolution(a, b []int) []int {
+	n, m := len(a), len(b)
+	limit := n + m - 1
+	f := newFFT(limit)
+	cmplxA := make([]complex128, limit)
+	for i, v := range a {
+		cmplxA[i] = complex(float64(v), 0)
+	}
+	cmplxB := make([]complex128, limit)
+	for i, v := range b {
+		cmplxB[i] = complex(float64(v), 0)
+	}
+	f.dft(cmplxA)
+	f.dft(cmplxB)
+	for i := range cmplxA {
+		cmplxA[i] *= cmplxB[i]
+	}
+	f.idft(cmplxA)
+	conv := make([]int, n+m-1)
+	for i := range conv {
+		conv[i] = int(math.Round(real(cmplxA[i])))
+	}
+	return conv
 }
 
 func solve(reader io.Reader, writer io.Writer) {
@@ -77,53 +95,25 @@ func solve(reader io.Reader, writer io.Writer) {
 	defer out.Flush()
 	read := func() (x int) {
 		in.Scan()
-		data := in.Bytes()
-		sig := 1
-		if data[0] == '-' {
-			sig = -1
-			data = data[1:]
-		}
-		for _, b := range data {
+		for _, b := range in.Bytes() {
 			x = x*10 + int(b-'0')
-		}
-		return sig * x
-	}
-	discrete := func(arr []int) (kthArr []int) {
-		n := len(arr)
-		type pair struct{ val, i int }
-		ps := make([]pair, n)
-		for i, v := range arr {
-			ps[i] = pair{v, i}
-		}
-		sort.Slice(ps, func(i, j int) bool { return ps[i].val < ps[j].val })
-		kthArr = make([]int, n)
-		for i, p := range ps {
-			kthArr[p.i] = i + 1
 		}
 		return
 	}
 
-	n, m := read(), read()
-	t := &pst{
-		make([]pstNode, 0, (bits.Len(uint(n))+3)*n),
-		make([]*pstNode, n+1),
-	}
-	t.init(n)
+	n, m := read()+1, read()+1
 	a := make([]int, n)
 	for i := range a {
 		a[i] = read()
 	}
-	sortedArr := make([]int, n)
-	copy(sortedArr, a)
-	sort.Ints(sortedArr)
-
-	kthArr := discrete(a)
-	for i, kth := range kthArr {
-		t.update(i+1, i, kth)
+	b := make([]int, m)
+	for i := range b {
+		b[i] = read()
 	}
-	for ; m > 0; m-- {
-		l, r, k := read(), read(), read()
-		Fprintln(out, sortedArr[t.queryKth(l, r, k)-1])
+	c := convolution(a, b)
+	Fprint(out, c[0])
+	for _, v := range c[1:] {
+		Fprint(out, " ", v)
 	}
 }
 
--- luogu/main_test.go
+++ luogu/main_test.go
@@ -7,20 +7,12 @@ import (
 
 func Test_solve(t *testing.T) {
 	customInputs := []string{
-		`5 5
-25957 6405 15770 26287 26465 
-2 2 1
-3 4 1
-4 5 1
-1 2 2
-4 4 1`,
+		`1 2
+1 2
+1 2 1`,
 	}
 	customAnswers := []string{
-		`6405
-15770
-26287
-25957
-26287`,
+		`1 4 5 2`,
 	}
 	testutil.AssertEqualStringCase(t, customInputs, customAnswers, 0, solve)
 }
