547773a83e02c42434d7da52abb3920235776654
Endless <loli.con@qq.com>
2019-12-01 12:29:43 +0800

    Add LeetCode Weekly Contest 165

--- leetcode/165/b/b.go
+++ leetcode/165/b/b.go
@@ -1,14 +1,14 @@
 package main
 
-func numOfBurgers(a int, b int) []int {
-	ans := []int{}
+func numOfBurgers(a int, b int) (ans []int) {
+	ans = []int{}
 	if a < 2*b || (a-2*b)%2 == 1 {
-		return ans
+		return
 	}
 	x := (a - 2*b) / 2
 	y := b - x
 	if y < 0 {
-		return ans
+		return
 	}
 	return []int{x, y}
 }
