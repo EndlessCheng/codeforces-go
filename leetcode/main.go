db81936e03121b59bfb31e9f4479e4336744a953
Endless <loli.con@qq.com>
2019-12-17 15:20:06 +0800

    update

--- leetcode/main.go
+++ leetcode/main.go
@@ -22,6 +22,7 @@ func main() {
 	}
 	_ = toBytes
 
+	Println()
 }
 
 func collections() {
--- leetcode/testutil/leetcode.go
+++ leetcode/testutil/leetcode.go
@@ -104,7 +104,7 @@ func RunLeetCodeFunc(t *testing.T, f interface{}, rawInputs [][]string, rawOutpu
 		}
 		actualOut := vFunc.Call(in)
 		for i, expectedRes := range rawOut {
-			assert.Equal(t, expectedRes, simpleValueString(actualOut[i]), "failed at example %d", testCase+1)
+			assert.Equal(t, expectedRes, simpleValueString(actualOut[i]), "please check case %d", testCase+1)
 		}
 	}
 	return nil
