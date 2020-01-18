b5f68846a4a1e7b25ace7c6ed89168f8c1aff7d3
Endless <loli.con@qq.com>
2020-01-18 19:46:21 +0800

    update

--- main/630F.go
+++ main/630F.go
@@ -3,7 +3,7 @@ package main
 import . "fmt"
 
 // github.com/EndlessCheng/codeforces-go
-func main() {
+func CF630F() {
 	var n int64
 	Scan(&n)
 	Print((n-6)*(n-5)/2*(n-4)/3*(n-3)/4*(n-2)/5*(n-1)/6*n/7 + (n-5)*(n-4)/2*(n-3)/3*(n-2)/4*(n-1)/5*n/6 + (n-4)*(n-3)/2*(n-2)/3*(n-1)/4*n/5)
