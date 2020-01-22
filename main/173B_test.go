d2bc73ce817b50cebee1c0c320f987aa94d1f95f
Endless <loli.con@qq.com>
2020-01-22 11:45:50 +0800

    [1800][1A] Add 173B with test (0-1BFS)

---
+++ main/173B_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"testing"
+)
+
+func TestCF173B(t *testing.T) {
+	// just copy from website
+	rawText := `
+3 3
+.#.
+...
+.#.
+outputCopy
+2
+inputCopy
+4 3
+##.
+...
+.#.
+.#.
+outputCopy
+2`
+	testutil.AssertEqualCase(t, rawText, 0, CF173B)
+}
