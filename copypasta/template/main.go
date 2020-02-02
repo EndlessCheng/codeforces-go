6346a6a860023d3681cab9c426c4c071333cd380
Endless <loli.con@qq.com>
2020-02-02 20:54:52 +0800

    update

--- copypasta/template/main.go
+++ copypasta/template/main.go
@@ -7,9 +7,9 @@ import (
 	"os"
 )
 
-func run(reader io.Reader, writer io.Writer) {
-	in := bufio.NewReader(reader)
-	out := bufio.NewWriter(writer)
+func run(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
 	defer out.Flush()
 
 	var n int
