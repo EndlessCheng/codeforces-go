79b2460e9101a103c0314a37358fb31bc61e7671
Endless <loli.con@qq.com>
2020-02-02 16:10:26 +0800

    Add LeetCode Weekly Contest 174

--- leetcode/174/d/d.go
+++ leetcode/174/d/d.go
@@ -10,31 +10,23 @@ func maxJumps(a []int, d int) (ans int) {
 	n := len(a)
 
 	dp := make([]int, n)
-	for i := range dp {
-		dp[i] = -1
-	}
 	var f func(p int) int
 	f = func(p int) (_ans int) {
-		if dp[p] != -1 {
+		if dp[p] > 0 {
 			return dp[p]
 		}
 		defer func() { dp[p] = _ans }()
-		ap := a[p]
-		hMax := 0
 		for i := p + 1; i <= p+d; i++ {
-			if i == n || a[i] >= ap {
+			if i == n || a[i] >= a[p] {
 				break
 			}
 			_ans = max(_ans, f(i))
-			hMax = max(hMax, a[i])
 		}
-		hMax = 0
 		for i := p - 1; i >= p-d; i-- {
-			if i == -1 || a[i] >= ap {
+			if i == -1 || a[i] >= a[p] {
 				break
 			}
 			_ans = max(_ans, f(i))
-			hMax = max(hMax, a[i])
 		}
 		return _ans + 1
 	}
