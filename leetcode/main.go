df3bd7f494e3f395b7e709656e146df2ff5ba84c
Endless <loli.con@qq.com>
2020-02-03 13:04:43 +0800

    update

--- leetcode/main.go
+++ leetcode/main.go
@@ -1,14 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
+func collections() {
+	var mat [1][1]int
 
-// n, m := len(mat), len(mat[0])
+	n, m := len(mat), len(mat[0])
 
+	dir4 := [...][2]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
 
-
-func main() {
 	toBytes := func(g [][]string) [][]byte {
 		n, m := len(g), len(g[0])
 		bytes := make([][]byte, n)
@@ -20,58 +18,6 @@ func main() {
 		}
 		return bytes
 	}
-	_ = toBytes
 
-	fmt.Println()
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func collections() {
-	const mod int = 1e9 + 7
-	dir4 := [...][2]int{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
-
-	min := func(a, b int) int {
-		if a < b {
-			return a
-		}
-		return b
-	}
-	max := func(a, b int) int {
-		if a > b {
-			return a
-		}
-		return b
-	}
-	abs := func(x int) int {
-		if x < 0 {
-			return -x
-		}
-		return x
-	}
-	ifElseI := func(cond bool, r1, r2 int) int {
-		if cond {
-			return r1
-		}
-		return r2
-	}
-	ifElseS := func(cond bool, r1, r2 string) string {
-		if cond {
-			return r1
-		}
-		return r2
-	}
-
-	_ = []interface{}{fmt.Print, ifElseI, ifElseS, dir4, min, max, abs}
+	_ = []interface{}{n, m, dir4, toBytes}
 }
