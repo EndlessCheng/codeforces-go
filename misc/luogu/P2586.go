bf91244219621b7bc6c75f2ce9337d0f81c9705c
Endless <loli.con@qq.com>
2020-02-04 22:42:46 +0800

    update

--- misc/luogu/P2586.go
+++ misc/luogu/P2586.go
@@ -10,7 +10,7 @@ import (
 
 // 一些常量定义
 const (
-	eps = 1e-8
+	eps = 1e-10
 
 	maxAnts              = 6   // 最大蚂蚁数
 	antRadius            = 0.5 // 蚂蚁半径
@@ -52,7 +52,7 @@ func (a vec) disToSeg(l line) float64 {
 // 地图格点
 type grid struct {
 	pheromone int  // 信息素
-	empty     bool // 是否为空
+	hasObj    bool // 是否有蚂蚁或炮塔
 }
 
 // 蚂蚁
@@ -90,9 +90,6 @@ func newGame(height, width int) *game {
 	g.board = make([][]grid, height+1)
 	for i := range g.board {
 		g.board[i] = make([]grid, width+1)
-		for j := range g.board[i] {
-			g.board[i][j].empty = true
-		}
 	}
 	return g
 }
@@ -100,15 +97,15 @@ func newGame(height, width int) *game {
 // 生成蚂蚁
 func (g *game) newAnt() {
 	// 如果地图上蚂蚁数不足 6，并且洞口没有蚂蚁，一只蚂蚁就会在洞口出生
-	if len(g.ants) >= maxAnts || !g.board[0][0].empty {
+	if len(g.ants) >= maxAnts || g.board[0][0].hasObj {
 		return
 	}
+	g.board[0][0].hasObj = true
 	level := g.antGenCounter/6 + 1
 	hp := int(4 * math.Pow(1.1, float64(level)))
 	a := &ant{level: level, maxHP: hp, curHP: hp}
 	g.ants = append(g.ants, a)
 	g.antGenCounter++
-	g.board[0][0].empty = false
 }
 
 // 移动前，蚂蚁们在自己所在点留下一些信息素
@@ -124,7 +121,16 @@ func (g *game) beginSecond() {
 
 // 格点 p 是否可达
 func (g *game) canReach(p vec) bool {
-	return 0 <= p.x && p.x <= g.height && 0 <= p.y && p.y <= g.width && g.board[p.x][p.y].empty
+	return 0 <= p.x && p.x <= g.height && 0 <= p.y && p.y <= g.width && !g.board[p.x][p.y].hasObj
+}
+
+// 更新拿到蛋糕的蚂蚁状态
+func (a *ant) getCake() {
+	a.hasCake = true
+	a.curHP += a.maxHP / 2
+	if a.curHP > a.maxHP {
+		a.curHP = a.maxHP
+	}
 }
 
 // 东南西北
@@ -140,7 +146,7 @@ func (g *game) moveAnts() {
 		for i, d := range dir4 {
 			// 蚂蚁只能移动到空格点上，且不能是上一秒所在的点
 			// 如果此时有多个选择，蚂蚁会选择信息素最多的那个点爬过去，
-			// 若有多个相同的最多信息素，则按照东南西北的顺序选择有最多信息素的格点 todo:check
+			// 若有多个相同的最多信息素，则按照东南西北的顺序选择有最多信息素的格点
 			if p := a.pos.add(d); p != a.prevPos && g.canReach(p) && g.board[p.x][p.y].pheromone > maxPheromone {
 				maxPheromone = g.board[p.x][p.y].pheromone
 				dirI = i
@@ -149,43 +155,25 @@ func (g *game) moveAnts() {
 		// 如果蚂蚁的四周都是不可达点，那么蚂蚁在这一秒内会选择停留在当前点。下一秒判断移动方向时，它上一秒所在点为其当前停留的点
 		if maxPheromone == -1 {
 			a.prevPos = a.pos
-			continue
-		}
-		// 若此时蚂蚁的年龄为 5n+4，它会在选择方向后不断逆时针转 90°，直到面对一个可达的点，这样定下的方向才是蚂蚁最终要爬去的方向
-		if a.age%5 == 4 {
-			for i := (dirI + 3) % 4; ; i = (i + 3) % 4 {
-				if p := a.pos.add(dir4[i]); p != a.prevPos && g.canReach(p) {
-					dirI = i
-					break
+		} else {
+			// 若此时蚂蚁的年龄为 5n+4，它会在选择方向后不断逆时针转 90°，直到面对一个可达的点，这样定下的方向才是蚂蚁最终要爬去的方向
+			if a.age%5 == 4 {
+				for dirI = (dirI + 3) % 4; ; dirI = (dirI + 3) % 4 {
+					// todo: a.pos == a.prevPos?
+					if p := a.pos.add(dir4[dirI]); (p != a.prevPos || a.pos == a.prevPos) && g.canReach(p) {
+						break
+					}
 				}
 			}
+			// 更新蚂蚁位置和地图状态
+			g.board[a.pos.x][a.pos.y].hasObj = false
+			a.prevPos, a.pos = a.pos, a.pos.add(dir4[dirI])
+			g.board[a.pos.x][a.pos.y].hasObj = true
 		}
-		// 移动蚂蚁
-		g.board[a.pos.x][a.pos.y].empty = true
-		a.prevPos, a.pos = a.pos, a.pos.add(dir4[dirI])
-		g.board[a.pos.x][a.pos.y].empty = false
-	}
-}
-
-// 更新拿到蛋糕的蚂蚁状态
-func (a *ant) getCake() {
-	a.hasCake = true
-	a.curHP += a.maxHP / 2
-	if a.curHP > a.maxHP {
-		a.curHP = a.maxHP
-	}
-}
-
-// 如果有蚂蚁在蛋糕的位置上并且蛋糕没被扛走，它把蛋糕扛上，血量增加
-func (g *game) checkCake() {
-	if g.antWithCake != nil {
-		return
-	}
-	for _, a := range g.ants {
-		if a.pos == g.cake {
+		// 如果蛋糕没被扛走并且有蚂蚁在蛋糕的位置上，它把蛋糕扛上，血量增加
+		if g.antWithCake == nil && a.pos == g.cake {
 			a.getCake()
 			g.antWithCake = a
-			break
 		}
 	}
 }
@@ -200,6 +188,10 @@ func (g *game) towerAttack() {
 			// 塔到目标蚂蚁圆心的连线上的所有蚂蚁都会被打到并损失 t.damage 血量，这里“被打到”指表示激光的线段与表示蚂蚁的圆有公共点
 			towerToAntSeg := line{t.pos, g.antWithCake.pos}
 			for _, a := range g.ants {
+				if t.pos.sub(a.pos).len2() > t.atkRange*t.atkRange {
+					// todo: needed?
+					continue
+				}
 				if a.pos.disToSeg(towerToAntSeg)-eps < antRadius {
 					a.curHP -= t.damage
 				}
@@ -224,15 +216,29 @@ func (g *game) towerAttack() {
 	// 移除死亡蚂蚁
 	newAnts := []*ant{}
 	for _, a := range g.ants {
-		// 当蚂蚁的血被打成负数时，它才算挂了
 		if a.curHP >= 0 {
 			newAnts = append(newAnts, a)
-		} else if a.hasCake {
-			// 如果攻击结束后那只扛着蛋糕的蚂蚁挂了，蛋糕瞬间归位
-			g.antWithCake = nil
+		} else {
+			// 当蚂蚁的血被打成负数时，它才算挂了
+			if a.hasCake {
+				// 如果攻击结束后那只扛着蛋糕的蚂蚁挂了，蛋糕瞬间归位
+				g.antWithCake = nil
+			}
+			// 易错点：蚂蚁死亡后位置需要设置成空
+			g.board[a.pos.x][a.pos.y].hasObj = false
 		}
 	}
 	g.ants = newAnts
+}
+
+// 如果发现扛蛋糕的蚂蚁没死并在窝的位置，就认为蚂蚁抢到了蛋糕，游戏结束
+func (g *game) isGameOver() bool {
+	for _, a := range g.ants {
+		if a.hasCake && a.pos == g.antNest {
+			return true
+		}
+	}
+	return false
 }
 
 // 地图上所有点的信息素损失 1 单位，所有蚂蚁的年龄加 1
@@ -249,23 +255,12 @@ func (g *game) endSecond() {
 	}
 }
 
-// 如果发现扛蛋糕的蚂蚁没死并在窝的位置，就认为蚂蚁抢到了蛋糕，游戏结束
-func (g *game) isGameOver() bool {
-	for _, a := range g.ants {
-		if a.hasCake && a.pos == g.antNest {
-			return true
-		}
-	}
-	return false
-}
-
 // 模拟游戏的前 t 秒钟
 func (g *game) runSeconds(t int) (gameOverAt int, isGameOver bool) {
 	for s := 1; s <= t; s++ {
 		g.newAnt()          // 生成蚂蚁
 		g.beginSecond()     // 蚂蚁留下信息素
-		g.moveAnts()        // 移动蚂蚁
-		g.checkCake()       // 蚂蚁能否拿上蛋糕，更新拿到蛋糕的蚂蚁的状态
+		g.moveAnts()        // 移动蚂蚁，扛起蛋糕
 		g.towerAttack()     // 炮台攻击蚂蚁
 		if g.isGameOver() { // 检查游戏是否结束
 			// game over 的时候，这个回合不会对蚂蚁的年龄产生贡献，直接 return
@@ -293,7 +288,7 @@ func p2586(_r io.Reader, _w io.Writer) {
 		var x, y int
 		Fscan(in, &x, &y)
 		g.towers[i] = &tower{damage, atkRange, vec{x, y}}
-		g.board[x][y].empty = false
+		g.board[x][y].hasObj = true
 	}
 
 	// 模拟游戏的前 t 秒钟
