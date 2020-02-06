88a2fdda496bc99d4cfabae70b7283fcd77e732f
Endless <loli.con@qq.com>
2020-02-06 20:55:05 +0800

    Add LeetCode Weekly Contest 174

--- leetcode/174/a/a.go
+++ leetcode/174/a/a.go
@@ -3,22 +3,17 @@ package main
 import "sort"
 
 func kWeakestRows(mat [][]int, k int) (ans []int) {
-	type pair struct{ cnt1, i int }
-	ps:= make([]pair, len(mat))
+	type pair struct{ x, y int }
+	ps := make([]pair, len(mat))
 	for i, mi := range mat {
 		for _, v := range mi {
-			if v == 1 {
-				ps[i].cnt1++
-			}
+			ps[i].x += v
 		}
-		ps[i].i = i
+		ps[i].y = i
 	}
-	sort.Slice(ps, func(i, j int) bool {
-		pi, pj := ps[i], ps[j]
-		return pi.cnt1 < pj.cnt1 || pi.cnt1 == pj.cnt1 && pi.i < pj.i
-	})
+	sort.Slice(ps, func(i, j int) bool { a, b := ps[i], ps[j]; return a.x < b.x || a.x == b.x && a.y < b.y })
 	for _, p := range ps[:k] {
-		ans = append(ans, p.i)
+		ans = append(ans, p.y)
 	}
 	return
 }
