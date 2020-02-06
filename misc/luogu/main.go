cc191d372153a1d9e9e762000ee4daf543d51054
Endless <loli.con@qq.com>
2020-02-06 11:16:48 +0800

    update

--- misc/luogu/main.go
+++ misc/luogu/main.go
@@ -4,7 +4,11 @@ import (
 	"bufio"
 	. "fmt"
 	"io"
+	"os"
 )
+
+// NOTE: 洛谷的机器是 64 位的，有时候为了避免不必要的内存浪费以及加快运算速度，请使用 typedef
+type int int32
 
 func run(_r io.Reader, _w io.Writer) {
 	in := bufio.NewScanner(_r)
@@ -24,4 +28,4 @@ func run(_r io.Reader, _w io.Writer) {
 	Fprintln(out, n)
 }
 
-//func main() { run(os.Stdin, os.Stdout) }
+func main() { run(os.Stdin, os.Stdout) }
