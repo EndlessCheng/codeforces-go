17182dd37b89fcb35bf81f316fe272910ef54d98
Endless <loli.con@qq.com>
2020-02-06 12:43:12 +0800

    update

--- misc/luogu/main.go
+++ misc/luogu/main.go
@@ -7,10 +7,9 @@ import (
 	"os"
 )
 
-// NOTE: 洛谷的机器是 64 位的，有时候为了避免不必要的内存浪费以及加快运算速度，请使用 typedef
-type int int32
-
 func run(_r io.Reader, _w io.Writer) {
+	type int int32
+	type uint uint32
 	in := bufio.NewScanner(_r)
 	in.Split(bufio.ScanWords)
 	out := bufio.NewWriter(_w)
