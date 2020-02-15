de6ebd80b1357dece2c544bbf113be87489b6947
Endless <loli.con@qq.com>
2020-02-15 11:09:31 +0800

    update

--- leetcode/155/b/b.go
+++ leetcode/155/b/b.go
@@ -10,5 +10,5 @@ func nthUglyNumber(n int, a int, b int, c int) int {
 		return b
 	}
 	lcm := func(a, b int) int { return a / gcd(a, b) * b }
-	return sort.Search(2e9+1, func(x int) bool { return x > 0 && x/a+x/b+x/c-x/lcm(a, b)-x/lcm(b, c)-x/lcm(a, c)+x/lcm(lcm(a, b), c) >= n })
+	return sort.Search(2e9+1, func(x int) bool { return x/a+x/b+x/c-x/lcm(a, b)-x/lcm(b, c)-x/lcm(a, c)+x/lcm(lcm(a, b), c) >= n })
 }
