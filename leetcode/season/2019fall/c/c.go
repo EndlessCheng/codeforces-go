350d8d222e63a3a936565d80d15873194bdf2f72
Endless <loli.con@qq.com>
2020-02-21 20:16:58 +0800

    Add 2019 力扣杯全国秋季编程大赛

--- leetcode/season/2019fall/c/c.go
+++ leetcode/season/2019fall/c/c.go
@@ -11,16 +11,28 @@ func robot(command string, obstacles [][]int, endX, endY int) (ans bool) {
 		}
 		path[cur] = true
 	}
+	cx, cy := cur[0], cur[1]
 
-	normal := func(x, y int) [2]int {
-		k := (x + y) / len(command)
-		return [2]int{x - k*cur[0], y - k*cur[1]}
+	// 可以推广至能往三个方向移动的情况
+	// 四个方向的话就只能枚举障碍物从进入到离开矩形区域的所有可能位置了
+	abs := func(x int) int {
+		if x < 0 {
+			return -x
+		}
+		return x
 	}
-	if !path[normal(endX, endY)] {
+	reduce := func(x, y int) [2]int {
+		k := 0
+		if abs(cx) + abs(cy) > 0 {
+			k = (abs(x) + abs(y)) / (abs(cx) + abs(cy))
+		}
+		return [2]int{x - k*cx, y - k*cy}
+	}
+	if !path[reduce(endX, endY)] {
 		return
 	}
 	for _, p := range obstacles {
-		if x, y := p[0], p[1]; x >= 0 && x <= endX && y >= 0 && y <= endY && path[normal(x, y)] {
+		if x, y := p[0], p[1]; x >= 0 && x <= endX && y >= 0 && y <= endY && path[reduce(x, y)] {
 			return
 		}
 	}
