18da855fd3563950d57ea61c2625d0fb74ccb7e5
Endless <loli.con@qq.com>
2020-03-06 17:08:04 +0800

    remove

--- copypasta/template/main_test.go
+++ copypasta/template/main_test.go
@@ -7,10 +7,6 @@ import (
 	"testing"
 )
 
-// 1e9~1e18 √n logn 1     二分 二进制
-// 1e5~1e6  nlogn nαn n   二分 RMQ 并查集
-// 1e3~1e4  n^2 n√n       RMQ DP 分块
-// 300~500  n^3           DP 二分图
 func Test_run(t *testing.T) {
 	// TODO: 测试参数的下界和上界！
 	customInputs := []string{
