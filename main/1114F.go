010b93e78a295401d9372db435fa14c940982aa0
Endless <loli.con@qq.com>
2020-04-10 16:06:58 +0800

    [2500][NEW!] Add 1114F with test (欧拉函数 + 乘法lazy线段树 + 素数状压lazy线段树)

--- main/1114F.go
+++ main/1114F.go
@@ -8,12 +8,13 @@ import (
 
 const mod1114 int64 = 1e9 + 7
 
-type mulST1114 []struct {
-	l, r      int
-	mul, todo int64
+type lazyST1114 []struct {
+	l, r         int
+	mul, mulTodo int64
+	or, orTodo   int64
 }
 
-func (mulST1114) pow(x int64, n int) int64 {
+func (lazyST1114) pow(x int64, n int) int64 {
 	res := int64(1)
 	for ; n > 0; n >>= 1 {
 		if n&1 == 1 {
@@ -24,139 +25,84 @@ func (mulST1114) pow(x int64, n int) int64 {
 	return res
 }
 
-func (t mulST1114) _pushUp(o int) {
-	t[o].mul = t[o<<1].mul * t[o<<1|1].mul % mod1114
+func (t lazyST1114) _pushUp(o int) {
+	lo, ro := t[o<<1], t[o<<1|1]
+	t[o].mul = lo.mul * ro.mul % mod1114
+	t[o].or = lo.or | ro.or
 }
 
-func (t mulST1114) _build(a []int, o, l, r int) {
-	t[o].l, t[o].r, t[o].todo = l, r, 1
+func (t lazyST1114) _build(a []int, ors []int64, o, l, r int) {
+	t[o].l, t[o].r, t[o].mulTodo = l, r, 1
 	if l == r {
 		t[o].mul = int64(a[l-1])
+		t[o].or = ors[l-1]
 		return
 	}
 	m := (l + r) >> 1
-	t._build(a, o<<1, l, m)
-	t._build(a, o<<1|1, m+1, r)
+	t._build(a, ors, o<<1, l, m)
+	t._build(a, ors, o<<1|1, m+1, r)
 	t._pushUp(o)
 }
 
-func (t mulST1114) _spread(o int) {
-	if mul := t[o].todo; mul != 1 {
-		lo, ro := &t[o<<1], &t[o<<1|1]
+func (t lazyST1114) _spread(o int) {
+	lo, ro := &t[o<<1], &t[o<<1|1]
+	if mul := t[o].mulTodo; mul != 1 {
 		lo.mul = lo.mul * t.pow(mul, lo.r-lo.l+1) % mod1114
 		ro.mul = ro.mul * t.pow(mul, ro.r-ro.l+1) % mod1114
-		lo.todo = lo.todo * mul % mod1114
-		ro.todo = ro.todo * mul % mod1114
-		t[o].todo = 1
+		lo.mulTodo = lo.mulTodo * mul % mod1114
+		ro.mulTodo = ro.mulTodo * mul % mod1114
+		t[o].mulTodo = 1
 	}
-}
-
-func (t mulST1114) _update(o, l, r, val int) {
-	ol, or := t[o].l, t[o].r
-	if l <= ol && or <= r {
-		t[o].mul = t[o].mul * t.pow(int64(val), or-ol+1) % mod1114
-		t[o].todo = t[o].todo * int64(val) % mod1114
-		return
-	}
-	t._spread(o)
-	m := (ol + or) >> 1
-	if l <= m {
-		t._update(o<<1, l, r, val)
-	}
-	if m < r {
-		t._update(o<<1|1, l, r, val)
-	}
-	t._pushUp(o)
-}
-
-func (t mulST1114) _query(o, l, r int) (res int64) {
-	if l <= t[o].l && t[o].r <= r {
-		return t[o].mul
-	}
-	t._spread(o)
-	res = 1
-	m := (t[o].l + t[o].r) >> 1
-	if l <= m {
-		res = t._query(o<<1, l, r)
-	}
-	if m < r {
-		res = res * t._query(o<<1|1, l, r) % mod1114
-	}
-	return
-}
-
-func (t mulST1114) init(a []int)         { t._build(a, 1, 1, len(a)) }
-func (t mulST1114) update(l, r, val int) { t._update(1, l, r, val) }
-func (t mulST1114) query(l, r int) int64 { return t._query(1, l, r) }
-
-type orST1114 []struct {
-	l, r     int
-	or, todo int64
-}
-
-func (t orST1114) _pushUp(o int) {
-	t[o].or = t[o<<1].or | t[o<<1|1].or
-}
-
-func (t orST1114) _build(a []int64, o, l, r int) {
-	t[o].l, t[o].r = l, r
-	if l == r {
-		t[o].or = a[l-1]
-		return
-	}
-	m := (l + r) >> 1
-	t._build(a, o<<1, l, m)
-	t._build(a, o<<1|1, m+1, r)
-	t._pushUp(o)
-}
-
-func (t orST1114) _spread(o int) {
-	if or := t[o].todo; or != 0 {
-		lo, ro := &t[o<<1], &t[o<<1|1]
+	if or := t[o].orTodo; or != 0 {
 		lo.or |= or
 		ro.or |= or
-		lo.todo |= or
-		ro.todo |= or
-		t[o].todo = 0
+		lo.orTodo |= or
+		ro.orTodo |= or
+		t[o].orTodo = 0
 	}
 }
 
-func (t orST1114) _update(o, l, r int, val int64) {
+func (t lazyST1114) _update(o, l, r, mul int, orVal int64) {
 	ol, or := t[o].l, t[o].r
 	if l <= ol && or <= r {
-		t[o].or |= val
-		t[o].todo |= val
+		t[o].mul = t[o].mul * t.pow(int64(mul), or-ol+1) % mod1114
+		t[o].mulTodo = t[o].mulTodo * int64(mul) % mod1114
+		t[o].or |= orVal
+		t[o].orTodo |= orVal
 		return
 	}
 	t._spread(o)
 	m := (ol + or) >> 1
 	if l <= m {
-		t._update(o<<1, l, r, val)
+		t._update(o<<1, l, r, mul, orVal)
 	}
 	if m < r {
-		t._update(o<<1|1, l, r, val)
+		t._update(o<<1|1, l, r, mul, orVal)
 	}
 	t._pushUp(o)
 }
 
-func (t orST1114) _query(o, l, r int) (res int64) {
+func (t lazyST1114) _query(o, l, r int) (mul, or int64) {
 	if l <= t[o].l && t[o].r <= r {
-		return t[o].or
+		return t[o].mul, t[o].or
 	}
 	t._spread(o)
+	mul = 1
 	m := (t[o].l + t[o].r) >> 1
 	if l <= m {
-		res = t._query(o<<1, l, r)
+		mul, or = t._query(o<<1, l, r)
 	}
 	if m < r {
-		res |= t._query(o<<1|1, l, r)
+		a, b := t._query(o<<1|1, l, r)
+		mul = mul * a % mod1114
+		or |= b
 	}
 	return
 }
 
-func (t orST1114) init(a []int64)             { t._build(a, 1, 1, len(a)) }
-func (t orST1114) update(l, r int, val int64) { t._update(1, l, r, val) }
-func (t orST1114) query(l, r int) int64       { return t._query(1, l, r) }
+func (t lazyST1114) init(a []int, ors []int64)      { t._build(a, ors, 1, 1, len(a)) }
+func (t lazyST1114) update(l, r, mul int, or int64) { t._update(1, l, r, mul, or) }
+func (t lazyST1114) query(l, r int) (int64, int64)  { return t._query(1, l, r) }
 
 // github.com/EndlessCheng/codeforces-go
 func CF1114F(_r io.Reader, _w io.Writer) {
@@ -196,19 +142,15 @@ func CF1114F(_r io.Reader, _w io.Writer) {
 		ors[i] = factors[a[i]]
 	}
 
-	mt := make(mulST1114, 4*n)
-	mt.init(a)
-	ot := make(orST1114, 4*n)
-	ot.init(ors)
+	t := make(lazyST1114, 4*n)
+	t.init(a, ors)
 	for ; q > 0; q-- {
 		Fscan(in, &s, &l, &r)
 		if s[0] == 'M' {
 			Fscan(in, &x)
-			mt.update(l, r, x)
-			ot.update(l, r, factors[x])
+			t.update(l, r, x, factors[x])
 		} else {
-			ans := mt.query(l, r)
-			ps := ot.query(l, r)
+			ans, ps := t.query(l, r)
 			for i, v := range p1p {
 				if ps>>i&1 == 1 {
 					ans = ans * v % mod1114
