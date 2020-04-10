6de707588fda2b69e7b2d8abda57fb9bd5001a34
Endless <loli.con@qq.com>
2020-04-10 15:28:17 +0800

    add dynamicMedians

--- copypasta/heap.go
+++ copypasta/heap.go
@@ -1,6 +1,9 @@
 package copypasta
 
-import "sort"
+import (
+	. "container/heap"
+	"sort"
+)
 
 // 下面这些都是最小堆
 // h.top() 即 h.IntSlice[0] 或 (*h)[0] （注意判断非空）
@@ -39,3 +42,35 @@ func (h pairHeap) Less(i, j int) bool    { return h[i].x < h[j].x || h[i].x == h
 func (h pairHeap) Swap(i, j int)         { h[i], h[j] = h[j], h[i] }
 func (h *pairHeap) Push(v interface{})   { *h = append(*h, v.(hPair)) }
 func (h *pairHeap) Pop() (v interface{}) { n := len(*h); *h, v = (*h)[:n-1], (*h)[n-1]; return }
+
+//
+
+func heapCollections() {
+	// 对顶堆求动态中位数（一个数组前 2k+1 项的中位数）
+	// https://www.luogu.com.cn/problem/P1168
+	dynamicMedians := func(a []int) []int {
+		n := len(a)
+		medians := make([]int, 1, (n+1)/2)
+		medians[0] = a[0]
+		small, big := &hp{}, &hp{}
+		Push(big, a[0]) // 下面保证 big.size() == small.size() || big.size()-1 == small.size()
+		for i, v := range a[1:] {
+			if v < big.IntSlice[0] {
+				Push(small, -v)
+			} else {
+				Push(big, v)
+			}
+			if len(big.IntSlice)-1 > len(small.IntSlice) {
+				Push(small, -Pop(big).(int))
+			} else if len(small.IntSlice) > len(big.IntSlice) {
+				Push(big, -Pop(small).(int))
+			}
+			if i&1 == 1 {
+				medians = append(medians, big.IntSlice[0])
+			}
+		}
+		return medians
+	}
+
+	_ = []interface{}{dynamicMedians}
+}
