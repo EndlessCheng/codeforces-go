218089b43ae3a4e1696b4da3085ab3a673b25ce2
Endless <loli.con@qq.com>
2020-04-11 14:23:06 +0800

    add combHalf

--- copypasta/math.go
+++ copypasta/math.go
@@ -613,6 +613,21 @@ func numberTheoryCollection() {
 		return res
 	}
 
+	// EXTRA: binomial(n, floor(n/2))
+	// https://oeis.org/A001405
+	combHalf := [...]int64{
+		1, 1, 2, 3, 6, 10, 20, 35, 70, 126, // C(9,4)
+		252, 462, 924, 1716, 3432, 6435, 12870, 24310, 48620, 92378, // C(19,9)
+		184756, 352716, 705432, 1352078, 2704156, 5200300, 10400600, 20058300, 40116600, 77558760, // C(29,14)
+		155117520, 300540195, 601080390, 1166803110, 2333606220, 4537567650, 9075135300, 17672631900, 35345263800, 68923264410, // C(39,19)
+		137846528820, 269128937220, 538257874440, 1052049481860, 2104098963720, 4116715363800, 8233430727600, 16123801841550, 32247603683100, 63205303218876, // C(49,24)
+		126410606437752, 247959266474052, 495918532948104, 973469712824056, 1946939425648112, 3824345300380220, 7648690600760440, 15033633249770520, 30067266499541040, 59132290782430712, // C(59,29)
+		118264581564861424, 232714176627630544, 465428353255261088, 916312070471295267, 1832624140942590534, 3609714217008132870, 7219428434016265740, // C(66,33)
+	}
+
+	// EXTRA: Central binomial coefficients: binomial(2*n,n) = (2*n)!/(n!)^2
+	// https://oeis.org/A000984
+
 	comb := func(n, k int) int {
 		res := 1
 		for i := 1; i <= k; i++ {
@@ -633,6 +648,9 @@ func numberTheoryCollection() {
 			}
 			C[i][i] = 1
 		}
+
+		comb := func(n, k int) int { return C[n][k] }
+		_ = comb
 	}
 
 	{
@@ -754,7 +772,7 @@ func numberTheoryCollection() {
 		isPrime, sieve, primeFactorization, primeExponentsCountAll,
 		divisors, doDivisors, doDivisors2, divisorsAll, primeFactorsAll, lpfAll, distinctPrimesCountAll, calcPhi, phiAll,
 		exgcd, invM, invP, divM, divP, crt, excrt, babyStepGiantStep,
-		factorial, factorialMod, comb, initComb,
+		factorial, factorialMod, comb, combHalf, initComb,
 		consecutiveNumbersSum, partition,
 	}
 }
