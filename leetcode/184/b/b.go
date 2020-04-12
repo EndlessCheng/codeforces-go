30631897ecc5ec17ead7d7cb48f197d24207c946
Endless <loli.con@qq.com>
2020-04-12 13:39:43 +0800

    Add LeetCode Weekly Contest 184

--- leetcode/184/b/b.go
+++ leetcode/184/b/b.go
@@ -9,9 +9,7 @@ func processQueries(qs []int, m int) (ans []int) {
 		for i, v := range a {
 			if v == q {
 				ans = append(ans, i)
-				tmp := append([]int{v}, a[:i]...)
-				tmp = append(tmp, a[i+1:]...)
-				a = tmp
+				a = append(append([]int{v}, a[:i]...), a[i+1:]...)
 				break
 			}
 		}
