21581a31f6588b8ce7f466cde88ed6beeb4e4a47
Endless <loli.con@qq.com>
2020-04-19 14:55:54 +0800

    rename

--- copypasta/big.go
+++ copypasta/big.go
@@ -2,32 +2,36 @@ package copypasta
 
 import "math/big"
 
+// CF1244C
+
 // a+=b: a.add(b)
 // a+b:  add(a,b)
-type bigInt struct{ *big.Int }
+type Int struct{ *big.Int }
 
-func (a bigInt) add(b bigInt) bigInt { a.Add(a.Int, b.Int); return a }
-func (a bigInt) sub(b bigInt) bigInt { a.Sub(a.Int, b.Int); return a }
-func (a bigInt) mul(b bigInt) bigInt { a.Mul(a.Int, b.Int); return a }
-func (a bigInt) div(b bigInt) bigInt { a.Quo(a.Int, b.Int); return a }
-func (a bigInt) mod(b bigInt) bigInt { a.Rem(a.Int, b.Int); return a }
-func (a bigInt) neg() bigInt         { a.Neg(a.Int); return a }
-func (a bigInt) cmp(b bigInt) int    { return a.Cmp(b.Int) }
+func newInt(x int64) Int    { return Int{big.NewInt(x)} }
+func (a Int) add(b Int) Int { a.Add(a.Int, b.Int); return a }
+func (a Int) sub(b Int) Int { a.Sub(a.Int, b.Int); return a }
+func (a Int) mul(b Int) Int { a.Mul(a.Int, b.Int); return a }
+func (a Int) div(b Int) Int { a.Quo(a.Int, b.Int); return a }
+func (a Int) mod(b Int) Int { a.Rem(a.Int, b.Int); return a }
+func (a Int) neg() Int      { a.Neg(a.Int); return a }
+func (a Int) cmp(b Int) int { return a.Cmp(b.Int) }
 
-func add(a, b bigInt) bigInt { return bigInt{(&big.Int{}).Add(a.Int, b.Int)} }
-func sub(a, b bigInt) bigInt { return bigInt{(&big.Int{}).Sub(a.Int, b.Int)} }
-func mul(a, b bigInt) bigInt { return bigInt{(&big.Int{}).Mul(a.Int, b.Int)} }
-func div(a, b bigInt) bigInt { return bigInt{(&big.Int{}).Quo(a.Int, b.Int)} }
-func mod(a, b bigInt) bigInt { return bigInt{(&big.Int{}).Rem(a.Int, b.Int)} }
-func neg(a bigInt) bigInt    { return bigInt{(&big.Int{}).Neg(a.Int)} }
+func add(a, b Int) Int { return Int{(&big.Int{}).Add(a.Int, b.Int)} }
+func sub(a, b Int) Int { return Int{(&big.Int{}).Sub(a.Int, b.Int)} }
+func mul(a, b Int) Int { return Int{(&big.Int{}).Mul(a.Int, b.Int)} }
+func div(a, b Int) Int { return Int{(&big.Int{}).Quo(a.Int, b.Int)} }
+func mod(a, b Int) Int { return Int{(&big.Int{}).Rem(a.Int, b.Int)} }
+func neg(a Int) Int    { return Int{(&big.Int{}).Neg(a.Int)} }
 
 //
 
-type bigRat struct{ *big.Rat }
+type rat struct{ *big.Rat }
 
-func (a bigRat) add(b bigRat) bigRat { a.Add(a.Rat, b.Rat); return a }
-func (a bigRat) sub(b bigRat) bigRat { a.Sub(a.Rat, b.Rat); return a }
-func (a bigRat) mul(b bigRat) bigRat { a.Mul(a.Rat, b.Rat); return a }
-func (a bigRat) div(b bigRat) bigRat { a.Quo(a.Rat, b.Rat); return a }
-func (a bigRat) neg() bigRat         { a.Neg(a.Rat); return a }
-func (a bigRat) cmp(b bigRat) int    { return a.Cmp(b.Rat) }
+func newRat(a, b int64) rat { return rat{big.NewRat(a, b)} }
+func (a rat) add(b rat) rat { a.Add(a.Rat, b.Rat); return a }
+func (a rat) sub(b rat) rat { a.Sub(a.Rat, b.Rat); return a }
+func (a rat) mul(b rat) rat { a.Mul(a.Rat, b.Rat); return a }
+func (a rat) div(b rat) rat { a.Quo(a.Rat, b.Rat); return a }
+func (a rat) neg() rat      { a.Neg(a.Rat); return a }
+func (a rat) cmp(b rat) int { return a.Cmp(b.Rat) }
