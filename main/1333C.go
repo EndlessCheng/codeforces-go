d19fba615f0452f32ebd7e9eca9830cd55925e34
Endless <loli.con@qq.com>
2020-04-28 15:55:12 +0800

    [1700][5A] Add 1333C with test (分治)

---
+++ main/1333C.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+// github.com/EndlessCheng/codeforces-go
+func CF1333C(_r io.Reader, _w io.Writer) {
+	ans := int64(0)
+	var f func([]int64)
+	f = func(a []int64) {
+		n := len(a)
+		if n == 1 {
+			if a[0] != 0 {
+				ans++
+			}
+			return
+		}
+		m := n >> 1
+		f(a[:m])
+		f(a[m:])
+		sumPos := map[int64]int{}
+		s := int64(0)
+		maxPos := m
+		for i := m - 1; i >= 0 && a[i] != 0; i-- {
+			s += a[i]
+			if s == 0 || sumPos[s] != 0 {
+				break
+			}
+			sumPos[s] = i
+			maxPos = i
+		}
+		s = 0
+		vis := map[int64]bool{}
+		for i := m; maxPos < m && i < n && a[i] != 0; i++ {
+			s += a[i]
+			if s == 0 || vis[s] {
+				break
+			}
+			vis[s] = true
+			if p, ok := sumPos[-s]; ok && p+1 > maxPos {
+				maxPos = p + 1
+			}
+			ans += int64(m - maxPos)
+		}
+	}
+
+	in := bufio.NewReader(_r)
+	var n int
+	Fscan(in, &n)
+	a := make([]int64, n)
+	for i := range a {
+		Fscan(in, &a[i])
+	}
+	f(a)
+	Fprint(_w, ans)
+}
+
+func main() { CF1333C(os.Stdin, os.Stdout) }
