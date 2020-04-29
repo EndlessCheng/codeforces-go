62da4cb50fd0ad0dbc77475128b02f3ec80d09f9
Endless <loli.con@qq.com>
2020-04-29 14:27:29 +0800

    CF 56B

---
+++ main/56B_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"testing"
+)
+
+func TestCF56B(t *testing.T) {
+	// just copy from website
+	rawText := `
+inputCopy
+8
+1 6 5 4 3 2 7 8
+outputCopy
+2 6
+inputCopy
+4
+2 3 4 1
+outputCopy
+0 0
+inputCopy
+4
+1 2 3 4
+outputCopy
+0 0`
+	testutil.AssertEqualCase(t, rawText, 0, CF56B)
+}
