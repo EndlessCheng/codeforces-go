a676302a3d8c7be108e089eb2492056e3cfe2e53
Endless <loli.con@qq.com>
2020-05-08 22:54:11 +0800

    add links

--- copypasta/math.go
+++ copypasta/math.go
@@ -585,6 +585,9 @@ func numberTheoryCollection() {
 		return inv
 	}
 
+	// 离线求逆元
+	// https://zhuanlan.zhihu.com/p/86561431
+
 	// 模数两两互质的线性同余方程组 - 中国剩余定理 (CRT)
 	// https://blog.csdn.net/synapse7/article/details/9946013
 	// todo https://codeforces.ml/blog/entry/61290
