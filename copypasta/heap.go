cab0f5d4795efbe1fe3b000c6dfc4fcf217226f0
Endless <loli.con@qq.com>
2020-05-08 17:44:02 +0800

    简化代码

--- copypasta/heap.go
+++ copypasta/heap.go
@@ -9,14 +9,14 @@ import (
 
 type hp struct{ sort.IntSlice }
 
-//func (h hp) Less(i, j int) bool    { return h.IntSlice[i] > h.IntSlice[j] } // 最大堆
-func (h *hp) Push(v interface{})   { h.IntSlice = append(h.IntSlice, v.(int)) }
-func (h *hp) Pop() (v interface{}) { a := h.IntSlice; h.IntSlice, v = a[:len(a)-1], a[len(a)-1]; return }
-func (h *hp) push(v int)           { heap.Push(h, v) }
-func (h *hp) pop() int             { return heap.Pop(h).(int) }
-func (h hp) empty() bool           { return len(h.IntSlice) == 0 }
-func (h hp) top() int              { return h.IntSlice[0] }
-func (h hp) size() int             { return len(h.IntSlice) }
+//func (h hp) Less(i, j int) bool  { return h.IntSlice[i] > h.IntSlice[j] } // 最大堆
+func (h *hp) Push(v interface{}) { h.IntSlice = append(h.IntSlice, v.(int)) }
+func (h *hp) Pop() interface{}   { a := h.IntSlice; v := a[len(a)-1]; h.IntSlice = a[:len(a)-1]; return v }
+func (h *hp) push(v int)         { heap.Push(h, v) }
+func (h *hp) pop() int           { return heap.Pop(h).(int) }
+func (h hp) empty() bool         { return len(h.IntSlice) == 0 }
+func (h hp) top() int            { return h.IntSlice[0] }
+func (h hp) size() int           { return len(h.IntSlice) }
 func (h *hp) pushPop(v int) int {
 	if len(h.IntSlice) > 0 && v > h.IntSlice[0] { // 大根堆改成 v < h.IntSlice[0]
 		v, h.IntSlice[0] = h.IntSlice[0], v
@@ -30,16 +30,16 @@ func (h *hp) popPush(v int) int { t := h.IntSlice[0]; h.IntSlice[0] = v; heap.Fi
 
 type hp64 []int64 // 自定义类型
 
-func (h hp64) Len() int              { return len(h) }
-func (h hp64) Less(i, j int) bool    { return h[i] < h[j] } // > 为最大堆
-func (h hp64) Swap(i, j int)         { h[i], h[j] = h[j], h[i] }
-func (h *hp64) Push(v interface{})   { *h = append(*h, v.(int64)) }
-func (h *hp64) Pop() (v interface{}) { a := *h; *h, v = a[:len(a)-1], a[len(a)-1]; return }
-func (h *hp64) push(v int64)         { heap.Push(h, v) }
-func (h *hp64) pop() int64           { return heap.Pop(h).(int64) }
-func (h hp64) empty() bool           { return len(h) == 0 }
-func (h hp64) top() int64            { return h[0] }
-func (h hp64) size() int             { return len(h) }
+func (h hp64) Len() int            { return len(h) }
+func (h hp64) Less(i, j int) bool  { return h[i] < h[j] } // > 为最大堆
+func (h hp64) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *hp64) Push(v interface{}) { *h = append(*h, v.(int64)) }
+func (h *hp64) Pop() interface{}   { a := *h; v := a[len(a)-1]; *h = a[:len(a)-1]; return v }
+func (h *hp64) push(v int64)       { heap.Push(h, v) }
+func (h *hp64) pop() int64         { return heap.Pop(h).(int64) }
+func (h hp64) empty() bool         { return len(h) == 0 }
+func (h hp64) top() int64          { return h[0] }
+func (h hp64) size() int           { return len(h) }
 func (h *hp64) pushPop(v int64) int64 {
 	if len(*h) > 0 && v > (*h)[0] { // 大根堆改成 v < (*h)[0]
 		v, (*h)[0] = (*h)[0], v
