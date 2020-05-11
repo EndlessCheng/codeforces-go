3871e68d3eb42d965e85b652ed4efbc1804199b1
Endless <loli.con@qq.com>
2020-05-11 15:39:18 +0800

    改进预处理阶乘逆元的方法

--- misc/atcoder/abc167/e/e.go
+++ misc/atcoder/abc167/e/e.go
@@ -9,22 +9,26 @@ import (
 // github.com/EndlessCheng/codeforces-go
 func run(_r io.Reader, _w io.Writer) {
 	const mod = 998244353
-	const mx = 2e5
+	const mx int = 2e5
 	F := [mx + 1]int{1}
 	for i := 1; i <= mx; i++ {
 		F[i] = F[i-1] * i % mod
 	}
 	pow := func(x, n int) int {
-		r := 1
+		res := 1
 		for ; n > 0; n >>= 1 {
 			if n&1 == 1 {
-				r = r * x % mod
+				res = res * x % mod
 			}
 			x = x * x % mod
 		}
-		return r
+		return res
 	}
-	c := func(n, k int) int { return F[n] * pow(F[k]*F[n-k]%mod, mod-2) % mod }
+	invF := [...]int{mx: pow(F[mx], mod-2)}
+	for i := mx; i > 0; i-- {
+		invF[i-1] = invF[i] * i % mod
+	}
+	c := func(n, k int) int { return F[n] * invF[k] % mod * invF[n-k] % mod }
 
 	var n, m, k, ans int
 	Fscan(_r, &n, &m, &k)
