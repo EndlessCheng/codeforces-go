5903412c8ec718e964e6cf35a8ac5977d1ea63f3
Endless <loli.con@qq.com>
2020-05-17 01:26:31 +0800

    [2600][1A] Add 1355F with test (枚举质数，不断增加系数)

---
+++ main/1355F.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	. "fmt"
+	"math/big"
+)
+
+// github.com/EndlessCheng/codeforces-go
+func CF1355F() {
+	const mx int = 1e3
+	primes := []int{}
+	isP := [mx]bool{}
+	for i := range isP {
+		isP[i] = true
+	}
+	isP[0], isP[1] = false, false
+	for i := 2; i < mx; i++ {
+		if isP[i] {
+			primes = append(primes, i)
+			for j := 2 * i; j < mx; j += i {
+				isP[j] = false
+			}
+		}
+	}
+	upper := big.NewInt(1e18)
+
+	var t int
+	var g int64
+	for Scan(&t); t > 0; t-- {
+		ans := 1
+		es := [mx]int{}
+		for i := 0; i < 22; i++ {
+			checks := []int64{}
+			q := big.NewInt(1)
+		o:
+			for _, p := range primes {
+				if e := es[p]; e >= 0 {
+					for ; e >= 0; e-- {
+						p := big.NewInt(int64(p))
+						if new(big.Int).Mul(q, p).Cmp(upper) > 0 {
+							break o
+						}
+						q.Mul(q, p)
+					}
+					checks = append(checks, int64(p))
+				}
+			}
+			Println("?", q.Int64())
+			Scan(&g)
+			for _, p := range checks {
+				e := 0
+				for ; g%p == 0; g /= p {
+					e++
+				}
+				if e > es[p] {
+					es[p] = e
+					if i == 21 {
+						ans *= e + 1
+					}
+				} else {
+					es[p] = -1
+					ans *= e + 1
+				}
+			}
+		}
+		ans *= 2
+		if ans < 8 {
+			ans = 8
+		}
+		Println("!", ans)
+	}
+}
+
+//func main() { CF1355F() }
---
+++ main/1355F_test.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCF1355F(t *testing.T) {
+}
