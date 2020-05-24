ab2abd076a57445a26ccdcffbac2779b7e82808d
Endless <loli.con@qq.com>
2020-05-24 19:03:18 +0800

    add

---
+++ misc/算法竞赛进阶指南/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+func run(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var n int
+	Fscan(in, &n)
+
+}
+
+func main() { run(os.Stdin, os.Stdout) }
