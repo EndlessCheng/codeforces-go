02e3d849ac771aa72c23481049bf231423a61403
Endless <loli.con@qq.com>
2020-05-24 11:47:54 +0800

    add a comment

--- copypasta/dp.go
+++ copypasta/dp.go
@@ -137,6 +137,7 @@ func dpCollections() {
 		for i, b1 := range s1 {
 			for j, b2 := range s2 {
 				if b1 == b2 {
+					// ignore values from dp[i][j+1] and dp[i+1][j]
 					dp[i+1][j+1] = dp[i][j] + 1
 				} else {
 					dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
