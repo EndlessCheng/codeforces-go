d6558cd3708aed5d4b91872b44b640a87f9999ab
Endless <loli.con@qq.com>
2020-05-25 11:52:03 +0800

    CF 35D (01背包)

---
+++ main/35D.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+)
+
+// github.com/EndlessCheng/codeforces-go
+func CF35D(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	var n, maxW int
+	Fscan(in, &n, &maxW)
+	ws := make([]int, n)
+	for i := range ws {
+		Fscan(in, &ws[i])
+		ws[i] *= n - i
+	}
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, maxW+1)
+	}
+	for i, wi := range ws {
+		for j, dpij := range dp[i] {
+			if j < wi {
+				dp[i+1][j] = dpij
+			} else {
+				dp[i+1][j] = max(dpij, dp[i][j-wi]+1)
+			}
+		}
+	}
+	Fprint(_w, dp[n][maxW])
+}
+
+//func main() { r, _ := os.Open("input.txt"); w, _ := os.Create("output.txt"); CF35D(r, w) }
---
+++ main/35D_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"testing"
+)
+
+func TestCF35D(t *testing.T) {
+	// just copy from website
+	rawText := `
+inputCopy
+3 4
+1 1 1
+outputCopy
+2
+inputCopy
+3 6
+1 1 1
+outputCopy
+3`
+	testutil.AssertEqualCase(t, rawText, 0, CF35D)
+}
