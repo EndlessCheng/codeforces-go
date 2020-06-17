3228cd6cb49a0efe63e95cfa35f3d2f0bf09cdbf
Endless <loli.con@qq.com>
2020-06-17 13:55:22 +0800

    CF 894C

--- main/894C.go
+++ main/894C.go
@@ -11,16 +11,23 @@ func CF894C(_r io.Reader, _w io.Writer) {
 	in := bufio.NewReader(_r)
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
+	gcd := func(a, b int) int {
+		for a != 0 {
+			a, b = b%a, a
+		}
+		return b
+	}
 
-	var n int
+	var n, g int
 	Fscan(in, &n)
 	a := make([]int, n)
 	for i := range a {
 		Fscan(in, &a[i])
-		if a[i]%a[0] != 0 {
-			Fprint(out, -1)
-			return
-		}
+		g = gcd(g, a[i])
+	}
+	if g != a[0] {
+		Fprint(out, -1)
+		return
 	}
 	Fprintln(out, 2*n)
 	for _, v := range a {
