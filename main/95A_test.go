8f11bc1d905a28873915e3e48acd08c9e833f173
Endless <loli.con@qq.com>
2020-06-29 10:42:09 +0800

    CF 95A

---
+++ main/95A_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"testing"
+)
+
+func TestCF95A(t *testing.T) {
+	// just copy from website
+	rawText := `
+inputCopy
+3
+bers
+ucky
+elu
+PetrLoveLuckyNumbers
+t
+outputCopy
+PetrLovtTttttNumtttt
+inputCopy
+4
+hello
+party
+abefglghjdhfgj
+IVan
+petrsmatchwin
+a
+outputCopy
+petrsmatchwin
+inputCopy
+2
+aCa
+cba
+abAcaba
+c
+outputCopy
+abCacba
+inputCopy
+2
+A
+B
+abababBabaBBaBBBBAaaaAAAAA
+a
+outputCopy
+bababaAbabAAbAAAABbbbBBBBB`
+	testutil.AssertEqualCase(t, rawText, 0, CF95A)
+}
