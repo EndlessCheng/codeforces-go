6b7fc56796126ec3b3ea58c6c18083e5b45ec85f
Endless <loli.con@qq.com>
2020-08-05 03:09:39 +0800

    add

---
+++ main/edu/2/disjointset/step1/b/main_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	. "fmt"
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"io"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	// TODO: 测试参数的下界和上界！
+	customTestCases := [][2]string{
+		{
+			`5 11
+union 1 2
+get 3
+get 2
+union 2 3
+get 2
+union 1 3
+get 5
+union 4 5
+get 5
+union 4 1
+get 5`,
+			`3 3 1
+1 2 2
+1 3 3
+5 5 1
+4 5 2
+1 5 5`,
+		},
+	}
+	testutil.AssertEqualStringCase(t, customTestCases, 0, run)
+}
+
+// 无尽对拍
+func Test2(t *testing.T) {
+	return
+	//rand.Seed(time.Now().UnixNano())
+	inputGenerator := func() string {
+		//return ``
+		rg := testutil.NewRandGenerator()
+		n := rg.Int(1, 10)
+		rg.NewLine()
+		rg.IntSlice(n, 1, n)
+		//Println(rg.String())
+		return rg.String()
+	}
+
+	// 暴力算法
+	runBF := func(in io.Reader, out io.Writer) {
+		//return
+		var n int
+		Fscan(in, &n)
+		a := make([]int, n)
+		for i := range a {
+			Fscan(in, &a[i])
+		}
+
+		ans := 0
+
+		Fprint(out, ans)
+	}
+
+	testutil.AssertEqualRunResultsInf(t, inputGenerator, runBF, run)
+}
