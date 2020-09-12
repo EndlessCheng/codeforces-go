8ac6063be3090917a06c8da0308f45aa486a132f
Endless <loli.con@qq.com>
2020-09-12 20:02:55 +0800

    add LC 94

--- leetcode/main.go
+++ leetcode/main.go
@@ -142,6 +142,38 @@ func exist(board [][]byte, word string) bool {
 	return false
 }
 
+// LC 94
+func inorderTraversal(root *TreeNode) (res []int) {
+	for root != nil {
+		if root.Left != nil {
+			// predecessor 节点即当前 root 节点向左走一步，然后一直向右走至无法走为止的节点
+			predecessor := root.Left
+			for predecessor.Right != nil && predecessor.Right != root {
+				predecessor = predecessor.Right
+			}
+			if predecessor.Right == nil {
+				// 将 predecessor 的右指针指向 root，这样后面遍历完左子树 root.Left 后，就能通过这个指向回到 root
+				predecessor.Right = root
+				// 遍历左子树
+				root = root.Left
+			} else {
+				// 若 predecessor 的右指针已经指向了 root，则表示左子树 root.Left 已经访问完了
+				res = append(res, root.Val)
+				// 恢复原样
+				predecessor.Right = nil
+				// 遍历右子树
+				root = root.Right
+			}
+		} else {
+			// 如果没有左孩子，则直接访问右孩子
+			// 若这颗子树已遍历完，则 root 会通过前面设置的指向回到这颗子树的父节点
+			res = append(res, root.Val)
+			root = root.Right
+		}
+	}
+	return
+}
+
 // LC 99
 func recoverTree(root *TreeNode) {
 	nodes := []*TreeNode{}
