4e16dd4345a6bcb48927c92eb8415e95fba1f6c4
Endless <loli.con@qq.com>
2020-09-12 20:22:00 +0800

    add LC 94

--- leetcode/main.go
+++ leetcode/main.go
@@ -147,9 +147,10 @@ func exist(board [][]byte, word string) bool {
 func inorderTraversal(root *TreeNode) (res []int) {
 	for root != nil {
 		if root.Left != nil {
-			// predecessor 节点即当前 root 节点向左走一步，然后一直向右走至无法走为止的节点
+			// predecessor 节点表示当前 root 节点向左走一步，然后一直向右走至无法走为止的节点
 			predecessor := root.Left
 			for predecessor.Right != nil && predecessor.Right != root {
+				// 有右子树且没有设置过指向 root，则继续向右走
 				predecessor = predecessor.Right
 			}
 			if predecessor.Right == nil {
@@ -157,18 +158,17 @@ func inorderTraversal(root *TreeNode) (res []int) {
 				predecessor.Right = root
 				// 遍历左子树
 				root = root.Left
-			} else {
-				// 若 predecessor 的右指针已经指向了 root，则表示左子树 root.Left 已经访问完了
+			} else { // predecessor 的右指针已经指向了 root，则表示左子树 root.Left 已经访问完了
 				res = append(res, root.Val)
 				// 恢复原样
 				predecessor.Right = nil
 				// 遍历右子树
 				root = root.Right
 			}
-		} else {
-			// 如果没有左孩子，则直接访问右孩子
-			// 若这颗子树已遍历完，则 root 会通过前面设置的指向回到这颗子树的父节点
+		} else { // 没有左子树
 			res = append(res, root.Val)
+			// 若有右子树，则遍历右子树
+			// 若没有右子树，则整颗左子树已遍历完，root 会通过之前设置的指向回到这颗子树的父节点
 			root = root.Right
 		}
 	}
