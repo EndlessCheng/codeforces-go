a2ebb9a1bb34c0f6fa34903fdaba647dff54ea1e
Endless <loli.con@qq.com>
2020-09-14 15:14:13 +0800

    LC 37 位运算+搜索

--- leetcode/main.go
+++ leetcode/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     . "github.com/EndlessCheng/codeforces-go/leetcode/testutil"
+    "math/bits"
     "sort"
 )
 
@@ -21,6 +22,77 @@ func main() {
     _ = MustBuildTreeNode
 
     _ = []interface{}{toBytes, ListNode{}, TreeNode{}}
+}
+
+// LC 37
+func solveSudoku(board [][]byte) {
+    var line, column [9]int
+    var block [3][3]int
+    var spaces [][2]int
+
+    flip := func(i, j int, digit byte) {
+        line[i] ^= 1 << digit
+        column[j] ^= 1 << digit
+        block[i/3][j/3] ^= 1 << digit
+    }
+
+    for i, row := range board {
+        for j, b := range row {
+            if b != '.' {
+                digit := b - '1'
+                flip(i, j, digit)
+            }
+        }
+    }
+
+    for {
+        modified := false
+        for i, row := range board {
+            for j, b := range row {
+                if b != '.' {
+                    continue
+                }
+                mask := 0x1ff &^ uint(line[i]|column[j]|block[i/3][j/3])
+                if mask&(mask-1) == 0 {
+                    digit := byte(bits.TrailingZeros(mask))
+                    flip(i, j, digit)
+                    board[i][j] = digit + '1'
+                    modified = true
+                }
+            }
+        }
+        if !modified {
+            break
+        }
+    }
+
+    for i, row := range board {
+        for j, b := range row {
+            if b == '.' {
+                spaces = append(spaces, [2]int{i, j})
+            }
+        }
+    }
+
+    var dfs func(int) bool
+    dfs = func(pos int) bool {
+        if pos == len(spaces) {
+            return true
+        }
+        i, j := spaces[pos][0], spaces[pos][1]
+        mask := 0x1ff &^ uint(line[i]|column[j]|block[i/3][j/3])
+        for ; mask > 0; mask &= mask - 1 {
+            digit := byte(bits.TrailingZeros(mask))
+            flip(i, j, digit)
+            board[i][j] = digit + '1'
+            if dfs(pos + 1) {
+                return true
+            }
+            flip(i, j, digit)
+        }
+        return false
+    }
+    dfs(0)
 }
 
 // LC 39
