2fdb27d2d7d57c0f487df2ffd663765188f8e483
Endless <loli.con@qq.com>
2020-09-27 14:12:14 +0800

    add comments

--- copypasta/template/leetcode/generator_test.go
+++ copypasta/template/leetcode/generator_test.go
@@ -6,6 +6,8 @@ import (
 )
 
 // TODO: 确认 1.是否登录 2.提交语言是否正确 3.输入法调成英文
+// 由于力扣的限制，登录后会让网页端退出
+// 建议额外用个号，这样可免去重登的麻烦
 func TestGenLeetCodeTests(t *testing.T) {
 	var username, password string
 	if host == hostZH {
