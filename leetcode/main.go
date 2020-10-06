35f34ddfe0330019dbd2bfb8af4db2a0c826de1e
Endless <loli.con@qq.com>
2020-10-06 12:17:39 +0800

    LC 75 荷兰国旗问题

--- leetcode/main.go
+++ leetcode/main.go
@@ -235,6 +235,21 @@ func permuteUnique(nums []int) (ans [][]int) {
     return
 }
 
+// LC 75
+// 荷兰国旗问题
+func sortColors(nums []int) {
+    p0, p2 := 0, len(nums)-1
+    for i := 0; i <= p2; i++ {
+        for ; i <= p2 && nums[i] == 2; p2-- {
+            nums[i], nums[p2] = nums[p2], nums[i]
+        }
+        if nums[i] == 0 {
+            nums[i], nums[p0] = nums[p0], nums[i]
+            p0++
+        }
+    }
+}
+
 // LC 79
 func exist(board [][]byte, word string) bool {
     type pair struct{ x, y int }
