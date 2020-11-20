837e358dc95aadbb581a8b3a8f3990d2f3120f65
Endless <loli.con@qq.com>
2020-11-20 14:45:32 +0800

    LC 148

--- leetcode/main.go
+++ leetcode/main.go
@@ -619,6 +619,55 @@ func detectCycle(head *ListNode) *ListNode {
     return nil
 }
 
+// LC 148
+func merge(head1, head2 *ListNode) *ListNode {
+    dummyHead := &ListNode{}
+    temp, temp1, temp2 := dummyHead, head1, head2
+    for temp1 != nil && temp2 != nil {
+        if temp1.Val <= temp2.Val {
+            temp.Next = temp1
+            temp1 = temp1.Next
+        } else {
+            temp.Next = temp2
+            temp2 = temp2.Next
+        }
+        temp = temp.Next
+    }
+    if temp1 != nil {
+        temp.Next = temp1
+    } else if temp2 != nil {
+        temp.Next = temp2
+    }
+    return dummyHead.Next
+}
+
+func sort2(head, tail *ListNode) *ListNode {
+    if head == nil {
+        return head
+    }
+
+    if head.Next == tail {
+        head.Next = nil
+        return head
+    }
+
+    slow, fast := head, head
+    for fast != tail {
+        slow = slow.Next
+        fast = fast.Next
+        if fast != tail {
+            fast = fast.Next
+        }
+    }
+
+    mid := slow
+    return merge(sort2(head, mid), sort2(mid, tail))
+}
+
+func sortList(head *ListNode) *ListNode {
+    return sort2(head, nil)
+}
+
 // LC 152
 func maxProduct(a []int) int {
     min := func(a, b int) int {
