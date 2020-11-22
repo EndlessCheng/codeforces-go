49c7c4af496eee435d7b5452ee94c2c5972fea49
Endless <loli.con@qq.com>
2020-11-22 23:21:08 +0800

    LC 222 O(logn) 求节点个数

--- leetcode/main.go
+++ leetcode/main.go
@@ -715,6 +715,34 @@ func combinationSum3(k int, n int) (ans [][]int) {
     return
 }
 
+// LC 222
+// O(logn) 求节点个数
+func countNodes(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    level := 0
+    for node := root; node.Left != nil; node = node.Left {
+        level++
+    }
+    return sort.Search(1<<(level+1), func(k int) bool {
+        if k <= 1<<level {
+            return false
+        }
+        bits := 1 << (level - 1)
+        node := root
+        for node != nil && bits > 0 {
+            if bits&k == 0 {
+                node = node.Left
+            } else {
+                node = node.Right
+            }
+            bits >>= 1
+        }
+        return node == nil
+    }) - 1
+}
+
 // LC 226
 func invertTree(root *TreeNode) *TreeNode {
     if root == nil {
