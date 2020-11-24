ff8ba7f4b018b783b04b6d316bbb5bcb8d0e8b76
Endless <loli.con@qq.com>
2020-11-24 11:17:11 +0800

    add countLIS 长度为 m 的 LIS 个数

--- copypasta/dp.go
+++ copypasta/dp.go
@@ -512,6 +512,56 @@ func dpCollections() {
 		return ans
 	}
 
+	// 长度为 m 的 LIS 个数
+	// 赤壁之战 https://www.acwing.com/problem/content/299/
+	// 定义 dp[i][j] 表示 a[:j+1] 的长度为 i 且以 a[j] 结尾的 LIS
+	// 则有 dp[i][j] = ∑dp[i-1][k]  (k<j && a[k]<a[j])
+	// 注意到当 j 增加 1 时，只多了 k=j 这一个新决策，这样可以用树状数组来维护
+	// 复杂度 O(mnlogn)
+	countLIS := func(a []int, m int) int {
+		// 将 a 离散化成从 2 开始的序列
+		b := append([]int(nil), a...)
+		sort.Ints(b)
+		for i, v := range a {
+			a[i] = sort.SearchInts(b, v) + 2
+		}
+
+		n := len(a)
+		const mod int = 1e9 + 7
+		tree := make([]int, n+2)
+		add := func(i, val int) {
+			for ; i < n+2; i += i & -i {
+				tree[i] = (tree[i] + val) % mod
+			}
+		}
+		sum := func(i int) (res int) {
+			for ; i > 0; i &= i - 1 {
+				res = (res + tree[i]) % mod
+			}
+			return
+		}
+
+		dp := make([][]int, m+1)
+		for i := range dp {
+			dp[i] = make([]int, n)
+		}
+		for i := 1; i <= m; i++ {
+			tree = make([]int, n+2)
+			if i == 1 {
+				add(1, 1)
+			}
+			for j, v := range a {
+				dp[i][j] = sum(v - 1)
+				add(v, dp[i-1][j])
+			}
+		}
+		ans := 0
+		for _, v := range dp[m] {
+			ans = (ans + v) % mod
+		}
+		return ans
+	}
+
 	// 本质不同子序列个数
 	// 定义 dp[i][j] 表示前 i 个字符中长度为 j 的本质不同子序列个数
 	// 转移 dp[i][j] = dp[i-1][j]（不选第 i 个字符）+ dp[i-1][j-1] - dp[prev[i]-1][j-1]（选第 i 个字符）
@@ -1263,7 +1313,7 @@ func dpCollections() {
 		maxSubArraySum, maxTwoSubArraySum, maxSubArrayAbsSum,
 		minCostSorted,
 		lcs, lcsPath, longestPalindromeSubsequence,
-		lisSlow, lis, lcis, distinctSubsequence,
+		lisSlow, lis, lcis, countLIS, distinctSubsequence,
 
 		zeroOneKnapsack, zeroOneKnapsackAtLeastFillUp, zeroOneWaysToSum,
 		unboundedKnapsack, unboundedWaysToSum,
