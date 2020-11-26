12f4f2a64c58fed3f321c00bcfcaed100f2aaf97
Endless <loli.con@qq.com>
2020-11-26 13:49:04 +0800

    重构

--- leetcode/177/d/d.go
+++ leetcode/177/d/d.go
@@ -1,59 +1,32 @@
 package main
 
-func largestMultipleOfThree(digits []int) string {
-	cnts := [10]int{}
+import "strings"
+
+func largestMultipleOfThree(digits []int) (ans string) {
+	cnt := [10]int{}
 	sum := 0
 	for _, d := range digits {
-		cnts[d]++
+		cnt[d]++
 		sum += d
 	}
-
-	func() {
-		if sum%3 == 0 {
-			return
-		}
-
-		// 另一种方法是暴力 for 判断
-		check1, check2 := [3]int{1, 4, 7}, [6][2]int{{2, 2}, {2, 5}, {5, 5}, {2, 8}, {5, 8}, {8, 8}}
-		if sum%3 == 2 {
-			check1, check2 = [3]int{2, 5, 8}, [6][2]int{{1, 1}, {1, 4}, {4, 4}, {1, 7}, {4, 7}, {7, 7}}
-		}
-		for _, v := range check1 {
-			if cnts[v] > 0 {
-				cnts[v]--
-				return
+	del := func(v int) bool {
+		for ; v < 10; v += 3 {
+			if cnt[v] > 0 {
+				cnt[v]--
+				return true
 			}
 		}
-		for _, p := range check2 {
-			if a, b := p[0], p[1]; a == b {
-				if cnts[a] > 1 {
-					cnts[a] -= 2
-					return
-				}
-			} else if cnts[a] > 0 && cnts[b] > 0 {
-				cnts[a]--
-				cnts[b]--
-				return
-			}
-		}
-	}()
-
-	digits = []int{}
-	for d, c := range cnts {
-		for ; c > 0; c-- {
-			digits = append(digits, d)
-		}
+		return false
 	}
-	n := len(digits)
-	if n == 0 {
-		return ""
+	if v := sum % 3; v > 0 && !del(v) {
+		del(3 - v)
+		del(3 - v)
 	}
-	if cnts[0] == n {
+	for i := 9; i >= 0; i-- {
+		ans += strings.Repeat(string(byte('0'+i)), cnt[i])
+	}
+	if ans != "" && ans[0] == '0' {
 		return "0"
 	}
-	ans := []byte{}
-	for i := n - 1; i >= 0; i-- {
-		ans = append(ans, byte('0'+digits[i]))
-	}
-	return string(ans)
+	return
 }
