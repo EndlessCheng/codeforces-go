f09a64f4fd11a818833168b87dc5cd2d5c7c95a3
Endless <loli.con@qq.com>
2020-12-01 16:25:07 +0800

    LC 321

--- leetcode/main.go
+++ leetcode/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     . "github.com/EndlessCheng/codeforces-go/leetcode/testutil"
     "math/bits"
     "sort"
@@ -21,6 +22,9 @@ func main() {
     }
     _ = toBytes
     _ = MustBuildTreeNode
+
+    fmt.Println(maxNumber([]int{3, 4, 6, 5}, []int{9, 1, 2, 5, 8, 3}, 5))
+
 }
 
 // LC 18
@@ -620,7 +624,7 @@ func detectCycle(head *ListNode) *ListNode {
 }
 
 // LC 148
-func merge(head1, head2 *ListNode) *ListNode {
+func mergeList(head1, head2 *ListNode) *ListNode {
     dummyHead := &ListNode{}
     temp, temp1, temp2 := dummyHead, head1, head2
     for temp1 != nil && temp2 != nil {
@@ -661,7 +665,7 @@ func sort2(head, tail *ListNode) *ListNode {
     }
 
     mid := slow
-    return merge(sort2(head, mid), sort2(mid, tail))
+    return mergeList(sort2(head, mid), sort2(mid, tail))
 }
 
 func sortList(head *ListNode) *ListNode {
@@ -767,6 +771,56 @@ func lowestCommonAncestor(root, p, q *TreeNode) (ancestor *TreeNode) {
             return
         }
     }
+}
+
+// LC 321
+func maxSubsequence(a []int, k int) (s []int) {
+    for i, v := range a {
+        for len(s) > 0 && len(s)+len(a)-1-i >= k && v > s[len(s)-1] {
+            s = s[:len(s)-1]
+        }
+        if len(s) < k {
+            s = append(s, v)
+        }
+    }
+    return
+}
+
+func lexicographicalLess(a, b []int) bool {
+    for i := 0; i < len(a) && i < len(b); i++ {
+        if a[i] != b[i] {
+            return a[i] < b[i]
+        }
+    }
+    return len(a) < len(b)
+}
+
+func greatFirstMerge(a, b []int) []int {
+    merged := make([]int, len(a)+len(b))
+    for i := range merged {
+        if lexicographicalLess(a, b) {
+            merged[i], b = b[0], b[1:]
+        } else {
+            merged[i], a = a[0], a[1:]
+        }
+    }
+    return merged
+}
+
+func maxNumber(nums1, nums2 []int, k int) (res []int) {
+    start := 0
+    if k > len(nums2) {
+        start = k - len(nums2)
+    }
+    for i := start; i <= k && i <= len(nums1); i++ {
+        s1 := maxSubsequence(nums1, i)
+        s2 := maxSubsequence(nums2, k-i)
+        merged := greatFirstMerge(s1, s2)
+        if lexicographicalLess(res, merged) {
+            res = merged
+        }
+    }
+    return
 }
 
 // LC 327 基于求逆序对的思路
