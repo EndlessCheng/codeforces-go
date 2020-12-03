6c2ca47ffd747b4ede0c2a468ee7128733e395fa
Endless <loli.con@qq.com>
2020-12-03 17:56:45 +0800

    LC 659

--- leetcode/main.go
+++ leetcode/main.go
@@ -946,6 +946,33 @@ func convertBST(root *TreeNode) *TreeNode {
     return root
 }
 
+// LC 659 分割数组为长度至少为三且元素连续的子序列
+func isPossible(nums []int) bool {
+    left := map[int]int{} // 每个数字的剩余个数
+    for _, v := range nums {
+        left[v]++
+    }
+    endCnt := map[int]int{} // 以某个数字结尾的连续子序列的个数
+    for _, v := range nums {
+        if left[v] == 0 {
+            continue
+        }
+        if endCnt[v-1] > 0 { // 若存在以 v-1 结尾的连续子序列，则将 v 加到其末尾
+            left[v]--
+            endCnt[v-1]--
+            endCnt[v]++
+        } else if left[v+1] > 0 && left[v+2] > 0 { // 否则，生成一个长度为 3 的连续子序列
+            left[v]--
+            left[v+1]--
+            left[v+2]--
+            endCnt[v+2]++
+        } else { // 无法生成
+            return false
+        }
+    }
+    return true
+}
+
 // LC 834
 // 返回一个表示节点 i 与其他所有节点距离之和的列表 ans
 func sumOfDistancesInTree(n int, edges [][]int) []int {
