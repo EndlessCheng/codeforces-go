0b5e5014f1b995d2f4074ec5abe0186945ddbd4c
Endless <loli.con@qq.com>
2020-12-22 21:53:51 +0800

    S2-11

--- misc/nowcoder/10323/b/b.go
+++ misc/nowcoder/10323/b/b.go
@@ -2,7 +2,7 @@ package main
 
 // github.com/EndlessCheng/codeforces-go
 func wwork(n int) (c int64) {
-	// or 母函数 1/(1-x^3) => C(n+2,2)
+	// or 母函数 1/(1-x)^3 => C(n+2,2)
 	for i := 0; i < 2; i++ {
 		for j := 0; j < 5; j++ {
 			v := n - i - j
