b359482dc7bda8e8dfbb9669195053a0fefa1f18
Endless <loli.con@qq.com>
2020-12-26 20:59:39 +0800

    Add LeetCode Weekly Contest 140

--- leetcode/140/d/d.go
+++ leetcode/140/d/d.go
@@ -1,5 +1,28 @@
 package main
 
-func smallestSubsequence(text string) string {
-    
+// github.com/EndlessCheng/codeforces-go
+func smallestSubsequence(s string) string {
+	left := [26]int{}
+	for _, b := range s {
+		left[b-'a']++
+	}
+	vis := [26]bool{}
+	stk := []byte{}
+	for i := range s {
+		b := s[i]
+		if !vis[b-'a'] {
+			for len(stk) > 0 && b < stk[len(stk)-1] {
+				last := stk[len(stk)-1] - 'a'
+				if left[last] == 0 {
+					break
+				}
+				vis[last] = false
+				stk = stk[:len(stk)-1]
+			}
+			vis[b-'a'] = true
+			stk = append(stk, b)
+		}
+		left[b-'a']--
+	}
+	return string(stk)
 }
