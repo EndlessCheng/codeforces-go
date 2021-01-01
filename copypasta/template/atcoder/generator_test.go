99b3ba341ef3e74bb193f0a0412cf7f14c07c26c
Endless <loli.con@qq.com>
2021-01-01 10:12:42 +0800

    update links

--- copypasta/template/atcoder/generator_test.go
+++ copypasta/template/atcoder/generator_test.go
@@ -12,10 +12,8 @@ func TestGenAtCoderContestTemplates(t *testing.T) {
 	}
 }
 
-// https://atcoder.jp/contests/abc162/tasks/abc162_e
-// https://atcoder.jp/contests/abc162/submissions
-// https://atcoder.jp/contests/abc162/submissions?f.Language=4026&f.Status=AC&f.Task=abc162_e&orderBy=source_length
-// https://atcoder.jp/contests/abc161/submissions?f.Language=3013&f.Status=AC&f.Task=abc161_f&orderBy=source_length
+// https://atcoder.jp/contests/abc161/tasks/abc161_f
+// https://atcoder.jp/contests/abc161/submissions?f.LanguageName=Go&f.Status=AC&f.Task=abc161_f&orderBy=source_length
 func TestGenAtCoderProblemTemplate(t *testing.T) {
 	const problemURL = ""
 	if err := GenAtCoderProblemTemplate(problemURL); err != nil {
