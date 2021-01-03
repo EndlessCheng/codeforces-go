6b13b3b2567e5b93ce8220905cda3ada0334ba02
Endless <loli.con@qq.com>
2021-01-03 12:31:17 +0800

    Add LeetCode Weekly Contest 222

--- leetcode/222/c/c.go
+++ leetcode/222/c/c.go
@@ -9,11 +9,9 @@ func waysToSplit(a []int) (ans int) {
 	for i, v := range a {
 		sum[i+1] = sum[i] + v
 	}
-	sn := sum[n]
-	for r := 2; r < n && 3*sum[r] <= 2*sn; r++ {
-		l1 := sort.SearchInts(sum[1:r], 2*sum[r]-sn) + 1
-		l2 := sort.Search(r, func(l int) bool { return 2*sum[l] > sum[r] })
-		ans += l2 - l1
+	for r := 2; r < n && 3*sum[r] <= 2*sum[n]; r++ {
+		l1 := sort.SearchInts(sum[1:r], 2*sum[r]-sum[n]) + 1
+		ans += sort.Search(r-l1, func(l int) bool { return 2*sum[l+l1] > sum[r] })
 	}
 	return ans % (1e9 + 7)
 }
