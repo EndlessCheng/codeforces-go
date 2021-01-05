f0f7af088e526c95ecb481a6d6637e49bd71e769
Endless <loli.con@qq.com>
2021-01-05 12:01:55 +0800

    LC 399 带权并查集

--- leetcode/main.go
+++ leetcode/main.go
@@ -1144,6 +1144,57 @@ func findItinerary(tickets [][]string) []string {
     return path
 }
 
+// LC 399 带权并查集
+func calcEquation(equations [][]string, values []float64, queries [][]string) []float64 {
+    id := map[string]int{}
+    for _, eq := range equations {
+        a, b := eq[0], eq[1]
+        if _, has := id[a]; !has {
+            id[a] = len(id)
+        }
+        if _, has := id[b]; !has {
+            id[b] = len(id)
+        }
+    }
+
+    fa := make([]int, len(id))
+    dis := make([]float64, len(id))
+    for i := range fa {
+        fa[i] = i
+        dis[i] = 1
+    }
+    var find func(int) int
+    find = func(x int) int {
+        if fa[x] != x {
+            ffx := find(fa[x])
+            dis[x] *= dis[fa[x]]
+            fa[x] = ffx
+        }
+        return fa[x]
+    }
+    merge := func(from, to int, val float64) {
+        fFrom, fTo := find(from), find(to)
+        dis[fFrom] = val * dis[to] / dis[from]
+        fa[fFrom] = fTo
+    }
+
+    for i, eq := range equations {
+        merge(id[eq[0]], id[eq[1]], values[i])
+    }
+
+    ans := make([]float64, len(queries))
+    for i, q := range queries {
+        start, hasS := id[q[0]]
+        end, hasE := id[q[1]]
+        if hasS && hasE && find(start) == find(end) {
+            ans[i] = dis[start] / dis[end]
+        } else {
+            ans[i] = -1
+        }
+    }
+    return ans
+}
+
 // LC 435
 func eraseOverlapIntervals(intervals [][]int) int {
     n := len(intervals)
