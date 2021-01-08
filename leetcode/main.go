95e701d8f3f6f6a0d261e3bc8660fd6911c5425d
Endless <loli.con@qq.com>
2021-01-08 17:59:18 +0800

    add maxProfitMaxTwice

--- leetcode/main.go
+++ leetcode/main.go
@@ -550,6 +550,25 @@ func connect(root *Node) *Node {
     return root
 }
 
+// LC 123
+func maxProfitMaxTwice(prices []int) int {
+    max := func(a, b int) int {
+        if a > b {
+            return a
+        }
+        return b
+    }
+    buy1, sell1 := -prices[0], 0
+    buy2, sell2 := -prices[0], 0
+    for i := 1; i < len(prices); i++ {
+        buy1 = max(buy1, -prices[i])
+        sell1 = max(sell1, buy1+prices[i])
+        buy2 = max(buy2, sell1-prices[i])
+        sell2 = max(sell2, buy2+prices[i])
+    }
+    return sell2
+}
+
 // LC 124
 func maxPathSum(root *TreeNode) int {
     max := func(a, b int) int {
