0021cbafad3a203ec0e9b42f72cc930efe57aa61
Endless <loli.con@qq.com>
2021-01-10 13:01:57 +0800

    Add LeetCode Weekly Contest 223

--- leetcode/223/d/d.go
+++ leetcode/223/d/d.go
@@ -2,47 +2,56 @@ package main
 
 import (
 	"math/bits"
-	"sort"
 )
 
 // github.com/EndlessCheng/codeforces-go
 func minimumTimeRequired(a []int, k int) int {
 	m := 1 << len(a)
 	sum := make([]int, m)
-	for i := range sum {
-		for s := uint(i); s > 0; s &= s - 1 {
-			sum[i] += a[bits.TrailingZeros(s)]
+	for i := 1; i < m; i++ {
+		p := bits.TrailingZeros(uint(i))
+		sum[i] = sum[i&^(1<<p)] + a[p]
+	}
+	dp := make([][]int, k)
+	for i := range dp {
+		dp[i] = make([]int, m)
+		for j := range dp[i] {
+			dp[i][j] = -1
 		}
 	}
-	return sort.Search(12e7, func(mx int) bool {
-		dp := make([][]int8, m)
-		for i := range dp {
-			dp[i] = make([]int8, k+1)
-			for j := range dp[i] {
-				dp[i][j] = -1
-			}
-		}
-		var f func(int, int8) int8
-		f = func(set int, left int8) (res int8) {
-			if set == 0 {
-				return 1
-			}
-			if left == 0 {
-				return
-			}
-			dv := &dp[set][left]
-			if *dv != -1 {
-				return *dv
-			}
-			defer func() { *dv = res }()
-			for sub, ok := set, true; ok; ok = sub != set {
-				if sum[sub] <= mx && f(set^sub, left-1) == 1 {
-					return 1
-				}
-				sub = (sub - 1) & set
-			}
+	var f func(int, int) int
+	f = func(p, set int) (res int) {
+		if set == 0 {
 			return
 		}
-		return f(m-1, int8(k)) == 1
-	})
+		if p == k {
+			return 1e9
+		}
+		dv := &dp[p][set]
+		if *dv != -1 {
+			return *dv
+		}
+		defer func() { *dv = res }()
+		res = 1e9
+		for sub, ok := set, true; ok; ok = sub != set {
+			res = min(res, max(sum[sub], f(p+1, set^sub)))
+			sub = (sub - 1) & set
+		}
+		return
+	}
+	return f(0, m-1)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
