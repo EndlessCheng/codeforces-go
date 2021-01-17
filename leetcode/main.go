efad2e1ce18509ea3a797f041fecaa87e0796ca3
Endless <loli.con@qq.com>
2021-01-17 09:58:07 +0800

    LC 721 并查集

--- leetcode/main.go
+++ leetcode/main.go
@@ -1361,6 +1361,56 @@ func isPossible(nums []int) bool {
     return true
 }
 
+// LC 721
+func accountsMerge(accounts [][]string) (ans [][]string) {
+    emailToIndex := map[string]int{}
+    emailToName := map[string]string{}
+    for _, account := range accounts {
+        name := account[0]
+        for _, email := range account[1:] {
+            if _, has := emailToIndex[email]; !has {
+                emailToIndex[email] = len(emailToIndex)
+                emailToName[email] = name
+            }
+        }
+    }
+
+    fa := make([]int, len(emailToIndex))
+    for i := range fa {
+        fa[i] = i
+    }
+    var find func(int) int
+    find = func(x int) int {
+        if fa[x] != x {
+            fa[x] = find(fa[x])
+        }
+        return fa[x]
+    }
+    union := func(from, to int) {
+        fa[find(from)] = find(to)
+    }
+
+    for _, account := range accounts {
+        firstIndex := emailToIndex[account[1]]
+        for _, email := range account[2:] {
+            union(emailToIndex[email], firstIndex)
+        }
+    }
+
+    indexToEmails := map[int][]string{}
+    for email, index := range emailToIndex {
+        index = find(index)
+        indexToEmails[index] = append(indexToEmails[index], email)
+    }
+
+    for _, emails := range indexToEmails {
+        sort.Strings(emails)
+        account := append([]string{emailToName[emails[0]]}, emails...)
+        ans = append(ans, account)
+    }
+    return
+}
+
 // LC 738 返回 <=N 的最大的非降整数
 func monotoneIncreasingDigits(N int) int {
     s := []byte(strconv.Itoa(N))
