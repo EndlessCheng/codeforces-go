5f2124e788d1cf85df882be8072df2c07cbb60f9
Endless <loli.con@qq.com>
2021-02-08 12:56:23 +0800

    优化计算子集和的逻辑

--- leetcode/227/d/d.go
+++ leetcode/227/d/d.go
@@ -1,43 +1,39 @@
 package main
 
-import (
-	"math/bits"
-	"sort"
-)
+import "sort"
 
 // github.com/EndlessCheng/codeforces-go
-func minAbsDifference(a []int, goal int) (ans int) {
-	ans = abs(goal)
+func minAbsDifference(a []int, g int) (ans int) {
+	ans = abs(g)
 
 	n := len(a)
 	if n == 1 {
-		return min(ans, abs(a[0]-goal))
+		return min(ans, abs(a[0]-g))
 	}
 
-	m := n / 2
-	b := a[:m]
-	x := make([]int, 0, 1<<m)
-	calc := func(sub uint) (res int) {
-		for ; sub > 0; sub &= sub - 1 {
-			res += b[bits.TrailingZeros(sub)]
-		}
-		return
-	}
-	for sub := uint(0); sub < 1<<m; sub++ {
-		x = append(x, calc(sub))
-	}
-	sort.Ints(x)
+	x := subSum(a[:n/2])
 
-	b = a[m:]
-	m = n - m
-	for sub := uint(0); sub < 1<<m; sub++ {
-		res := calc(sub) - goal
-		i := sort.SearchInts(x, -res)
-		if i < len(x) {
-			ans = min(ans, abs(res+x[i]))
-		}
-		if i > 0 {
-			ans = min(ans, abs(res+x[i-1]))
+	k := sort.SearchInts(x, g)
+	if k < len(x) {
+		ans = min(ans, abs(x[k]-g))
+	}
+	if k > 0 {
+		ans = min(ans, abs(x[k-1]-g))
+	}
+
+	b := a[n/2:]
+	y := make([]int, 1<<len(b))
+	for i, v := range b {
+		for j := 0; j < 1<<i; j++ {
+			y[1<<i|j] = y[j] + v
+			s := y[1<<i|j] - g
+			k := sort.SearchInts(x, -s)
+			if k < len(x) {
+				ans = min(ans, abs(s+x[k]))
+			}
+			if k > 0 {
+				ans = min(ans, abs(s+x[k-1]))
+			}
 		}
 	}
 	return
@@ -55,4 +51,37 @@ func min(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func merge(a, b []int) []int {
+	i, n := 0, len(a)
+	j, m := 0, len(b)
+	res := make([]int, 0, n+m)
+	for {
+		if i == n {
+			return append(res, b[j:]...)
+		}
+		if j == m {
+			return append(res, a[i:]...)
+		}
+		if a[i] < b[j] {
+			res = append(res, a[i])
+			i++
+		} else {
+			res = append(res, b[j])
+			j++
+		}
+	}
+}
+
+func subSum(a []int) []int {
+	x := []int{0}
+	for _, v := range a {
+		b := make([]int, len(x))
+		for i, w := range x {
+			b[i] = w + v
+		}
+		x = merge(x, b)
+	}
+	return x
 }
