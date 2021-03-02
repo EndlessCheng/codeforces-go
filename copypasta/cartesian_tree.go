0627a7fbe76cb20b80198893f274692e882e7cbe
Endless <loli.con@qq.com>
2021-03-02 16:58:10 +0800

    添加 笛卡尔树 Cartesian tree

---
+++ copypasta/cartesian_tree.go
@@ -0,0 +1,59 @@
+package copypasta
+
+/* 笛卡尔树 Cartesian tree
+https://en.wikipedia.org/wiki/Cartesian_tree
+https://oi-wiki.org/ds/cartesian-tree/
+todo 一些题目 https://www.luogu.com.cn/blog/AAAbbb123/di-ka-er-shu-xue-xi-bi-ji
+
+模板题 https://www.luogu.com.cn/problem/P5854
+      https://www.luogu.com.cn/problem/P1377
+*/
+
+type ctNode struct {
+	lr      [2]*ctNode
+	id, val int
+}
+
+func buildCartesianTree(a []int) *ctNode {
+	if len(a) == 0 {
+		return nil
+	}
+	s := []*ctNode{}
+	for i, v := range a {
+		o := &ctNode{id: i, val: v}
+		for len(s) > 0 {
+			top := s[len(s)-1]
+			if top.val < v {
+				top.lr[1] = o
+				break
+			}
+			o.lr[0] = top
+			s = s[:len(s)-1]
+		}
+		s = append(s, o)
+	}
+	return s[0]
+}
+
+// 非指针版，返回每个节点的左右儿子的编号
+func buildCartesianTree2(a []int) [][2]int {
+	n := len(a)
+	lr := make([][2]int, n)
+	for i := range lr {
+		lr[i] = [2]int{-1, -1}
+	}
+	s := []int{}
+	for i, v := range a {
+		for len(s) > 0 {
+			topI := s[len(s)-1]
+			if a[topI] < v {
+				lr[topI][1] = i
+				break
+			}
+			lr[i][0] = topI
+			s = s[:len(s)-1]
+		}
+		s = append(s, i)
+	}
+	return lr
+}
