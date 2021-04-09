041ba1b787ff9fd0cb69e3573d5eddd959b23185
Endless <loli.con@qq.com>
2021-04-09 16:03:55 +0800

    简化逻辑

--- leetcode/biweekly/47/d/d.go
+++ leetcode/biweekly/47/d/d.go
@@ -3,42 +3,32 @@ package main
 import "sort"
 
 // github.com/EndlessCheng/codeforces-go
-func countPairs(n int, edges [][]int, queries []int) []int {
+func countPairs(n int, es [][]int, qs []int) []int {
 	deg := make([]int, n)
-	for _, e := range edges {
-		if e[0] > e[1] {
-			e[0], e[1] = e[1], e[0]
+	type edge struct{ v, w int }
+	cntE := map[edge]int{}
+	for _, e := range es {
+		v, w := e[0]-1, e[1]-1
+		if v > w {
+			v, w = w, v
 		}
-		e[0]--
-		e[1]--
-		deg[e[0]]++
-		deg[e[1]]++
+		deg[v]++
+		deg[w]++
+		cntE[edge{v, w}]++
 	}
+
 	d := append([]int(nil), deg...)
 	sort.Ints(d)
-	ans := make([]int, len(queries))
-	for q, low := range queries {
-		for i, v := range d {
-			ans[q] += i - sort.SearchInts(d[:i], low-v+1)
+	ans := make([]int, len(qs))
+	for q, low := range qs {
+		low++
+		for e, c := range cntE {
+			if s := deg[e.v] + deg[e.w]; low <= s && s-c < low {
+				ans[q]--
+			}
 		}
-		mp := map[[2]int]int{}
-		for _, e := range edges {
-			sum := deg[e[0]] + deg[e[1]]
-			if sum <= low {
-				continue
-			}
-			p := [2]int{e[0], e[1]}
-			s, has := mp[p]
-			if !has {
-				mp[p] = sum
-				s = sum
-			}
-			if s > low {
-				mp[p]--
-				if s-1 == low {
-					ans[q]--
-				}
-			}
+		for i, c := range d {
+			ans[q] += i - sort.SearchInts(d[:i], low-c)
 		}
 	}
 	return ans
