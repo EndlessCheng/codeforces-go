5d534c05d6ac8e38bff5a457711a41db40cb3288
Endless <loli.con@qq.com>
2021-04-25 15:15:38 +0800

    添加链接，添加动态开点，重构主席树

--- copypasta/segment_tree.go
+++ copypasta/segment_tree.go
@@ -3,36 +3,31 @@ package copypasta
 // 推荐阅读《算法竞赛进阶指南》0x43 和 0x48 节
 // https://oi-wiki.org/ds/seg/
 // https://cp-algorithms.com/data_structures/segment_tree.html
-// https://codeforces.com/blog/entry/18051
-// https://codeforces.com/blog/entry/15890
-// https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/SegmentTree.java.html
-// https://leetcode.com/articles/a-recursive-approach-to-segment-trees-range-sum-queries-lazy-propagation/
 // 总结得比较详细 https://www.acwing.com/blog/content/1684/
+// https://codeforces.com/blog/entry/18051
+// https://codeforces.com/blog/entry/89313
+// https://codeforces.com/blog/entry/15890
+// todo 高效线段树 crazySegmentTree https://codeforces.com/blog/entry/89399
 
-// 本质上来说，每个位置对应着 O(logn) 个线段树上的节点，每个区间可以拆分成至多 O(logn) 个线段树上的区间
+// 注：对于指针写法，必要时禁止 GC，能加速不少
+// func init() { debug.SetGCPercent(-1) }
 
-// EXTRA: 离散化线段树
-// https://codeforces.com/edu/course/2/lesson/5/4/practice/contest/280801/problem/F https://www.luogu.com.cn/problem/P5848
-
-// EXTRA: zkw 线段树
-// 虽说写起来很快，但是 debug 就比较难受了
-// 张昆玮《统计的力量》
-// https://zhuanlan.zhihu.com/p/29876526
-// https://zhuanlan.zhihu.com/p/29937723
-// https://codeforces.com/blog/entry/18051 Efficient and easy segment trees
-
-// todo 李超线段树
-//  https://zhuanlan.zhihu.com/p/64946571
-//  https://www.luogu.com.cn/blog/fzber0103/Li-Chao-Tree
-
+// 最值及其下标 https://codeforces.com/contest/474/problem/E
+// 最大子段和 https://codeforces.com/edu/course/2/lesson/4/2/practice/contest/273278/problem/A https://www.acwing.com/problem/content/246/
+// 最大子段和+按位或 https://www.luogu.com.cn/problem/P7492 (https://www.luogu.com.cn/contest/42328)
 // 根号 https://codeforces.com/problemset/problem/920/F
+// 取模 https://codeforces.com/problemset/problem/438/D
 // 转换的好题 https://codeforces.com/problemset/problem/1187/D
 // 合并 https://codeforces.com/problemset/problem/380/C
 // 区间 mex https://blog.csdn.net/includelhc/article/details/79593496
 //     反向构造题 https://www.luogu.com.cn/problem/P6852
+// 区间（绝对）众数及其次数（摩尔投票算法）https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
+//     LC1157 https://leetcode-cn.com/problems/online-majority-element-in-subarray/
+//     https://www.luogu.com.cn/problem/P3567
+//     https://www.luogu.com.cn/problem/P3765
+//     https://codeforces.com/contest/1514/problem/D
 // GCD https://codeforces.com/problemset/problem/914/D
-// todo 经典题 ai>aj>ak 三元组的数量 https://codeforces.com/problemset/problem/61/E
-// todo 整理 CF961E: 用归并树查询区间内大于等于某个数的元素个数（rank）     其他方法？
+// todo ai>aj>ak 三元组的数量 https://codeforces.com/problemset/problem/61/E
 // 题目推荐 https://cp-algorithms.com/data_structures/segment_tree.html#toc-tgt-12
 // LC https://leetcode-cn.com/tag/segment-tree/
 // 另见 dp.go 的动态 DP 部分
@@ -41,19 +36,29 @@ package copypasta
 // http://poj.org/problem?id=3470
 // todo http://poj.org/problem?id=1201
 
+// EXTRA: 线段树优化建图
+// 每个位置对应着 O(logn) 个线段树上的节点，每个区间可以拆分成至多 O(logn) 个线段树上的区间
+// 这个性质可以用于优化建图
+// https://www.luogu.com.cn/blog/forever-captain/DS-optimize-graph
+// https://codeforces.com/problemset/problem/786/B
+// todo https://www.luogu.com.cn/problem/P6348
+
 // l 和 r 也可以写到方法参数上，实测二者在执行效率上无异
 // 考虑到 debug 和 bug free 上的优点，写到结构体参数中
 type seg []struct {
 	l, r int
-	val  int64
+	val  int
 }
 
-func (t seg) set(o int, val int64) {
+// 单点更新：build 和 update 通用
+func (t seg) set(o, val int) {
 	t[o].val = val
 }
 
-func (t seg) do(a, b int64) int64 {
-	// + * | & ^ min max gcd maxPos mulMatrix ...
+// 合并两个节点上的数据：maintain 和 query 通用
+// 要求操作满足区间可加性
+// 例如 + * | & ^ min max gcd mulMatrix 摩尔投票 最大子段和 ...
+func (seg) op(a, b int) int {
 	if a > b {
 		return a
 	}
@@ -62,10 +67,10 @@ func (t seg) do(a, b int64) int64 {
 
 func (t seg) maintain(o int) {
 	lo, ro := t[o<<1], t[o<<1|1]
-	t[o].val = t.do(lo.val, ro.val)
+	t[o].val = t.op(lo.val, ro.val)
 }
 
-func (t seg) build(a []int64, o, l, r int) {
+func (t seg) build(a []int, o, l, r int) {
 	t[o].l, t[o].r = l, r
 	if l == r {
 		t.set(o, a[l-1])
@@ -78,12 +83,12 @@ func (t seg) build(a []int64, o, l, r int) {
 }
 
 // o=1  1<=i<=n
-func (t seg) update(o, i int, val int64) {
+func (t seg) update(o, i, val int) {
 	if t[o].l == t[o].r {
 		t.set(o, val)
 		return
 	}
-	if i <= (t[o].l+t[o].r)>>1 {
+	if m := (t[o].l + t[o].r) >> 1; i <= m {
 		t.update(o<<1, i, val)
 	} else {
 		t.update(o<<1|1, i, val)
@@ -92,11 +97,10 @@ func (t seg) update(o, i int, val int64) {
 }
 
 // o=1  [l,r] 1<=l<=r<=n
-func (t seg) query(o, l, r int) (res int64) {
+func (t seg) query(o, l, r int) int {
 	if l <= t[o].l && t[o].r <= r {
 		return t[o].val
 	}
-	//defer t.maintain(o)
 	m := (t[o].l + t[o].r) >> 1
 	if r <= m {
 		return t.query(o<<1, l, r)
@@ -106,28 +110,20 @@ func (t seg) query(o, l, r int) (res int64) {
 	}
 	vl := t.query(o<<1, l, r)
 	vr := t.query(o<<1|1, l, r)
-	return t.do(vl, vr)
+	return t.op(vl, vr)
 }
 
-func (t seg) queryAll() int64 { return t[1].val }
+func (t seg) queryAll() int { return t[1].val }
 
-// a 从 0 开始
-func newSegmentTree(a []int64) seg {
+func newSegmentTree(a []int) seg {
 	t := make(seg, 4*len(a))
 	t.build(a, 1, 1, len(a))
 	return t
 }
 
-// EXTRA: 返回最值及其下标
-func (seg) maxPos(a, b int64, pa, pb int) (int64, int) {
-	if b >= a { // >= 为相同元素最右侧位置；若为 > 符号则是相同元素最左侧位置
-		return b, pb
-	}
-	return a, pa
-}
-
 //
 
+// 延迟标记（区间修改）
 // 单个更新操作：
 // + min/max https://codeforces.com/edu/course/2/lesson/5/2/practice/contest/279653/problem/A https://codeforces.com/problemset/problem/1321/E
 // + min/max 转换 https://codeforces.com/gym/294041/problem/E
@@ -143,6 +139,11 @@ func (seg) maxPos(a, b int64, pa, pb int) (int64, int) {
 // * + Σ https://www.luogu.com.cn/problem/P3373 https://leetcode-cn.com/problems/fancy-sequence/
 // = + Σ https://codeforces.com/edu/course/2/lesson/5/4/practice/contest/280801/problem/A
 //
+// 吉老师线段树 Segment Tree Beats
+// todo https://codeforces.com/blog/entry/57319
+//     区间最值操作与区间历史最值详解 https://www.luogu.com.cn/blog/Hakurei-Reimu/seg-beats
+//     https://www.luogu.com.cn/problem/P6242
+//
 // EXTRA: 多项式更新 Competitive Programmer’s Handbook Ch.28
 // 比如区间加等差数列（差分法）https://www.luogu.com.cn/problem/P1438 https://codeforces.com/edu/course/2/lesson/5/4/practice/contest/280801/problem/B
 type lazyST []struct {
@@ -151,9 +152,13 @@ type lazyST []struct {
 	sum  int64
 }
 
+func (lazyST) op(a, b int64) int64 {
+	return a + b // % mod
+}
+
 func (t lazyST) maintain(o int) {
 	lo, ro := t[o<<1], t[o<<1|1]
-	t[o].sum = lo.sum + ro.sum // % mod
+	t[o].sum = t.op(lo.sum, ro.sum)
 }
 
 func (t lazyST) build(a []int64, o, l, r int) {
@@ -168,10 +173,10 @@ func (t lazyST) build(a []int64, o, l, r int) {
 	t.maintain(o)
 }
 
-func (t lazyST) do(O int, v int64) {
-	o := &t[O]
-	o.todo += v                   // % mod
-	o.sum += int64(o.r-o.l+1) * v // % mod
+// 把要 todo 的事情 do 了
+func (t lazyST) do(o int, add int64) {
+	t[o].todo += add                         // % mod
+	t[o].sum += int64(t[o].r-t[o].l+1) * add // % mod
 }
 
 func (t lazyST) spread(o int) {
@@ -218,11 +223,10 @@ func (t lazyST) update(o, l, r int, add int64) {
 }
 
 // o=1  [l,r] 1<=l<=r<=n
-func (t lazyST) query(o, l, r int) (res int64) {
+func (t lazyST) query(o, l, r int) int64 {
 	if l <= t[o].l && t[o].r <= r {
 		return t[o].sum
 	}
-	//defer t.maintain(o)
 	t.spread(o)
 	m := (t[o].l + t[o].r) >> 1
 	if r <= m {
@@ -233,7 +237,7 @@ func (t lazyST) query(o, l, r int) (res int64) {
 	}
 	vl := t.query(o<<1, l, r)
 	vr := t.query(o<<1|1, l, r)
-	return vl + vr // % mod
+	return t.op(vl, vr)
 }
 
 func (t lazyST) queryAll() int64 { return t[1].sum }
@@ -247,132 +251,317 @@ func newLazySegmentTree(a []int64) lazyST {
 
 //
 
-// todo 权值线段树
-// 浅谈权值线段树到主席树 https://www.luogu.com.cn/blog/your-alpha1022/WeightSegmentTree-ChairmanTree
+// 动态开点线段树·其一·单点修改
+// LC327 https://leetcode-cn.com/problems/count-of-range-sum/
+type stNode struct { // rt := &stNode{l: 1, r: 1e9}
+	lo, ro *stNode
+	l, r   int
+	val    int64
+}
 
-//
+func (o *stNode) get() int64 {
+	if o != nil {
+		return o.val
+	}
+	return 0
+}
+
+func (stNode) op(a, b int64) int64 {
+	return a + b //
+}
+
+func (o *stNode) maintain() {
+	o.val = o.op(o.lo.get(), o.ro.get())
+}
+
+func (o *stNode) update(i int, add int64) {
+	if o.l == o.r {
+		o.val += add //
+		return
+	}
+	m := (o.l + o.r) >> 1
+	if i <= m {
+		if o.lo == nil {
+			o.lo = &stNode{l: o.l, r: m}
+		}
+		o.lo.update(i, add)
+	} else {
+		if o.ro == nil {
+			o.ro = &stNode{l: m + 1, r: o.r}
+		}
+		o.ro.update(i, add)
+	}
+	o.val = o.op(o.lo.get(), o.ro.get())
+}
+
+func (o *stNode) query(l, r int) int64 {
+	if o == nil || l > o.r || r < o.l {
+		return 0
+	}
+	if l <= o.l && o.r <= r {
+		return o.val
+	}
+	return o.op(o.lo.query(l, r), o.ro.query(l, r))
+}
+
+// 动态开点线段树·其二·延迟标记（区间修改）
+// http://codeforces.com/problemset/problem/915/E（注：此题有多种解法）
+// https://codeforces.com/edu/course/2/lesson/5/4/practice/contest/280801/problem/F https://www.luogu.com.cn/problem/P5848
+type lazyNode struct { // rt := &lazyNode{l: 1, r: 1e9}
+	lo, ro *lazyNode
+	l, r   int
+	sum    int64
+	todo   int64
+}
+
+func (o *lazyNode) get() int64 {
+	if o != nil {
+		return o.sum
+	}
+	return 0
+}
+
+func (lazyNode) op(a, b int64) int64 {
+	return a + b //
+}
+
+func (o *lazyNode) maintain() {
+	o.sum = o.op(o.lo.get(), o.ro.get())
+}
+
+func (o *lazyNode) do(add int64) {
+	o.todo += add                   // % mod
+	o.sum += int64(o.r-o.l+1) * add // % mod
+}
+
+func (o *lazyNode) spread() {
+	if add := o.todo; add != 0 {
+		o.lo.do(add)
+		o.ro.do(add)
+		o.todo = 0
+	}
+}
+
+func (o *lazyNode) update(l, r int, add int64) {
+	if l <= o.l && o.r <= r {
+		o.do(add)
+		return
+	}
+	o.spread()
+	m := (o.l + o.r) >> 1
+	if l <= m {
+		if o.lo == nil {
+			o.lo = &lazyNode{l: o.l, r: m}
+		}
+		o.lo.update(l, r, add)
+	}
+	if m < r {
+		if o.ro == nil {
+			o.ro = &lazyNode{l: m + 1, r: o.r}
+		}
+		o.ro.update(l, r, add)
+	}
+	o.maintain()
+}
+
+func (o *lazyNode) query(l, r int) int64 {
+	if o == nil || l > o.r || r < o.l {
+		return 0
+	}
+	if l <= o.l && o.r <= r {
+		return o.sum
+	}
+	o.spread()
+	return o.op(o.lo.query(l, r), o.ro.query(l, r))
+}
+
+// EXTRA: 权值线段树
+// todo 讲解与习题 https://www.luogu.com.cn/blog/bfqaq/qian-tan-quan-zhi-xian-duan-shu
+//  浅谈权值线段树到主席树 https://www.luogu.com.cn/blog/your-alpha1022/WeightSegmentTree-ChairmanTree
+//  谈树状数组套权值树 https://www.luogu.com.cn/blog/bfqaq/qian-tan-shu-zhuang-shuo-zu-quan-zhi-shu
 
 // EXTRA: 线段树合并
-// todo https://www.luogu.com.cn/blog/styx-ferryman/xian-duan-shu-ge-bing-zong-ru-men-dao-fang-qi
-// todo https://www.luogu.com.cn/problem/P4556
+// todo 一些题目 https://www.luogu.com.cn/blog/styx-ferryman/xian-duan-shu-ge-bing-zong-ru-men-dao-fang-qi
+//   https://codeforces.com/blog/entry/83969
+//   https://www.luogu.com.cn/problem/P4556
+//   https://codeforces.com/problemset/problem/600/E
+func (o *stNode) merge(b *stNode) *stNode {
+	if o == nil {
+		return b
+	}
+	if b == nil {
+		return o
+	}
+	if o.l == o.r {
+		// 按照所需合并，如加法
+		o.val += b.val
+		return o
+	}
+	o.lo = o.lo.merge(b.lo)
+	o.ro = o.ro.merge(b.ro)
+	o.maintain()
+	return o
+}
 
 //
 
 // 可持久化线段树（又称函数式线段树、主席树） Persistent Segment Tree
-// NOTE: 对于 CF 上的 2e5 数据，直接 new(pstNode) 比在数组上创建指针要慢 700ms
-//       因此建议对时限比较紧的题目改成在数组上创建指针，这里为了简化代码使用的是 new(pstNode)
-// todo 区间最值操作与区间历史最值详解 https://www.luogu.com.cn/blog/Hakurei-Reimu/seg-beats
 // https://oi-wiki.org/ds/persistent-seg/
-// 模板题 https://www.luogu.com.cn/problem/P3834
-// todo 区间元素种类数 https://codeforces.com/problemset/problem/620/E
-// todo 整理模板+重写 https://codeforces.com/problemset/problem/1262/D2
-//                  https://codeforces.com/problemset/problem/813/E (差分)
-// TODO 查询出现次数大于区间长度一半的元素(强制在线) LC1157 https://leetcode-cn.com/problems/online-majority-element-in-subarray/
-// TODO: 补题 https://codeforces.com/contest/786/problem/C
+// 静态+动态 https://www.acwing.com/blog/content/487/
+// https://zhuanlan.zhihu.com/p/250565583
+// https://blog.csdn.net/weixin_43914593/article/details/108861279
+//
+// 模板题 https://www.luogu.com.cn/problem/P3919
+//       https://www.luogu.com.cn/problem/P3834 https://www.acwing.com/problem/content/257/ https://ac.nowcoder.com/acm/contest/7613/C
+// todo 种类数 https://codeforces.com/problemset/problem/620/E
+//  https://codeforces.com/problemset/problem/786/C
+//  差分 https://codeforces.com/problemset/problem/813/E
+//  https://codeforces.com/problemset/problem/837/G
 //  https://codeforces.com/problemset/problem/840/D
 //  https://codeforces.com/problemset/problem/893/F
-//  https://codeforces.com/problemset/problem/837/G
-// todo 标记永久化 一本通 p239
+//  https://codeforces.com/problemset/problem/961/E（不止一种做法）
+//  https://codeforces.com/problemset/problem/1262/D2
 type pstNode struct {
-	l, r   int
 	lo, ro *pstNode
+	l, r   int
 	sum    int64
 }
-type pst []*pstNode // t := make(pst, versions+1) // 一般 versions 为 n
 
-func (pst) _pushUp(o *pstNode) {
-	o.sum = o.lo.sum + o.ro.sum
+func (pstNode) op(a, b int64) int64 {
+	return a + b //
 }
 
-func (t pst) _build(l, r int) *pstNode {
+func (o *pstNode) maintain() {
+	o.sum = o.op(o.lo.sum, o.ro.sum)
+}
+
+// t := make([]*pstNode, 1, maxVersion+1)
+// t[0] = buildPST(a, 1, len(a))
+// 或者 t := []*pstNode{buildPST(a, 1, len(a))}
+func buildPST(a []int64, l, r int) *pstNode {
 	o := &pstNode{l: l, r: r}
 	if l == r {
+		o.sum = a[l-1]
 		return o
 	}
 	m := (l + r) >> 1
-	o.lo = t._build(l, m)
-	o.ro = t._build(m+1, r)
-	//t._pushUp(o)
+	o.lo = buildPST(a, l, m)
+	o.ro = buildPST(a, m+1, r)
+	o.maintain()
 	return o
 }
 
-func (t pst) _buildArr(arr []int64, l, r int) *pstNode {
-	o := &pstNode{l: l, r: r}
-	if l == r {
-		// arr starts at 1
-		o.sum = arr[l]
-		return o
-	}
-	m := (l + r) >> 1
-	o.lo = t._buildArr(arr, l, m)
-	o.ro = t._buildArr(arr, m+1, r)
-	t._pushUp(o)
-	return o
-}
-
-func (t pst) _update(o *pstNode, idx int, val int64) *pstNode {
-	tmp := *o
-	o = &tmp
+// 一般写法是更新到当前版本，然后把返回的新版本加在 t 的末尾，即 t = append(t, t[len(t)-1].update(i, add))
+// 注意为了拷贝一份 pstNode，这里的接收器不是指针
+func (o pstNode) update(i int, add int64) *pstNode {
 	if o.l == o.r {
-		o.sum += val
-		//o.sum = val
-		return o
+		o.sum += add
+		return &o
 	}
-	if m := o.lo.r; idx <= m {
-		o.lo = t._update(o.lo, idx, val)
+	if m := o.lo.r; i <= m {
+		o.lo = o.lo.update(i, add)
 	} else {
-		o.ro = t._update(o.ro, idx, val)
+		o.ro = o.ro.update(i, add)
 	}
-	t._pushUp(o)
-	return o
+	o.maintain()
+	return &o
 }
 
-func (t pst) _query(o *pstNode, l, r int) (res int64) {
+func (o *pstNode) query(l, r int) int64 {
 	if l <= o.l && o.r <= r {
 		return o.sum
 	}
 	m := o.lo.r
-	if l <= m {
-		res += t._query(o.lo, l, r)
+	if r <= m {
+		return o.lo.query(l, r)
 	}
-	if m < r {
-		res += t._query(o.ro, l, r)
+	if m < l {
+		return o.ro.query(l, r)
 	}
-	return
+	vl := o.lo.query(l, r)
+	vr := o.ro.query(l, r)
+	return o.op(vl, vr)
 }
 
-func (t pst) _queryKth(o1, o2 *pstNode, k int) (allKth int) {
-	if o1.l == o1.r {
-		return o1.l
+// 主席树相当于对数组的每个前缀建立一颗线段树
+
+// EXTRA: 查询区间 [l,r] 中第 k 小在整个数组上的名次（从 1 开始）
+// 注意返回的是（排序去重后的数组的）下标，不是元素值
+// 另见 common.go 中 discrete 的代码注释（采用 kth[p.i] = i+1 的离散化方案）
+// 初始 t[0] = buildPST(1, len(a))
+//     t[i+1] = t[i].update(kth[i], 1)   kth[i] 为 a[i] 离散化后的值（从 1 开始）
+// 查询 t[r].kth(t[l-1], k)               类似前缀和 [l,r] 1<=l<=r<=n
+func (o *pstNode) kth(old *pstNode, k int) int {
+	if o.l == o.r {
+		return o.l
 	}
-	if d := o2.lo.sum - o1.lo.sum; d >= int64(k) {
-		return t._queryKth(o1.lo, o2.lo, k)
+	if cntL := o.lo.sum - old.lo.sum; k <= int(cntL) {
+		return o.lo.kth(old.lo, k)
 	} else {
-		return t._queryKth(o1.ro, o2.ro, k-int(d))
+		return o.ro.kth(old.ro, k-int(cntL))
 	}
 }
 
-func (t pst) init(n int)              { t[0] = t._build(1, n) }                  // 创建版本为 0 的线段树
-func (t pst) initArr(arr []int64)     { t[0] = t._buildArr(arr, 1, len(arr)-1) } // arr starts at 1
-func (t pst) copy(dstVer, srcVer int) { t[dstVer] = t[srcVer] }
+// EXTRA: 查询区间 [l,r] 中在 [low,high] 范围内的元素个数
+// low 和 high 为离散化后的值（从 1 开始）
+// http://acm.hdu.edu.cn/showproblem.php?pid=4417
+// 调用方式同上 t[r].countRange(t[l-1], low, high)
+func (o *pstNode) countRange(old *pstNode, low, high int) int {
+	if low <= o.l && o.r <= high {
+		return int(o.sum - old.sum)
+	}
+	m := o.lo.r
+	if high <= m {
+		return o.lo.countRange(old.lo, low, high)
+	}
+	if m < low {
+		return o.ro.countRange(old.ro, low, high)
+	}
+	return o.lo.countRange(old.lo, low, high) + o.ro.countRange(old.ro, low, high)
+}
 
-// 单点更新：基于版本为 srcVer 的线段树，用更新后的结果覆盖 dstVer 版本
-// 1<=idx<=n   dstVer 可以和 srcVer 相同
-// EXTRA: 求区间第 k 大时，遍历 kthArr 作为 idx 传入，见 segment_tree_test.go
-func (t pst) update(dstVer, srcVer, idx int, val int64) { t[dstVer] = t._update(t[srcVer], idx, val) }
-func (t pst) updateKth(ver, kth int)                    { t[ver+1] = t._update(t[ver], kth, 1) }
+// EXTRA: countDiff 区间不同元素个数
+// 做法是维护左侧相同元素的位置
+// todo 参考 https://www.luogu.com.cn/problem/solution/SP3267
+// https://www.luogu.com.cn/problem/P1972
+// https://www.luogu.com.cn/problem/SP3267
 
-// 查询第 ver 个版本下的区间值
-// [l,r] 1<=l<=r<=n
-func (t pst) query(ver, l, r int) int64 { return t._query(t[ver], l, r) }
+// EXTRA: 区间绝对众数及其出现次数（注意返回的众数是离散化后的值）
+// 没找到时返回的 mode = -1
+// 题目链接见最上
+// 调用方式 t[r].countMode(t[l-1], k)，取 k = (r-l+2)/2，即区间长度一半的上取整
+// 这种 k 值使得递归时左右儿子至多调用其中一个
+func (o *pstNode) countMode(old *pstNode, k int) (mode, count int) {
+	if o.l == o.r {
+		return o.l, int(o.sum - old.sum)
+	}
+	if int(o.lo.sum-old.lo.sum) >= k {
+		return o.lo.countMode(old.lo, k)
+	}
+	if int(o.ro.sum-old.ro.sum) >= k {
+		return o.ro.countMode(old.ro, k)
+	}
+	return -1, 0
+}
 
-// EXTRA: 查询区间第 k 大/小在整个数组上的名次 1<=allKth<=n，即排序后的数组下标 (+1)
-// [l,r] 1<=l<=r<=n
-func (t pst) queryKth(l, r, kth int) (allKth int) { return t._queryKth(t[l-1], t[r], kth) }
+// EXTRA: 标记永久化
+// todo 一本通 p239
 
-//sortedArr := make([]int, n)
-//copy(sortedArr, a)
-//sort.Ints(sortedArr)
+//
+
+// 自底向上（zkw）线段树
+// todo  张昆玮《统计的力量》
+//   https://zhuanlan.zhihu.com/p/361935620
+//   https://zhuanlan.zhihu.com/p/29876526
+//   https://zhuanlan.zhihu.com/p/29937723
+//   https://codeforces.com/blog/entry/18051 Efficient and easy segment trees
+
+//
+
+// 李超线段树
+// todo https://oi-wiki.org/ds/li-chao-tree/
+//   https://zhuanlan.zhihu.com/p/64946571
+//   https://www.luogu.com.cn/blog/fzber0103/Li-Chao-Tree
 
 //
 
