2830b2656ed98a2bb7327e9eb7dea51e906edd72
Endless <loli.con@qq.com>
2021-04-27 19:43:13 +0800

    Add LeetCode Weekly Contest 100

--- leetcode/100/a/a.go
+++ leetcode/100/a/a.go
@@ -4,5 +4,5 @@ import "sort"
 
 // github.com/EndlessCheng/codeforces-go
 func isMonotonic(a []int) bool {
-	return sort.IntsAreSorted(a) || sort.SliceIsSorted(a, func(i, j int) bool { return a[i] > a[j] })
+	return sort.IntsAreSorted(a) || sort.IsSorted(sort.Reverse(sort.IntSlice(a)))
 }
