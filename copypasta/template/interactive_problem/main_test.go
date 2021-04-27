029cc1c7ccf699ec27c53c75b31823af3e52f7f6
Endless <loli.con@qq.com>
2021-04-28 00:45:28 +0800

    改进模板

--- copypasta/template/interactive_problem/main_test.go
+++ copypasta/template/interactive_problem/main_test.go
@@ -12,18 +12,24 @@ import (
 func Test_run(t *testing.T) { testRun(t, 0) }
 
 func testRun(t *testing.T, debugCaseNum int) {
+	assert := assert.New(t)
+
 	type testCase struct {
 		input
 		guess
 		innerData []int
 	}
-	testCases := []testCase{
-
+	format := func(tc testCase) (s string) {
+		s = Sprintf("%v", tc.innerData)
+		//s = strings.Join(tc.innerData, "\n")
+		return
 	}
-	for i := 0; i < 1e5; i++ {
+
+	testCases := []testCase{}
+	for tc := 0; tc < 3e5; tc++ {
 		rg := testutil.NewRandGenerator()
-		n := rg.Int(2, 5)         // 输入
-		a := rg.IntSlice(n, 1, 5) // 猜测对象或内部数据
+		n := rg.Int(2, 4)         // 输入
+		a := rg.IntSlice(n, 1, 4) // 猜测对象或内部数据
 		testCases = append(testCases, testCase{
 			input:     input{n},
 			guess:     guess{a},
@@ -31,25 +37,34 @@ func testRun(t *testing.T, debugCaseNum int) {
 		})
 	}
 
-	const queryLimit = 60
 	queryChecker := func(caseNum int, tc testCase) func(req) resp {
-		//n := tc.n
+		n := tc.n
 		//a := append([]int(nil), tc.ans...)
-		_queryCnt := 0
+
+		queryCnt, queryLimit := 0, 4*n
+
 		return func(req req) (resp resp) {
 			if caseNum == debugCaseNum {
 				Print(req, " ")
 				defer func() { Println(resp) }()
 			}
-			_queryCnt++
-			if _queryCnt > queryLimit {
+
+			queryCnt++
+			if queryCnt > queryLimit {
 				panic("query limit exceeded")
 			}
+
 			// ...
 
 			resp.v = -1
 			return
 		}
+	}
+
+	// 有些题目我们仅需要检查答案的合法性就行了
+	ansChecker := func(caseNum int, tc testCase, actualAns guess) bool {
+
+		return true
 	}
 
 	// do test
@@ -65,7 +80,13 @@ func testRun(t *testing.T, debugCaseNum int) {
 		}
 		expectedAns := tc.guess
 		actualAns := run(tc.input, queryChecker(caseNum, tc))
-		if !assert.EqualValues(t, expectedAns, actualAns, "Wrong Answer %d\nInner Data:\n%v", caseNum, tc.innerData) {
+		if !assert.EqualValues(expectedAns, actualAns, "Wrong Answer %d\nInner Data:\n%s", caseNum, format(tc)) {
+			failedCount++
+			if failedCount > failedCountLimit {
+				t.Fatal("too many wrong cases, terminated")
+			}
+		}
+		if !assert.Truef(ansChecker(caseNum, tc, actualAns), "Wrong Answer %d\nMy Answer:\n%s\nInner Data:\n%s", caseNum, actualAns, format(tc)) {
 			failedCount++
 			if failedCount > failedCountLimit {
 				t.Fatal("too many wrong cases, terminated")
