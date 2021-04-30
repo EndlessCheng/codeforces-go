a24b8a213a9fe24b3c43898e239a5b50242fd466
Endless <loli.con@qq.com>
2021-04-30 14:04:25 +0800

    add

--- misc/算法竞赛进阶指南/5.A_斜率优化/任务安排3/main.go
+++ misc/算法竞赛进阶指南/5.A_斜率优化/任务安排3/main.go
@@ -31,7 +31,7 @@ func run(_r io.Reader, out io.Writer) {
 	less := func(i0, i1, i2 int) bool {
 		y0, y1, y2 := Y(i0), Y(i1), Y(i2)
 		x0, x1, x2 := X(i0), X(i1), X(i2)
-		return (y1-y0)*(x2-x1) < (y2-y1)*(x1-x0) // 注意 < 还是 > 以及是否会爆 int64
+		return float64(y1-y0)/float64(x1-x0) < float64(y2-y1)/float64(x2-x1)
 	}
 	push := func(i int) {
 		for len(q) > 1 && !less(q[len(q)-2], q[len(q)-1], i) {
@@ -44,7 +44,7 @@ func run(_r io.Reader, out io.Writer) {
 		// 在队列中二分第一个斜率大于（小于）k 的位置
 		// 下凸包：>
 		// 上凸包：<
-		j := sort.Search(len(q)-1, func(j int) bool { return Y(q[j+1])-Y(q[j]) > k*(X(q[j+1])-X(q[j])) })
+		j := sort.Search(len(q)-1, func(j int) bool { return float64(Y(q[j+1])-Y(q[j]))/float64(X(q[j+1])-X(q[j])) > float64(k) })
 		dp[i] = Y(q[j]) - k*X(q[j]) + sumT[i]*sumC[i] + s*sumC[n]
 		push(i)
 	}
