fb875cd3acaaba9303462db0122c277284e24663
Endless <loli.con@qq.com>
2021-05-19 15:34:24 +0800

    add link

--- copypasta/sort.go
+++ copypasta/sort.go
@@ -391,10 +391,13 @@ func sortCollections() {
 
 	// CDQ 分治
 	// todo https://oi-wiki.org/misc/cdq-divide/
+	//      推荐 https://blog.nowcoder.net/n/f44d4aada5a24f619442dd6ddffa7320
+	//      推荐 https://zhuanlan.zhihu.com/p/332996578
 	//      https://www.bilibili.com/video/BV1mC4y1s7ic
 	//      [学习笔记]CDQ分治和整体二分 https://www.luogu.com.cn/blog/Owencodeisking/post-xue-xi-bi-ji-cdq-fen-zhi-hu-zheng-ti-er-fen
 	//      https://www.luogu.com.cn/blog/ljc20020730/cdq-fen-zhi-xue-xi-bi-ji
 	//      动态逆序对 https://www.luogu.com.cn/problem/P3157 https://www.luogu.com.cn/problem/UVA11990
+	//      CDQ 优化 DP https://www.luogu.com.cn/problem/P2487
 
 	// 整体二分
 	// todo https://oi-wiki.org/misc/parallel-binsearch/
