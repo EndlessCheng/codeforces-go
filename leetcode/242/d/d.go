d0085da41925caa9e0a4b9bfde9e49dba2acd1e1
Endless <loli.con@qq.com>
2021-05-23 14:41:21 +0800

    Add LeetCode Weekly Contest 242

--- leetcode/242/d/d.go
+++ leetcode/242/d/d.go
@@ -2,16 +2,16 @@ package main
 
 // github.com/EndlessCheng/codeforces-go
 func stoneGameVIII(a []int) int {
-	n := len(a)
-	sum := make([]int, n+1)
-	for i, v := range a {
-		sum[i+1] = sum[i] + v
+	sum := 0
+	for _, v := range a {
+		sum += v
 	}
-	f0, f1 := sum[n], sum[n]
-	for i := n - 1; i > 1; i-- {
-		f0, f1 = max(f0, sum[i]-f1), max(f1, sum[i]-f0)
+	f := sum
+	for i := len(a) - 1; i > 1; i-- {
+		sum -= a[i]
+		f = max(f, sum-f)
 	}
-	return f0
+	return f
 }
 
 func max(a, b int) int {
