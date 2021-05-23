37d7634d461d171d6e43fad8cda46931b7c770fa
Endless <loli.con@qq.com>
2021-05-23 13:20:59 +0800

    Add LeetCode Weekly Contest 242

--- leetcode/242/c/c.go
+++ leetcode/242/c/c.go
@@ -1,37 +1,22 @@
 package main
 
 // github.com/EndlessCheng/codeforces-go
-type fenwick struct{ tree []int }
-
-func (f fenwick) inc(i int) {
-	for i++; i < len(f.tree); i += i & -i {
-		f.tree[i]++
-	}
-}
-func (f fenwick) sum(i int) (res int) {
-	for i++; i > 0; i &= i - 1 {
-		res += f.tree[i]
-	}
-	return
-}
-func (f fenwick) query(l, r int) (res int) {
-	return f.sum(r) - f.sum(l-1)
-}
-
 func canReach(s string, minJump, maxJump int) bool {
 	n := len(s)
-	if s[n-1] == '1' {
-		return false
-	}
-	f := fenwick{make([]int, n)}
-	f.inc(0)
-	for i := minJump; i < n; i++ {
-		if s[i] == '0' && f.query(i-maxJump, i-minJump) > 0 {
-			if i == n-1 {
-				return true
-			}
-			f.inc(i)
+	sum := make([]int, n+1) // dp 前缀和
+	sum[1] = 1
+	for i := 1; i < n; i++ {
+		sum[i+1] = sum[i]
+		if i >= minJump && s[i] == '0' && sum[i-minJump+1] > sum[max(0, i-maxJump)] {
+			sum[i+1]++
 		}
 	}
-	return false
+	return sum[n] > sum[n-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
