1c221b9afb2daade9b4446c48c9ade65c1a3aaf8
Endless <loli.con@qq.com>
2021-06-02 15:42:38 +0800

    添加 FWT 解法

--- leetcode/233/d/d.go
+++ leetcode/233/d/d.go
@@ -1,6 +1,63 @@
 package main
 
+import "math/bits"
+
 // github.com/EndlessCheng/codeforces-go
+func fwt(a []int) {
+	n := len(a)
+	for l, k := 2, 1; l <= n; l, k = l<<1, k<<1 {
+		for i := 0; i < n; i += l {
+			for j := 0; j < k; j++ {
+				a[i+j], a[i+j+k] = a[i+j]+a[i+j+k], a[i+j]-a[i+j+k]
+			}
+		}
+	}
+}
+
+func ifwt(a []int) {
+	n := len(a)
+	for l, k := 2, 1; l <= n; l, k = l<<1, k<<1 {
+		for i := 0; i < n; i += l {
+			for j := 0; j < k; j++ {
+				a[i+j], a[i+j+k] = (a[i+j]+a[i+j+k])/2, (a[i+j]-a[i+j+k])/2
+			}
+		}
+	}
+}
+
+func countPairs(nums []int, low, high int) int {
+	mx := 0
+	for _, v := range nums {
+		if v > mx {
+			mx = v
+		}
+	}
+	a := make([]int, 1<<bits.Len(uint(mx)))
+	for _, v := range nums {
+		a[v]++
+	}
+	fwt(a)
+	for i, v := range a {
+		a[i] *= v
+	}
+	ifwt(a)
+	ans := 0
+	for _, v := range a[low:min(high+1, len(a))] {
+		ans += v
+	}
+	return ans / 2
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+//
+
+// 普通解法
 type trieNode struct {
 	son [2]*trieNode
 	cnt int
@@ -43,7 +100,7 @@ func (t *trie) countLimitXOR(v, limit int) (cnt int) {
 	return
 }
 
-func countPairs(a []int, low, high int) (ans int) {
+func countPairs2(a []int, low, high int) (ans int) {
 	t := newTrie()
 	t.put(a[0])
 	for _, v := range a[1:] {
