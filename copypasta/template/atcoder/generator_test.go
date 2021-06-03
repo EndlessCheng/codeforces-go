63569672f992cee7e5fcddc1ac9d5c1426eb781f
Endless <loli.con@qq.com>
2021-06-03 15:48:14 +0800

    use data.txt as url

--- copypasta/template/atcoder/generator_test.go
+++ copypasta/template/atcoder/generator_test.go
@@ -1,6 +1,10 @@
 package atcoder
 
-import "testing"
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+)
 
 // TODO: update REVEL_SESSION
 func TestGenAtCoderContestTemplates(t *testing.T) {
@@ -16,7 +20,11 @@ func TestGenAtCoderContestTemplates(t *testing.T) {
 // https://atcoder.jp/contests/abc161/tasks/abc161_f
 // https://atcoder.jp/contests/abc161/submissions?f.LanguageName=Go&f.Status=AC&f.Task=abc161_f&orderBy=source_length
 func TestGenAtCoderProblemTemplate(t *testing.T) {
-	const problemURL = ""
+	raw, err := ioutil.ReadFile("data.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	problemURL := strings.TrimSpace(string(raw))
 	if err := GenAtCoderProblemTemplate(problemURL); err != nil {
 		t.Fatal(err)
 	}
