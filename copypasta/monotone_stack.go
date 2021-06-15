059fc8a447079e7f675539473782850debd45b7a
Endless <loli.con@qq.com>
2021-06-15 20:58:40 +0800

    update

--- copypasta/monotone_stack.go
+++ copypasta/monotone_stack.go
@@ -26,131 +26,125 @@ https://cp-algorithms.com/data_structures/stack_queue_modification.html
 全 1 子矩阵个数 O(n^2) LC周赛196C https://leetcode-cn.com/contest/weekly-contest-196/problems/count-submatrices-with-all-ones/ 原题为 http://poj.org/problem?id=3494
 已知部分 posR 还原全部 posR；已知 posR 还原 a https://codeforces.com/problemset/problem/1158/C
 */
-func monotoneStackCollections() {
-	max := func(a, b int) int {
-		if a > b {
-			return a
+func monotoneStack(a []int) ([]int, []int) {
+	const border int = -2e9 // 求两侧大的话用 2e9
+	type pair struct{ v, i int }
+
+	// 求左侧严格小于
+	n := len(a)
+	posL := make([]int, n)
+	stack := []pair{{border, -1}}
+	for i, v := range a {
+		for {
+			if top := stack[len(stack)-1]; top.v < v { //
+				posL[i] = top.i
+				break
+			}
+			stack = stack[:len(stack)-1]
 		}
-		return b
+		stack = append(stack, pair{v, i})
 	}
 
-	monotoneStack := func(a []int) ([]int, []int) {
-		const border int = -2e9 // 求两侧大的话用 2e9
-		type pair struct{ v, i int }
+	// 求右侧严格小于
+	posR := make([]int, n)
+	stack = []pair{{border, n}}
+	for i := n - 1; i >= 0; i-- {
+		v := a[i]
+		for {
+			if top := stack[len(stack)-1]; top.v < v { //
+				posR[i] = top.i
+				break
+			}
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, pair{v, i})
+	}
 
-		// 求左侧严格小于
-		n := len(a)
-		posL := make([]int, n)
-		stack := []pair{{border, -1}}
-		for i, v := range a {
+	// EXTRA
+	mx := 0
+	for i, v := range a {
+		l, r := posL[i]+1, posR[i] // [l,r)
+		if v *= r - l; v > mx {
+			mx = v
+		}
+	}
+
+	return posL, posR
+}
+
+// 注：若输入的是一个 1~n 的排列，有更简单的写法（求两侧大于位置）
+// 为简单起见，求出的下标从 1 开始（不存在时表示为 0 或 n+1）
+// https://codeforces.com/contest/1156/problem/E
+func permPosLR(a []int) ([]int, []int) {
+	n := len(a)
+	posV := make([]int, n+1)
+	posL := make([]int, n+2)
+	posR := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		posV[a[i-1]] = i
+		posL[i], posR[i] = i-1, i+1
+	}
+	// 正序遍历求出的是两侧大于位置
+	// 倒序遍历求出的是两侧小于位置
+	for v := 1; v <= n; v++ {
+		p := posV[v]
+		posR[posL[p]] = posR[p]
+		posL[posR[p]] = posL[p]
+	}
+	return posL, posR
+}
+
+// 最大全 1 矩形
+// LC85 https://leetcode-cn.com/problems/maximal-rectangle/
+func maximalRectangleArea(a [][]int) int {
+	const target = 1
+	n, m, ans := len(a), len(a[0]), 0
+	heights := make([][]int, n) // heights(i,j) 表示从 (i,j) 往上看的高度，a(i,j) = 0 时为 0
+	for i, row := range a {
+		heights[i] = make([]int, m)
+		for j, v := range row {
+			if v == target {
+				if i == 0 {
+					heights[i][j] = 1
+				} else {
+					heights[i][j] = heights[i-1][j] + 1
+				}
+			}
+		}
+	}
+
+	type pair struct{ h, i int }
+	for _, hs := range heights {
+		posL := make([]int, m)
+		stack := []pair{{-1, -1}}
+		for j, h := range hs {
 			for {
-				if top := stack[len(stack)-1]; top.v < v { //
-					posL[i] = top.i
+				if top := stack[len(stack)-1]; top.h < h {
+					posL[j] = top.i
 					break
 				}
 				stack = stack[:len(stack)-1]
 			}
-			stack = append(stack, pair{v, i})
+			stack = append(stack, pair{h, j})
 		}
-
-		// 求右侧严格小于
-		posR := make([]int, n)
-		stack = []pair{{border, n}}
-		for i := n - 1; i >= 0; i-- {
-			v := a[i]
+		posR := make([]int, m)
+		stack = []pair{{-1, m}}
+		for j := m - 1; j >= 0; j-- {
+			h := hs[j]
 			for {
-				if top := stack[len(stack)-1]; top.v < v { //
-					posR[i] = top.i
+				if top := stack[len(stack)-1]; top.h < h {
+					posR[j] = top.i
 					break
 				}
 				stack = stack[:len(stack)-1]
 			}
-			stack = append(stack, pair{v, i})
+			stack = append(stack, pair{h, j})
 		}
-
-		// EXTRA
-		mx := 0
-		for i, v := range a {
-			l, r := posL[i]+1, posR[i] // [l,r)
-			mx = max(mx, v*(r-l))
-			//mx = max(mx, v*(sum[r]-sum[l]))
+		for j, h := range hs {
+			if s := (posR[j] - posL[j] - 1) * h; s > ans {
+				ans = s
+			}
 		}
-
-		return posL, posR
 	}
-
-	// 注：若输入的是一个 1~n 的排列，有更简单的写法（求两侧大于位置）
-	// 为简单起见，求出的下标从 1 开始（不存在时表示为 0 或 n+1）
-	// https://codeforces.com/contest/1156/problem/E
-	permPosLR := func(a []int) ([]int, []int) {
-		n := len(a)
-		posV := make([]int, n+1)
-		posL := make([]int, n+2)
-		posR := make([]int, n+1)
-		for i := 1; i <= n; i++ {
-			posV[a[i-1]] = i
-			posL[i], posR[i] = i-1, i+1
-		}
-		// 正序遍历求出的是两侧大于位置
-		// 倒序遍历求出的是两侧小于位置
-		for v := 1; v <= n; v++ {
-			p := posV[v]
-			posR[posL[p]] = posR[p]
-			posL[posR[p]] = posL[p]
-		}
-		return posL, posR
-	}
-
-	// 最大全 1 矩形 LC85 https://leetcode-cn.com/problems/maximal-rectangle/
-	maximalRectangleArea := func(a [][]int) int {
-		const target = 1
-		n, m, ans := len(a), len(a[0]), 0
-		heights := make([][]int, n) // heights(i,j) 表示从 (i,j) 往上看的高度，a(i,j) = 0 时为 0
-		for i, row := range a {
-			heights[i] = make([]int, m)
-			for j, v := range row {
-				if v == target {
-					if i == 0 {
-						heights[i][j] = 1
-					} else {
-						heights[i][j] = heights[i-1][j] + 1
-					}
-				}
-			}
-		}
-		type pair struct{ h, i int }
-		for _, hs := range heights {
-			posL := make([]int, m)
-			stack := []pair{{-1, -1}}
-			for j, h := range hs {
-				for {
-					if top := stack[len(stack)-1]; top.h < h {
-						posL[j] = top.i
-						break
-					}
-					stack = stack[:len(stack)-1]
-				}
-				stack = append(stack, pair{h, j})
-			}
-			posR := make([]int, m)
-			stack = []pair{{-1, m}}
-			for j := m - 1; j >= 0; j-- {
-				h := hs[j]
-				for {
-					if top := stack[len(stack)-1]; top.h < h {
-						posR[j] = top.i
-						break
-					}
-					stack = stack[:len(stack)-1]
-				}
-				stack = append(stack, pair{h, j})
-			}
-			for j, h := range hs {
-				ans = max(ans, (posR[j]-posL[j]-1)*h)
-			}
-		}
-		return ans
-	}
-
-	_ = []interface{}{monotoneStack, permPosLR, maximalRectangleArea}
+	return ans
 }
