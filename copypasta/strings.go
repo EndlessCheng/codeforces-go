1769bf3fc2d959234f215ddb4cd3877a9534f8e3
Endless <loli.con@qq.com>
2021-06-19 01:24:38 +0800

    bug fix

--- copypasta/strings.go
+++ copypasta/strings.go
@@ -402,7 +402,7 @@ func stringCollection() {
 
 		// 返回值含义同 strings.Compare
 		compareSub2 := func(l1, r1, l2, r2 int) int {
-			len1, len2 := r1-l2, r2-l2
+			len1, len2 := r1-l1, r2-l2
 			l := lcp(l1, l2)
 			if len1 == len2 && l == len1 {
 				return 0
