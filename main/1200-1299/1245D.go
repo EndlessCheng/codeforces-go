c3355e8ece8ae99e7f75035d397d718b3392758f
Endless <loli.con@qq.com>
2021-06-26 20:40:16 +0800

    CF1245D 最小生成树 Prim 打印方案

--- main/1200-1299/1245D.go
+++ main/1200-1299/1245D.go
@@ -4,95 +4,100 @@ import (
 	"bufio"
 	. "fmt"
 	"io"
+	"math"
 )
 
 // github.com/EndlessCheng/codeforces-go
-func Sol1245D(reader io.Reader, writer io.Writer) {
+func CF1245D(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
 	abs := func(x int) int {
 		if x < 0 {
 			return -x
 		}
 		return x
 	}
-	in := bufio.NewReader(reader)
-	out := bufio.NewWriter(writer)
-	defer out.Flush()
 
 	var n int
 	Fscan(in, &n)
 	type pair struct{ x, y int }
-	ps := make([]pair, n)
-	for i := range ps {
-		Fscan(in, &ps[i].x, &ps[i].y)
+	city := make([]pair, n)
+	for i := range city {
+		Fscan(in, &city[i].x, &city[i].y)
 	}
-	costCity := make([]int, n)
-	for i := range costCity {
-		Fscan(in, &costCity[i])
+	cityCost := make([]int, n)
+	for i := range cityCost {
+		Fscan(in, &cityCost[i])
 	}
-	costLine := make([]int, n)
-	for i := range costLine {
-		Fscan(in, &costLine[i])
+	wireCost := make([]int, n)
+	for i := range wireCost {
+		Fscan(in, &wireCost[i])
 	}
 
-	dist := [2001][2001]int64{}
-	for i := 0; i < n; i++ {
-		dist[i][n] = int64(costCity[i])
-		dist[n][i] = dist[i][n]
+	// 构建距离（费用）矩阵
+	dis := make([][]int64, n+1)
+	for i := range dis {
+		dis[i] = make([]int64, n+1)
+	}
+	for i, c := range cityCost {
+		dis[i][n] = int64(c) // 建立虚点 n，将所有城市连到点 n 上，值为在该城市上建立发电站的费用
+		dis[n][i] = int64(c)
 		for j := i + 1; j < n; j++ {
-			dist[i][j] = int64(abs(ps[i].x-ps[j].x)+abs(ps[i].y-ps[j].y)) * int64(costLine[i]+costLine[j])
-			dist[j][i] = dist[i][j]
+			// 搭建电线的费用
+			dis[i][j] = int64(abs(city[i].x-city[j].x)+abs(city[i].y-city[j].y)) * int64(wireCost[i]+wireCost[j])
+			dis[j][i] = dis[i][j]
 		}
 	}
 
-	ans := int64(0)
+	totCost := int64(0)
 	ansCity := []interface{}{}
-	ansConn := []pair{}
-	type distCity struct {
-		d int64
-		v int
+	ansWire := []pair{}
+
+	// 由于是稠密图，用 Prim 算法求最小生成树
+	type pair2 struct {
+		cost int64
+		v    int
 	}
-	minCost := make([]distCity, n+1)
+	minCost := make([]pair2, n+1)
 	for i := range minCost {
-		minCost[i].d = int64(1e18)
+		minCost[i].cost = math.MaxInt64
 	}
-	minCost[0].d = 0
+	minCost[0].cost = 0
 	used := make([]bool, n+1)
 	for {
 		v := -1
-		for i := 0; i <= n; i++ {
-			if !used[i] && (v == -1 || minCost[i].d < minCost[v].d) {
-				v = i
+		for w, b := range used {
+			if !b && (v < 0 || minCost[w].cost < minCost[v].cost) {
+				v = w
 			}
 		}
-		if v == -1 {
+		if v < 0 {
 			break
 		}
 		used[v] = true
-		if minCost[v].d > 0 {
-			ans += minCost[v].d
-			if v == n {
-				ansCity = append(ansCity, minCost[v].v+1)
-			} else if minCost[v].v == n {
-				ansCity = append(ansCity, v+1)
-			} else {
-				ansConn = append(ansConn, pair{minCost[v].v + 1, v + 1})
-			}
+		totCost += minCost[v].cost
+		if v == n { // 发电
+			ansCity = append(ansCity, minCost[v].v+1)
+		} else if minCost[v].v == n { // 发电
+			ansCity = append(ansCity, v+1)
+		} else if minCost[v].v != v { // 连电线，注意避免自环
+			ansWire = append(ansWire, pair{minCost[v].v + 1, v + 1})
 		}
-		for w := 0; w <= n; w++ {
-			if dist[v][w] < minCost[w].d {
-				minCost[w] = distCity{dist[v][w], v}
+		for w, d := range dis[v] {
+			if d < minCost[w].cost {
+				minCost[w] = pair2{d, v}
 			}
 		}
 	}
-	Fprintln(out, ans)
+
+	Fprintln(out, totCost)
 	Fprintln(out, len(ansCity))
 	Fprintln(out, ansCity...)
-	Fprintln(out, len(ansConn))
-	for _, p := range ansConn {
-		Fprintln(out, p.x, p.y)
+	Fprintln(out, len(ansWire))
+	for _, conn := range ansWire {
+		Fprintln(out, conn.x, conn.y)
 	}
 }
 
-//func main() {
-//	Sol1245D(os.Stdin, os.Stdout)
-//}
+//func main() { CF1245D(os.Stdin, os.Stdout) }
--- main/1200-1299/1245D_test.go
+++ main/1200-1299/1245D_test.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"testing"
+)
+
+// https://codeforces.com/problemset/problem/1245/D
+// https://codeforces.com/problemset/status/1245/problem/D
+func TestCF1245D(t *testing.T) {
+	// just copy from website
+	rawText := `
+inputCopy
+3
+2 3
+1 1
+3 2
+3 2 3
+3 2 3
+outputCopy
+8
+3
+1 2 3 
+0
+inputCopy
+3
+2 1
+1 2
+3 3
+23 2 23
+3 2 3
+outputCopy
+27
+1
+2 
+2
+1 2
+2 3`
+	testutil.AssertEqualCase(t, rawText, 0, CF1245D)
+}
