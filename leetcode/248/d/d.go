21bbffdd3e6bbc054dc0cafeb22d6bb6d039d912
Endless <loli.con@qq.com>
2021-07-04 15:18:30 +0800

    后缀数组优化

--- leetcode/248/d/d.go
+++ leetcode/248/d/d.go
@@ -1,22 +1,27 @@
 package main
 
-import "sort"
+import (
+	"index/suffixarray"
+	"reflect"
+	"sort"
+	"unsafe"
+)
 
 // github.com/EndlessCheng/codeforces-go
 func longestCommonSubpath(_ int, paths [][]int) (ans int) {
-	s := []int{}
-	r := int(1e9)
+	a := []int{}
+	minLen := int(1e9) // 二分右边界
 	for _, p := range paths {
-		r = min(r, len(p))
-		s = append(s, 1e9) // 用一个不存在 paths 中的数拼接所有路径
-		s = append(s, p...)
+		minLen = min(minLen, len(p))
+		a = append(a, 1e9) // 用一个不存在 paths 中的数拼接所有路径
+		a = append(a, p...)
 	}
-	n, m := len(s), len(paths)
+	n, m := len(a), len(paths)
 
 	// 标记每个元素属于哪条路径
 	ids := make([]int, n)
 	id := -1
-	for i, v := range s {
+	for i, v := range a {
 		if v == 1e9 {
 			id++
 			ids[i] = m
@@ -25,44 +30,19 @@ func longestCommonSubpath(_ int, paths [][]int) (ans int) {
 		}
 	}
 
-	// 构建后缀数组和高度数组
-	sa := make([]int, n+1)
-	rank := make([]int, n+1)
-	k := 0
-	cmp := func(i, j int) bool {
-		if rank[i] != rank[j] {
-			return rank[i] < rank[j]
-		}
-		ri, rj := -1, -1
-		if i+k <= n {
-			ri = rank[i+k]
-		}
-		if j+k <= n {
-			rj = rank[j+k]
-		}
-		return ri < rj
+	// 构建 a 的后缀数组和高度数组
+	s := make([]byte, 0, n*4)
+	for _, v := range a {
+		s = append(s, byte(v>>24), byte(v>>16&0xff), byte(v>>8&0xff), byte(v&0xff))
 	}
-	tmp := make([]int, n+1)
-	for i := 0; i <= n; i++ {
-		sa[i] = i
-		rank[i] = -1
-		if i < n {
-			rank[i] = s[i]
+	_sa := *(*[]int32)(unsafe.Pointer(reflect.ValueOf(suffixarray.New(s)).Elem().FieldByName("sa").Field(0).UnsafeAddr()))
+	sa := make([]int32, 0, n)
+	for _, v := range _sa {
+		if v&3 == 0 {
+			sa = append(sa, v>>2)
 		}
 	}
-	for k = 1; k <= n; k *= 2 {
-		sort.Slice(sa, func(i, j int) bool { return cmp(sa[i], sa[j]) })
-		tmp[sa[0]] = 0
-		for i := 1; i <= n; i++ {
-			tmp[sa[i]] = tmp[sa[i-1]]
-			if cmp(sa[i-1], sa[i]) {
-				tmp[sa[i]]++
-			}
-		}
-		copy(rank, tmp)
-	}
-	sa = sa[1:]
-	rank = make([]int, n)
+	rank := make([]int, n)
 	for i := range rank {
 		rank[sa[i]] = i
 	}
@@ -73,31 +53,29 @@ func longestCommonSubpath(_ int, paths [][]int) (ans int) {
 			h--
 		}
 		if rk > 0 {
-			for j := sa[rk-1]; i+h < n && j+h < n && s[i+h] == s[j+h]; h++ {
+			for j := int(sa[rk-1]); i+h < n && j+h < n && a[i+h] == a[j+h]; h++ {
 			}
 		}
 		height[rk] = h
 	}
 
 	// 二分求答案
-	return sort.Search(r, func(limit int) bool {
+	return sort.Search(minLen, func(limit int) bool {
 		limit++
 		vis := make([]int, m)
-		for i := 1; i < n; {
+		for i := 1; i < n; i++ {
 			if height[i] < limit {
-				i++
 				continue
 			}
-			st := i
 			cnt := 0
-			for ; i < n && height[i] >= limit; i++ {
+			for st := i; i < n && height[i] >= limit; i++ {
 				// 检查 sa[i] 和 sa[i-1]
-				if i := ids[sa[i]]; i < m && vis[i] != st {
-					vis[i] = st
+				if j := ids[sa[i]]; j < m && vis[j] != st {
+					vis[j] = st
 					cnt++
 				}
-				if i := ids[sa[i-1]]; i < m && vis[i] != st {
-					vis[i] = st
+				if j := ids[sa[i-1]]; j < m && vis[j] != st {
+					vis[j] = st
 					cnt++
 				}
 			}
