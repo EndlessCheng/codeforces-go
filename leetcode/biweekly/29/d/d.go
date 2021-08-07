e2fce9d0a940134354378cc483c05a86181595f1
Endless <loli.con@qq.com>
2021-08-07 11:11:08 +0800

    改成递推写法

--- leetcode/biweekly/29/d/d.go
+++ leetcode/biweekly/29/d/d.go
@@ -1,42 +1,46 @@
 package main
 
-import "math/bits"
+import (
+	"math/bits"
+)
 
-// github.com/EndlessCheng/codeforces-go
-func minNumberOfSemesters(n int, dependencies [][]int, k int) (ans int) {
+func minNumberOfSemesters(n int, dependencies [][]int, k int) int {
+	// 计算每门课的先修课集合
 	pre := make([]int, n)
 	for _, d := range dependencies {
 		pre[d[1]-1] |= 1 << (d[0] - 1)
 	}
+	m := 1 << n
+	// 计算所有课程集合的先修课集合，不合法的标记为 -1
+	totPre := make([]int, m)
+	for i := range totPre {
+		if bits.OnesCount(uint(i)) > k {
+			totPre[i] = -1
+			continue
+		}
+		for s := uint(i); s > 0; s &= s - 1 {
+			p := pre[bits.TrailingZeros(s)]
+			if p&i > 0 {
+				totPre[i] = -1
+				break
+			}
+			totPre[i] |= p
+		}
+	}
 	dp := make([]int, 1<<n)
 	for i := range dp {
-		dp[i] = -1
+		dp[i] = n
 	}
-	var f func(int) int
-	f = func(subset int) (res int) {
-		if subset == 0 {
-			return
-		}
-		dv := &dp[subset]
-		if *dv != -1 {
-			return *dv
-		}
-		defer func() { *dv = res }()
-		res = 1e9
-	o:
-		for sub := subset; sub > 0; sub = (sub - 1) & subset {
-			if bits.OnesCount(uint(sub)) <= k {
-				for mask := uint(sub); mask > 0; mask &= mask - 1 {
-					if pre[bits.TrailingZeros(mask)]&subset > 0 {
-						continue o
-					}
-				}
-				res = min(res, f(subset^sub))
+	dp[0] = 0
+	for s, v := range dp {
+		t := m - 1 ^ s // 补集
+		for sub := t; sub > 0; sub = (sub - 1) & t { // 枚举下个学期要学的课
+			if p := totPre[sub]; p >= 0 && s&p == p { // 这些课的先修课必须合法且在之前的学期里必须上过
+				dp[s|sub] = min(dp[s|sub], v+1)
 			}
 		}
-		return res + 1
 	}
-	return f(1<<n - 1)
+	return dp[m-1]
 }
 
 func min(a, b int) int {
