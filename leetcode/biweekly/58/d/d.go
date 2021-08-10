a1d480e6b6b7b95455beb314cb394698769a6eae
Endless <loli.con@qq.com>
2021-08-10 22:23:44 +0800

    马拉车

--- leetcode/biweekly/58/d/d.go
+++ leetcode/biweekly/58/d/d.go
@@ -9,36 +9,37 @@ func maxProduct(s string) int64 {
 		if i < r {
 			hl = min(halfLen[mid*2-i], r-i)
 		}
-		for ; i >= hl && i+hl < n && s[i-hl] == s[i+hl]; hl++ {}
+		for ; i >= hl && i+hl < n && s[i-hl] == s[i+hl]; hl++ {
+		}
 		if i+hl > r {
 			mid, r = i, i+hl
 		}
 		halfLen[i] = hl
 	}
 
-	leftTo := make([]int, n)
-	rightTo := make([]int, n)
+	startPL := make([]int, n)
+	endPL := make([]int, n)
 	for i, hl := range halfLen {
-		pl, pr := i-hl+1, i+hl-1
-		leftTo[pr] = max(leftTo[pr], hl*2-1)
-		rightTo[pl] = max(rightTo[pl], hl*2-1)
-	}
-	for i := n - 2; i >= 0; i-- {
-		leftTo[i] = max(leftTo[i], leftTo[i+1]-2)
+		left, right := i-hl+1, i+hl-1
+		startPL[left] = max(startPL[left], hl*2-1)
+		endPL[right] = max(endPL[right], hl*2-1)
 	}
 	for i := 1; i < n; i++ {
-		leftTo[i] = max(leftTo[i], leftTo[i-1])
-	}
-	for i := 1; i < n; i++ {
-		rightTo[i] = max(rightTo[i], rightTo[i-1]-2)
+		startPL[i] = max(startPL[i], startPL[i-1]-2)
 	}
 	for i := n - 2; i >= 0; i-- {
-		rightTo[i] = max(rightTo[i], rightTo[i+1])
+		startPL[i] = max(startPL[i], startPL[i+1])
+	}
+	for i := n - 2; i >= 0; i-- {
+		endPL[i] = max(endPL[i], endPL[i+1]-2)
+	}
+	for i := 1; i < n; i++ {
+		endPL[i] = max(endPL[i], endPL[i-1])
 	}
 
 	ans := 0
 	for i := 1; i < n; i++ {
-		ans = max(ans, leftTo[i-1]*rightTo[i])
+		ans = max(ans, endPL[i-1]*startPL[i])
 	}
 	return int64(ans)
 }
