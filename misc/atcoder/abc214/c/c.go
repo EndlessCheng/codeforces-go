189d011a86a29e6061f308662cae136651b6b954
Endless <loli.con@qq.com>
2021-08-16 16:01:08 +0800

    ABC214C

---
+++ misc/atcoder/abc214/c/c.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+// github.com/EndlessCheng/codeforces-go
+func run(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var n, mi int
+	Fscan(in, &n)
+	s := make([]int, n)
+	for i := range s {
+		Fscan(in, &s[i])
+	}
+	t := make([]int, n)
+	for i := range t {
+		Fscan(in, &t[i])
+		if t[i] < t[mi] {
+			mi = i
+		}
+	}
+
+	ans := make([]int, n)
+	ans[mi] = t[mi]
+	for i := 1; i < n; i++ {
+		cur := (mi + i) % n
+		pre := (cur + n - 1) % n
+		ans[cur] = min(t[cur], ans[pre]+s[pre])
+	}
+	for _, v := range ans {
+		Fprintln(out, v)
+	}
+}
+
+func main() { run(os.Stdin, os.Stdout) }
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
