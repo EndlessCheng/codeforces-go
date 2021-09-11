1921fe9eccc2cb7cc5d32ce5bab6c88eb0e5a347
Endless <loli.con@qq.com>
2021-09-12 06:10:42 +0800

    改进写法

--- leetcode/season/2021fall/d/d.go
+++ leetcode/season/2021fall/d/d.go
@@ -1,55 +1,46 @@
 package main
 
-import (
-	"sort"
-)
-
-/* 二分+暴力枚举
-
-注意到半径很小，我们可以枚举每个玩具，并暴力枚举该玩具**周围**是否有可以套住该玩具的圈。
-
-具体来说，将 $\textit{circles}$ 排序后，将横坐标相同的圈分为一组。对每个玩具，可以套住该玩具的圈，在横坐标上，必然满足圈的最右端点不小于玩具的最右端点，且圈的最左端点不超过玩具的最左端点。对于纵坐标也是类似。这样我们可以二分要枚举的圈的初始位置，并向后遍历即可。
-
-*/
+import "sort"
 
 // github.com/EndlessCheng/codeforces-go
 func circleGame(toys [][]int, circles [][]int, r0 int) (ans int) {
 	sort.Slice(circles, func(i, j int) bool { a, b := circles[i], circles[j]; return a[0] < b[0] || a[0] == b[0] && a[1] < b[1] })
 
 	// 将横坐标相同的圈分为一组
-	type pair struct{ x, y int }
-	a, y := [][]pair{}, -1
+	type pair struct {
+		x  int
+		ys []int
+	}
+	a, y := []pair{}, -1
 	for _, p := range circles {
-		if len(a) == 0 || p[0] > a[len(a)-1][0].x {
-			a = append(a, []pair{{p[0], p[1]}})
+		if len(a) == 0 || p[0] > a[len(a)-1].x {
+			a = append(a, pair{p[0], []int{p[1]}})
 			y = -1
 		} else if p[1] > y { // 去重
-			a[len(a)-1] = append(a[len(a)-1], pair{p[0], p[1]})
+			a[len(a)-1].ys = append(a[len(a)-1].ys, p[1])
 			y = p[1]
 		}
 	}
 
-outer:
 	for _, t := range toys {
 		x, y, r := t[0], t[1], t[2]
 		if r > r0 {
 			continue
 		}
-		i := sort.Search(len(a), func(i int) bool { return a[i][0].x+r0 >= x+r })
-		for ; i < len(a); i++ {
-			col := a[i]
-			if col[0].x-r0 > x-r {
-				break
-			}
-			j := sort.Search(len(col), func(j int) bool { return col[j].y+r0 >= y+r })
-			for ; j < len(col); j++ {
-				c := col[j]
-				if c.y-r0 > y-r {
+		i := sort.Search(len(a), func(i int) bool { return a[i].x+r0 >= x+r })
+		for ; i < len(a) && a[i].x-r0 <= x-r; i++ {
+			cx, ys := a[i].x, a[i].ys
+			j := sort.SearchInts(ys, y)
+			if j < len(ys) {
+				if cy := ys[j]; (x-cx)*(x-cx)+(y-cy)*(y-cy) <= (r0-r)*(r0-r) {
+					ans++
 					break
 				}
-				if (x-c.x)*(x-c.x)+(y-c.y)*(y-c.y) <= (r0-r)*(r0-r) {
+			}
+			if j > 0 {
+				if cy := ys[j-1]; (x-cx)*(x-cx)+(y-cy)*(y-cy) <= (r0-r)*(r0-r) {
 					ans++
-					continue outer
+					break
 				}
 			}
 		}
