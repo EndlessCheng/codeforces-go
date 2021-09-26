dfead89d4c12f7711641e3b3b728f2cec1991511
Endless <loli.con@qq.com>
2021-09-26 21:23:58 +0800

    LC1235

--- leetcode/weekly/159/d/d.go
+++ leetcode/weekly/159/d/d.go
@@ -1,6 +1,23 @@
 package main
 
-func jobScheduling(startTime []int, endTime []int, profit []int) (ans int) {
-	
-	return
+import "sort"
+
+// github.com/EndlessCheng/codeforces-go
+func jobScheduling(startTime, endTime, profit []int) (ans int) {
+	type job struct{ l, r, p int }
+	a := make([]job, len(startTime))
+	for i, l := range startTime {
+		a[i] = job{l, endTime[i], profit[i]}
+	}
+	sort.Slice(a, func(i, j int) bool { return a[i].r < a[j].r })
+
+	type pair struct{ p, v int }
+	dp := []pair{{}}
+	for _, p := range a {
+		i := sort.Search(len(dp), func(i int) bool { return dp[i].p > p.l }) - 1
+		if dp[i].v+p.p > dp[len(dp)-1].v {
+			dp = append(dp, pair{p.r, dp[i].v + p.p})
+		}
+	}
+	return dp[len(dp)-1].v
 }
