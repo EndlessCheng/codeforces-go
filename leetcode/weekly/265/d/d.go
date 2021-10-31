8f29e3f028f35b36c90d2acb687f21270a620bb9
Endless <loli.con@qq.com>
2021-10-31 18:42:35 +0800

    Add LeetCode Weekly Contest 265

--- leetcode/weekly/265/d/d.go
+++ leetcode/weekly/265/d/d.go
@@ -6,57 +6,48 @@ import "unicode"
 func possiblyEquals(s1, s2 string) bool {
 	n, m := len(s1), len(s2)
 	const mx, bias = 2000, 1000
-	dp := make([][][mx]int8, n+1)
-	for i := range dp {
-		dp[i] = make([][mx]int8, m+1)
-		for j := range dp[i] {
-			for k := range dp[i][j] {
-				dp[i][j][k] = -1
-			}
-		}
+	vis := make([][][mx]bool, n+1)
+	for i := range vis {
+		vis[i] = make([][mx]bool, m+1)
 	}
-	var f func(i, j, d int) int8
-	f = func(i, j, d int) (res int8) {
+	var dfs func(i, j, d int) bool
+	dfs = func(i, j, d int) bool {
 		if i == n && j == m {
-			if d == bias { // 匹配成功
-				return 1
-			}
-			return
+			return d == 0 // 匹配成功
 		}
-		dv := &dp[i][j][d]
-		if *dv != -1 {
-			return *dv
+		if vis[i][j][d+bias] {
+			return false
 		}
-		defer func() { *dv = res }()
-		// 原始字符串长度相同时，s1[:i] 和 s2[:j] 均可以向后扩展一个字母，要求为相同小写字母
-		if d == bias && i < n && j < m && s1[i] == s2[j] && unicode.IsLower(rune(s1[i])) && f(i+1, j+1, bias) > 0 {
-			return 1
+		vis[i][j][d+bias] = true
+		// 原始字符串长度相同时，若 s1[i] == s2[j]，则 s1[:i] 和 s2[:j] 均可以向后扩展一个字母
+		if d == 0 && i < n && j < m && s1[i] == s2[j] && dfs(i+1, j+1, 0) {
+			return true
 		}
-		if d <= bias && i < n { // s1[:i] 的原始字符串长度不超过 s2[:j] 的原始字符串长度时，扩展 s1[:i]
+		if d <= 0 && i < n { // s1[:i] 的原始字符串长度不超过 s2[:j] 的原始字符串长度时，扩展 s1[:i]
 			if unicode.IsDigit(rune(s1[i])) { // 数字
 				for p, v := i, 0; p < n && unicode.IsDigit(rune(s1[p])); p++ {
 					v = v*10 + int(s1[p]&15)
-					if f(p+1, j, d+v) > 0 {
-						return 1
+					if dfs(p+1, j, d+v) {
+						return true
 					}
 				}
-			} else if d < bias && f(i+1, j, d+1) > 0 { // 字符，扩展一位
-				return 1
+			} else if d < 0 && dfs(i+1, j, d+1) { // 字符，扩展一位，注意这里需要 d != bias
+				return true
 			}
 		}
-		if d >= bias && j < m { // s2[:j] 的原始字符串长度不超过 s1[:i] 的原始字符串长度时，扩展 s2[:j]
+		if d >= 0 && j < m { // s2[:j] 的原始字符串长度不超过 s1[:i] 的原始字符串长度时，扩展 s2[:j]
 			if unicode.IsDigit(rune(s2[j])) { // 数字
 				for q, v := j, 0; q < m && unicode.IsDigit(rune(s2[q])); q++ {
 					v = v*10 + int(s2[q]&15)
-					if f(i, q+1, d-v) > 0 {
-						return 1
+					if dfs(i, q+1, d-v) {
+						return true
 					}
 				}
-			} else if d > bias && f(i, j+1, d-1) > 0 { // 字符，扩展一位
-				return 1
+			} else if d > 0 && dfs(i, j+1, d-1) { // 字符，扩展一位，注意这里需要 d != bias
+				return true
 			}
 		}
-		return
+		return false
 	}
-	return f(0, 0, bias) > 0
+	return dfs(0, 0, 0)
 }
