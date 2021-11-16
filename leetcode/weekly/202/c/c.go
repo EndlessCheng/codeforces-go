b53a1f7d7ccf5648be12f84c1089e182497ed6af
Endless <loli.con@qq.com>
2021-11-16 14:09:39 +0800

    简化代码

--- leetcode/weekly/202/c/c.go
+++ leetcode/weekly/202/c/c.go
@@ -5,18 +5,15 @@ import "sort"
 // github.com/EndlessCheng/codeforces-go
 func maxDistance(ps []int, m int) int {
 	sort.Ints(ps)
-	return sort.Search(1e9+1, func(d int) bool {
-		if d == 0 {
-			return false
-		}
+	return sort.Search(1e9, func(d int) bool {
 		cnt, p := 1, ps[0]
 		for {
-			i := sort.SearchInts(ps, p+d)
+			i := sort.SearchInts(ps, p+d+1)
 			if i == len(ps) {
 				return cnt < m
 			}
 			cnt++
 			p = ps[i]
 		}
-	}) - 1
+	})
 }
