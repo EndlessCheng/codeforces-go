3ede87372799bb09347305028067ea2e6b11ca20
Endless <loli.con@qq.com>
2021-11-16 13:40:33 +0800

    更新 sort.Search 的使用技巧·其一

--- copypasta/sort.go
+++ copypasta/sort.go
@@ -90,11 +90,23 @@ func sortCollections() {
 	// upperBound-1 为 <=x 的最大值的下标（-1 表示不存在），存在多个最大值时下标取最大的
 
 	// sort.Search 的使用技巧·其一
-	// 由于 sort.Search 需要满足在 x 从小到大时，f(x) 先 false 后 true
-	// 在遇到先 true 后 false 的 f(x) 时
-	// 若目标是找到最大的使 f(x) == true 的 x
-	// 可以考虑二分 !f(x)，则二分结果是最小的使 f(x) == false 的 x，将其 -1 就得到了最大的使 f(x) == true 的 x
-	// 由于要对结果 -1，sort.Search 传入的上界需要 +1（也可以改为在 f 内部对传入的 x++）
+	// sort.Search(n, f) 需要满足当 x 从小到大时，f(x) 先 false 后 true
+	// 若 f(x) 是先 true 后 false，且目标是找到最大的使 f(x) 为 true 的 x
+	// 这种情况可以考虑二分 !f(x)，则二分结果是最小的使 f(x) 为 false 的 x，将其 -1 就得到了最大的使 f(x) 为 true 的 x
+	// 由于要对结果 -1，sort.Search 传入的上界需要 +1
+	// 更加简单的写法是，在 f(x) 内部将 x++，这样就不需要对上界和结果调整 ±1 了
+	//
+	// 下面以二分求 int(sqrt(90)) 为例来说明这一技巧
+	// 这相当于求最大的满足 x*x<=90 的 x
+	// 于是定义 f(x) 返回 x*x<=90，注意这是一个先 true 后 false 的 f(x)
+	// 我们可以改为判断 f(x+1)，即用 f(x+1) 的返回结果代替 f(x) 的返回结果
+	// 同时，将 f(x) 改为先 false 后 true，即返回 x*x>90
+	// 这样二分的结果就恰好停在最大的满足原 f(x) 为 true 的 x 上
+	sort.Search(10, func(x int) bool {
+		x++
+		return x*x > 90
+	})
+
 	// 好题 https://atcoder.jp/contests/abc149/tasks/abc149_e
 
 	// sort.Search 的使用技巧·其二
