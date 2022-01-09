66722218cd0079565d3f45fb92344e1f7734f9d9
Endless <loli.con@qq.com>
2022-01-09 15:30:58 +0800

    update

--- copypasta/sparse_table.go
+++ copypasta/sparse_table.go
@@ -16,62 +16,68 @@ Tarjan RMQ https://codeforces.com/blog/entry/48994
 GCD https://codeforces.com/contest/1548/problem/B
 题目推荐 https://cp-algorithms.com/data_structures/sparse-table.html#toc-tgt-5
 */
-func sparseTableCollections() {
-	core := func(int, int) (_ int) { return } // min, max, gcd, ...
 
-	// 17: n<131072, 18: n<262144, 19: n<524288, 20: n<1048576
-	// 动态写法 mx = bits.Len(uint(n))
-	const mx = 17
-	var st [][mx]int
-	stInit := func(a []int) {
-		n := len(a)
-		st = make([][mx]int, n)
-		for i, v := range a {
-			st[i][0] = v
+type ST [][]int
+
+// min, max, gcd, ...
+func stFunc(int, int) (_ int) { return }
+
+func newST(a []int) ST {
+	n := len(a)
+	sz := bits.Len(uint(n))
+	st := make(ST, n)
+	for i, v := range a {
+		st[i] = make([]int, sz)
+		st[i][0] = v
+	}
+	for j := 1; 1<<j <= n; j++ {
+		for i := 0; i+1<<j <= n; i++ {
+			st[i][j] = stFunc(st[i][j-1], st[i+1<<(j-1)][j-1])
 		}
-		for j := 1; 1<<j <= n; j++ {
-			for i := 0; i+1<<j <= n; i++ {
-				st[i][j] = core(st[i][j-1], st[i+1<<(j-1)][j-1])
+	}
+	return st
+}
+
+// 查询区间 [l,r)，注意 l 和 r 是从 0 开始算的
+func (st ST) query(l, r int) int {
+	k := bits.Len(uint(r-l)) - 1
+	return stFunc(st[l][k], st[r-1<<k][k])
+}
+
+//
+
+// 下标版本，查询返回的是区间最值的下标
+// https://codeforces.com/problemset/problem/675/E
+// - 此题另一种做法是单调栈二分，见 https://www.luogu.com.cn/problem/solution/CF675E
+type stPair struct{ v, i int }
+type ST2 [][]stPair
+
+func newST2(a []int) ST2 {
+	n := len(a)
+	sz := bits.Len(uint(n))
+	st := make(ST2, n)
+	for i, v := range a {
+		st[i] = make([]stPair, sz)
+		st[i][0] = stPair{v, i}
+	}
+	for j := 1; 1<<j <= n; j++ {
+		for i := 0; i+1<<j <= n; i++ {
+			if a, b := st[i][j-1], st[i+1<<(j-1)][j-1]; a.v <= b.v { // 最小值，相等时下标取左侧
+				st[i][j] = a
+			} else {
+				st[i][j] = b
 			}
 		}
 	}
-	// [l,r) 注意 l r 是从 0 开始算的
-	stQuery := func(l, r int) int { k := bits.Len(uint(r-l)) - 1; return core(st[l][k], st[r-1<<k][k]) }
+	return st
+}
 
-	_, _ = stInit, stQuery
-
-	// 下标版本，查询返回的是区间最值的下标
-	// https://codeforces.com/problemset/problem/675/E
-	// - 此题另一种做法是单调栈二分，见 https://www.luogu.com.cn/problem/solution/CF675E
-	{
-		type pair struct{ v, i int }
-		const mx = 17
-		var st [][mx]pair
-		stInit := func(a []int) {
-			n := len(a)
-			st = make([][mx]pair, n)
-			for i, v := range a {
-				st[i][0] = pair{v, i}
-			}
-			for j := 1; 1<<j <= n; j++ {
-				for i := 0; i+1<<j <= n; i++ {
-					if a, b := st[i][j-1], st[i+1<<(j-1)][j-1]; a.v <= b.v { // 最小值，相等时下标取左侧
-						st[i][j] = a
-					} else {
-						st[i][j] = b
-					}
-				}
-			}
-		}
-		// [l,r) 注意 l r 是从 0 开始算的
-		stQuery := func(l, r int) int {
-			k := bits.Len(uint(r-l)) - 1
-			a, b := st[l][k], st[r-1<<k][k]
-			if a.v <= b.v { // 最小值，相等时下标取左侧
-				return a.i
-			}
-			return b.i
-		}
-		_, _ = stInit, stQuery
+// 查询区间 [l,r)，注意 l 和 r 是从 0 开始算的
+func (st ST2) query(l, r int) int {
+	k := bits.Len(uint(r-l)) - 1
+	a, b := st[l][k], st[r-1<<k][k]
+	if a.v <= b.v { // 最小值，相等时下标取左侧
+		return a.i
 	}
+	return b.i
 }
