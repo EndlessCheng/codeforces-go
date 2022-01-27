1510ce28b2e252e7480753938e147b3c53b41b0b
Endless <loli.con@qq.com>
2022-01-28 03:49:04 +0800

    rename

--- copypasta/template/main.go
+++ copypasta/template/main.go
@@ -29,9 +29,9 @@ func run(_r io.Reader, _w io.Writer) {
 
 	// 如果是多组数据，请务必加上这段保险 —— 已经无法统计，有多少位竞赛选手在漏读数据上损失大把分数（包括我）
 	// 此外，如果不小心写成了 scan(v) 而不是 scan(&v)，由于未传入指针，不会读入任何数据，这样本地运行一次就能立马发现错误
-	_leftData, _ := ioutil.ReadAll(in)
-	if _s := strings.TrimSpace(string(_leftData)); _s != "" {
-		panic("有未读入的数据：\n" + _s)
+	leftData, _ := ioutil.ReadAll(in)
+	if s := strings.TrimSpace(string(leftData)); s != "" {
+		panic("有未读入的数据：\n" + s)
 	}
 
 }
