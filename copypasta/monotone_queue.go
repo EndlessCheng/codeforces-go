0956be47cf2635d210083c831af53925e68ac9b8
Endless <loli.con@qq.com>
2022-03-03 19:19:00 +0800

    添加一段关于单调队列的入门描述

--- copypasta/monotone_queue.go
+++ copypasta/monotone_queue.go
@@ -2,6 +2,11 @@ package copypasta
 
 /* 单调队列 Monotone Queue
 需要不断维护队列的单调性，时刻保证队列元素从大到小或从小到大
+前置知识：双指针
+以固定窗口大小的区间最大值为例（此时维护的是一个从大到小的单调队列）：
+每次向右移动一格左指针，在移动前，如果左指针指向的元素在队首左侧，说明左指针指向的元素小于队首，移动左指针不会改变区间最大值，直接移动左指针即可；
+如果左指针指向的就是队首，那么移动左指针会使区间最大值变小（变为单调队列队首之后的那个元素），我们要弹出队首。
+这样无论是何种情况，都保证了在移动左指针后，单调队列的队首始终为当前区间的最大值。
 https://oi-wiki.org/ds/monotonous-queue/
 https://oi-wiki.org/dp/opt/monotonous-queue-stack/
 https://cp-algorithms.com/data_structures/stack_queue_modification.html
