d82e0d554f8b38e8263233a5355190571d458ab4
Endless <loli.con@qq.com>
2022-03-14 10:36:48 +0800

    不会溢出，去掉取模

--- leetcode/other/cnunionpay-2022spring/c/c.go
+++ leetcode/other/cnunionpay-2022spring/c/c.go
@@ -12,17 +12,17 @@ func maxInvestment(product []int, limit int) (ans int) {
 			}
 		}
 		return cnt <= limit
-	}) + 1 // 这里算的是最后一次交易价格 +1 后的值
+	})
 
 	for _, p := range product {
-		if p >= price {
-			cnt := p - price + 1
-			ans = (ans + (price+p)*cnt/2) % (1e9 + 7)
+		if p > price {
+			cnt := p - price
+			ans += (p + price + 1) * cnt / 2
 			limit -= cnt
 		}
 	}
 	if limit > len(product) {
 		limit = len(product)
 	}
-	return (ans + limit*(price-1)) % (1e9 + 7)
+	return (ans + limit*price) % (1e9 + 7)
 }
