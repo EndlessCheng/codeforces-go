132aae88af84eefb16ead53886c62de14d38a0af
Endless <loli.con@qq.com>
2022-04-03 07:07:39 +0800

    改进速度

--- copypasta/strings.go
+++ copypasta/strings.go
@@ -166,8 +166,8 @@ func _(min, max func(int, int) int) {
 		n := len(s)
 		z := make([]int, n)
 		for i, l, r := 1, 0, 0; i < n; i++ {
-			if v := r - i + 1; v > 0 {
-				z[i] = min(z[i-l], v)
+			if i <= r {
+				z[i] = min(z[i-l], r-i+1)
 			}
 			for i+z[i] < n && s[z[i]] == s[i+z[i]] {
 				l, r = i, i+z[i]
