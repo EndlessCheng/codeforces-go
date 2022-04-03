2beaeaa20832dee1c84181b1ffa8d9dcb874f78a
Endless <loli.con@qq.com>
2022-04-03 18:57:35 +0800

    update template

--- misc/gcj/template/a/a.go
+++ misc/gcj/template/a/a.go
@@ -14,26 +14,25 @@ func run(_r io.Reader, _w io.Writer) {
 	in := bufio.NewReader(_r)
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
-	defer func() {
-		leftData, _ := ioutil.ReadAll(in)
-		s := strings.TrimSpace(string(leftData))
-		if s != "" {
-			panic("有未读入的数据：\n" + s)
-		}
-	}()
 
-	solve := func(Case int) {
+	solve := func(curCase int) {
 		var n int
 		Fscan(in, &n)
 
 	}
 
-	var t int
-	Fscan(in, &t)
-	for Case := 1; Case <= t; Case++ {
-		Fprintf(out, "Case #%d: ", Case)
-		solve(Case)
+	var cases int
+	Fscan(in, &cases)
+	for curCase := 1; curCase <= cases; curCase++ {
+		Fprintf(out, "Case #%d: ", curCase)
+		solve(curCase)
 	}
+
+	leftData, _ := ioutil.ReadAll(in)
+	if s := strings.TrimSpace(string(leftData)); s != "" {
+		panic("有未读入的数据：\n" + s)
+	}
+
 }
 
 func main() { run(os.Stdin, os.Stdout) }
