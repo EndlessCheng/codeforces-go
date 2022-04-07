dfd1c9cc9b54dc08b1a6be940dcee7fce5514ee6
Endless <loli.con@qq.com>
2022-04-07 18:07:51 +0800

    [2700][1A] CF1617E 结论题 建模 树的直径

--- main/1600-1699/1617E.go
+++ main/1600-1699/1617E.go
@@ -8,17 +8,6 @@ import (
 )
 
 // github.com/EndlessCheng/codeforces-go
-func f17(v, w uint) (c int) {
-	for v != w {
-		if v < w {
-			v, w = w, v
-		}
-		v = 1<<bits.Len(v-1) - v
-		c++
-	}
-	return
-}
-
 func CF1617E(_r io.Reader, out io.Writer) {
 	in := bufio.NewReader(_r)
 	var n, p, q int
@@ -28,9 +17,20 @@ func CF1617E(_r io.Reader, out io.Writer) {
 		Fscan(in, &a[i])
 	}
 
+	f := func(v, w uint) (c int) {
+		for v != w {
+			if v < w {
+				v, w = w, v
+			}
+			v = 1<<bits.Len(v-1) - v
+			c++
+		}
+		return
+	}
+
 	mx := -1
 	for i := 1; i < n; i++ {
-		c := f17(a[i], a[0])
+		c := f(a[i], a[0])
 		if c > mx {
 			mx, p = c, i
 		}
@@ -39,7 +39,7 @@ func CF1617E(_r io.Reader, out io.Writer) {
 	mx = -1
 	for i, v := range a {
 		if i != p {
-			c := f17(v, a[p])
+			c := f(v, a[p])
 			if c > mx {
 				mx, q = c, i
 			}
