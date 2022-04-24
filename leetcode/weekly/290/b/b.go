dfb9c1e401ac94b77fc8260c10d34d61372b5647
Endless <loli.con@qq.com>
2022-04-24 16:41:22 +0800

    Add LeetCode Weekly Contest 290

--- leetcode/weekly/290/b/b.go
+++ leetcode/weekly/290/b/b.go
@@ -3,13 +3,12 @@ package main
 // github.com/EndlessCheng/codeforces-go
 func countLatticePoints(circles [][]int) (ans int) {
 	for x := 0; x <= 200; x++ {
-	next:
 		for y := 0; y <= 200; y++ {
 			for _, c := range circles {
 				// 判断 (x,y) 是否在圆 c 内
 				if (x-c[0])*(x-c[0])+(y-c[1])*(y-c[1]) <= c[2]*c[2] {
 					ans++
-					continue next
+					break
 				}
 			}
 		}
