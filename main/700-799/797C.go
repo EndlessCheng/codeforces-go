b2491c850761f300874bf874930cac2b4f030eb4
Endless <loli.con@qq.com>
2022-05-19 16:19:30 +0800

    CF797C 贪心

--- main/700-799/797C.go
+++ main/700-799/797C.go
@@ -9,26 +9,25 @@ import (
 // github.com/EndlessCheng/codeforces-go
 func CF797C(_r io.Reader, out io.Writer) {
 	in := bufio.NewReader(_r)
-	var s, ans, st []byte
+	var s []byte
 	Fscan(in, &s)
+	st := s[:0]
+	ans := make([]byte, 0, len(s))
 	cnt := ['z' + 1]int{}
 	for _, b := range s {
 		cnt[b]++
 	}
 	nxt := byte('a')
 	for _, b := range s {
-		for cnt[nxt] == 0 {
+		cnt[b]--
+		for nxt < 'z' && cnt[nxt] == 0 {
 			nxt++
 		}
-		cnt[b]--
+		st = append(st, b)
 		for len(st) > 0 && st[len(st)-1] <= nxt {
 			ans = append(ans, st[len(st)-1])
 			st = st[:len(st)-1]
 		}
-		st = append(st, b)
-	}
-	for i := len(st) - 1; i >= 0; i-- {
-		ans = append(ans, st[i])
 	}
 	Fprintf(out, "%s", ans)
 }
