ffc0c8c3cea20c62d6008716821a29fbeda5b947
Endless <loli.con@qq.com>
2022-08-24 15:51:45 +0800

    CF482B 更新成更加简单的差分做法

--- main/400-499/482B.go
+++ main/400-499/482B.go
@@ -11,56 +11,38 @@ func CF482B(_r io.Reader, _w io.Writer) {
 	in := bufio.NewReader(_r)
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
-	type pair struct{ l, r int }
 
-	var n, q, l, r, v int
-	Fscan(in, &n, &q)
-	fas := [30][]int{}
-	for i := range fas {
-		fas[i] = make([]int, n+1)
-		for j := range fas[i] {
-			fas[i][j] = j
-		}
+	var n, m int
+	Fscan(in, &n, &m)
+	qs := make([]struct{ l, r, q int }, m)
+	for i := range qs {
+		Fscan(in, &qs[i].l, &qs[i].r, &qs[i].q)
+		qs[i].l--
 	}
-	var find func([]int, int) int
-	find = func(fa []int, x int) int {
-		if fa[x] != x {
-			fa[x] = find(fa, fa[x])
-		}
-		return fa[x]
-	}
-	same := func(fa []int, x, y int) bool { return find(fa, x) == find(fa, y) }
-	mergeRange := func(fa []int, l, r int) {
-		for i := find(fa, l); i < r; i = find(fa, i+1) {
-			fa[i] = r
-		}
-	}
-	check0 := [30][]pair{}
-	for ; q > 0; q-- {
-		Fscan(in, &l, &r, &v)
-		l--
-		for i, fa := range fas {
-			if v>>i&1 == 0 {
-				check0[i] = append(check0[i], pair{l, r})
-			} else {
-				mergeRange(fa, l, r)
-			}
-		}
-	}
-	for i, qs := range check0 {
-		fa := fas[i]
-		for _, q := range qs {
-			if same(fa, q.l, q.r) {
-				Fprintln(out, "NO")
-				return
-			}
-		}
-	}
+
 	ans := make([]int, n)
-	for j, fa := range fas {
-		for i, f := range fa {
-			if f != i {
-				ans[i] |= 1 << j
+	for mask := 1; mask < 1<<30; mask <<= 1 {
+		d := make([]int, n+1)
+		for _, q := range qs {
+			if q.q&mask > 0 {
+				d[q.l]++
+				d[q.r]--
+			}
+		}
+		p0 := -1
+		for i, v := range d[:n] {
+			d[i+1] += v
+			if v > 0 {
+				ans[i] |= mask
+			} else {
+				p0 = i
+			}
+			d[i] = p0 // 复用一下节省空间，存储左侧最近 0 的位置
+		}
+		for _, q := range qs {
+			if q.q&mask == 0 && d[q.r-1] < q.l {
+				Fprint(out, "NO")
+				return
 			}
 		}
 	}
