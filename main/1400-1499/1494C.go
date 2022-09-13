ae30c8c2e1997ae869554a5a3fd6fd41903cec5a
Endless <loli.con@qq.com>
2022-09-13 14:43:10 +0800

    [1900][1A] CF1494C 替换为三指针做法

--- main/1400-1499/1494C.go
+++ main/1400-1499/1494C.go
@@ -7,11 +7,17 @@ import (
 	"sort"
 )
 
-// github.com/EndlessCheng/codeforces-go
+// https://space.bilibili.com/206214
 func CF1494C(_r io.Reader, _w io.Writer) {
 	in := bufio.NewReader(_r)
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
+	max := func(a, b int) int {
+		if b > a {
+			return b
+		}
+		return a
+	}
 
 	var T, n, m int
 	for Fscan(in, &T); T > 0; T-- {
@@ -24,31 +30,37 @@ func CF1494C(_r io.Reader, _w io.Writer) {
 		for i := range b {
 			Fscan(in, &b[i])
 		}
-		f := func(a, b []int) (res int) {
-			has := map[int]bool{}
-			for _, v := range a {
-				has[v] = true
-			}
-			same := 0
+		f := func(a, b []int) int {
+			same, i, n := 0, 0, len(a)
 			for _, v := range b {
-				if has[v] {
+				for i < n && a[i] < v {
+					i++
+				}
+				if i < n && a[i] == v {
 					same++
+					i++
 				}
 			}
-			for i, v := range b {
-				if has[v] {
+			res := same
+			i, left := 0, 0
+			for right, v := range b {
+				for i < n && a[i] < v {
+					i++
+				}
+				if i < n && a[i] == v {
 					same--
+					i++
 				}
-				cnt := sort.SearchInts(a, v+1)
-				j := sort.SearchInts(b, v-cnt+1)
-				if i-j+1+same > res {
-					res = i - j + 1 + same
+				for left <= right && v-b[left]+1 > i { // b[left] 到 b[right] 的特殊位置无法被 i 个连续箱子覆盖
+					left++
 				}
+				res = max(res, right-left+1+same)
 			}
-			return
+			return res
 		}
 		x, y := sort.SearchInts(a, 0), sort.SearchInts(b, 0)
 		ans := f(a[x:], b[y:])
+		// 负数变正数且递增，方便复用 f
 		for i := 0; i < (x+1)/2; i++ {
 			a[i], a[x-1-i] = -a[x-1-i], -a[i]
 		}
