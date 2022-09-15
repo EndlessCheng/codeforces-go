85eb3b11334332ae12694f6533b6b322f86e04bc
Endless <loli.con@qq.com>
2022-09-15 16:39:51 +0800

    [2100] CF837D 二维 01 背包（把一个维度转换成 DP 的定义） 循环优化

--- main/800-899/837D.go
+++ main/800-899/837D.go
@@ -9,45 +9,47 @@ import (
 // github.com/EndlessCheng/codeforces-go
 func CF837D(in io.Reader, out io.Writer) {
 	min := func(a, b int) int {
-		if a < b {
-			return a
+		if a > b {
+			return b
 		}
-		return b
+		return a
 	}
 	max := func(a, b int) int {
-		if a > b {
-			return a
+		if b > a {
+			return b
 		}
-		return b
+		return a
 	}
+
 	var n, k, ans int
-	var v int64
+	var v uint64
 	Fscan(in, &n, &k)
-	dp := make([][]int, k+1)
-	for i := range dp {
-		dp[i] = make([]int, k*25+1)
-		for j := range dp[i] {
-			dp[i][j] = -1
+	f := make([][]int, k+1)
+	for i := range f {
+		f[i] = make([]int, k*25+1)
+		for j := range f[i] {
+			f[i][j] = -1
 		}
 	}
-	dp[0][0] = 0
-	for ; n > 0; n-- {
+	f[0][0] = 0
+	for i, s5 := 1, 0; i <= n; i++ {
 		Fscan(in, &v)
-		c2 := bits.TrailingZeros64(uint64(v))
+		c2 := bits.TrailingZeros64(v)
 		c5 := 0
 		for ; v%5 == 0; v /= 5 {
 			c5++
 		}
-		for i := k; i > 0; i-- {
-			for j := k * 25; j >= c5; j-- {
-				if dp[i-1][j-c5] >= 0 {
-					dp[i][j] = max(dp[i][j], dp[i-1][j-c5]+c2)
+		s5 += c5
+		for j := min(i, k); j > 0; j-- {
+			for p := min(s5, k*25); p >= c5; p-- {
+				if f[j-1][p-c5] >= 0 {
+					f[j][p] = max(f[j][p], f[j-1][p-c5]+c2)
 				}
 			}
 		}
 	}
-	for f, t := range dp[k] {
-		ans = max(ans, min(f, t))
+	for i, c2 := range f[k] {
+		ans = max(ans, min(i, c2))
 	}
 	Fprint(out, ans)
 }
