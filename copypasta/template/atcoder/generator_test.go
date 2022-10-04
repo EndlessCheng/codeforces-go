b407a1a1ef1d73b7d4e0a40d35ef6e7212e1a009
Endless <loli.con@qq.com>
2022-10-04 16:33:03 +0800

    update

--- copypasta/template/atcoder/generator_test.go
+++ copypasta/template/atcoder/generator_test.go
@@ -6,16 +6,6 @@ import (
 	"testing"
 )
 
-// TODO: update REVEL_SESSION
-func TestGenAtCoderContestTemplates(t *testing.T) {
-	const contestID = "abc260"
-	// todo username := os.Getenv("ATCODER_USERNAME")
-	//   password := os.Getenv("ATCODER_PASSWORD")
-	if err := GenAtCoderContestTemplates(contestID); err != nil {
-		t.Fatal(err)
-	}
-}
-
 // 仅限结束的比赛
 // https://atcoder.jp/contests/abc161/tasks/abc161_f
 // https://atcoder.jp/contests/abc161/submissions?f.LanguageName=Go&f.Status=AC&f.Task=abc161_f&orderBy=source_length
@@ -26,6 +16,16 @@ func TestGenAtCoderProblemTemplate(t *testing.T) {
 	}
 	problemURL := strings.TrimSpace(string(raw))
 	if err := GenAtCoderProblemTemplate(problemURL); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// TODO: update REVEL_SESSION
+func TestGenAtCoderContestTemplates(t *testing.T) {
+	const contestID = "abc260"
+	// todo username := os.Getenv("ATCODER_USERNAME")
+	//   password := os.Getenv("ATCODER_PASSWORD")
+	if err := GenAtCoderContestTemplates(contestID); err != nil {
 		t.Fatal(err)
 	}
 }
