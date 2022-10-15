a8a12ceed6b0ae9e383c0615a72a66c136f77667
Endless <loli.con@qq.com>
2022-10-15 10:52:33 +0800

    [1800] CF1420D 区间问题 离散化差分 扫描线 组合数学

--- main/1400-1499/1420D.go
+++ main/1400-1499/1420D.go
@@ -37,6 +37,57 @@ func CF1420D(_r io.Reader, out io.Writer) {
 		return F[n] * invF[k] % mod * invF[n-k] % mod
 	}
 
+	var n, k, l, r, s, c int
+	Fscan(in, &n, &k)
+	a := make([]int, 0, n*2)
+	for ; n > 0; n-- {
+		Fscan(in, &l, &r)
+		a = append(a, l<<1|1, (r+1)<<1)
+	}
+	sort.Ints(a)
+	ans := int64(0)
+	for i, x := range a {
+		s += x&1*2 - 1
+		if x&1 > 0 {
+			c++
+			if a[i+1]&1 == 0 {
+				ans += C(s, k) - C(s-c, k)
+				c = 0
+			}
+		}
+	}
+	Fprint(out, (ans%mod+mod)%mod)
+}
+
+func CF1420D_diffMap(_r io.Reader, out io.Writer) {
+	in := bufio.NewReader(_r)
+	const mod = 998244353
+	const mx int = 3e5
+	F := [mx + 1]int64{1}
+	for i := 1; i <= mx; i++ {
+		F[i] = F[i-1] * int64(i) % mod
+	}
+	pow := func(x, n int64) (res int64) {
+		res = 1
+		for ; n > 0; n >>= 1 {
+			if n&1 > 0 {
+				res = res * x % mod
+			}
+			x = x * x % mod
+		}
+		return res
+	}
+	invF := [...]int64{mx: pow(F[mx], mod-2)}
+	for i := mx; i > 0; i-- {
+		invF[i-1] = invF[i] * int64(i) % mod
+	}
+	C := func(n, k int) int64 {
+		if k < 0 || k > n {
+			return 0
+		}
+		return F[n] * invF[k] % mod * invF[n-k] % mod
+	}
+
 	var n, k int
 	Fscan(in, &n, &k)
 	a := make([]struct{ l, r int }, n)
