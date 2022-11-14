71ccf8fedd4199f9b67d3bab9a5d7f67b6550657
Endless <loli.con@qq.com>
2022-11-14 08:12:08 +0800

    [1468] ABC185E LCS 变形

--- misc/atcoder/abc185/e/e.go
+++ misc/atcoder/abc185/e/e.go
@@ -7,53 +7,41 @@ import (
 	"os"
 )
 
-// github.com/EndlessCheng/codeforces-go
+// https://space.bilibili.com/206214
 func run(_r io.Reader, out io.Writer) {
 	in := bufio.NewReader(_r)
 	var n, m int
 	Fscan(in, &n, &m)
-	a := make([]int, n)
-	for i := range a {
+	f := make([][]int, n+1)
+	for i := range f {
+		f[i] = make([]int, m+1)
+	}
+	a := make([]int, n+1)
+	for i := 1; i <= n; i++ {
 		Fscan(in, &a[i])
+		f[i][0] = i
 	}
-	b := make([]int, m)
-	for i := range b {
+	b := make([]int, m+1)
+	for i := 1; i <= m; i++ {
 		Fscan(in, &b[i])
+		f[0][i] = i
 	}
-	dp := make([][]int, n)
-	for i := range dp {
-		dp[i] = make([]int, m)
-		for j := range dp[i] {
-			dp[i][j] = -1
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			if a[i] == b[j] {
+				f[i][j] = min(min(f[i-1][j], f[i][j-1])+1, f[i-1][j-1])
+			} else {
+				f[i][j] = min(min(f[i-1][j], f[i][j-1])+1, f[i-1][j-1]+1)
+			}
 		}
 	}
-	var f func(int, int) int
-	f = func(i, j int) (res int) {
-		if i < 0 {
-			return j + 1
-		}
-		if j < 0 {
-			return i + 1
-		}
-		dv := &dp[i][j]
-		if *dv != -1 {
-			return *dv
-		}
-		defer func() { *dv = res }()
-		res = f(i-1, j-1)
-		if a[i] != b[j] {
-			res++
-		}
-		return min(res, 1+min(f(i, j-1), f(i-1, j)))
-	}
-	Fprint(out, f(n-1, m-1))
+	Fprint(out, f[n][m])
 }
 
 func main() { run(os.Stdin, os.Stdout) }
-
 func min(a, b int) int {
-	if a < b {
-		return a
+	if a > b {
+		return b
 	}
-	return b
+	return a
 }
