50252e005f29fb9015779439cf501538333f2d0e
Endless <loli.con@qq.com>
2022-11-16 22:14:03 +0800

    [1507][1A] ABC210D 滚动前后缀最小值

---
+++ misc/atcoder/abc210/d/d.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+// https://space.bilibili.com/206214
+func run(_r io.Reader, out io.Writer) {
+	in := bufio.NewReader(_r)
+	const inf int = 1e18
+	var n, m, c int
+	Fscan(in, &n, &m, &c)
+	pre := make([]int, m+1)
+	suf := make([]int, m+1)
+	for i := range pre {
+		pre[i] = inf
+		suf[i] = inf
+	}
+	ans := inf
+	a := make([]int, m)
+	for i := 0; i < n; i++ {
+		minRow := inf
+		for j := range a {
+			Fscan(in, &a[j])
+			ans = min(ans, a[j]+c*j+minRow)
+			minRow = min(minRow, a[j]-c*j)
+			ans = min(ans, a[j]+c*(i+j)+pre[j+1])
+			pre[j+1] = min(min(pre[j+1], pre[j]), a[j]-c*(i+j))
+		}
+		for j := m - 1; j >= 0; j-- {
+			ans = min(ans, a[j]+c*(i-j)+suf[j])
+			suf[j] = min(min(suf[j], suf[j+1]), a[j]-c*(i-j))
+		}
+	}
+	Fprint(out, ans)
+}
+
+func main() { run(os.Stdin, os.Stdout) }
+func min(a, b int) int { if a > b { return b }; return a }
