40800e0b907d3f132ae4b11de1151af2b6b8bcc0
Endless <loli.con@qq.com>
2022-11-18 08:46:36 +0800

    [1554][1A] ABC175E 路径DP 每行至多选三个

--- misc/atcoder/abc175/e/e.go
+++ misc/atcoder/abc175/e/e.go
@@ -26,7 +26,7 @@ func run(_r io.Reader, out io.Writer) {
 	}
 	for i := 1; i <= n; i++ {
 		for j := 1; j <= m; j++ {
-			f[i][j][0] = max(f[i][j-1][0], max(max(f[i-1][j][1], f[i-1][j][2]), f[i-1][j][3]))
+			f[i][j][0] = max(max(f[i-1][j][1], f[i-1][j][2]), f[i-1][j][3])
 			f[i][j][1] = max(f[i][j-1][1], f[i][j][0]+a[i][j])
 			f[i][j][2] = max(f[i][j-1][2], f[i][j-1][1]+a[i][j])
 			f[i][j][3] = max(f[i][j-1][3], f[i][j-1][2]+a[i][j])
