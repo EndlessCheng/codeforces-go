074d0065c49f9fc1855a1e27ccf1af6d7f18dddb
Endless <loli.con@qq.com>
2023-03-14 10:07:27 +0800

    CF1156B 构造

--- main/1100-1199/1156B.go
+++ main/1100-1199/1156B.go
@@ -4,71 +4,43 @@ import (
 	"bufio"
 	. "fmt"
 	"io"
-	"strings"
+	"sort"
 )
 
-func Sol1156B(reader io.Reader, writer io.Writer) {
-	in := bufio.NewReader(reader)
-	out := bufio.NewWriter(writer)
+// https://space.bilibili.com/206214
+func Sol1156B(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
 	defer out.Flush()
+	abs := func(x int) int {
+		if x < 0 {
+			return -x
+		}
+		return x
+	}
 
-	const na = "No answer"
-
-	var n int
-	for Fscan(in, &n); n > 0; n-- {
-		var s string
+	var T int
+	var s []byte
+	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &s)
-
-		cnt := make([]int, 26)
+		sort.Slice(s, func(i, j int) bool { return s[i] < s[j] })
+		a := [2][]byte{}
 		for _, b := range s {
-			cnt[b-'a']++
+			a[b&1] = append(a[b&1], b)
 		}
-		indexes := []int{}
-		for i, c := range cnt {
-			if c > 0 {
-				indexes = append(indexes, i)
-			}
+		x, y := a[0], a[1]
+		if len(x) == 0 {
+			Fprintf(out, "%s\n", y)
+		} else if len(y) == 0 {
+			Fprintf(out, "%s\n", x)
+		} else if abs(int(x[len(x)-1])-int(y[0])) != 1 {
+			Fprintf(out, "%s%s\n", x, y)
+		} else if abs(int(y[len(y)-1])-int(x[0])) != 1 {
+			Fprintf(out, "%s%s\n", y, x)
+		} else {
+			Fprintln(out, "No answer")
 		}
-
-		ans := ""
-		switch len(indexes) {
-		case 1:
-			ans = s
-		case 2:
-			if indexes[0]+1 == indexes[1] {
-				ans = na
-			} else {
-				ans = s
-			}
-		case 3:
-			i0, i1, i2 := indexes[0], indexes[1], indexes[2]
-			if i0+1 == i1 && i1+1 == i2 {
-				ans = na
-			} else if i0+1 == i1 {
-				ans = strings.Repeat(string(byte('a'+i0)), cnt[i0]) +
-					strings.Repeat(string(byte('a'+i2)), cnt[i2]) +
-					strings.Repeat(string(byte('a'+i1)), cnt[i1])
-			} else if i1+1 == i2 {
-				ans = strings.Repeat(string(byte('a'+i1)), cnt[i1]) +
-					strings.Repeat(string(byte('a'+i0)), cnt[i0]) +
-					strings.Repeat(string(byte('a'+i0)), cnt[i2])
-			} else {
-				ans = s
-			}
-		default:
-			for i := 1; i < len(indexes); i += 2 {
-				idx := indexes[i]
-				ans += strings.Repeat(string(byte('a'+idx)), cnt[idx])
-			}
-			for i := 0; i < len(indexes); i += 2 {
-				idx := indexes[i]
-				ans += strings.Repeat(string(byte('a'+idx)), cnt[idx])
-			}
-		}
-		Fprintln(out, ans)
 	}
 }
 
-//func main() {
-//	Sol1156B(os.Stdin, os.Stdout)
-//}
+//func main() { Sol1156B(os.Stdin, os.Stdout) }
