f78cd6f31f08fa76c7c9a46f09c051d66324d468
Endless <loli.con@qq.com>
2023-03-21 17:55:39 +0800

    update

--- copypasta/template/interactive_problem/main.go
+++ copypasta/template/interactive_problem/main.go
@@ -25,13 +25,17 @@ type io struct {
 	out *bufio.Writer
 }
 
-func (io io) readInitData() (d initData) {
-	Fscan(io.in, &d.n)
-	return
+func (io io) readInitData() initData {
+	var n int
+	Fscan(io.in, &n)
+	// TODO 初始输入
+
+	return initData{n}
 }
 
 func (io io) query(q request) (resp response) {
 	Fprint(io.out, "?")
+	//Fprint(io.out, " ", len(q.q)) // TODO 询问是否需要输出长度？
 	for _, v := range q.q {
 		Fprint(io.out, " ", v)
 	}
@@ -47,21 +51,22 @@ func (io io) query(q request) (resp response) {
 
 func (io io) printAnswer(a answer) {
 	Fprint(io.out, "!")
-	//Fprint(io.out, " ", len(a.ans))
+	//Fprint(io.out, " ", len(a.ans)) // TODO 输出最终答案，是否需要输出其长度？
 	for _, v := range a.ans {
 		Fprint(io.out, " ", v)
 	}
 	Fprintln(io.out)
 	io.out.Flush()
 
-	// TODO: optional, panic if we got -1
-	var res int
-	if Fscan(io.in, &res); res < 0 {
-		panic(res)
-	}
+	// TODO 可选，如果题目还提供返回值的话
+	//var res int
+	//if Fscan(io.in, &res); res < 0 {
+	//	panic(res)
+	//}
 }
 
 func doInteraction(it interaction) {
+	// 初始输入
 	dt := it.readInitData()
 	n := dt.n
 
@@ -72,14 +77,13 @@ func doInteraction(it interaction) {
 		return it.query(request{q}).v
 	}
 
+	//var ans int
 	var ans []int
-	ans = make([]int, n) //
+	ans = make([]int, n)
 	defer func() { it.printAnswer(answer{ans}) }()
-
 
 }
 
-// TODO: check output format before submit
 func run() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
@@ -91,4 +95,5 @@ func run() {
 	}
 }
 
+// TODO: 运行一下，检查格式是否正确
 func main() { run() }
