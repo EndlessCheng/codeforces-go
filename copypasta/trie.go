76656b052c364859bb98b02d3f9233e25623e762
Endless <loli.con@qq.com>
2023-03-22 11:34:33 +0800

    update

--- copypasta/trie.go
+++ copypasta/trie.go
@@ -248,7 +248,6 @@ func (t *trie) countDistinctSubstring(s []byte) (cnt int) {
 // EXTRA: AC 自动机 Aho–Corasick algorithm / Deterministic Finite Automaton (DFA)
 // https://en.wikipedia.org/wiki/Aho%E2%80%93Corasick_algorithm
 // https://en.wikipedia.org/wiki/Deterministic_finite_automaton
-// https://www.bilibili.com/video/BV1tF41157Dy
 // 基础实现 https://zhuanlan.zhihu.com/p/80325757
 // 基础实现 https://www.cnblogs.com/nullzx/p/7499397.html
 // 改进实现 https://oi-wiki.org/string/ac-automaton/
