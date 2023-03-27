69f89d0aacbea6cb8b92abc8bd8692d3a4a81c09
Endless <loli.con@qq.com>
2023-03-27 12:43:23 +0800

    add 题解

--- copypasta/math.go
+++ copypasta/math.go
@@ -317,7 +317,7 @@ func _(abs func(int64) int64, max func(int64, int64) int64) {
 
 	// 统计数组的所有子序列的 GCD 的不同个数，复杂度 O(Clog^2C)
 	// LC1819 https://leetcode-cn.com/problems/number-of-different-subsequences-gcds/
-	// todo 补充题解
+	// 我的题解 https://leetcode.cn/problems/number-of-different-subsequences-gcds/solution/ji-bai-100mei-ju-gcdxun-huan-you-hua-pyt-get7/
 	countDifferentSubsequenceGCDs := func(a []int, gcd func(int, int) int) (ans int) {
 		const mx int = 4e5 //
 		has := [mx + 1]bool{}
@@ -325,13 +325,13 @@ func _(abs func(int64) int64, max func(int64, int64) int64) {
 			has[v] = true
 		}
 		for i := 1; i <= mx; i++ {
-			g := 0
-			for j := i; j <= mx; j += i {
-				if has[j] {
-					g = gcd(g, j)
+			g := 0 // 0 和任何数 x 的最大公约数都是 x
+			for j := i; j <= mx && g != i; j += i { // 枚举 i 的倍数 j
+				if has[j] { // 如果 j 在 nums 中
+					g = gcd(g, j) // 更新最大公约数
 				}
 			}
-			if g == i {
+			if g == i { // 找到一个答案
 				ans++
 			}
 		}
