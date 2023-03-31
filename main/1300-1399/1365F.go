3dee3af0c0b5b3ea64b3e1c877b984fe9efe435a
Endless <loli.con@qq.com>
2023-03-31 08:14:41 +0800

    [2100] CF1365F

--- main/1300-1399/1365F.go
+++ main/1300-1399/1365F.go
@@ -16,25 +16,22 @@ func CF1365F(_r io.Reader, _w io.Writer) {
 o:
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &n)
-		cnt := map[int]int{}
 		type pair struct{ x, y int }
-		cnt2 := map[pair]int{}
+		cnt := map[pair]int{}
 		a := make([]int, n)
 		for i := range a {
 			Fscan(in, &a[i])
-			cnt[a[i]]++
 			if i >= (n+1)/2 {
-				cnt2[pair{a[i], a[n-1-i]}]++
-				cnt2[pair{a[n-1-i], a[i]}]++
+				cnt[pair{a[i], a[n-1-i]}]++
+				cnt[pair{a[n-1-i], a[i]}]++
 			}
 		}
 		b := make([]int, n)
 		for i := range b {
 			Fscan(in, &b[i])
-			cnt[b[i]]--
 			if i >= (n+1)/2 {
-				cnt2[pair{b[i], b[n-1-i]}]--
-				cnt2[pair{b[n-1-i], b[i]}]--
+				cnt[pair{b[i], b[n-1-i]}]--
+				cnt[pair{b[n-1-i], b[i]}]--
 			}
 		}
 		if n%2 > 0 && a[n/2] != b[n/2] {
@@ -42,12 +39,6 @@ o:
 			continue
 		}
 		for _, c := range cnt {
-			if c != 0 {
-				Fprintln(out, "No")
-				continue o
-			}
-		}
-		for _, c := range cnt2 {
 			if c != 0 {
 				Fprintln(out, "No")
 				continue o
