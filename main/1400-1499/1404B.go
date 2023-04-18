7d43a559559d9241182d32ea4df77f96599199b0
Endless <loli.con@qq.com>
2023-04-18 13:33:24 +0800

    添加一点注释

--- main/1400-1499/1404B.go
+++ main/1400-1499/1404B.go
@@ -6,7 +6,7 @@ import (
 	"io"
 )
 
-// github.com/EndlessCheng/codeforces-go
+// https://space.bilibili.com/206214
 func CF1404B(_r io.Reader, _w io.Writer) {
 	in := bufio.NewReader(_r)
 	out := bufio.NewWriter(_w)
@@ -21,7 +21,12 @@ func CF1404B(_r io.Reader, _w io.Writer) {
 			g[v] = append(g[v], w)
 			g[w] = append(g[w], v)
 		}
-		var f func(v, fa, d int)
+		if 2*da >= db { // Bob 在 Alice 的覆盖范围内
+			Fprintln(out, "Alice")
+			continue
+		}
+
+		var f func(int, int, int)
 		f = func(v, fa, d int) {
 			if v == b {
 				dab = d
@@ -37,13 +42,14 @@ func CF1404B(_r io.Reader, _w io.Writer) {
 		}
 		maxD = -1
 		f(a, 0, 0)
-		if 2*da >= db || da >= dab {
+		if da >= dab { // Alice 一步到达 Bob
 			Fprintln(out, "Alice")
 			continue
 		}
+
 		maxD = -1
 		f(u, 0, 0)
-		if 2*da >= maxD {
+		if 2*da >= maxD { // Alice 可以覆盖整棵树
 			Fprintln(out, "Alice")
 		} else {
 			Fprintln(out, "Bob")
