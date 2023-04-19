defea1fcf2033b96ba3691c687bf6da760944721
Endless <loli.con@qq.com>
2023-04-19 09:47:13 +0800

    add mergeIntervals & minJumpNumbers

--- copypasta/misc.go
+++ copypasta/misc.go
@@ -878,3 +878,50 @@ func parseTime(s string) (hour, minute, total int) {
 	total = hour*60 + minute
 	return
 }
+
+// 合并 a 中所有重叠的区间（哪怕只有一个端点重叠，也算重叠）
+// 注：这种做法在变形题中容易写错，更加稳定的做法是差分数组
+// - [56. 合并区间](https://leetcode.cn/problems/merge-intervals/)
+// - [55. 跳跃游戏](https://leetcode.cn/problems/jump-game/)
+// - [1024. 视频拼接](https://leetcode.cn/problems/video-stitching/)
+// - [2580. 统计将重叠区间合并成组的方案数](https://leetcode.cn/problems/count-ways-to-group-overlapping-ranges/)
+// - [2584. 分割数组使乘积互质](https://leetcode.cn/problems/split-the-array-to-make-coprime-products/)
+// https://codeforces.com/contest/1630/problem/C
+func mergeIntervals(a [][]int, max func(int, int) int) (ans [][]int) {
+	sort.Slice(a, func(i, j int) bool { return a[i][0] < a[j][0] }) // 按区间左端点排序
+	l0, maxR := a[0][0], a[0][1]
+	for _, p := range a[1:] { // 从第二个区间开始
+		l, r := p[0], p[1]
+		if l > maxR { // 发现一个新区间
+			ans = append(ans, []int{l0, maxR}) // 先把旧的加入答案
+			l0 = l                             // 记录新区间左端点
+		}
+		maxR = max(maxR, r)
+	}
+	ans = append(ans, []int{l0, maxR}) // 最后发现的新区间加入答案
+	return
+}
+
+// 从 i 可以跳到 [i,i+a[i]] 中的任意整点
+// 返回从 0 跳到 n-1 的最小跳跃次数
+// 如果无法到达 n-1，返回 -1
+// - [45. 跳跃游戏 II](https://leetcode.cn/problems/jump-game-ii/)
+// - [1326. 灌溉花园的最少水龙头数目](https://leetcode.cn/problems/minimum-number-of-taps-to-open-to-water-a-garden/)
+// 【图解】https://leetcode.cn/problems/minimum-number-of-taps-to-open-to-water-a-garden/solution/yi-zhang-tu-miao-dong-pythonjavacgo-by-e-wqry/
+func minJumpNumbers(a []int, max func(int, int) int) (ans int) {
+	curR := 0 // 已建造的桥的右端点
+	nxtR := 0 // 下一座桥的右端点的最大值
+	// 这里没有遍历到 n-1，因为它已经是终点了
+	for i, d := range a[:len(a)-1] {
+		r := i + d
+		nxtR = max(nxtR, r)
+		if i == curR { // 到达已建造的桥的右端点
+			if i == nxtR { // 无论怎么造桥，都无法从 i 到 i+1
+				return -1
+			}
+			curR = nxtR // 建造下一座桥
+			ans++
+		}
+	}
+	return
+}
