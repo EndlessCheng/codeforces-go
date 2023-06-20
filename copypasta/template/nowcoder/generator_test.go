15d3dc540b27d1ba995fe65815e5263d44f74d49
Endless <loli.con@qq.com>
2023-06-20 20:32:38 +0800

    upd

--- copypasta/template/nowcoder/generator_test.go
+++ copypasta/template/nowcoder/generator_test.go
@@ -11,7 +11,7 @@ func TestGenNowCoderTemplates(t *testing.T) {
 	cipherPwd := os.Getenv("NOWCODER_CIPHER_PWD")
 	const contestID = 6778
 	contestDir := fmt.Sprintf("../../../misc/nowcoder/%d/", contestID)
-	if err := GenNowCoderTemplates(emailOrPhone, cipherPwd, contestDir, contestID, `// github.com/EndlessCheng/codeforces-go`); err != nil {
+	if err := GenNowCoderTemplates(emailOrPhone, cipherPwd, contestDir, contestID, `// https://space.bilibili.com/206214`); err != nil {
 		t.Fatal(err)
 	}
 }
