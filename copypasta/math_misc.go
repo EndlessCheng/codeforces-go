947b9b80d0aab0dbbc1d8583858237011541adcc
Endless <loli.con@qq.com>
2023-07-05 20:17:10 +0800

    add

---
+++ copypasta/math_misc.go
@@ -0,0 +1,9 @@
+package copypasta
+
+/*
+朗伯 W 函数   Lambert W function
+https://en.wikipedia.org/wiki/Lambert_W_function
+https://math.stackexchange.com/questions/433717/how-to-solve-equations-with-logarithms-like-this-ax-b-logx-c-0
+a*x + b*ln(x) + c = 0 的解：
+x = (b/a)*W((a/b)*e^(-c/b))
+*/
