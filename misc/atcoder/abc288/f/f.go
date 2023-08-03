83f0db4784d2d7162cec0ae92e936e61496db31d
Endless <loli.con@qq.com>
2023-08-03 16:53:01 +0800

    [2024][1A] ABC288F 前缀和优化 DP 式子变形

--- misc/atcoder/abc288/f/f.go
+++ misc/atcoder/abc288/f/f.go
@@ -9,13 +9,12 @@ import (
 
 // https://space.bilibili.com/206214
 func run(in io.Reader, out io.Writer) {
-	const mod = 998244353
 	var s string
 	Fscan(bufio.NewReader(in), &s, &s)
 	f, sumF := 0, 1
 	for _, c := range s {
-		f = (f*10 + sumF*int(c-'0')) % mod
-		sumF = (sumF + f) % mod
+		f = (f*10 + sumF*int(c-'0')) % 998244353
+		sumF += f
 	}
 	Fprint(out, f)
 }
