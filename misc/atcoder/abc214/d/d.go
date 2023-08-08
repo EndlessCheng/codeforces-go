72e2a6b484acaf0d159b6fcabe6bca3115dcedaa
Endless <loli.con@qq.com>
2023-08-08 23:28:16 +0800

    ABC214D

---
+++ misc/atcoder/abc214/d/d.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+	"sort"
+)
+
+// https://space.bilibili.com/206214
+func run(_r io.Reader, out io.Writer) {
+	in := bufio.NewReader(_r)
+	var n, ans int
+	Fscan(in, &n)
+	es := make([]struct{ x, y, wt int }, n-1)
+	for i := range es {
+		Fscan(in, &es[i].x, &es[i].y, &es[i].wt)
+	}
+	sort.Slice(es, func(i, j int) bool { return es[i].wt < es[j].wt })
+
+	fa := make([]int, n+1)
+	size := make([]int, n+1)
+	for i := range fa {
+		fa[i] = i
+		size[i] = 1
+	}
+	var find func(int) int
+	find = func(x int) int {
+		if fa[x] != x {
+			fa[x] = find(fa[x])
+		}
+		return fa[x]
+	}
+
+	for _, e := range es {
+		x, y := find(e.x), find(e.y)
+		ans += size[x] * size[y] * e.wt
+		fa[x] = y
+		size[y] += size[x]
+	}
+	Fprint(out, ans)
+}
+
+func main() { run(os.Stdin, os.Stdout) }
