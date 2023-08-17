45b8557f6d9ae8eb32209fcce07b21ba22c175f9
Endless <loli.con@qq.com>
2023-08-17 13:46:57 +0800

    ABC298D

--- misc/atcoder/abc298/d/d.go
+++ misc/atcoder/abc298/d/d.go
@@ -17,18 +17,18 @@ func run(_r io.Reader, _w io.Writer) {
 
 	var q, op, x int
 	cur, pow10 := 1, 1
-	a := []int{1}
+	s := []int{1}
 	for Fscan(in, &q); q > 0; q-- {
 		Fscan(in, &op)
 		if op == 1 {
 			Fscan(in, &x)
 			cur = (cur*10 + x) % mod
 			pow10 = pow10 * 10 % mod
-			a = append(a, x)
+			s = append(s, x)
 		} else if op == 2 {
-			cur = (cur - a[0]*pow10) % mod
+			cur = (cur - s[0]*pow10) % mod
 			pow10 = pow10 * inv10 % mod
-			a = a[1:]
+			s = s[1:]
 		} else {
 			Fprintln(out, (cur+mod)%mod)
 		}
