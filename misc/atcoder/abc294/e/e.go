fc448f3b8a7d494cc387d160f94cd38be1dfdfed
Endless <loli.con@qq.com>
2023-08-25 11:39:46 +0800

    ABC294E

---
+++ misc/atcoder/abc294/e/e.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+// https://space.bilibili.com/206214
+func run(_r io.Reader, out io.Writer) {
+	in := bufio.NewReader(_r)
+	var l, n, m, ans int
+	Fscan(in, &l, &n, &m)
+	type pair struct{ v, l int }
+	a := make([]pair, n)
+	for i := range a {
+		Fscan(in, &a[i].v, &a[i].l)
+	}
+	b := make([]pair, m)
+	for i := range b {
+		Fscan(in, &b[i].v, &b[i].l)
+	}
+
+	i, j := 0, 0
+	for k := 1; k < n+m; k++ {
+		if a[i].v == b[j].v {
+			ans += min(a[i].l, b[j].l)
+		}
+		if a[i].l > b[j].l {
+			a[i].l -= b[j].l
+			j++
+		} else {
+			b[j].l -= a[i].l
+			i++
+		}
+	}
+	Fprint(out, ans)
+}
+
+func main() { run(os.Stdin, os.Stdout) }
+func min(a, b int) int { if b < a { return b }; return a }
