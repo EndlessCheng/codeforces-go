1c62caf39c17de57f4fa429d4a2d39b2c8640be8
Endless <loli.con@qq.com>
2023-08-26 08:44:14 +0800

    ARC092D 加法拆位

--- misc/atcoder/arc092/b/b.go
+++ misc/atcoder/arc092/b/b.go
@@ -27,14 +27,13 @@ func run(_r io.Reader, out io.Writer) {
 		sort.Slice(a, func(i, j int) bool { return a[i]&mask < a[j]&mask })
 		sort.Slice(b, func(i, j int) bool { return b[i]&mask < b[j]&mask })
 		cnt := 0
-		i, j, p, q := n-1, n-1, n-1, n-1
-		l1, r1, l2, r2 := 1<<k, 1<<(k+1)-1, 3<<k, 1<<(k+2)-1
+		i, j, p := n-1, n-1, n-1
+		l1, r1, l2 := 1<<k, 1<<(k+1)-1, 3<<k
 		for _, v := range a {
 			for i >= 0 && v&mask+b[i]&mask >= l1 { i-- }
 			for j >= 0 && v&mask+b[j]&mask >  r1 { j-- }
 			for p >= 0 && v&mask+b[p]&mask >= l2 { p-- }
-			for q >= 0 && v&mask+b[q]&mask >  r2 { q-- }
-			cnt ^= i ^ j ^ p ^ q // cnt += j - i + q - p
+			cnt ^= i ^ j ^ p
 		}
 		ans |= cnt & 1 << k
 	}
