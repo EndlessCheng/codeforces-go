d4937747113c92fdd4905ee9b7bf9d95e2950dcb
Endless <loli.con@qq.com>
2023-08-27 21:36:15 +0800

    ABC317

--- misc/atcoder/abc317/g/g.go
+++ misc/atcoder/abc317/g/g.go
@@ -13,52 +13,44 @@ func run(_r io.Reader, _w io.Writer) {
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
 
-	var n, m int
+	var n, m, v int
 	Fscan(in, &n, &m)
-	a := make([][]int, n)
-	for i := range a {
-		a[i] = make([]int, m)
-		for j := range a[i] {
-			Fscan(in, &a[i][j])
-			a[i][j]--
+	g := make([]map[int]int, n)
+	for i := 0; i < n; i++ {
+		g[i] = make(map[int]int, m)
+		for j := 0; j < m; j++ {
+			Fscan(in, &v)
+			g[i][j] = v - 1
 		}
 	}
 
-	g := make([]map[int]int, n)
-	for i, r := range a {
-		g[i] = map[int]int{}
-		for j, v := range r {
-			g[i][j] = v
-		}
-	}
-	
 	ans := make([][]int, n)
 	for i := range ans {
 		ans[i] = make([]int, m)
 	}
+	matchL := make([]int, n)
+	matchR := make([]int, n)
 	for k := 0; k < m; k++ {
-		matchL := make([]int, n)
-		matchR := make([]int, n)
 		for i := range matchL {
 			matchL[i] = -1
 			matchR[i] = -1
 		}
-		var used []bool
-		var f func(int) bool
-		f = func(v int) bool {
-			used[v] = true
-			for id, w := range g[v] {
-				if lv := matchR[w]; lv == -1 || !used[lv] && f(lv) {
-					matchR[w] = v
-					matchL[v] = id
-					return true
+		vis := make([]int, n)
+		for ts := range g {
+			var f func(int) bool
+			f = func(v int) bool {
+				vis[v] = ts + 1
+				for id, w := range g[v] {
+					lv := matchR[w]
+					if lv == -1 || vis[lv] != ts+1 && f(lv) {
+						matchR[w] = v
+						matchL[v] = id
+						return true
+					}
 				}
+				return false
 			}
-			return false
-		}
-		for v := range g {
-			used = make([]bool, n)
-			f(v)
+			f(ts)
 		}
 		for i, id := range matchL {
 			ans[i][k] = g[i][id]
