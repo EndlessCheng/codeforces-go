829ec0985e9d853356ed3847baacf093d87a01ef
Endless <loli.con@qq.com>
2023-08-31 08:48:00 +0800

    add links

--- copypasta/stack.go
+++ copypasta/stack.go
@@ -1,12 +1,21 @@
 package copypasta
 
-// 栈+懒删除 https://codeforces.com/problemset/problem/1000/F
-// 栈的应用 https://codeforces.com/problemset/problem/1092/D1
-//         https://codeforces.com/problemset/problem/1092/D2
+/*
+入门
+LC735 https://leetcode.cn/problems/asteroid-collision/
+- 变形：碰撞后，大行星的质量会减去的行星的质量
+- 变形 2：你可以决定每颗行星的方向，问最后的行星质量之和最小是多少      
+-- f[i][j] 表示前 i 颗行星，向右的质量之和为 j 时，向左的质量之和的最小值
+LC2751 https://leetcode.cn/problems/robot-collisions/
 
-// 括号匹配
-// 下标范围有限制 https://codeforces.com/problemset/problem/543/D
+栈+懒删除 https://codeforces.com/problemset/problem/1000/F
+栈的应用 https://codeforces.com/problemset/problem/1092/D1
+        https://codeforces.com/problemset/problem/1092/D2
 
-// 对顶栈
-// LC2296 https://leetcode.cn/problems/design-a-text-editor/
-// http://acm.hdu.edu.cn/showproblem.php?pid=4699
+括号匹配
+下标范围有限制 https://codeforces.com/problemset/problem/543/D
+
+对顶栈
+LC2296 https://leetcode.cn/problems/design-a-text-editor/
+http://acm.hdu.edu.cn/showproblem.php?pid=4699
+*/
