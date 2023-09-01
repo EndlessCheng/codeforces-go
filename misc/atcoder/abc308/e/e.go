7c7d31c3405c941f990856c3ddbe11d9a35fe92d
Endless <loli.con@qq.com>
2023-09-01 15:59:55 +0800

    ABC308E

---
+++ misc/atcoder/abc308/e/e.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+// https://space.bilibili.com/206214
+func run(_r io.Reader, out io.Writer) {
+	in := bufio.NewReader(_r)
+	var n, ans int
+	var s string
+	Fscan(in, &n)
+	a := make([]int, n)
+	for i := range a {
+		Fscan(in, &a[i])
+	}
+	Fscan(in, &s)
+
+	var pre, suf [3]int
+	for i, c := range s {
+		if c == 'X' {
+			suf[a[i]]++
+		}
+	}
+
+	mex := [...]int{0, 1, 0, 2, 0, 1, 0, 3}
+	for i, v := range a {
+		if s[i] == 'M' {
+			pre[v]++
+		} else if s[i] == 'E' {
+			for j, p := range pre {
+				for k, q := range suf {
+					ans += mex[1<<v|1<<j|1<<k] * p * q
+				}
+			}
+		} else {
+			suf[v]--
+		}
+	}
+	Fprint(out, ans)
+}
+
+func main() { run(os.Stdin, os.Stdout) }
