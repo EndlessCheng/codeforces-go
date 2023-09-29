1aae593988da16c924f838114b9df2e5dd5226ee
Endless <loli.con@qq.com>
2023-09-29 15:59:53 +0800

    upd

--- copypasta/template/atcoder/generator_test.go
+++ copypasta/template/atcoder/generator_test.go
@@ -10,7 +10,7 @@ import (
 // https://atcoder.jp/contests/abc161/tasks/abc161_f
 // https://atcoder.jp/contests/abc161/submissions?f.LanguageName=Go&f.Status=AC&f.Task=abc161_f&orderBy=source_length
 func TestGenAtCoderProblemTemplate(t *testing.T) {
-	raw, err := os.ReadFile("data.txt")
+	raw, err := os.ReadFile("data-atcoder.txt")
 	if err != nil {
 		t.Fatal(err)
 	}
