422b6b1560a57cb571fe6513cdba0e4e9268ff3b
Endless <loli.con@qq.com>
2023-10-05 20:30:03 +0800

    [2237] 逆向思维 0-1 背包

---
+++ misc/atcoder/tenka1_2019/d/d.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+// https://space.bilibili.com/206214
+func run(_r io.Reader, out io.Writer) {
+	in := bufio.NewReader(_r)
+	const mod = 998244353
+	var n, v, s, dup int
+	var f, g [90001]int
+	f[0] = 1
+	g[0] = 1
+	pow3 := 1
+	for Fscan(in, &n); n > 0; n-- {
+		Fscan(in, &v)
+		s += v
+		for j := s; j >= 0; j-- {
+			f[j] = (f[j]*2 + f[max(j-v, 0)]) % mod
+			if j >= v {
+				g[j] = (g[j] + g[j-v]) % mod
+			}
+		}
+		pow3 = pow3 * 3 % mod
+	}
+	if s%2 == 0 {
+		dup = g[s/2]
+	}
+	ans := pow3 - (f[(s+1)/2]-dup)*3
+	Fprint(out, (ans%mod+mod)%mod)
+}
+
+func main() { run(os.Stdin, os.Stdout) }
+func max(a, b int) int { if b > a { return b }; return a }
