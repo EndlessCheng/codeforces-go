1764bf79c67cda47670f36a8265a012d91ce1b33
Endless <loli.con@qq.com>
2023-10-14 21:04:44 +0800

    use any

--- copypasta/deque.go
+++ copypasta/deque.go
@@ -6,7 +6,7 @@ package copypasta
 // 应用见 graph.go 中的 01 最短路
 
 // l-1,...1,0,0,1...,r-1
-type Deque struct{ l, r []interface{} }
+type Deque struct{ l, r []any }
 
 func (q Deque) Empty() bool {
 	return len(q.l) == 0 && len(q.r) == 0
@@ -16,15 +16,15 @@ func (q Deque) Size() int {
 	return len(q.l) + len(q.r)
 }
 
-func (q *Deque) PushFront(v interface{}) {
+func (q *Deque) PushFront(v any) {
 	q.l = append(q.l, v)
 }
 
-func (q *Deque) PushBack(v interface{}) {
+func (q *Deque) PushBack(v any) {
 	q.r = append(q.r, v)
 }
 
-func (q *Deque) PopFront() (v interface{}) {
+func (q *Deque) PopFront() (v any) {
 	if len(q.l) > 0 {
 		q.l, v = q.l[:len(q.l)-1], q.l[len(q.l)-1]
 	} else {
@@ -33,7 +33,7 @@ func (q *Deque) PopFront() (v interface{}) {
 	return
 }
 
-func (q *Deque) PopBack() (v interface{}) {
+func (q *Deque) PopBack() (v any) {
 	if len(q.r) > 0 {
 		q.r, v = q.r[:len(q.r)-1], q.r[len(q.r)-1]
 	} else {
@@ -42,14 +42,14 @@ func (q *Deque) PopBack() (v interface{}) {
 	return
 }
 
-func (q Deque) Front() interface{} {
+func (q Deque) Front() any {
 	if len(q.l) > 0 {
 		return q.l[len(q.l)-1]
 	}
 	return q.r[0]
 }
 
-func (q Deque) Back() interface{} {
+func (q Deque) Back() any {
 	if len(q.r) > 0 {
 		return q.r[len(q.r)-1]
 	}
@@ -57,7 +57,7 @@ func (q Deque) Back() interface{} {
 }
 
 // 0 <= i < q.Size()
-func (q Deque) Get(i int) interface{} {
+func (q Deque) Get(i int) any {
 	if i < len(q.l) {
 		return q.l[len(q.l)-1-i]
 	}
