61c7246858f5af3d94e2902496f3fc3b88937146
Endless <loli.con@qq.com>
2023-10-16 14:34:26 +0800

    use int

--- copypasta/math_matrix.go
+++ copypasta/math_matrix.go
@@ -45,8 +45,8 @@ todo poj 2345 3532 3526
 func readMatrix(in io.Reader, n, m int) matrix {
 	a := make(matrix, n)
 	for i := range a {
-		a[i] = make([]int64, m)
-		//a[i] = make([]int64, m, m+1) // 方便高斯消元
+		a[i] = make([]int, m)
+		//a[i] = make([]int, m, m+1) // 方便高斯消元
 		for j := range a[i] {
 			Fscan(in, &a[i][j])
 		}
@@ -57,7 +57,7 @@ func readMatrix(in io.Reader, n, m int) matrix {
 func copyMatrix(a matrix) matrix {
 	b := make(matrix, len(a))
 	for i, row := range a {
-		b[i] = append([]int64(nil), row...)
+		b[i] = append([]int(nil), row...)
 	}
 	return b
 }
@@ -66,7 +66,7 @@ func copyMatrix(a matrix) matrix {
 func rotateMatrix(a matrix) matrix {
 	b := make(matrix, len(a[0]))
 	for j := range b {
-		b[j] = make([]int64, len(a))
+		b[j] = make([]int, len(a))
 		for i, row := range a {
 			b[j][len(a)-1-i] = row[j]
 		}
@@ -87,12 +87,12 @@ func rotateMatrix(a matrix) matrix {
 - [2851. ](https://leetcode.cn/problems/string-transformation/)
 
 */
-type matrix [][]int64
+type matrix [][]int
 
 func newMatrix(n, m int) matrix {
 	a := make(matrix, n)
 	for i := range a {
-		a[i] = make([]int64, m)
+		a[i] = make([]int, m)
 	}
 	return a
 }
@@ -100,14 +100,14 @@ func newMatrix(n, m int) matrix {
 func newIdentityMatrix(n int) matrix {
 	a := make(matrix, n)
 	for i := range a {
-		a[i] = make([]int64, n)
+		a[i] = make([]int, n)
 		a[i][i] = 1
 	}
 	return a
 }
 
 func (a matrix) mul(b matrix) matrix {
-	const mod int64 = 1e9 + 7 // 998244353
+	const mod = 1_000_000_007 // 998244353
 	c := newMatrix(len(a), len(b[0]))
 	for i, row := range a {
 		for j := range b[0] {
@@ -122,7 +122,7 @@ func (a matrix) mul(b matrix) matrix {
 	return c
 }
 
-func (a matrix) pow(n int64) matrix {
+func (a matrix) pow(n int) matrix {
 	res := newIdentityMatrix(len(a))
 	for ; n > 0; n /= 2 {
 		if n%2 > 0 {
@@ -140,8 +140,8 @@ func (a matrix) pow(n int64) matrix {
 // 然后用一个 [1 * (n^2)] 的矩阵初始矩阵乘 a^k
 // 得到一个 [1 * (n^2)] 的结果矩阵 res
 // res[0][tx*n+ty] 就是答案
-func (a matrix) solve(n, sx, sy, tx, ty int, k int64) int64 {
-	b := matrix{make([]int64, n*n)}
+func (a matrix) solve(n, sx, sy, tx, ty, k int) int {
+	b := matrix{make([]int, n*n)}
 	b[0][sx*n+sy] = 1
 	res := b.mul(a.pow(k))
 	return res[0][tx*n+ty]
@@ -152,8 +152,8 @@ func (a matrix) solve(n, sx, sy, tx, ty int, k int64) int64 {
 // https://ac.nowcoder.com/acm/contest/9247/A
 // m 项递推式，以及包含常数项的情况见《挑战》P201
 // a(n) = a(n-1) + a(n-m) https://codeforces.com/problemset/problem/1117/D
-func calcFibonacci(p, q, a0, a1, n int64) int64 {
-	const mod int64 = 1e9 + 7 // 998244353
+func calcFibonacci(p, q, a0, a1, n int) int {
+	const mod = 1_000_000_007 // 998244353
 	//n--
 	if n == 0 {
 		return (a0%mod + mod) % mod
@@ -199,13 +199,13 @@ func (a matrix) swapCols(i, j int) {
 	}
 }
 
-func (a matrix) mulRow(i int, k int64) {
+func (a matrix) mulRow(i, k int) {
 	for j := range a[i] {
 		a[i][j] *= k // % mod
 	}
 }
 
-func (a matrix) trace() (sum int64) {
+func (a matrix) trace() (sum int) {
 	for i, row := range a {
 		sum += row[i]
 	}
@@ -215,25 +215,12 @@ func (a matrix) trace() (sum int64) {
 // NxN 矩阵求逆
 // 模板题 https://www.luogu.com.cn/problem/P4783
 func (matrix) inv(A matrix) matrix {
-	const mod int64 = 1e9 + 7
-	pow := func(x int64) (res int64) {
-		//x %= mod
-		res = 1
-		for n := mod - 2; n > 0; n >>= 1 {
-			if n&1 == 1 {
-				res = res * x % mod
-			}
-			x = x * x % mod
-		}
-		return
-	}
-
 	// 增广一个单位矩阵
 	n := len(A)
 	m := 2 * n
 	a := make(matrix, n)
 	for i := range a {
-		a[i] = make([]int64, m)
+		a[i] = make([]int, m)
 		for j := range a {
 			a[i][j] = A[i][j] // or read
 		}
@@ -251,7 +238,7 @@ func (matrix) inv(A matrix) matrix {
 			// 矩阵不是满秩的
 			return nil
 		}
-		inv := pow(a[i][i])
+		inv := pow(a[i][i], mod-2)
 		for j := i; j < m; j++ {
 			a[i][j] = a[i][j] * inv % mod
 		}
@@ -285,7 +272,7 @@ func (matrix) inv(A matrix) matrix {
 // 模板题 https://www.luogu.com.cn/problem/P3389 https://www.luogu.com.cn/problem/P2455
 //       https://codeforces.com/problemset/problem/21/B
 // 与 SCC 结合 https://www.luogu.com.cn/problem/P6030
-func gaussJordanElimination(A matrix, B []int64) (sol []float64, infSol bool) {
+func gaussJordanElimination(A matrix, B []int) (sol []float64, infSol bool) {
 	const eps = 1e-8
 	n := len(A)
 	// 构造增广矩阵 (or read)
@@ -344,9 +331,9 @@ func gaussJordanElimination(A matrix, B []int64) (sol []float64, infSol bool) {
 // EXTRA: 求行列式（对结果模 mod）
 // https://en.wikipedia.org/wiki/Determinant
 // 参考 https://www.luogu.com.cn/blog/Stormy-Rey/calculate-det
-func (a matrix) determinant(mod int64) int64 {
+func (a matrix) determinant(mod int) int {
 	n := len(a)
-	res, sign := int64(1), 1
+	res, sign := 1, 1
 	for i := range a {
 		for j := i + 1; j < n; j++ {
 			for a[i][i] != 0 {
@@ -362,7 +349,7 @@ func (a matrix) determinant(mod int64) int64 {
 	for i, r := range a {
 		res = res * r[i] % mod
 	}
-	res = (res*int64(sign) + mod) % mod
+	res = (res*sign + mod) % mod
 	return res
 }
 
@@ -391,17 +378,17 @@ func (a matrix) determinant(mod int64) int64 {
 //  https://www.luogu.com.cn/problem/P3857
 //  最右线性基 https://codeforces.com/problemset/problem/1778/E
 type xorBasis struct {
-	b   []int64
+	b   []int
 	num uint8
 
 	canBeZero bool
-	basis     []int64
+	basis     []int
 
 	rightMost []int
 }
 
-func newXorBasis(a []int64) *xorBasis {
-	b := &xorBasis{b: make([]int64, 64)} // 32
+func newXorBasis(a []int) *xorBasis {
+	b := &xorBasis{b: make([]int, 64)} // 32
 	b.rightMost = make([]int, len(b.b))
 	for _, v := range a {
 		b.insert(v)
@@ -410,7 +397,7 @@ func newXorBasis(a []int64) *xorBasis {
 }
 
 // 尝试插入 v，看能否找到一个新的线性无关基
-func (b *xorBasis) insert(v int64) {
+func (b *xorBasis) insert(v int) {
 	// 从高到低遍历，方便计算下面的 maxXor 和 minXor
 	for i := len(b.b) - 1; i >= 0; i-- {
 		if v>>i&1 == 0 {
@@ -428,7 +415,7 @@ func (b *xorBasis) insert(v int64) {
 
 // EXTRA: 如果遇到线性相关的基，保留位置最靠右的
 // https://codeforces.com/problemset/problem/1778/E
-func (b *xorBasis) insertRightMost(idx int, v int64) {
+func (b *xorBasis) insertRightMost(idx, v int) {
 	// 从高到低遍历，方便计算下面的 maxXor 和 minXor
 	for i := len(b.b) - 1; i >= 0; i-- {
 		if v>>i&1 == 0 {
@@ -450,7 +437,7 @@ func (b *xorBasis) insertRightMost(idx int, v int64) {
 }
 
 // v 能否被线性基表出
-func (b *xorBasis) decompose(v int64) bool {
+func (b *xorBasis) decompose(v int) bool {
 	for i := len(b.b) - 1; i >= 0; i-- {
 		if v>>i&1 > 0 {
 			if b.b[i] == 0 {
@@ -463,7 +450,7 @@ func (b *xorBasis) decompose(v int64) bool {
 }
 
 // https://www.luogu.com.cn/problem/P3812 https://loj.ac/p/113
-func (b *xorBasis) maxXor() (xor int64) {
+func (b *xorBasis) maxXor() (xor int) {
 	for i := len(b.b) - 1; i >= 0; i-- {
 		//if xor>>i&1 > 0 {
 		//	continue
@@ -478,7 +465,7 @@ func (b *xorBasis) maxXor() (xor int64) {
 // 考虑插入的过程，因为每一次跳转操作，x 的二进制最高位必定单调降低，所以不可能插入两个二进制最高位相同的数。
 // 而此时，线性基中最小值异或上其他数，必定会增大。
 // 所以，直接输出线性基中的最小值即可。
-func (b *xorBasis) minXor() int64 {
+func (b *xorBasis) minXor() int {
 	if b.canBeZero {
 		return 0
 	}
@@ -493,7 +480,7 @@ func (b *xorBasis) initOnce() {
 	if b.basis != nil {
 		return
 	}
-	tmp := append([]int64{}, b.b...)
+	tmp := append([]int{}, b.b...)
 	for i := range tmp {
 		if tmp[i] == 0 {
 			continue
@@ -510,12 +497,12 @@ func (b *xorBasis) initOnce() {
 // 线性基能表出的所有不同元素中的第 k 小值（不允许空）
 // k 从 1 开始
 // https://loj.ac/p/114 http://acm.hdu.edu.cn/showproblem.php?pid=3949
-func (b *xorBasis) kthXor(k int64) (xor int64) {
+func (b *xorBasis) kthXor(k int) (xor int) {
 	b.initOnce()
 	if b.canBeZero { // 0 是最小的
 		k-- // 占用了一个数
 	}
-	if k >= int64(1)<<len(b.basis) { // 非空子集有 2^len(b.basis) - 1 个
+	if k >= 1<<len(b.basis) { // 非空子集有 2^len(b.basis) - 1 个
 		return -1
 	}
 	for i, v := range b.basis {
@@ -527,7 +514,7 @@ func (b *xorBasis) kthXor(k int64) (xor int64) {
 }
 
 // todo https://www.luogu.com.cn/problem/P4869
-func (b *xorBasis) rank(xor int64) (k int64) {
+func (b *xorBasis) rank(xor int) (k int) {
 	panic("todo")
 }
 
@@ -540,9 +527,14 @@ func (b *xorBasis) merge(other *xorBasis) {
 	}
 }
 
-// 矩阵树定理 基尔霍夫定理 Kirchhoff‘s theorem
-// https://oi-wiki.org/graph/matrix-tree/
-// https://en.wikipedia.org/wiki/Kirchhoff%27s_theorem
+/* 矩阵树定理 基尔霍夫定理 Kirchhoff‘s theorem
+https://oi-wiki.org/graph/matrix-tree/
+https://en.wikipedia.org/wiki/Kirchhoff%27s_theorem
+
+https://atcoder.jp/contests/jsc2021/tasks/jsc2021_g
+https://atcoder.jp/contests/abc253/tasks/abc253_h
+https://atcoder.jp/contests/abc323/tasks/abc323_g
+*/
 
 // 线性规划（单纯形法）  LP, linear programming (simplex method)
 // https://en.wikipedia.org/wiki/Mathematical_optimization
