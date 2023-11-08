bbb8bb8c0b4a890f41c9ffba20036024cf00c52e
Endless <loli.con@qq.com>
2023-11-08 22:44:54 +0800

    CF1861C

---
+++ main/1800-1899/1861C.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"math"
+)
+
+// https://space.bilibili.com/206214
+func CF1861C(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	T, s := 0, ""
+o:
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &s)
+		curSize := 0
+		sortedSize := 1
+		unsortedSize := math.MaxInt
+		for _, b := range s {
+			if b == '+' {
+				curSize++
+			} else if b == '-' {
+				curSize--
+				if curSize < unsortedSize {
+					unsortedSize = math.MaxInt // 后面 s[i]='1' 是可以的
+				}
+				if curSize < sortedSize {
+					sortedSize = max(curSize, 1)
+				}
+			} else if b == '0' {
+				if curSize <= sortedSize {
+					Fprintln(out, "NO")
+					continue o
+				}
+				unsortedSize = min(unsortedSize, curSize)
+			} else {
+				if curSize >= unsortedSize {
+					Fprintln(out, "NO")
+					continue o
+				}
+				sortedSize = max(curSize, 1)
+			}
+		}
+		Fprintln(out, "YES")
+	}
+}
+
+//func main() { CF1861C(os.Stdin, os.Stdout) }
