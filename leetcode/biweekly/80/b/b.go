1e11a56e2d3687e017fcd47c31f3efab0ffe23b1
Endless <loli.con@qq.com>
2023-11-10 07:31:18 +0800

    完善题解

--- leetcode/biweekly/80/b/b.go
+++ leetcode/biweekly/80/b/b.go
@@ -1,10 +1,13 @@
 package main
 
-import "sort"
+import (
+	"slices"
+	"sort"
+)
 
 // https://space.bilibili.com/206214/dynamic
 func successfulPairs(spells, potions []int, success int64) []int {
-	sort.Ints(potions)
+	slices.Sort(potions)
 	for i, x := range spells {
 		spells[i] = len(potions) - sort.SearchInts(potions, (int(success)-1)/x+1)
 	}
