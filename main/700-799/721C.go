1f7afc385174a69bc27880b22d39d470aa38e9ed
Endless <loli.con@qq.com>
2023-11-11 17:30:57 +0800

    rename

--- main/700-799/721C.go
+++ main/700-799/721C.go
@@ -6,49 +6,52 @@ import (
 	"io"
 )
 
-// github.com/EndlessCheng/codeforces-go
+// https://space.bilibili.com/206214
 func CF721C(_r io.Reader, _w io.Writer) {
 	in := bufio.NewReader(_r)
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
 
-	var n, m, maxT int
+	var n, m int
+	var maxT int32
 	Fscan(in, &n, &m, &maxT)
-	es := make([][3]int, m)
+	es := make([][3]int32, m)
 	for i := range es {
 		Fscan(in, &es[i][0], &es[i][1], &es[i][2])
 	}
 
 	const mx = 5001
-	dp := make([][mx]int, n+1) // int32
-	for i := 1; i <= n; i++ {
+	f := make([][mx]int32, n)
+	for i := range f {
 		for j := 1; j <= n; j++ {
-			dp[i][j] = maxT + 1
+			f[i][j] = maxT + 1
 		}
 	}
-	dp[1][1] = 0
-	fa := make([][mx]int16, n+1)
+	f[0][1] = 0
+	from := make([][mx]int16, n+1)
 
 	// 不需要拓扑，直接跑 n-1 次就行了
 	ans := 0
-	for i := 2; i <= n; i++ {
+	for i := 1; i < n; i++ {
 		for _, e := range es {
 			v, w, t := e[0], e[1], e[2]
-			if sumT := dp[i-1][v] + t; sumT < dp[i][w] {
-				dp[i][w] = sumT
-				fa[i][w] = int16(v)
+			sumT := f[i-1][v] + t
+			if sumT < f[i][w] {
+				f[i][w] = sumT
+				from[i][w] = int16(v)
 			}
 		}
-		if dp[i][n] <= maxT {
+		if f[i][n] <= maxT {
 			ans = i
 		}
 	}
-	Fprintln(out, ans)
-	path := make([]interface{}, ans)
-	o := int16(n)
-	for i := ans; i > 0; i-- {
-		path[i-1] = o
-		o = fa[i][o]
+
+	Fprintln(out, ans+1)
+	path := make([]any, ans+1)
+	v := int16(n)
+	for i := ans; i >= 0; i-- {
+		path[i] = v
+		v = from[i][v]
 	}
 	Fprint(out, path...)
 }
