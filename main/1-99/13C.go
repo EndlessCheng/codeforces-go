923447054c3bb12c419705463d62f16645ff0aa1
Endless <loli.con@qq.com>
2023-11-18 20:21:03 +0800

    upd

--- main/1-99/13C.go
+++ main/1-99/13C.go
@@ -8,23 +8,23 @@ import (
 	"sort"
 )
 
-type hp struct{ sort.IntSlice }
+type hp13 struct{ sort.IntSlice }
 
-func (h hp) Less(i, j int) bool  { return h.IntSlice[i] > h.IntSlice[j] }
-func (h *hp) Push(v interface{}) { h.IntSlice = append(h.IntSlice, v.(int)) }
-func (h *hp) Pop() interface{}   { a := h.IntSlice; v := a[len(a)-1]; h.IntSlice = a[:len(a)-1]; return v }
+func (h hp13) Less(i, j int) bool  { return h.IntSlice[i] > h.IntSlice[j] }
+func (h *hp13) Push(v interface{}) { h.IntSlice = append(h.IntSlice, v.(int)) }
+func (h *hp13) Pop() interface{}   { a := h.IntSlice; v := a[len(a)-1]; h.IntSlice = a[:len(a)-1]; return v }
 
 // github.com/EndlessCheng/codeforces-go
 func CF13C(_r io.Reader, out io.Writer) {
 	in := bufio.NewReader(_r)
-	var n, v int
+	var n, v, ans int
 	Fscan(in, &n)
-	ans := int64(0)
-	for h := (hp{}); n > 0; n-- {
+	h := hp13{}
+	for ; n > 0; n-- {
 		Fscan(in, &v)
 		heap.Push(&h, v)
 		if d := h.IntSlice[0] - v; d > 0 {
-			ans += int64(d)
+			ans += d
 			h.IntSlice[0] = v
 			heap.Fix(&h, 0)
 		}
