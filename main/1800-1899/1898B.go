14526af80c6ff72abe4e259ddc8cccad727bcd4a
Endless <loli.con@qq.com>
2023-11-26 22:32:46 +0800

    CF1898B

---
+++ main/1800-1899/1898B.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+)
+
+// https://space.bilibili.com/206214
+func CF1898B(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var T, n int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n)
+		a := make([]int, n)
+		for i := range a {
+			Fscan(in, &a[i])
+		}
+		ans := 0
+		for i := n - 2; i >= 0; i-- {
+			v, w := a[i], a[i+1]
+			if v <= w {
+				continue
+			}
+			k := v / w
+			ans += k
+			if v%w > 0 {
+				a[i] = v / (k + 1)
+			} else {
+				ans--
+				a[i] = w
+			}
+		}
+		Fprintln(out, ans)
+	}
+}
+
+//func main() { CF1898B(os.Stdin, os.Stdout) }
