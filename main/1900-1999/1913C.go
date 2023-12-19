8bf89d3f05f2c3c7ad4355f541aaa0526d89c26f
Endless <loli.con@qq.com>
2023-12-19 10:21:46 +0800

    CF1913C

---
+++ main/1900-1999/1913C.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+)
+
+// https://space.bilibili.com/206214
+func cf1913C(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var m, op, v int
+	cnt := [30]int{}
+o:
+	for Fscan(in, &m); m > 0; m-- {
+		Fscan(in, &op, &v)
+		if op == 1 {
+			cnt[v]++
+			continue
+		}
+		s := 0
+		for i, c := range cnt {
+			s += c
+			if v>>i&1 > 0 {
+				if s == 0 {
+					Fprintln(out, "NO")
+					continue o
+				}
+				s--
+			}
+			s >>= 1
+		}
+		Fprintln(out, "YES")
+	}
+}
+
+//func main() { cf1913C(os.Stdin, os.Stdout) }
