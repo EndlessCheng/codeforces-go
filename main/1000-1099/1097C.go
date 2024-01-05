9cfdd2b0584beb5706795fb8b5e6634ccecadcbf
Endless <loli.con@qq.com>
2024-01-05 23:03:48 +0800

    CF1097C

---
+++ main/1000-1099/1097C.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+)
+
+// https://space.bilibili.com/206214
+func cf1097C(_r io.Reader, out io.Writer) {
+	in := bufio.NewReader(_r)
+	var n, ans int
+	var s string
+	cnt := map[int]int{}
+	for Fscan(in, &n); n > 0; n-- {
+		Fscan(in, &s)
+		c, mn := 0, 0
+		for _, b := range s {
+			if b == '(' {
+				c++
+			} else {
+				c--
+				mn = min(mn, c)
+			}
+		}
+		if mn == 0 || c == mn {
+			if cnt[-c] > 0 {
+				cnt[-c]--
+				ans++
+			} else {
+				cnt[c]++
+			}
+		}
+	}
+	Fprint(out, ans)
+}
+
+//func main() { cf1097C(os.Stdin, os.Stdout) }
