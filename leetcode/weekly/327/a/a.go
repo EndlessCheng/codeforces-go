d09299a7343430ac08511a076ec29f08acc4aba9
Endless <loli.con@qq.com>
2024-01-15 15:55:40 +0800

    upd

--- leetcode/weekly/327/a/a.go
+++ leetcode/weekly/327/a/a.go
@@ -6,5 +6,3 @@ import "sort"
 func maximumCount(nums []int) int {
 	return max(sort.SearchInts(nums, 0), len(nums)-sort.SearchInts(nums, 1))
 }
-
-func max(a, b int) int { if b > a { return b }; return a }
