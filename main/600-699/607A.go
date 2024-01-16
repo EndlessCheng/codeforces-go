c0113130f26b51eb02b58c54760e2d207e2d0bb7
Endless <loli.con@qq.com>
2024-01-16 22:11:52 +0800

    rename

--- main/600-699/607A.go
+++ main/600-699/607A.go
@@ -7,12 +7,9 @@ import (
 	"sort"
 )
 
-// github.com/EndlessCheng/codeforces-go
-func CF607A(_r io.Reader, _w io.Writer) {
+// https://space.bilibili.com/206214
+func CF607A(_r io.Reader, out io.Writer) {
 	in := bufio.NewReader(_r)
-	out := bufio.NewWriter(_w)
-	defer out.Flush()
-
 	var n, ans int
 	Fscan(in, &n)
 	a := make([]struct{ p, d int }, n)
@@ -20,17 +17,16 @@ func CF607A(_r io.Reader, _w io.Writer) {
 		Fscan(in, &a[i].p, &a[i].d)
 	}
 	sort.Slice(a, func(i, j int) bool { return a[i].p < a[j].p })
-	dp := make([]int, n)
+
+	f := make([]int, n)
 	for i, p := range a {
-		j := sort.Search(len(a), func(i int) bool { return a[i].p >= p.p-p.d })
+		j := sort.Search(n, func(j int) bool { return a[j].p >= p.p-p.d })
 		if j > 0 {
-			dp[i] = dp[j-1] + 1
+			f[i] = f[j-1] + 1
 		} else {
-			dp[i] = 1
+			f[i] = 1
 		}
-		if dp[i] > ans {
-			ans = dp[i]
-		}
+		ans = max(ans, f[i])
 	}
 	Fprint(out, n-ans)
 }
