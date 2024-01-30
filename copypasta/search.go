5d999d1a1bffd89cd03bd64f3cfc6e3ed7d61f49
Endless <0x3f@0x3f3f3f3f.com>
2024-01-30 11:38:40 +0800

    add links

--- copypasta/search.go
+++ copypasta/search.go
@@ -1102,12 +1102,15 @@ NOTE: 对于 n*m 的网格图，BFS 最多只占用 O(min(n,m)) 的空间，而 
 - [695. 岛屿的最大面积](https://leetcode.cn/problems/max-area-of-island/)
 - [面试题 16.19. 水域大小](https://leetcode.cn/problems/pond-sizes-lcci/)
 - [463. 岛屿的周长](https://leetcode.cn/problems/island-perimeter/)
+- [2658. 网格图中鱼的最大数目](https://leetcode.cn/problems/maximum-number-of-fish-in-a-grid/) 1490
 - [1034. 边界着色](https://leetcode.cn/problems/coloring-a-border/) 1579
 - [1020. 飞地的数量](https://leetcode.cn/problems/number-of-enclaves/) 1615
 - [1254. 统计封闭岛屿的数目](https://leetcode.cn/problems/number-of-closed-islands/) 1659
 - [130. 被围绕的区域](https://leetcode.cn/problems/surrounded-regions/)
+- [1391. 检查网格中是否存在有效路径](https://leetcode.cn/problems/check-if-there-is-a-valid-path-in-a-grid/) 1746
 - [417. 太平洋大西洋水流问题](https://leetcode.cn/problems/pacific-atlantic-water-flow/)
 - [529. 扫雷游戏](https://leetcode.cn/problems/minesweeper/)
+- [1559. 二维网格图中探测环](https://leetcode.cn/problems/detect-cycles-in-2d-grid/) 1838
 - [827. 最大人工岛](https://leetcode.cn/problems/making-a-large-island/) 1934
    - https://codeforces.com/contest/616/problem/C
 
@@ -1115,15 +1118,22 @@ NOTE: 对于 n*m 的网格图，BFS 最多只占用 O(min(n,m)) 的空间，而 
 - [542. 01 矩阵](https://leetcode.cn/problems/01-matrix/) *多源 BFS
 - [994. 腐烂的橘子](https://leetcode.cn/problems/rotting-oranges/) 1433
 - [2684. 矩阵中移动的最大次数](https://leetcode.cn/problems/maximum-number-of-moves-in-a-grid/) 1626
+- [1926. 迷宫中离入口最近的出口](https://leetcode.cn/problems/nearest-exit-from-entrance-in-maze/) 1638
 - [1162. 地图分析](https://leetcode.cn/problems/as-far-from-land-as-possible/) 1666
 - [934. 最短的桥](https://leetcode.cn/problems/shortest-bridge/) 1826
 - [2146. 价格范围内最高排名的 K 样物品](https://leetcode.cn/problems/k-highest-ranked-items-within-a-price-range/) 1837
+- [1293. 网格中的最短路径](https://leetcode.cn/problems/shortest-path-in-a-grid-with-obstacles-elimination/) 1967
+- [1210. 穿过迷宫的最少移动次数](https://leetcode.cn/problems/minimum-moves-to-reach-target-with-rotations/) 2022
+- [490. 迷宫](https://leetcode.cn/problems/the-maze/)（会员题）
+- [505. 迷宫 II](https://leetcode.cn/problems/the-maze-ii/)（会员题）
 - [317. 离建筑物最近的距离](https://leetcode.cn/problems/shortest-distance-from-all-buildings/)（会员题）
 https://atcoder.jp/contests/abc317/tasks/abc317_e
+另见 graph.go 中的 0-1 BFS
 
 #### 综合应用
 - [1631. 最小体力消耗路径](https://leetcode.cn/problems/path-with-minimum-effort/) 1948
 - [778. 水位上升的泳池中游泳](https://leetcode.cn/problems/swim-in-rising-water/) 2097
+- [1036. 逃离大迷宫](https://leetcode.cn/problems/escape-a-large-maze/) 2165
 - [1263. 推箱子](https://leetcode.cn/problems/minimum-moves-to-move-a-box-to-their-target-location/) 2297
 - [2258. 逃离火灾](https://leetcode.cn/problems/escape-the-spreading-fire/) 2347
    - https://www.luogu.com.cn/problem/UVA11624
@@ -1131,6 +1141,7 @@ https://atcoder.jp/contests/abc317/tasks/abc317_e
 - [LCP 75. 传送卷轴](https://leetcode.cn/problems/rdmXM7/)
 - [LCP 13. 寻宝](https://leetcode.cn/problems/xun-bao/)
 - [LCP 31. 变换的迷宫](https://leetcode-cn.com/problems/Db3wC1/)
+- [1728. 猫和老鼠 II](https://leetcode.cn/problems/cat-and-mouse-ii/) 2849
 易错题 https://codeforces.com/problemset/problem/540/C 2000
 */
 func gridCollection() {
