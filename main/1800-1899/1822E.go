3a45bc7c2ea46cc735a89c42f465724c10604a61
Endless <0x3f@0x3f3f3f3f.com>
2024-01-30 21:14:46 +0800

    CF1822E 分类讨论

--- main/1800-1899/1822E.go
+++ main/1800-1899/1822E.go
@@ -14,42 +14,33 @@ func cf1822E(_r io.Reader, _w io.Writer) {
 
 	var T, n int
 	var s string
+o:
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &n, &s)
 		if n%2 > 0 {
 			Fprintln(out, -1)
 			continue
 		}
+
 		cnt := [26]int{}
+		for _, b := range s {
+			cnt[b-'a']++
+			if cnt[b-'a'] > n/2 {
+				Fprintln(out, -1)
+				continue o
+			}
+		}
+
+		tot, mx := 0, 0
+		diff := [26]int{}
 		for i := 0; i < n/2; i++ {
 			if s[i] == s[n-1-i] {
-				cnt[s[i]-'a']++
+				tot++
+				diff[s[i]-'a']++
+				mx = max(mx, diff[s[i]-'a'])
 			}
 		}
-		tot, mxI := 0, 0
-		for i, c := range cnt {
-			tot += c
-			if c > cnt[mxI] {
-				mxI = i
-			}
-		}
-		mx := cnt[mxI]
-		if mx*2 <= tot {
-			Fprintln(out, (tot+1)/2)
-			continue
-		}
-		left := mx*2 - tot
-		t := 'a' + byte(mxI)
-		for i := 0; i < n/2; i++ {
-			if s[i] != s[n-1-i] && s[i] != t && s[n-1-i] != t {
-				left--
-			}
-		}
-		if left > 0 {
-			Fprintln(out, -1)
-		} else {
-			Fprintln(out, mx)
-		}
+		Fprintln(out, max((tot+1)/2, mx))
 	}
 }
 
