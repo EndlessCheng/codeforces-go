171dc87c4dc1b457dee6f71e5c53c2c6b4c72a21
Endless <0x3f@0x3f3f3f3f.com>
2024-01-30 19:14:09 +0800

    add link

--- copypasta/math_misc.go
+++ copypasta/math_misc.go
@@ -11,6 +11,8 @@ a*x + b*ln(x) + c = 0 的解：
 x = (b/a)*W((a/b)*e^(-c/b))
 注意 W(x) 当 x < 0 的时候有两个值 W_0(x) 和 W_{-1}(x) 
 
-用于一些题目的时间复杂度的说明上。
+用于一些题目的时间复杂度的说明上
 LC2749 https://leetcode.cn/problems/minimum-operations-to-make-the-integer-zero/
+
+一元五次方程的根 布灵根式 Bring radical https://en.wikipedia.org/wiki/Bring_radical
 */
