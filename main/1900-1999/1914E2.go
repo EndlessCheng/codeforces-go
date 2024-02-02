d195389c9b22e8eed8d4f75c1de2eb1bae1aad7c
Endless <0x3f@0x3f3f3f3f.com>
2024-02-02 09:38:41 +0800

    CF1914E2

---
+++ main/1900-1999/1914E2.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"sort"
+)
+
+// https://space.bilibili.com/206214
+func cf1914E2(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var T, n int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n)
+		a := make([]struct{ x, y int }, n)
+		for i := range a {
+			Fscan(in, &a[i].x)
+		}
+		for i := range a {
+			Fscan(in, &a[i].y)
+		}
+		sort.Slice(a, func(i, j int) bool { a, b := a[i], a[j]; return a.x+a.y > b.x+b.y })
+		d := 0
+		for i, p := range a {
+			if i%2 == 0 {
+				d += p.x - 1
+			} else {
+				d -= p.y - 1
+			}
+		}
+		Fprintln(out, d)
+	}
+}
+
+//func main() { cf1914E2(os.Stdin, os.Stdout) }
