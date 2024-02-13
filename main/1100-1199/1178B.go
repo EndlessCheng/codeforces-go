dd48c831b8581563627e3fac13be60bbc3748181
Endless <0x3f@0x3f3f3f3f.com>
2024-02-13 20:10:59 +0800

    CF1178B

--- main/1100-1199/1178B.go
+++ main/1100-1199/1178B.go
@@ -22,4 +22,29 @@ func cf1178B(in io.Reader, out io.Writer) {
 	Fprint(out, f2)
 }
 
+func cf1178B2(in io.Reader, out io.Writer) {
+	var s string
+	Fscan(bufio.NewReader(in), &s)
+	var pre, suf, ans int
+	n := len(s)
+	for i := 1; i < n-1; i++ {
+		if s[i] == 'v' && s[i+1] == 'v' {
+			suf++
+		}
+	}
+	for i := 1; i < n-2; i++ {
+		if s[i] == 'o' {
+			ans += pre * suf
+		} else {
+			if s[i-1] == 'v' {
+				pre++
+			}
+			if s[i+1] == 'v' {
+				suf--
+			}
+		}
+	}
+	Fprint(out, ans)
+}
+
 //func main() { cf1178B(os.Stdin, os.Stdout) }
