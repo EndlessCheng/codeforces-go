78743a95fcf08db84831067efa32bcbb7d9d1afc
Endless <0x3f@0x3f3f3f3f.com>
2024-02-21 18:44:22 +0800

    CF432D

--- main/400-499/432D.go
+++ main/400-499/432D.go
@@ -7,43 +7,75 @@ import (
 	"sort"
 )
 
-// github.com/EndlessCheng/codeforces-go
+// O(n) 做法
 func CF432D(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
-	min := func(a, b int) int {
-		if a < b {
-			return a
-		}
-		return b
-	}
-	max := func(a, b int) int {
-		if a > b {
-			return a
-		}
-		return b
-	}
 
-	var s []byte
-	Fscan(_r, &s)
+	var s string
+	Fscan(in, &s)
 	n := len(s)
 	z := make([]int, n)
-	for i, l, r := 1, 0, 0; i < n; i++ {
-		z[i] = max(0, min(z[i-l], r-i+1))
+	cnt := make([]int, n+2)
+	l, r := 0, 0
+	for i := 1; i < n; i++ {
+		if i <= r {
+			z[i] = min(z[i-l], r-i+1)
+		}
+		for i+z[i] < n && s[z[i]] == s[i+z[i]] {
+			l, r = i, i+z[i]
+			z[i]++
+		}
+		cnt[z[i]]++
+	}
+	z[0] = n
+	cnt[n]++
+
+	ans := [][2]int{}
+	for i := n; i > 0; i-- {
+		cnt[i] += cnt[i+1]
+		if z[n-i] == i {
+			ans = append(ans, [2]int{i, cnt[i]})
+		}
+	}
+	Fprintln(out, len(ans))
+	for i := len(ans) - 1; i >= 0; i-- {
+		p := ans[i]
+		Fprintln(out, p[0], p[1])
+	}
+}
+
+// O(nlogn) 做法
+func CF432D2(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var s string
+	Fscan(in, &s)
+	n := len(s)
+	z := make([]int, n)
+	l, r := 0, 0
+	for i := 1; i < n; i++ {
+		if i <= r {
+			z[i] = min(z[i-l], r-i+1)
+		}
 		for i+z[i] < n && s[z[i]] == s[i+z[i]] {
 			l, r = i, i+z[i]
 			z[i]++
 		}
 	}
 	z[0] = n
-	sorted := make([]int, len(z))
-	copy(sorted, z)
-	sort.Ints(sorted)
+
+	sortedZ := append([]int{}, z...)
+	sort.Ints(sortedZ)
+
 	ans := [][2]int{}
-	for l := 1; l <= n; l++ {
-		if z[n-l] == l {
-			i := sort.SearchInts(sorted, l)
-			ans = append(ans, [2]int{l, n - i})
+	for i := 1; i <= n; i++ {
+		if z[n-i] == i {
+			j := sort.SearchInts(sortedZ, i)
+			ans = append(ans, [2]int{i, n - j})
 		}
 	}
 	Fprintln(out, len(ans))
