c471af123f41cd6f8acf66f36621abf5dd97a11a
Endless <0x3f@0x3f3f3f3f.com>
2024-02-26 20:26:02 +0800

    upd

--- leetcode/biweekly/59/c/c.go
+++ leetcode/biweekly/59/c/c.go
@@ -1,72 +1,51 @@
 package main
 
-// github.com/EndlessCheng/codeforces-go
-func countPaths(n int, roads [][]int) (ans int) {
-	type nb struct{ to, wt int }
+import "math"
+
+// https://space.bilibili.com/206214
+func countPaths(n int, roads [][]int) int {
 	g := make([][]int, n)
 	for i := range g {
 		g[i] = make([]int, n)
 		for j := range g[i] {
-			g[i][j] = 1e18
+			g[i][j] = math.MaxInt / 2 // 防止溢出
 		}
 	}
 	for _, r := range roads {
-		v, w, wt := r[0], r[1], r[2]
-		g[v][w] = wt
-		g[w][v] = wt
+		x, y, d := r[0], r[1], r[2]
+		g[x][y] = d
+		g[y][x] = d
 	}
 
-	// 求 0 到其余点的最短路
-	d := make([]int, n)
-	for i := range d {
-		d[i] = 1e18
+	dis := make([]int, n)
+	for i := 1; i < n; i++ {
+		dis[i] = math.MaxInt / 2
 	}
-	d[0] = 0
-	used := make([]bool, n)
+	f := make([]int, n)
+	f[0] = 1
+	done := make([]bool, n)
 	for {
-		v := -1
-		for w, u := range used {
-			if !u && (v < 0 || d[w] < d[v]) {
-				v = w
+		x := -1
+		for i, ok := range done {
+			if !ok && (x < 0 || dis[i] < dis[x]) {
+				x = i
 			}
 		}
-		if v < 0 {
-			break
+		if x == n-1 {
+			// 不可能找到比 dis[n-1] 更短，或者一样短的最短路了（注意本题边权都是正数）
+			return f[n-1]
 		}
-		used[v] = true
-		for w, wt := range g[v] {
-			if newD := d[v] + wt; newD < d[w] {
-				d[w] = newD
-			}
-		}
-	}
-
-	// 最短路构成了一个 DAG，这里不需要建一个新图，直接根据距离来判断每条边是否在 DAG 上
-	// 计算新图的入度数组
-	deg := make([]int, n)
-	for v, r := range g {
-		for w, wt := range r {
-			if d[v]+wt == d[w] {
-				deg[w]++
+		done[x] = true
+		for y, d := range g[x] {
+			newDis := dis[x] + d
+			if newDis < dis[y] { 
+				// 就目前来说，最短路必须经过 x
+				dis[y] = newDis
+				f[y] = f[x]
+			} else if newDis == dis[y] {
+				// 和之前求的最短路一样长
+				f[y] = (f[y] + f[x]) % 1_000_000_007
 			}
 		}
 	}
-
-	// 在新图上跑拓扑排序
-	dp := make([]int, n) // dp[i] 表示 0 到 i 的最短路个数
-	dp[0] = 1
-	q := []int{0}
-	for len(q) > 0 {
-		v := q[0]
-		q = q[1:]
-		for w, wt := range g[v] {
-			if d[v]+wt == d[w] {
-				dp[w] = (dp[w] + dp[v]) % (1e9 + 7)
-				if deg[w]--; deg[w] == 0 {
-					q = append(q, w)
-				}
-			}
-		}
-	}
-	return dp[n-1]
 }
