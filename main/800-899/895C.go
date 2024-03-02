91a6a7aaec1dee9815309b54ae5c1e45b8ee264b
Endless <0x3f@0x3f3f3f3f.com>
2024-03-02 08:44:34 +0800

    CF895C 状压 DP

--- main/800-899/895C.go
+++ main/800-899/895C.go
@@ -6,48 +6,44 @@ import (
 	"io"
 )
 
-// github.com/EndlessCheng/codeforces-go
-func CF895C(_r io.Reader, _w io.Writer) {
+// https://space.bilibili.com/206214
+func CF895C(_r io.Reader, out io.Writer) {
 	in := bufio.NewReader(_r)
-	out := bufio.NewWriter(_w)
-	defer out.Flush()
-	const mod int64 = 1e9 + 7
-	cnt := [71]int64{}
+	const mod = 1_000_000_007
+	primes := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67}
 	var n, v int
 	Fscan(in, &n)
+	cnt := [71]int{}
 	for i := 0; i < n; i++ {
 		Fscan(in, &v)
 		cnt[v]++
 	}
-	pow2 := make([]int64, n)
+	pow2 := make([]int, n)
 	pow2[0] = 1
 	for i := 1; i < n; i++ {
-		pow2[i] = pow2[i-1] << 1 % mod
+		pow2[i] = pow2[i-1] * 2 % mod
 	}
-	ps := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67}
 
-	dp := [2][1 << 19]int64{}
-	dp[0][0] = 1
-	cur := 0
-	for i, c := range cnt {
+	f := make([]int, 1<<len(primes))
+	f[0] = 1
+	for x, c := range cnt {
 		if c == 0 {
 			continue
 		}
-		e, x := 0, i
-		for j, p := range ps {
+		mask := 0
+		for i, p := range primes {
 			for ; x%p == 0; x /= p {
-				e ^= 1 << j
+				mask ^= 1 << i
 			}
 		}
-		cur ^= 1
-		dp[cur] = [1 << 19]int64{}
-		for s := 0; s < 1<<19; s++ {
-			v := dp[cur^1][s] * pow2[cnt[i]-1]
-			dp[cur][s^e] = (dp[cur][s^e] + v) % mod
-			dp[cur][s] = (dp[cur][s] + v) % mod
+		nf := make([]int, len(f))
+		for s, fs := range f {
+			nf[s] = (nf[s] + fs*pow2[c-1]) % mod           // 选偶数个 x
+			nf[s^mask] = (nf[s^mask] + fs*pow2[c-1]) % mod // 选奇数个 x
 		}
+		f = nf
 	}
-	Fprint(out, (dp[cur][0]-1+mod)%mod)
+	Fprint(out, (f[0]-1+mod)%mod)
 }
 
 //func main() { CF895C(os.Stdin, os.Stdout) }
