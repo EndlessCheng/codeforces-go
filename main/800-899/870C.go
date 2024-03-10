e310c0cee946a30710a54659c88b67e6dd77a620
Endless <0x3f@0x3f3f3f3f.com>
2024-03-10 08:38:00 +0800

    CF870C 分类讨论

---
+++ main/800-899/870C.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+)
+
+// https://space.bilibili.com/206214
+func cf870C(_r io.Reader, _w io.Writer) {
+	in := bufio.NewReader(_r)
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+
+	var T, n int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n)
+		if n == 2 || n == 3 || n == 5 || n == 7 || n == 11 {
+			Fprintln(out, -1)
+		} else {
+			Fprintln(out, n/4-n%2)
+		}
+	}
+}
+
+//func main() { cf870C(os.Stdin, os.Stdout) }
