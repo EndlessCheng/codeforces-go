5efbae1f6cc9ec81729a29e1979fa3a55df39cbd
Endless <0x3f@0x3f3f3f3f.com>
2024-03-10 08:48:43 +0800

    CF870C 分类讨论

--- main/800-899/870C.go
+++ main/800-899/870C.go
@@ -15,10 +15,10 @@ func cf870C(_r io.Reader, _w io.Writer) {
 	var T, n int
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &n)
-		if n == 2 || n == 3 || n == 5 || n == 7 || n == 11 {
+		if n == 2 || n == 5 || n == 7 || n == 11 {
 			Fprintln(out, -1)
 		} else {
-			Fprintln(out, n/4-n%2)
+			Fprintln(out, n/4-n%2) // 1 和 3 输出 -1
 		}
 	}
 }
