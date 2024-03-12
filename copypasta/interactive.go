368a6263d99da7e90dbc973a41b78bbbdfbe5d91
Endless <0x3f@0x3f3f3f3f.com>
2024-03-12 14:31:13 +0800

    add links

--- copypasta/interactive.go
+++ copypasta/interactive.go
@@ -4,8 +4,7 @@ package copypasta
 
 模板见 template/interactive_problem
 
-快排 https://codeforces.com/contest/1918/problem/E
-
-Floyd 判圈 https://codeforces.com/contest/1137/problem/D 2400
-
+https://codeforces.com/contest/1918/problem/E 快排
+https://codeforces.com/contest/1137/problem/D 2400 Floyd 判圈
+https://codeforces.com/problemset/problem/835/E 2400
 */
