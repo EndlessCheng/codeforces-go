70c581b8c067e2152edae377f9e83209cfb50667
Endless <0x3f@0x3f3f3f3f.com>
2024-03-12 19:10:42 +0800

    [2400] CF1042F 树形贪心

--- main/1000-1099/1042F.go
+++ main/1000-1099/1042F.go
@@ -7,49 +7,52 @@ import (
 )
 
 // https://space.bilibili.com/206214
+var k42, ans42 int
+var g42 [][]int
+
+func dfs42(v, fa int) (mx int) {
+	if len(g42[v]) == 1 {
+		return 1
+	}
+	for _, w := range g42[v] {
+		if w == fa {
+			continue
+		}
+		d := dfs42(w, v)
+		if mx+d <= k42 {
+			mx = max(mx, d)
+		} else {
+			ans42++
+			mx = min(mx, d)
+		}
+	}
+	if mx > 0 {
+		mx++
+	}
+	return
+}
+
 func cf1042F(_r io.Reader, out io.Writer) {
 	in := bufio.NewReader(_r)
-	var n, k, v, w, rt, ans int
-	Fscan(in, &n, &k)
-	g := make([][]int, n+1)
+	var n, v, w, rt int
+	Fscan(in, &n, &k42)
+	g42 = make([][]int, n+1)
 	for i := 1; i < n; i++ {
 		Fscan(in, &v, &w)
-		g[v] = append(g[v], w)
-		g[w] = append(g[w], v)
+		g42[v] = append(g42[v], w)
+		g42[w] = append(g42[w], v)
 	}
-	for i, vs := range g {
+	for i, vs := range g42 {
 		if len(vs) > 1 {
 			rt = i
 			break
 		}
 	}
-	var dfs func(int, int) int
-	dfs = func(v, fa int) (mx int) {
-		if len(g[v]) == 1 {
-			return 1
-		}
-		for _, w := range g[v] {
-			if w == fa {
-				continue
-			}
-			d := dfs(w, v)
-			if mx+d <= k {
-				mx = max(mx, d)
-			} else {
-				ans++
-				mx = min(mx, d)
-			}
-		}
-		if mx > 0 {
-			mx++
-		}
-		return
-	}
-	d := dfs(rt, -1)
+	d := dfs42(rt, -1)
 	if d > 0 {
-		ans++
+		ans42++
 	}
-	Fprint(out, ans)
+	Fprint(out, ans42)
 }
 
 //func main() { cf1042F(os.Stdin, os.Stdout) }
