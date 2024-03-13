9ffa6c36793d85c5060c14969afc591c98238018
Endless <0x3f@0x3f3f3f3f.com>
2024-03-13 10:14:12 +0800

    CF869E

--- main/800-899/869E.go
+++ main/800-899/869E.go
@@ -8,20 +8,27 @@ import (
 )
 
 // https://space.bilibili.com/206214
-type fenwick69 [][]int
+type fenwickDiff69 [][]int
 
-func (f fenwick69) add(x, y, val int) {
-	for i := x; i < len(f); i += i & -i {
-		for j := y; j < len(f[i]); j += j & -j {
-			f[i][j] += val
+func (t fenwickDiff69) add(x, y, val int) {
+	for i := x; i < len(t); i += i & -i {
+		for j := y; j < len(t[i]); j += j & -j {
+			t[i][j] += val
 		}
 	}
 }
 
-func (f fenwick69) get(x, y int) (res int) {
+func (t fenwickDiff69) update(x1, y1, x2, y2, val int) {
+	t.add(x1, y1, val)
+	t.add(x1, y2+1, -val)
+	t.add(x2+1, y1, -val)
+	t.add(x2+1, y2+1, val)
+}
+
+func (t fenwickDiff69) get(x, y int) (res int) {
 	for i := x; i > 0; i &= i - 1 {
 		for j := y; j > 0; j &= j - 1 {
-			res += f[i][j]
+			res += t[i][j]
 		}
 	}
 	return
@@ -35,18 +42,14 @@ func cf869E(_r io.Reader, _w io.Writer) {
 
 	var n, m, q, op, r1, c1, r2, c2 int
 	Fscan(in, &n, &m, &q)
-	t := make(fenwick69, n+1)
+	t := make(fenwickDiff69, n+1)
 	for i := range t {
 		t[i] = make([]int, m+1)
 	}
 	for ; q > 0; q-- {
 		Fscan(in, &op, &r1, &c1, &r2, &c2)
 		if op < 3 {
-			v := (r1 + c1*M + r2*M*M + c2*M*M*M) * (3 - op*2)
-			t.add(r1, c1, v)
-			t.add(r1, c2+1, -v)
-			t.add(r2+1, c1, -v)
-			t.add(r2+1, c2+1, v)
+			t.update(r1, c1, r2, c2, (r1+c1*M+r2*M*M+c2*M*M*M)*(3-op*2))
 		} else if t.get(r1, c1) == t.get(r2, c2) {
 			Fprintln(out, "Yes")
 		} else {
