ecd17d9ad3c8718e6b015a48609846ab2f113d3d
Endless <0x3f@0x3f3f3f3f.com>
2024-03-19 20:18:24 +0800

    upd

--- leetcode/biweekly/89/c/c.go
+++ leetcode/biweekly/89/c/c.go
@@ -1,7 +1,22 @@
 package main
 
+import (
+	"slices"
+	"sort"
+)
+
 // https://space.bilibili.com/206214
-func minimizeArrayValue(nums []int) (ans int) {
+func minimizeArrayValue(nums []int) int {
+	return sort.Search(slices.Max(nums), func(limit int) bool {
+		extra := 0
+		for i := len(nums) - 1; i > 0; i-- {
+			extra = max(nums[i]+extra-limit, 0)
+		}
+		return nums[0]+extra <= limit
+	})
+}
+
+func minimizeArrayValue2(nums []int) (ans int) {
 	s := 0
 	for i, x := range nums {
 		s += x
@@ -9,5 +24,3 @@ func minimizeArrayValue(nums []int) (ans int) {
 	}
 	return
 }
-
-func max(a, b int) int { if b > a { return b }; return a }
