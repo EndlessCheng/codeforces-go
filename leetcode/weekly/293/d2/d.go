1c70a32983404517339d5ade6cb2465c64829b1b
Endless <0x3f@0x3f3f3f3f.com>
2024-04-09 20:43:12 +0800

    upd

--- leetcode/weekly/293/d2/d.go
+++ leetcode/weekly/293/d2/d.go
@@ -4,18 +4,18 @@ import "github.com/emirpasic/gods/trees/redblacktree"
 
 // github.com/EndlessCheng/codeforces-go
 type CountIntervals struct {
-	*redblacktree.Tree
+	*redblacktree.Tree[int, int]
 	cnt int
 }
 
 func Constructor() CountIntervals {
-	return CountIntervals{redblacktree.NewWithIntComparator(), 0}
+	return CountIntervals{redblacktree.New[int, int](), 0}
 }
 
 func (t *CountIntervals) Add(left, right int) {
 	// 遍历所有被 [left,right] 覆盖到的区间（部分覆盖也算）
-	for node, _ := t.Ceiling(left); node != nil && node.Value.(int) <= right; node, _ = t.Ceiling(left) {
-		l, r := node.Value.(int), node.Key.(int)
+	for node, _ := t.Ceiling(left); node != nil && node.Value <= right; node, _ = t.Ceiling(left) {
+		l, r := node.Value, node.Key
 		if l < left { left = l }   // 合并后的新区间，其左端点为所有被覆盖的区间的左端点的最小值
 		if r > right { right = r } // 合并后的新区间，其右端点为所有被覆盖的区间的右端点的最大值
 		t.cnt -= r - l + 1
