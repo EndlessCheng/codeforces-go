87cf0a624fe87890f1f3a794de71c18d75ba38ff
Endless <0x3f@0x3f3f3f3f.com>
2024-04-11 21:43:22 +0800

    upd

--- main/900-999/988C.go
+++ main/900-999/988C.go
@@ -6,30 +6,28 @@ import (
 	"io"
 )
 
-// github.com/EndlessCheng/codeforces-go
+// https://space.bilibili.com/206214
 func CF988C(_r io.Reader, out io.Writer) {
 	in := bufio.NewReader(_r)
-	type pair struct{ i, j int }
-
 	var k, n int
 	Fscan(in, &k)
+	type pair struct{ i, j int }
 	mp := map[int]pair{}
 	for i := 1; i <= k; i++ {
 		Fscan(in, &n)
 		a := make([]int, n)
-		s := 0
+		sum := 0
 		for j := range a {
 			Fscan(in, &a[j])
-			s += a[j]
+			sum += a[j]
 		}
 		for j, v := range a {
-			if p := mp[s-v]; p.i > 0 && p.i < i {
-				Fprintln(out, "YES")
-				Fprintln(out, p.i, p.j+1)
-				Fprint(out, i, j+1)
+			p := mp[sum-v]
+			if p.i > 0 && p.i < i {
+				Fprintln(out, "YES", p.i, p.j+1, i, j+1)
 				return
 			}
-			mp[s-v] = pair{i, j}
+			mp[sum-v] = pair{i, j}
 		}
 	}
 	Fprint(out, "NO")
