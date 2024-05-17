d4fa6bb80b4e4a15ede4a91e6b83f3220353fd96
Endless <0x3f@0x3f3f3f3f.com>
2024-05-17 10:54:39 +0800

    upd

--- leetcode/biweekly/17/b/b.go
+++ leetcode/biweekly/17/b/b.go
@@ -3,12 +3,12 @@ package main
 // github.com/EndlessCheng/codeforces-go
 func matrixBlockSum(a [][]int, k int) (ans [][]int) {
 	n, m := len(a), len(a[0])
-	sum2d := make([][]int, n+1)
-	sum2d[0] = make([]int, m+1)
+	s := make([][]int, n+1)
+	s[0] = make([]int, m+1)
 	for i, row := range a {
-		sum2d[i+1] = make([]int, m+1)
+		s[i+1] = make([]int, m+1)
 		for j, v := range row {
-			sum2d[i+1][j+1] = sum2d[i+1][j] + sum2d[i][j+1] - sum2d[i][j] + v
+			s[i+1][j+1] = s[i+1][j] + s[i][j+1] - s[i][j] + v
 		}
 	}
 	ans = make([][]int, n)
@@ -19,20 +19,8 @@ func matrixBlockSum(a [][]int, k int) (ans [][]int) {
 			c1 := max(0, j-k)
 			r2 := min(n, i+k+1)
 			c2 := min(m, j+k+1)
-			ans[i][j] = sum2d[r2][c2] - sum2d[r2][c1] - sum2d[r1][c2] + sum2d[r1][c1]
+			ans[i][j] = s[r2][c2] - s[r2][c1] - s[r1][c2] + s[r1][c1]
 		}
 	}
 	return
-}
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
