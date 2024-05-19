6f14f14af90375b04004b87d5f29b3e80bb01127
Endless <0x3f@0x3f3f3f3f.com>
2024-05-19 09:57:12 +0800

    CF1689C 树形 DP 二叉树

---
+++ main/1600-1699/1689C.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://space.bilibili.com/206214
+func cf1689C(in io.Reader, out io.Writer) {
+	var T, n int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n)
+		g := make([][]int, n+1)
+		g[1] = []int{0}
+		for i := 1; i < n; i++ {
+			var v, w int
+			Fscan(in, &v, &w)
+			g[v] = append(g[v], w)
+			g[w] = append(g[w], v)
+		}
+		var dfs func(int, int) int
+		dfs = func(v, fa int) int {
+			if len(g[v]) == 1 {
+				return 1
+			}
+			if len(g[v]) == 2 {
+				return 2
+			}
+			mx := int(1e9)
+			for _, w := range g[v] {
+				if w != fa {
+					mx = min(mx, dfs(w, v)+2)
+				}
+			}
+			return mx
+		}
+		Fprintln(out, n-dfs(1, 0))
+	}
+}
+
+//func main() { cf1689C(bufio.NewReader(os.Stdin), os.Stdout) }
