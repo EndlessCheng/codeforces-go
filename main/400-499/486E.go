872a68a5193bb65157729a66e2749035d2a49b25
Endless <0x3f@0x3f3f3f3f.com>
2024-05-31 08:58:41 +0800

    rename & 加注释

--- main/400-499/486E.go
+++ main/400-499/486E.go
@@ -1,59 +1,61 @@
 package main
 
 import (
-	"bufio"
 	. "fmt"
 	"io"
 	"sort"
 )
 
 // github.com/EndlessCheng/codeforces-go
-func CF486E(_r io.Reader, out io.Writer) {
-	in := bufio.NewReader(_r)
+func CF486E(in io.Reader, out io.Writer) {
 	var n int
 	Fscan(in, &n)
-	l := make([]int, n)
-	dp := []int{}
 	a := make([]int, n)
+	pre := make([]int, n)
+	g := []int{}
 	for i := range a {
 		Fscan(in, &a[i])
 		v := a[i]
-		p := sort.SearchInts(dp, v)
-		if p < len(dp) {
-			dp[p] = v
+		p := sort.SearchInts(g, v)
+		if p < len(g) {
+			g[p] = v
 		} else {
-			dp = append(dp, v)
+			g = append(g, v)
 		}
-		l[i] = p + 1
+		pre[i] = p + 1
 	}
-	r := make([]int, n)
-	dp = nil
+
+	suf := make([]int, n)
+	g = g[:0]
 	for i := n - 1; i >= 0; i-- {
 		v := -a[i]
-		p := sort.SearchInts(dp, v)
-		if p < len(dp) {
-			dp[p] = v
+		p := sort.SearchInts(g, v)
+		if p < len(g) {
+			g[p] = v
 		} else {
-			dp = append(dp, v)
+			g = append(g, v)
 		}
-		r[i] = p + 1
+		suf[i] = p + 1
 	}
+
+	lis := len(g)
 	ans := make([]byte, n)
 	cnt := make([]int, n+1)
-	for i, l := range l {
-		if l+r[i]-1 != len(dp) {
-			ans[i] = '1'
+	for i, p := range pre {
+		if p+suf[i]-1 == lis {
+			ans[i] = '3' // 暂定是 3
+			cnt[p]++     // 如果有多个相同的 p，则 ans[i] = '2'（下面判断）
 		} else {
-			ans[i] = '3'
-			cnt[l]++
+			ans[i] = '1' // 不在任何 LIS 上
 		}
 	}
+
 	for i, tp := range ans {
-		if tp == '3' && cnt[l[i]] > 1 {
+		if tp == '3' && cnt[pre[i]] > 1 {
 			ans[i] = '2'
 		}
 	}
 	Fprintf(out, "%s", ans)
 }
 
-//func main() { CF486E(os.Stdin, os.Stdout) }
+//func main() { CF486E(bufio.NewReader(os.Stdin), os.Stdout) }
