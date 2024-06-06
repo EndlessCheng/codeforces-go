3cad19efcc5efe2750b69067295030c75f1a7341
Endless <0x3f@0x3f3f3f3f.com>
2024-06-06 09:11:46 +0800

    简化代码

--- main/200-299/264C.go
+++ main/200-299/264C.go
@@ -16,14 +16,13 @@ func cf264C(in io.Reader, out io.Writer) {
 	for i := range ps {
 		Fscan(in, &ps[i].c)
 	}
-	const inf int = 1e18
 	f := make([]int, n+1)
 	for ; q > 0; q-- {
 		Fscan(in, &a, &b)
 		for i := range f {
-			f[i] = -inf
+			f[i] = -1e18
 		}
-		mx1, mx2, mxC := -inf, -inf, 0
+		var mx1, mx2, mxC int
 		for _, p := range ps {
 			c := p.c
 			if c != mxC {
@@ -35,14 +34,14 @@ func cf264C(in io.Reader, out io.Writer) {
 			if f[c] > mx1 {
 				if c != mxC {
 					mx2 = mx1
+					mxC = c
 				}
 				mx1 = f[c]
-				mxC = c
 			} else if c != mxC && f[c] > mx2 {
 				mx2 = f[c]
 			}
 		}
-		Fprintln(out, max(mx1, 0))
+		Fprintln(out, mx1)
 	}
 }
 
