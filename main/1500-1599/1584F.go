8c8299189ed40885e1eb7568b9846b0ff978306b
Endless <0x3f@0x3f3f3f3f.com>
2024-06-08 20:30:54 +0800

    [2600][1A] CF1584F 状压 DP 多串 LCS

--- main/1500-1599/1584F.go
+++ main/1500-1599/1584F.go
@@ -11,13 +11,12 @@ func cf1584F(in io.Reader, out io.Writer) {
 	var s string
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &n)
-		// 记录字母在各个字符串中的出现位置
 		pos := [123][10][]int{}
 		for i := 0; i < n; i++ {
 			pos[0][i] = []int{-1} // 假定在 LCS 前面还有个字符 \0，下标为 -1
 			Fscan(in, &s)
 			for j, b := range s {
-				pos[b][i] = append(pos[b][i], j)
+				pos[b][i] = append(pos[b][i], j) // 记录字母 b 在字符串 s[i] 中的出现位置 j
 			}
 		}
 
@@ -38,16 +37,18 @@ func cf1584F(in io.Reader, out io.Writer) {
 			var frm pair
 			// 枚举 LCS 的下一个字母 ch
 			// 要求：ch 在所有字符串中的下标 > c 在对应字符串中的下标
-			// 如果有两个 ch 都满足要求，优先取左边的，对应下面代码中的 p[0] > pos[c][i][mask>>i&1]
+			// 如果有两个 ch 都满足要求，优先取左边的，对应下面代码中的 p[0] > cur
 			for ch := byte('A'); ch <= 'z'; {
 				mask2 := 0
 				for i, p := range pos[ch][:n] {
 					if p == nil {
 						goto nxt
 					}
-					if p[0] > pos[c][i][mask>>i&1] {
+					cur := pos[c][i][mask>>i&1] // 当前字母 c 的下标
+					// p[0] 或者 p[1] 是下一个字母 ch 的下标
+					if p[0] > cur {
 						// 0
-					} else if len(p) > 1 && p[1] > pos[c][i][mask>>i&1] {
+					} else if len(p) > 1 && p[1] > cur {
 						mask2 |= 1 << i
 					} else {
 						goto nxt
