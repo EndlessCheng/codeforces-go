2ad6556c5e0d0d951920a3128e1bac3254406d5a
Endless <0x3f@0x3f3f3f3f.com>
2024-06-08 11:41:05 +0800

    简化代码

--- main/1200-1299/1237D.go
+++ main/1200-1299/1237D.go
@@ -6,62 +6,34 @@ import (
 	"io"
 )
 
-// github.com/EndlessCheng/codeforces-go
-type mqData37 struct {
-	v   int
-	del int
-}
-type mq37 struct {
-	data []mqData37
-	size int
-}
-
-func (q mq37) less(a, b mqData37) bool { return a.v >= b.v }
-func (q *mq37) push(v int) {
-	q.size++
-	d := mqData37{v, 1}
-	for len(q.data) > 0 && q.less(d, q.data[len(q.data)-1]) {
-		d.del += q.data[len(q.data)-1].del
-		q.data = q.data[:len(q.data)-1]
-	}
-	q.data = append(q.data, d)
-}
-func (q *mq37) pop() {
-	q.size--
-	if q.data[0].del > 1 {
-		q.data[0].del--
-	} else {
-		q.data = q.data[1:]
-	}
-}
-func (q mq37) top() (v int) { return q.data[0].v }
-
-func CF1237D(_r io.Reader, _w io.Writer) {
-	in := bufio.NewReader(_r)
+// https://space.bilibili.com/206214
+func cf1237D(in io.Reader, _w io.Writer) {
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
-
 	var n int
 	Fscan(in, &n)
-	a := make([]int, n, 3*n)
+	a := make([]int, n, n*3)
 	for i := range a {
 		Fscan(in, &a[i])
 	}
 	a = append(append(a, a...), a...)
-	q := mq37{}
-	for i, j := 0, 0; i < n; i++ {
-		for ; j < 3*n && (q.size == 0 || 2*a[j] >= q.top()); j++ {
-			q.push(a[j])
+	q := []int{}
+	for l, r := 0, 0; l < n; l++ {
+		for ; r < n*3 && (len(q) == 0 || a[r]*2 >= a[q[0]]); r++ {
+			for len(q) > 0 && a[q[len(q)-1]] <= a[r] {
+				q = q[:len(q)-1]
+			}
+			q = append(q, r)
 		}
-		sz := j - i
-		if sz > 2*n {
-			sz = -1
+		if r == n*3 {
+			Fprint(out, "-1 ")
+		} else {
+			Fprint(out, r-l, " ")
 		}
-		Fprint(out, sz, " ")
-		if q.size == sz {
-			q.pop()
+		if q[0] == l {
+			q = q[1:]
 		}
 	}
 }
 
-//func main() { CF1237D(os.Stdin, os.Stdout) }
+//func main() { cf1237D(bufio.NewReader(os.Stdin), os.Stdout) }
