06c6b06072d6b7d6edb41248063698be771c5fda
Endless <0x3f@0x3f3f3f3f.com>
2024-06-17 21:20:05 +0800

    CF1950F

---
+++ main/1900-1999/1950F.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+	"math/bits"
+)
+
+func cf1950F(in io.Reader, out io.Writer) {
+	var T, a, b, c int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &a, &b, &c)
+		if a+1 != c {
+			Fprintln(out, -1)
+			continue
+		}
+		h := bits.Len(uint(a))
+		b -= 1<<h - 1 - a
+		if b > 0 {
+			h += (b + a) / (a + 1)
+		}
+		Fprintln(out, h)
+	}
+}
+
+//func main() { cf1950F(bufio.NewReader(os.Stdin), os.Stdout) }
