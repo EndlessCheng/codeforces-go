0d8d1758658a61b073542d25ebf531378a8c3896
Endless <0x3f@0x3f3f3f3f.com>
2024-07-01 17:37:02 +0800

    空间优化

--- main/1900-1999/1987D.go
+++ main/1900-1999/1987D.go
@@ -6,33 +6,31 @@ import (
 )
 
 func cf1987D(in io.Reader, out io.Writer) {
-	var T, n, v int
+	var T, n, v int16
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &n)
 		cnt := make([]int16, n)
-		for i := 0; i < n; i++ {
+		for i := int16(0); i < n; i++ {
 			Fscan(in, &v)
 			cnt[v-1]++
 		}
-		f := make([][]int16, n+1)
-		for i := range f {
-			f[i] = make([]int16, n+1)
-		}
+		f := make([]int16, n+1)
 		for i := n - 1; i >= 0; i-- {
-			c := int(cnt[i])
-			for j := 0; j < n; j++ {
-				if c == 0 {
-					f[i][j] = f[i+1][j]
-				} else {
-					res := f[i+1][j+1] + 1
-					if j >= c {
-						res = min(res, f[i+1][j-c])
-					}
-					f[i][j] = res
+			c := cnt[i]
+			if c == 0 {
+				continue
+			}
+			pre := int16(0)
+			for j := n - 1; j >= 0; j-- {
+				res := pre + 1
+				pre = f[j]
+				if j >= c {
+					res = min(res, f[j-c])
 				}
+				f[j] = res
 			}
 		}
-		Fprintln(out, f[0][0])
+		Fprintln(out, f[0])
 	}
 }
 
