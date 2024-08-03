756a30d0802fd8eab8406263c8277ea5faf7c037
Endless <0x3f@0x3f3f3f3f.com>
2024-08-03 14:46:25 +0800

    简化代码

--- main/1900-1999/1996F.go
+++ main/1900-1999/1996F.go
@@ -7,7 +7,7 @@ import (
 )
 
 func cf1996F(in io.Reader, out io.Writer) {
-	var T, n, k int
+	var T, n, k, ans int
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &n, &k)
 		a := make([]struct{ a, b int }, n)
@@ -17,30 +17,24 @@ func cf1996F(in io.Reader, out io.Writer) {
 		for i := range a {
 			Fscan(in, &a[i].b)
 		}
-		left := k
-		mx := sort.Search(1e9, func(mx int) bool {
-			k := k
+		sort.Search(1e9+1, func(mx int) bool {
+			k, s := k, 0
 			for _, p := range a {
 				if p.a > mx {
-					k -= (p.a-mx-1)/p.b + 1
+					t := (p.a-mx-1)/p.b + 1
+					k -= t
+					s += (p.a*2 - (t-1)*p.b) * t / 2
 				}
 			}
 			if k < 0 {
 				return false
 			}
-			left = min(left, k)
+			// 二分结束后，剩余的 k 次操作，每次操作的得分一定都恰好等于 mx
+			// 反证法：如果操作若干次后，后续的操作只能得到 < mx 的分数，那么二分结果必然 < mx，矛盾
+			ans = s + mx*k
 			return true
 		})
-		ans := 0
-		for _, p := range a {
-			if p.a > mx {
-				k := (p.a-mx-1)/p.b + 1
-				ans += (p.a*2 - (k-1)*p.b) * k / 2
-			}
-		}
-		// 二分结束后，剩余的 left 次操作，每次操作的得分一定都恰好等于 mx
-		// 反证法：如果操作若干次后，后续的操作只能得到 < mx 的分数，那么上面的二分结果必然 < mx，矛盾
-		Fprintln(out, ans+mx*left)
+		Fprintln(out, ans)
 	}
 }
 
