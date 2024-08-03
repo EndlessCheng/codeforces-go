f9827154c8ab05df094074744c10b35df6ba1ab9
Endless <0x3f@0x3f3f3f3f.com>
2024-08-03 19:13:10 +0800

    简化代码

--- main/1000-1099/1088D.go
+++ main/1000-1099/1088D.go
@@ -3,25 +3,21 @@ package main
 import . "fmt"
 
 func cf1088D() {
+	q := func(c, d int) (x int) {
+		Println("?", c, d)
+		Scan(&x)
+		return
+	}
 	ans := [2]int{}
-	var cmp, t int
-	Println("? 0 0")
-	Scan(&cmp)
+	cmp := q(0, 0)
 	for b := 1 << 29; b > 0; b >>= 1 {
-		Println("?", ans[0]|b, ans[1]|b)
-		Scan(&t)
+		t := q(ans[0]|b, ans[1]|b)
 		if t != 0 && t != cmp { // 两个比特一定不同
 			ans[(t+1)/2] |= b
-			// 看看有没有相等的机会
-			Println("?", ans[0], ans[1])
-			Scan(&cmp)
-		} else { // 两个比特一定相同，要么 0 0 要么 1 1
-			Println("?", ans[0]|b, ans[1])
-			Scan(&t)
-			if t < 0 { // 是 1 1
-				ans[0] |= b
-				ans[1] |= b
-			}
+			cmp = q(ans[0], ans[1])
+		} else if q(ans[0]|b, ans[1]) < 0 { // 两个比特一定相同，要么 0 0 要么 1 1，这里 < 0 说明是 1 1
+			ans[0] |= b
+			ans[1] |= b
 			// cmp 不变
 		}
 	}
