a0c5546ff1d4e1b07ddde488b571542249f1606c
Endless <0x3f@0x3f3f3f3f.com>
2024-08-07 08:55:09 +0800

    CF1999G2

---
+++ main/1900-1999/1999G2.go
@@ -0,0 +1,26 @@
+package main
+
+import . "fmt"
+
+func cf1999G2() {
+	var T, s int
+	for Scan(&T); T > 0; T-- {
+		l, r := 1, 999
+		for l+1 < r {
+			sz := (r - l + 2) / 3
+			p, q := l+sz, r-sz
+			Println("?", p, q)
+			Scan(&s)
+			if p*q == s {
+				l = q
+			} else if p*(q+1) == s {
+				l, r = p, q
+			} else {
+				r = p
+			}
+		}
+		Println("!", r)
+	}
+}
+
+//func main() { cf1999G2() }
