8cece038d4aec088abc6b75cadc2f86404b401d8
Endless <0x3f@0x3f3f3f3f.com>
2024-08-12 18:52:01 +0800

    图一乐：记忆化搜索套记忆化搜索

--- leetcode/weekly/410/d/d.go
+++ leetcode/weekly/410/d/d.go
@@ -2,6 +2,68 @@ package main
 
 import "slices"
 
+// 图一乐：记忆化搜索套记忆化搜索
+func countOfPairs(a []int) (ans int) {
+	const mod = 1_000_000_007
+	n := len(a)
+	m := slices.Max(a)
+	dp0 := make([][]int, n)
+	for i := range dp0 {
+		dp0[i] = make([]int, m+1)
+		for j := range dp0[i] {
+			dp0[i][j] = -1
+		}
+	}
+
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, m+1)
+		for j := range dp[i] {
+			dp[i][j] = -1
+		}
+	}
+	var sum func(int, int) int
+	var f func(int, int) int
+
+	sum = func(i, j int) int {
+		dv := &dp[i][j]
+		if *dv != -1 {
+			return *dv
+		}
+		if j == 0 {
+			if i == 0 {
+				*dv = 1
+			} else {
+				*dv = f(i, j)
+			}
+		} else {
+			*dv = (sum(i, j-1) + f(i, j)) % mod
+		}
+		return *dv
+	}
+
+	f = func(i, j int) int {
+		if i == 0 {
+			return 1
+		}
+		dv := &dp0[i][j]
+		if *dv != -1 {
+			return *dv
+		}
+		maxK := min(j, a[i-1]-a[i]+j)
+		if maxK < 0 {
+			*dv = 0
+		} else {
+			*dv = sum(i-1, maxK)
+		}
+		return *dv
+	}
+	for j := 0; j <= a[n-1]; j++ {
+		ans += f(n-1, j)
+	}
+	return ans % mod
+}
+
 // https://space.bilibili.com/206214
 const mod = 1_000_000_007
 const mx = 3001 // maxN + maxM = 2000 + 1000 = 3000
@@ -25,7 +87,7 @@ func comb(n, m int) int {
 	return f[n] * invF[m] % mod * invF[n-m] % mod
 }
 
-func countOfPairs(nums []int) int {
+func countOfPairsComb(nums []int) int {
 	n := len(nums)
 	m := nums[n-1]
 	for i := 1; i < n; i++ {
