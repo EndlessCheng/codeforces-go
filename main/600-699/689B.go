6f8b7212ca7f8a2cbb8ad8c96fadd6e82f93956a
Endless <0x3f@0x3f3f3f3f.com>
2024-08-17 21:19:56 +0800

    CF689B BFS

--- main/600-699/689B.go
+++ main/600-699/689B.go
@@ -9,22 +9,44 @@ import (
 func cf689B(in io.Reader, _w io.Writer) {
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
-	var n, w int
+	var n int
 	Fscan(in, &n)
-	from := make([][]int, n)
-	for i := 0; i < n; i++ {
-		Fscan(in, &w)
-		from[w-1] = append(from[w-1], i)
+	to := make([]int, n)
+	for i := range to {
+		Fscan(in, &to[i])
+		to[i]--
 	}
-
-	f := make([]int, n)
-	Fprint(out, 0)
-	for i := 1; i < n; i++ {
-		f[i] = f[i-1] + 1
-		for _, v := range from[i] {
-			f[i] = min(f[i], f[v]+1)
+	dis := make([]int, n)
+	for i := range dis {
+		dis[i] = -1
+	}
+	dis[0] = 0
+	q := []int{0}
+	for len(q) > 0 {
+		v := q[0]
+		q = q[1:]
+		w := to[v]
+		if dis[w] < 0 {
+			dis[w] = dis[v] + 1
+			q = append(q, w)
 		}
-		Fprint(out, " ", f[i])
+		if v < n-1 {
+			w := v + 1
+			if dis[w] < 0 {
+				dis[w] = dis[v] + 1
+				q = append(q, w)
+			}
+		}
+		if v > 0 {
+			w := v - 1
+			if dis[w] < 0 {
+				dis[w] = dis[v] + 1
+				q = append(q, w)
+			}
+		}
+	}
+	for _, v := range dis {
+		Fprint(out, v, " ")
 	}
 }
 
