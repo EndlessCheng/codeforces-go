2710330740a2173613c55d9ccb796172a325b7bc
Endless <0x3f@0x3f3f3f3f.com>
2024-08-17 21:46:36 +0800

    CF1468J MST

---
+++ main/1400-1499/1468J.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+	"slices"
+)
+
+func cf1468J(in io.Reader, out io.Writer) {
+	var T, n, m, k int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n, &m, &k)
+		fa := make([]int, n+1)
+		for i := range fa {
+			fa[i] = i
+		}
+		find := func(x int) int {
+			rt := x
+			for fa[rt] != rt {
+				rt = fa[rt]
+			}
+			for fa[x] != rt {
+				fa[x], x = rt, fa[x]
+			}
+			return rt
+		}
+		cc := n
+
+		type edge struct{ v, w, wt int }
+		es := []edge{}
+		mx := 0
+		for ; m > 0; m-- {
+			var v, w, wt int
+			Fscan(in, &v, &w, &wt)
+			if wt > k {
+				es = append(es, edge{v, w, wt - k})
+				continue
+			}
+			mx = max(mx, wt)
+			v = find(v)
+			w = find(w)
+			if v != w {
+				cc--
+				fa[v] = w
+			}
+		}
+		slices.SortFunc(es, func(a, b edge) int { return a.wt - b.wt })
+
+		ans := int(1e18)
+		if cc == 1 {
+			ans = k - mx
+			if len(es) > 0 {
+				ans = min(ans, es[0].wt)
+			}
+		}
+
+		sum := 0
+		for _, e := range es {
+			v, w, wt := find(e.v), find(e.w), e.wt
+			if v != w {
+				fa[v] = w
+				sum += wt
+			}
+		}
+		if sum > 0 {
+			ans = min(ans, sum)
+		}
+		Fprintln(out, ans)
+	}
+}
+
+//func main() { cf1468J(bufio.NewReader(os.Stdin), os.Stdout) }
