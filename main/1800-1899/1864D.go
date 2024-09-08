e657fa4781331a37180089801f0cb5d57e25cbe5
Endless <0x3f@0x3f3f3f3f.com>
2024-09-08 09:09:28 +0800

    简化代码

--- main/1800-1899/1864D.go
+++ main/1800-1899/1864D.go
@@ -10,27 +10,24 @@ func cf1864D(in io.Reader, _w io.Writer) {
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
 	var T, n int
-	var s string
+	var s []byte
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &n)
 		ans := 0
+		f := make([]byte, n)
 		diag1 := make([]byte, n*2)
 		diag2 := make([]byte, n*2)
-		op := make([]byte, n)
 		for i := 0; i < n; i++ {
-			for j := range op {
-				op[j] ^= diag1[i-j+n] ^ diag2[i+j]
-			}
 			Fscan(in, &s)
-			for j, v := range op {
-				if v != s[j]&1 {
+			for j, c := range s {
+				f[j] ^= diag1[i-j+n] ^ diag2[i+j]
+				if f[j] != c&1 {
 					ans++
-					op[j] ^= 1
+					f[j] ^= 1
 					diag1[i-j+n] ^= 1
 					diag2[i+j] ^= 1
 				}
 			}
-			// 进入下一轮循环，op 数组直接复用，这样只需要考虑两条斜线上的影响
 		}
 		Fprintln(out, ans)
 	}
