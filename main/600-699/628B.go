2de48f59dd160a3747dc868389a3f76d0a45feeb
Endless <0x3f@0x3f3f3f3f.com>
2024-09-09 08:32:08 +0800

    简化代码

--- main/600-699/628B.go
+++ main/600-699/628B.go
@@ -8,17 +8,13 @@ import (
 func cf628B(in io.Reader, out io.Writer) {
 	var s string
 	Fscan(in, &s)
-	var ans, x int
+	ans := 0
 	for i, c := range s {
 		if c%4 == 0 {
 			ans++
 		}
-		x = x*10 + int(c-'0')
-		if x%4 == 0 {
+		if i > 0 && ((s[i-1]-'0')*10+(s[i]-'0'))%4 == 0 {
 			ans += i
-		}
-		if i > 0 {
-			x -= int(s[i-1]-'0') * 10
 		}
 	}
 	Fprint(out, ans)
