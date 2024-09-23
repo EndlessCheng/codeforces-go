3429da4c358c7f9c6934b88be0b6bb5651274e72
Endless <0x3f@0x3f3f3f3f.com>
2024-09-23 20:58:51 +0800

    CF812C 二分答案

---
+++ main/800-899/812C.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+	"slices"
+	"sort"
+)
+
+// https://github.com/EndlessCheng
+func cf812C(in io.Reader, out io.Writer) {
+	var n, s, mn int
+	Fscan(in, &n, &s)
+	a := make([]int, n)
+	for i := range a {
+		Fscan(in, &a[i])
+	}
+	ans := sort.Search(n, func(k int) bool {
+		k++
+		b := slices.Clone(a)
+		for i := range b {
+			b[i] += (i + 1) * k
+		}
+		slices.Sort(b)
+		tot := 0
+		for _, v := range b[:k] {
+			tot += v
+		}
+		if tot > s {
+			return true
+		}
+		mn = tot
+		return false
+	})
+	Fprintln(out, ans, mn)
+}
+
+//func main() { cf812C(bufio.NewReader(os.Stdin), os.Stdout) }
