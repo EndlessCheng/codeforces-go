f672e5f7600ea6face2cf761b2bf75e57b3b4fe8
Endless <0x3f@0x3f3f3f3f.com>
2024-09-26 21:31:00 +0800

    整理交互题单

--- copypasta/interactive.go
+++ copypasta/interactive.go
@@ -4,10 +4,76 @@ package copypasta
 
 模板见 template/interactive_problem
 
-https://codeforces.com/problemset/problem/1918/E 2200 快排
+## 二分
+
+https://codeforces.com/problemset/problem/1807/E 1300 数组
+https://codeforces.com/problemset/problem/1999/G1 1500
+https://codeforces.com/problemset/problem/1479/A 1700 数组（同 LC162）
+https://codeforces.com/problemset/problem/1999/G2 1700 三分
+https://codeforces.com/problemset/problem/1010/B 1800
+https://codeforces.com/problemset/problem/1063/C 1900
+https://codeforces.com/problemset/problem/1486/C2 1900 数组
+https://codeforces.com/problemset/problem/1697/D 1900
+https://codeforces.com/problemset/problem/1363/D 2100
+https://codeforces.com/problemset/problem/713/B 2200 分类讨论
+https://codeforces.com/problemset/problem/1520/F2 2200
+https://codeforces.com/problemset/problem/1838/F 3000
+
+## 位运算
+
+https://codeforces.com/problemset/problem/1780/D 1800
+https://codeforces.com/problemset/problem/1207/E 1900 脑筋急转弯
+https://codeforces.com/problemset/problem/1088/D 2000
+https://codeforces.com/problemset/problem/1451/E2 2300
+https://codeforces.com/problemset/problem/1534/E 2300
 https://codeforces.com/problemset/problem/835/E 2400
-https://codeforces.com/problemset/problem/1137/D 2400 Floyd 判圈
+https://codeforces.com/problemset/problem/1934/D2 2400
+https://codeforces.com/problemset/problem/1365/G 2800
+
+## 数组 思维 构造
+
+https://codeforces.com/problemset/problem/727/C 1400
+https://codeforces.com/problemset/problem/896/B 2000
+https://codeforces.com/problemset/problem/1521/C 2000
+https://codeforces.com/problemset/problem/1634/D 2000
+https://codeforces.com/problemset/problem/1023/E 2100 网格图
+https://codeforces.com/problemset/problem/1617/D2 2400
+LC1538 找出隐藏数组中出现次数最多的元素 https://leetcode.cn/problems/guess-the-majority-in-a-hidden-array/
+
+## 随机
+
+https://codeforces.com/problemset/problem/843/B 2000 数组 链表
+https://codeforces.com/problemset/problem/1114/E 2200 数论
+https://codeforces.com/problemset/problem/1840/G2 2500
+
+## DP
+
+https://codeforces.com/problemset/problem/1839/E 2400
+
+## 树
+
+https://codeforces.com/problemset/problem/2001/C 1500 二分
+https://codeforces.com/problemset/problem/1146/C 1700 二分 直径
+https://codeforces.com/problemset/problem/1305/D 1900 LCA 拓扑排序
 https://codeforces.com/problemset/problem/1174/F 2400
 
-- [1538. 找出隐藏数组中出现次数最多的元素](https://leetcode.cn/problems/guess-the-majority-in-a-hidden-array/)（会员题）
+## 图
+
+https://codeforces.com/problemset/problem/1498/E 2200
+https://codeforces.com/problemset/problem/1514/E 2700
+
+## 数论
+
+https://codeforces.com/problemset/problem/679/A 1400 判断质数
+https://codeforces.com/problemset/problem/1155/E 2200 拉格朗日插值
+https://codeforces.com/problemset/problem/1355/F 2600
+
+## 其他
+
+https://codeforces.com/problemset/problem/1503/B 1700 构造、棋盘染色
+https://codeforces.com/problemset/problem/1918/E 2200 快排
+https://codeforces.com/problemset/problem/1137/D 2400 Floyd 判圈
+https://codeforces.com/problemset/problem/1470/C 2500 打表找规律
+https://codeforces.com/problemset/problem/1826/F 2800 数学
+
 */
