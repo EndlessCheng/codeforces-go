a871ac2f39ea4fbfeea9db451c0e422e8c707068
Endless <0x3f@0x3f3f3f3f.com>
2024-09-28 17:38:20 +0800

    CF2013D

--- main/2000-2099/2013D.go
+++ main/2000-2099/2013D.go
@@ -4,21 +4,20 @@ import (
 	"bufio"
 	. "fmt"
 	"io"
-	"os"
 )
 
 // https://github.com/EndlessCheng
 func cf2013D(in io.Reader, _w io.Writer) {
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
-	var T, n, v int
+	var T, n, s int
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &n)
 		type pair struct{ s, c int }
 		st := []pair{}
 		for ; n > 0; n-- {
-			Fscan(in, &v)
-			s, c := v, 1
+			Fscan(in, &s)
+			c := 1
 			for len(st) > 0 {
 				p := st[len(st)-1]
 				if (p.s-1)/p.c+1 <= s/c {
@@ -35,4 +34,4 @@ func cf2013D(in io.Reader, _w io.Writer) {
 	}
 }
 
-func main() { cf2013D(bufio.NewReader(os.Stdin), os.Stdout) }
+//func main() { cf2013D(bufio.NewReader(os.Stdin), os.Stdout) }
