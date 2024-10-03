2a78a9e947eb017dbe100e1e6b8d0b8da2d3af29
Endless <0x3f@0x3f3f3f3f.com>
2024-10-03 12:50:19 +0800

    补充注释

--- main/700-799/758E.go
+++ main/700-799/758E.go
@@ -33,20 +33,25 @@ func cf758E(in io.Reader, _w io.Writer) {
 			if w == fa {
 				continue
 			}
+			// 递归计算子树 w 的最小重量和 mn、最大重量和 mx
 			mn, mx := dfs(w, v)
 			p := es[e.i].p
+			// v-w 边的强度不能小于子树 w 的最小重量和
 			if mn < 0 || p < mn {
 				return -1, 0
 			}
 			wt := es[e.i].wt
+			// v-w 边的强度可以减少到子树 w 的最小重量和
 			minSum += max(wt-(p-mn), 1) + mn
+			// 子树 w 的最大重量和不能超过 v-w 边的强度
 			maxSum += wt + min(mx, p)
-			// 如果 v-w 这条边的强度 p < mx，那么 w 子树的最大重量和要额外减少 mx-p
+			// 如果 v-w 这条边的强度 p < mx，那么 w 子树的最大重量和 mx 要额外减少 mx-p（减少到 p）
 			// 说【额外】是因为 mx 已经是 w 子树内部减重之后的最大重量和了
 			// 如果 p < mx，那么 mx 内部还要再减少 mx-p
 			a[w].extraDec = max(mx-p, 0)
 		}
 		a[v].minSum = minSum
+		// 最后返回子树 v 的最小重量和、最大重量和
 		return
 	}
 	minSum, _ := dfs(1, 0)
@@ -56,6 +61,8 @@ func cf758E(in io.Reader, _w io.Writer) {
 	}
 
 	// 第二次 DFS：减重
+	// 核心思想：优先减重最下面的边
+	// 如果不这样做，先减重上面的，那么由于上面的边强度变小，下面的边也得跟着减重，不如先减重下面的边优
 	dec := 0
 	var modify func(int, int)
 	modify = func(v, fa int) {
@@ -64,12 +71,13 @@ func cf758E(in io.Reader, _w io.Writer) {
 			if w == fa {
 				continue
 			}
+			// 递归之前，只累加需要减重的量，在递归之后处理减重，这样就可以保证下面的边先减重
 			dec += a[w].extraDec
 			// 处理 w 子树内部的减重
-			modify(w, v) 
+			modify(w, v)
 			// 处理 v-w 这条边的减重
 			e := &es[ew.i]
-			// v-w 这条边重量可以减到 1，强度可以减到子树 w 的最小重量和
+			// v-w 这条边，重量可以减到 1，强度可以减到子树 w 的最小重量和
 			d := min(e.wt-1, e.p-a[w].minSum, dec)
 			e.wt -= d
 			e.p -= d
