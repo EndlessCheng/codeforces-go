cc2985bc82ee6e33ad292b698b735b1f1e578f90
Endless <0x3f@0x3f3f3f3f.com>
2024-10-21 09:39:02 +0800

    upd

--- leetcode/main.go
+++ leetcode/main.go
@@ -839,27 +839,6 @@ func maxProduct(a []int) int {
     return ans
 }
 
-// LC 160
-func getIntersectionNode(headA, headB *ListNode) *ListNode {
-    if headA == nil || headB == nil {
-        return nil
-    }
-    pa, pb := headA, headB
-    for pa != pb {
-        if pa == nil {
-            pa = headB
-        } else {
-            pa = pa.Next
-        }
-        if pb == nil {
-            pb = headA
-        } else {
-            pb = pb.Next
-        }
-    }
-    return pa
-}
-
 // LC 162
 func findPeakElement(a []int) int {
     return sort.Search(len(a)-1, func(i int) bool { return a[i] > a[i+1] })
