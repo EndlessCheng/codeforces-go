28ed09bac61d15e49ad2b0fdb5b28487a914abc4
Endless <0x3f@0x3f3f3f3f.com>
2024-10-24 10:11:53 +0800

    CF1988D 树形 DP

---
+++ main/1900-1999/1988D.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf1988D(in io.Reader, out io.Writer) {
+	var T, n int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n)
+		a := make([]int, n)
+		s := 0
+		for i := range a {
+			Fscan(in, &a[i])
+			s += a[i]
+		}
+		g := make([][]int, n)
+		for i := 1; i < n; i++ {
+			var v, w int
+			Fscan(in, &v, &w)
+			v--
+			w--
+			g[v] = append(g[v], w)
+			g[w] = append(g[w], v)
+		}
+		var dfs func(int, int) (int, int, int)
+		dfs = func(v, fa int) (mn, mn2, mnI int) {
+			f := make([]int, min(20, len(g[v])+1))
+			for _, w := range g[v] {
+				if w == fa {
+					continue
+				}
+				m, m2, i := dfs(w, v)
+				for j := range f {
+					if j != i {
+						f[j] += m
+					} else {
+						f[j] += m2
+					}
+				}
+			}
+			mn = int(1e18)
+			for i, res := range f {
+				res += i * a[v]
+				if res < mn {
+					mn2 = mn
+					mn = res
+					mnI = i
+				} else if res < mn2 {
+					mn2 = res
+				}
+			}
+			return
+		}
+		mn, _, _ := dfs(0, -1)
+		Fprintln(out, s+mn)
+	}
+}
+
+//func main() { cf1988D(bufio.NewReader(os.Stdin), os.Stdout) }
