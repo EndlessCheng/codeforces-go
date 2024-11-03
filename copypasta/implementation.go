9b91317dcba7d9e98b74e032f7a924db6b6f77b1
Endless <0x3f@0x3f3f3f3f.com>
2024-11-03 20:21:50 +0800

    add links

--- copypasta/implementation.go
+++ copypasta/implementation.go
@@ -1,18 +1,29 @@
 package copypasta
 
 /* 小模拟
+
 LC588 https://leetcode.cn/problems/design-in-memory-file-system/
 LC2534 https://leetcode.cn/problems/time-taken-to-cross-the-door/
 LC723 https://leetcode.cn/problems/candy-crush/（会员题）
 - [2254. 设计视频共享平台](https://leetcode.cn/problems/design-video-sharing-platform/)（会员题）
 - [723. 粉碎糖果](https://leetcode.cn/problems/candy-crush/)（会员题）消除游戏
+
+*/
+
+/* 有较多下标参与运算的题目
+
+https://codeforces.com/problemset/problem/2009/G2
+LC3261 https://leetcode.cn/problems/count-substrings-that-satisfy-k-constraint-ii/ 子串的子串
+
 */
 
 /* 大模拟
+
 题单
 https://www.luogu.com.cn/training/3210#problems
 https://www.luogu.com.cn/training/126320#problems
 
 todo https://www.luogu.com.cn/problem/P1737
  https://www.luogu.com.cn/problem/P10396 https://www.luogu.com.cn/contest/116062
+
 */
