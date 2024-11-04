2151c1bc9f99c718cf3584f7e2c5a277442f3562
Endless <0x3f@0x3f3f3f3f.com>
2024-11-04 09:26:27 +0800

    补充注释

--- main/1700-1799/1793D.go
+++ main/1700-1799/1793D.go
@@ -26,8 +26,11 @@ func cf1793D(in io.Reader, out io.Writer) {
 	if i > j {
 		i, j = j, i
 	}
+	// 不含 1 的方案数
+	// 最后 +1 是指整个数组的方案数
 	ans := i*(i+1)/2 + (j-i-1)*(j-i)/2 + (n-j-1)*(n-j)/2 + 1
 	for v = 2; v <= n; v++ {
+		// 计算含 v-1 不含 v 的方案数
 		i, j = a[v], b[v]
 		if !(l1 < i && i < r1 || l2 < j && j < r2) {
 			l := -1
