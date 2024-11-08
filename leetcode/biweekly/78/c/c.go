192e8f13b969234a74ccb42a4720912d2a2fa129
Endless <0x3f@0x3f3f3f3f.com>
2024-11-08 13:47:12 +0800

    upd

--- leetcode/biweekly/78/c/c.go
+++ leetcode/biweekly/78/c/c.go
@@ -1,21 +1,20 @@
 package main
 
-import "sort"
+import "slices"
 
 // github.com/EndlessCheng/codeforces-go
 func maximumWhiteTiles(tiles [][]int, carpetLen int) (ans int) {
-	sort.Slice(tiles, func(i, j int) bool { return tiles[i][0] < tiles[j][0] })
+	slices.SortFunc(tiles, func(a, b []int) int { return a[0] - b[0] })
 	cover, left := 0, 0
-	for _, t := range tiles {
-		tl, tr := t[0], t[1]
+	for _, tile := range tiles {
+		tl, tr := tile[0], tile[1]
 		cover += tr - tl + 1
 		for tiles[left][1]+carpetLen-1 < tr {
 			cover -= tiles[left][1] - tiles[left][0] + 1
 			left++
 		}
-		ans = max(ans, cover-max(tr-carpetLen+1-tiles[left][0], 0)) // 0 表示毯子左端点不在瓷砖内的情况
+		uncover := max(tr-carpetLen+1-tiles[left][0], 0)
+		ans = max(ans, cover-uncover)
 	}
 	return
 }
-
-func max(a, b int) int { if b > a { return b }; return a }
