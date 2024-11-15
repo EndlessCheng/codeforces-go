04c36d2bd106872be779d4ab9df535a94c30100b
Endless <0x3f@0x3f3f3f3f.com>
2024-11-15 10:08:05 +0800

    CF666A

---
+++ main/600-699/666A.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"slices"
+)
+
+// https://github.com/EndlessCheng
+func cf666A(in io.Reader, _w io.Writer) {
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+	var s string
+	Fscan(in, &s)
+	ans := map[string]bool{}
+
+	n := len(s)
+	vis := make([][4]bool, n)
+	var dfs func(int, string)
+	dfs = func(i int, pre string) {
+		if i < 6 || vis[i][len(pre)] {
+			return
+		}
+		vis[i][len(pre)] = true
+
+		suf := s[i-1 : i+1]
+		if suf != pre {
+			ans[suf] = true
+			dfs(i-2, suf)
+		}
+
+		suf = s[i-2 : i+1]
+		if i > 6 && suf != pre {
+			ans[suf] = true
+			dfs(i-3, suf)
+		}
+	}
+	dfs(n-1, "")
+
+	suf := make([]string, 0, len(ans))
+	for k := range ans {
+		suf = append(suf, k)
+	}
+	slices.Sort(suf)
+	Fprintln(out, len(suf))
+	for _, v := range suf {
+		Fprintln(out, v)
+	}
+}
+
+//func main() { cf666A(bufio.NewReader(os.Stdin), os.Stdout) }
