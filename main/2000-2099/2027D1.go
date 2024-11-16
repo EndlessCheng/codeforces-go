0e52be3f04483e4502660eee0a6b05aab72b026f
Endless <0x3f@0x3f3f3f3f.com>
2024-11-16 20:30:55 +0800

    CF2027D1

---
+++ main/2000-2099/2027D1.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf2027D1(in io.Reader, out io.Writer) {
+	var T, n, m int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n, &m)
+		s := make([]int, n+1)
+		for i := 1; i <= n; i++ {
+			Fscan(in, &s[i])
+			s[i] += s[i-1]
+		}
+		b := make([]int, m)
+		for i := range b {
+			Fscan(in, &b[i])
+		}
+		f := make([]int, n+1)
+		for j := range n {
+			f[j] = 1e18
+		}
+		for i := m - 1; i >= 0; i-- {
+			k := n
+			for j := n - 1; j >= 0; j-- {
+				for s[k] > s[j]+b[i] {
+					k--
+				}
+				if k > j {
+					f[j] = min(f[j], f[k]+m-1-i)
+				}
+			}
+		}
+		ans := f[0]
+		if ans >= 1e18 {
+			ans = -1
+		}
+		Fprintln(out, ans)
+	}
+}
+
+//func main() { cf2027D1(bufio.NewReader(os.Stdin), os.Stdout) }
