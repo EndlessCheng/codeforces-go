ad1be3e26c3c1d2378e1aa907a39812efe8cf445
Endless <0x3f@0x3f3f3f3f.com>
2024-11-16 15:31:55 +0800

    CF2031D 前后缀分解+分组循环

---
+++ main/2000-2099/2031D.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf2031D(in io.Reader, _w io.Writer) {
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+	var T, n, mx int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n)
+		a := make([]int, n)
+		for i := range a {
+			Fscan(in, &a[i])
+		}
+
+		suf := make([]int, n)
+		suf[n-1] = a[n-1]
+		for i := n - 2; i >= 0; i-- {
+			suf[i] = min(suf[i+1], a[i])
+		}
+
+		for i := 0; i < n; {
+			st := i
+			mx = a[i]
+			for i++; i < n && mx > suf[i]; i++ {
+				mx = max(mx, a[i])
+			}
+			for ; st < i; st++ {
+				Fprint(out, mx, " ")
+			}
+		}
+		Fprintln(out)
+	}
+}
+
+//func main() { cf2031D(bufio.NewReader(os.Stdin), os.Stdout) }
