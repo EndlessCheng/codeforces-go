d0af8ababd230ca678cd89370aa4950e61b35dae
Endless <0x3f@0x3f3f3f3f.com>
2024-12-04 11:02:12 +0800

    CF2046B

---
+++ main/2000-2099/2046B.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"slices"
+)
+
+// https://github.com/EndlessCheng
+func cf2046B(in io.Reader, _w io.Writer) {
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+	var T, n, v int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n)
+		var a, b []int
+		minB := int(1e9)
+		for range n {
+			Fscan(in, &v)
+			for len(a) > 0 && v < a[len(a)-1] {
+				t := a[len(a)-1]
+				b = append(b, t+1)
+				minB = min(minB, t+1)
+				a = a[:len(a)-1]
+			}
+			if v > minB {
+				b = append(b, v+1)
+			} else {
+				a = append(a, v)
+			}
+		}
+		slices.Sort(b)
+		for _, v := range append(a, b...) {
+			Fprint(out, v, " ")
+		}
+		Fprintln(out)
+	}
+}
+
+//func main() { cf2046B(bufio.NewReader(os.Stdin), os.Stdout) }
