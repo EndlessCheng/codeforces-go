a37c3785416340695bbd3f0a63160588efe11557
Endless <0x3f@0x3f3f3f3f.com>
2024-12-12 08:44:53 +0800

    CF2020C 方法二

--- main/2000-2099/2020C.go
+++ main/2000-2099/2020C.go
@@ -11,26 +11,14 @@ func cf2020C(in io.Reader, _w io.Writer) {
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
 	var T, b, c, d int
-o:
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &b, &c, &d)
-		a := 0
-		for bit := 1 << 59; bit > 0; bit >>= 1 {
-			if d&bit == 0 {
-				if b&bit > 0 && c&bit == 0 {
-					Fprintln(out, -1)
-					continue o
-				}
-				a |= c & bit
-			} else if b&bit == 0 {
-				if c&bit > 0 {
-					Fprintln(out, -1)
-					continue o
-				}
-				a |= bit
-			}
+		a := b ^ d
+		if a|b-a&c == d {
+			Fprintln(out, a)
+		} else {
+			Fprintln(out, -1)
 		}
-		Fprintln(out, a)
 	}
 }
 
