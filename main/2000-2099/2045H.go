568d4943eb71330c7a89fe6a8b9737c7d7a3e5bb
Endless <0x3f@0x3f3f3f3f.com>
2024-12-19 10:21:00 +0800

    CF2045H

---
+++ main/2000-2099/2045H.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf2045H(in io.Reader, out io.Writer) {
+	var s string
+	Fscan(in, &s)
+	n := len(s)
+	lcp := make([][]int16, n+1)
+	for i := range lcp {
+		lcp[i] = make([]int16, n+1)
+	}
+	for i := n - 1; i >= 0; i-- {
+		for j := n - 1; j >= 0; j-- {
+			if s[i] == s[j] {
+				lcp[i][j] = lcp[i+1][j+1] + 1
+			}
+		}
+	}
+	less := func(l1, r1, l2, r2 int) bool {
+		len1, len2 := r1-l1, r2-l2
+		l := int(lcp[l1][l2])
+		if l >= min(len1, len2) {
+			return len1 < len2
+		}
+		return s[l1+l] < s[l2+l]
+	}
+
+	f := make([][]int16, n)
+	type pair struct{ f, k int16 }
+	sufMax := make([][]pair, n)
+	next := make([][]int16, n)
+	for i := range f {
+		f[i] = make([]int16, n)
+		sufMax[i] = make([]pair, n)
+		next[i] = make([]int16, n)
+	}
+	for i := n - 1; i >= 0; i-- {
+		f[i][n-1] = 1
+		p := pair{1, int16(n - 1)}
+		for j := n - 1; j >= i; j-- {
+			if less(i, j+1, j+1, n) {
+				l := min(int(lcp[i][j+1]), j-i+1)
+				f[i][j] = sufMax[j+1][j+1+l].f + 1
+				next[i][j] = sufMax[j+1][j+1+l].k
+				if f[i][j] > p.f {
+					p = pair{f[i][j], int16(j)}
+				}
+			} else {
+				next[i][j] = int16(n)
+			}
+			sufMax[i][j] = p
+		}
+	}
+	Fprintln(out, sufMax[0][0].f)
+	for i, j := 0, int(sufMax[0][0].k); i < n; i, j = j+1, int(next[i][j]) {
+		Fprintln(out, s[i:j+1])
+	}
+}
+
+//func main() { cf2045H(bufio.NewReader(os.Stdin), os.Stdout) }
