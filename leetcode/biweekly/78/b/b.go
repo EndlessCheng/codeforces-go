868df546fd91ad220cb8b64f3599e73aab2708d0
Endless <0x3f@0x3f3f3f3f.com>
2024-12-25 21:53:33 +0800

    重写题解

--- leetcode/biweekly/78/b/b.go
+++ leetcode/biweekly/78/b/b.go
@@ -2,11 +2,17 @@ package main
 
 // github.com/EndlessCheng/codeforces-go
 func waysToSplitArray(nums []int) (ans int) {
-	sum := 0
-	for _, v := range nums { sum += v }
-	for i, s := 0, 0; i < len(nums)-1; i++ {
-		s += nums[i]
-		if s*2 >= sum { ans++ }
+	total := 0
+	for _, x := range nums {
+		total += x
+	}
+
+	s := 0
+	for _, x := range nums[:len(nums)-1] {
+		s += x
+		if s*2 >= total {
+			ans++
+		}
 	}
 	return
 }
