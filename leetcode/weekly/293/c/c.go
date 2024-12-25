ee86de194941651efccf3108253af4da8e312282
Endless <0x3f@0x3f3f3f3f.com>
2024-12-25 19:45:49 +0800

    重写题解

--- leetcode/weekly/293/c/c.go
+++ leetcode/weekly/293/c/c.go
@@ -1,15 +1,29 @@
 package main
 
+import (
+	"math/bits"
+	"slices"
+)
+
 // github.com/EndlessCheng/codeforces-go
-func largestCombination(candidates []int) (ans int) {
-	for i := 0; i < 24; i++ {
-		s := 0
-		for _, v := range candidates {
-			s += v >> i & 1
+func largestCombination(candidates []int) int {
+	cnt := [24]int{}
+	for _, x := range candidates {
+		for s := uint(x); s > 0; s &= s - 1 {
+			cnt[bits.TrailingZeros(s)]++
 		}
-		ans = max(ans, s)
+	}
+	return slices.Max(cnt[:])
+}
+
+func largestCombination2(candidates []int) (ans int) {
+	m := bits.Len(uint(slices.Max(candidates)))
+	for i := range m {
+		cnt := 0
+		for _, x := range candidates {
+			cnt += x >> i & 1
+		}
+		ans = max(ans, cnt)
 	}
 	return
 }
-
-func max(a, b int) int { if b > a { return b }; return a }
