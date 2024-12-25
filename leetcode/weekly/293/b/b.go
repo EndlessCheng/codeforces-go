b8b2ae7393b7b057d53b10105496f02ec5cccb2d
Endless <0x3f@0x3f3f3f3f.com>
2024-12-25 21:21:15 +0800

    重写题解

--- leetcode/weekly/293/b/b.go
+++ leetcode/weekly/293/b/b.go
@@ -1,15 +1,23 @@
 package main
 
-import "sort"
+import "slices"
 
 // github.com/EndlessCheng/codeforces-go
-func maxConsecutive(bottom, top int, a []int) (ans int) {
-	a = append(a, bottom-1, top+1)
-	sort.Ints(a)
-	for i := 1; i < len(a); i++ {
-		ans = max(ans, a[i]-a[i-1]-1)
+func maxConsecutive(bottom, top int, special []int) int {
+	slices.Sort(special)
+	n := len(special)
+	ans := max(special[0]-bottom, top-special[n-1])
+	for i := 1; i < n; i++ {
+		ans = max(ans, special[i]-special[i-1]-1)
+	}
+	return ans
+}
+
+func maxConsecutive2(bottom, top int, special []int) (ans int) {
+	special = append(special, bottom-1, top+1)
+	slices.Sort(special)
+	for i := 1; i < len(special); i++ {
+		ans = max(ans, special[i]-special[i-1]-1)
 	}
 	return
 }
-
-func max(a, b int) int { if b > a { return b }; return a }
