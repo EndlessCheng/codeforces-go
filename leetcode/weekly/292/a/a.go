e6be56bb659e99afc5a99b5af1ec54d54791b6a4
Endless <0x3f@0x3f3f3f3f.com>
2024-12-27 16:28:27 +0800

    2264

--- leetcode/weekly/292/a/a.go
+++ leetcode/weekly/292/a/a.go
@@ -1,11 +1,47 @@
 package main
 
-import "strings"
+import (
+	"strings"
+)
 
 // github.com/EndlessCheng/codeforces-go
 func largestGoodInteger(num string) string {
-	for c := '9'; c >= '0'; c-- {
-		s := strings.Repeat(string(c), 3)
+	mx := byte(0)
+	cnt := 1
+	for i := 1; i < len(num); i++ {
+		d := num[i]
+		if d != num[i-1] {
+			cnt = 1
+			continue
+		}
+		cnt++
+		if cnt == 3 && d > mx {
+			mx = d
+		}
+	}
+	if mx == 0 {
+		return ""
+	}
+	return strings.Repeat(string(mx), 3)
+}
+
+func largestGoodInteger2(num string) string {
+	mx := byte(0)
+	for i := range len(num) - 2 {
+		d := num[i]
+		if d > mx && d == num[i+1] && d == num[i+2] {
+			mx = d
+		}
+	}
+	if mx == 0 {
+		return ""
+	}
+	return strings.Repeat(string(mx), 3)
+}
+
+func largestGoodInteger3(num string) string {
+	for d := '9'; d >= '0'; d-- {
+		s := strings.Repeat(string(d), 3)
 		if strings.Contains(num, s) {
 			return s
 		}
