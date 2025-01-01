74308dda3f29d684d1c9db4b1e147978838678e9
Endless <0x3f@0x3f3f3f3f.com>
2025-01-01 16:25:24 +0800

    添加题解

--- leetcode/biweekly/76/a/a.go
+++ leetcode/biweekly/76/a/a.go
@@ -1,4 +1,14 @@
 package main
 
 // github.com/EndlessCheng/codeforces-go
+func findClosestNumber(nums []int) int {
+	ans := nums[0]
+	for _, x := range nums {
+		if abs(x) < abs(ans) || abs(x) == abs(ans) && x > 0 {
+			ans = x
+		}
+	}
+	return ans
+}
 
+func abs(x int) int { if x < 0 { return -x }; return x }
