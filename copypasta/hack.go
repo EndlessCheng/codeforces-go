ebd2e16ec8e90ba051c5ae445fd4452c0d812776
Endless <0x3f@0x3f3f3f3f.com>
2025-01-06 09:26:05 +0800

    add hack

---
+++ copypasta/hack.go
@@ -0,0 +1,23 @@
+package copypasta
+
+/*
+我贡献的 LC hack 数据
+https://github.com/LeetCode-Feedback/LeetCode-Feedback/issues?q=is%3Aissue+author%3AEndlessCheng+is%3Aclosed
+
+hack 乘法溢出
+https://leetcode.cn/problems/frog-position-after-t-seconds/solutions/2281408/dfs-ji-yi-ci-you-qu-de-hack-by-endlessch-jtsr/
+
+用所有的偶数 hack，O(U^logU)
+https://leetcode.cn/problems/maximum-total-reward-using-operations-i/solutions/2805422/0-1-bei-bao-by-endlesscheng-702p/comments/2435993
+
+https://yukicoder.me/problems/no/2700
+
+LC2556
+https://leetcode.cn/circle/discuss/gXygF3/view/ftWmDG/
+考虑这样一个结构
+1 1 1
+1 0 1
+1 1 1
+……
+
+*/
