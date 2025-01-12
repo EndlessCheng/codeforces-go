94751abc9dd8875acd93ec7be3c6c7e27e62c315
Endless <0x3f@0x3f3f3f3f.com>
2025-01-12 09:23:22 +0800

    abc384_f

---
+++ misc/atcoder/abc384/f/f.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+// https://space.bilibili.com/206214
+func run(in io.Reader, out io.Writer) {
+	var n, ans int
+	Fscan(in, &n)
+	a := make([]int, n)
+	for i := range a {
+		Fscan(in, &a[i])
+		ans += a[i] / (a[i] & -a[i])
+	}
+	for m := 1; m <= 1<<24; m <<= 1 {
+		type pair struct{ c, s int }
+		cnt := map[int]pair{}
+		s := 0
+		for _, v := range a {
+			p := cnt[(m-v%m)%m]
+			s += p.c*v + p.s
+			p = cnt[v%m]
+			cnt[v%m] = pair{p.c + 1, p.s + v}
+		}
+		if m == 1 {
+			ans += s
+		} else {
+			ans -= s / m
+		}
+	}
+	Fprint(out, ans)
+}
+
+func main() { run(bufio.NewReader(os.Stdin), os.Stdout) }
