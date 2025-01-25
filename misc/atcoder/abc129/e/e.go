c1aed348ad0b2c991297e3adb0ef8d7e9e886157
Endless <0x3f@0x3f3f3f3f.com>
2025-01-25 13:49:42 +0800

    abc129_e

---
+++ misc/atcoder/abc129/e/e.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+)
+
+// https://github.com/EndlessCheng
+const mod = 1_000_000_007
+
+func run(in io.Reader, out io.Writer) {
+	const inv3 = 333333336
+	var s string
+	Fscan(in, &s)
+	ans := 0
+	pow2 := 1
+	pow3 := pow(3, len(s))
+	for _, b := range s {
+		pow3 = pow3 * inv3 % mod
+		if b == '1' {
+			ans = (ans + pow2*pow3) % mod
+			pow2 = pow2 * 2 % mod
+		}
+	}
+	Fprint(out, (ans+pow2)%mod)
+}
+
+func main() { run(bufio.NewReader(os.Stdin), os.Stdout) }
+
+func pow(x, n int) int {
+	res := 1
+	for ; n > 0; n /= 2 {
+		if n%2 > 0 {
+			res = res * x % mod
+		}
+		x = x * x % mod
+	}
+	return res
+}
