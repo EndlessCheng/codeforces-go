277a29732257fbff195ebe9fe3a95ec5b2d32a1a
Endless <0x3f@0x3f3f3f3f.com>
2025-02-06 05:24:00 +0800

    abc215_f 最大化最小值

---
+++ misc/atcoder/abc215/f/f.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+	"sort"
+)
+
+// https://github.com/EndlessCheng
+func run(in io.Reader, out io.Writer) {
+	var n int
+	Fscan(in, &n)
+	a := make([]struct{ x, y int }, n)
+	for i := range a {
+		Fscan(in, &a[i].x, &a[i].y)
+	}
+	sort.Slice(a, func(i, j int) bool { return a[i].x < a[j].x })
+	ans := sort.Search(1e9, func(low int) bool {
+		low++
+		mn, mx := int(1e9), 0
+		l := 0
+		for _, p := range a {
+			for a[l].x <= p.x-low {
+				mn = min(mn, a[l].y)
+				mx = max(mx, a[l].y)
+				l++
+			}
+			if p.y-mn >= low || mx-p.y >= low {
+				return false
+			}
+		}
+		return true
+	})
+	Fprint(out, ans)
+}
+
+func main() { run(bufio.NewReader(os.Stdin), os.Stdout) }
