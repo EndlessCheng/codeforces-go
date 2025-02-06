be37d36eb6947ef5dc7671f4de18d2d97db3e1df
Endless <0x3f@0x3f3f3f3f.com>
2025-02-06 14:39:56 +0800

    upd

--- copypasta/template/atcoder/generator_test.go
+++ copypasta/template/atcoder/generator_test.go
@@ -7,8 +7,6 @@ import (
 )
 
 // 仅限结束的比赛
-// https://atcoder.jp/contests/abc161/tasks/abc161_f
-// https://atcoder.jp/contests/abc161/submissions?f.LanguageName=Go&f.Status=AC&f.Task=abc161_f&orderBy=source_length
 func TestGenAtCoderProblemTemplate(t *testing.T) {
 	raw, err := os.ReadFile("data-atcoder.txt")
 	if err != nil {
