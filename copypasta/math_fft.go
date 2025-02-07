2145c71b41211d5402a2e9841d41501a8164a8e4
Endless <0x3f@0x3f3f3f3f.com>
2025-02-07 13:32:32 +0800

    add links

--- copypasta/math_fft.go
+++ copypasta/math_fft.go
@@ -10,14 +10,12 @@ https://en.wikipedia.org/wiki/Fast_Fourier_transform
 
 视频讲解 https://www.youtube.com/watch?v=h7apO7q16V0
 中文翻译 https://www.bilibili.com/video/BV1za411F76U/
-[Tutorial] FFT https://codeforces.com/blog/entry/111371
-一小时学会快速傅里叶变换 https://zhuanlan.zhihu.com/p/31584464
+代码参考：一小时学会快速傅里叶变换 https://zhuanlan.zhihu.com/p/31584464
+todo 优化 https://github.com/atcoder/ac-library/blob/master/atcoder/convolution.hpp
+https://www.luogu.com.cn/blog/105254/qian-tan-fft-zong-ft-dao-fft
 多项式基础：插值、函数逼近、快速傅里叶变换 (蒋炎岩) https://www.bilibili.com/video/BV1a14y1M7v1/
-为什么 FFT 可以加速卷积运算 https://www.zhihu.com/question/394657296/answer/2329522108
-傅里叶变换学习笔记 https://www.luogu.com.cn/blog/command-block/fft-xue-xi-bi-ji
 从多项式乘法到快速傅里叶变换 http://blog.miskcoo.com/2015/04/polynomial-multiplication-and-fast-fourier-transform
 整数乘法的长征 by EI https://www.cnblogs.com/Elegia/p/18020040/integer-multiplication
-优化技巧 https://www.luogu.com.cn/blog/105254/qian-tan-fft-zong-ft-dao-fft
 https://codeforces.com/blog/entry/43499 https://codeforces.com/blog/entry/48798
 https://oi-wiki.org/math/poly/fft/
 https://cp-algorithms.com/algebra/fft.html
@@ -26,12 +24,14 @@ https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/Polynomial.java.html
 用 FFT 做字符串匹配 https://zhuanlan.zhihu.com/p/267765026
 Arrow product: How to enumerate directed graphs https://codeforces.com/blog/entry/115617
 能否在 O(nlogn) 时间内实现大数的进制转换？ https://www.zhihu.com/question/395557989
+信息论的角度理解 https://www.zhihu.com/question/394657296/answer/2302858330
 
 todo https://github.com/OI-wiki/gitment/discussions/670#discussioncomment-4496021
  若多项式系数没有复数的话，可以构造多项式 H(x)=F(x)+G(x)i，把 F(x) 放实部，把 G(x) 放虚部，然后对 H(x) 跑一遍 DFT，之后把 H(x) 平方一下，可以得到
  H2(x)=F2(x)−G2(x)+2F(x)G(x)i
  可以发现它的虚部就是多项式的乘积，于是对 H2(x) 跑一遍 IDFT，之后把虚部取出来除以 2，得到的就是 F(x)G(x)
  这样只需要跑 2 次 FFT，常数更小一些（如果是对 F(x) 和 G(x) 分别跑 DFT，然后乘起来，再跑 IDFT 的话，需要跑 3 次 FFT）
+https://www.luogu.com.cn/blog/command-block/fft-xue-xi-bi-ji
 
 有关快速数论变换（NTT）以及多项式运算的内容见 math_ntt.go
 
@@ -60,25 +60,26 @@ func newFFT(n int) *fft {
 
 // 注：下面 swap 的代码，另一种写法是初始化每个 i 对应的 j https://blog.csdn.net/Flag_z/article/details/99163939
 func (t *fft) transform(a, omega []complex128) {
-	for i, j := 0, 0; i < t.n; i++ {
-		if i > j {
+	n := t.n
+	for i, j := 0, 0; i < n; i++ {
+		if i > j { // 保证同一对元素只交换一次
 			a[i], a[j] = a[j], a[i]
 		}
-		for l := t.n >> 1; ; l >>= 1 {
+		for l := n / 2; ; l /= 2 {
 			j ^= l
 			if j >= l {
 				break
 			}
 		}
 	}
-	for l := 2; l <= t.n; l <<= 1 {
-		m := l >> 1
-		for st := 0; st < t.n; st += l {
+	for l := 2; l <= n; l *= 2 {
+		m := l / 2
+		for st := 0; st < n; st += l {
 			b := a[st:]
-			for i := 0; i < m; i++ {
-				d := omega[t.n/l*i] * b[m+i]
-				b[m+i] = b[i] - d
-				b[i] += d
+			for i := range m {
+				v := omega[n/l*i] * b[m+i]
+				b[m+i] = b[i] - v
+				b[i] += v
 			}
 		}
 	}
