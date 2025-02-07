347bddcdeb5fddda4a0487d361b223cb6dea0e8d
Endless <0x3f@0x3f3f3f3f.com>
2025-02-07 08:39:14 +0800

    arc088_b 更简单的写法

--- misc/atcoder/arc088/b/b.go
+++ misc/atcoder/arc088/b/b.go
@@ -12,15 +12,13 @@ func run(in io.Reader, out io.Writer) {
 	s := ""
 	Fscan(in, &s)
 	n := len(s)
-	ans0, ans1 := n, n
-	for i, b := range s {
-		if b == '0' {
-			ans0 = min(ans0, max(n-1-i, i))
-		} else {
-			ans1 = min(ans1, max(n-1-i, i))
+	ans := n
+	for i := 1; i < n; i++ {
+		if s[i] != s[i-1] {
+			ans = min(ans, max(i, n-i))
 		}
 	}
-	Fprint(out, max(ans0, ans1))
+	Fprint(out, ans)
 }
 
 func main() { run(bufio.NewReader(os.Stdin), os.Stdout) }
