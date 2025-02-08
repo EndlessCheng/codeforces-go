25a46d2ed31b96ad8dda3d7b70a5d5ab82392cfb
Endless <0x3f@0x3f3f3f3f.com>
2025-02-08 21:01:47 +0800

    arc087_b x 和 y 分别计算 DP bitset

---
+++ misc/atcoder/arc087/b/b.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"math/big"
+	"os"
+	"strings"
+)
+
+// https://github.com/EndlessCheng
+func run(in io.Reader, out io.Writer) {
+	var s string
+	var x, y int
+	Fscan(in, &s, &x, &y)
+	a := strings.Split(s, "T")
+	f := func(a []string, i, x int) bool {
+		x = abs(x)
+		f := big.NewInt(1)
+		p := new(big.Int)
+		for ; i < len(a); i += 2 {
+			f.Or(f, p.Lsh(f, uint(len(a[i])*2)))
+			x += len(a[i])
+		}
+		return f.Bit(x) > 0
+	}
+	if f(a, 2, x-len(a[0])) && f(a, 1, y) {
+		Fprintln(out, "Yes")
+	} else {
+		Fprintln(out, "No")
+	}
+}
+
+func main() { run(bufio.NewReader(os.Stdin), os.Stdout) }
+func abs(x int) int { if x < 0 { return -x }; return x }
