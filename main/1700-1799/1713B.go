255cac59701a4152e761cc13607ebc3a27d53476
Endless <0x3f@0x3f3f3f3f.com>
2025-02-12 09:00:34 +0800

    cf1713B

---
+++ main/1700-1799/1713B.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+	"slices"
+)
+
+// https://github.com/EndlessCheng
+func cf1713B(in io.Reader, out io.Writer) {
+	var T, n int
+o:
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n)
+		a := make([]int, n)
+		for i := range a {
+			Fscan(in, &a[i])
+		}
+		a = slices.Compact(a)
+		for i := 1; i < n-1; i++ {
+			if a[i-1] > a[i] && a[i] < a[i+1] {
+				Fprintln(out, "NO")
+				continue o
+			}
+		}
+		Fprintln(out, "YES")
+	}
+}
+
+//func main() { cf1713B(bufio.NewReader(os.Stdin), os.Stdout) }
