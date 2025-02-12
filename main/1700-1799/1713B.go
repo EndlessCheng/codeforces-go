6ec15520e79b395ae379b699404c5dfde2ac110b
Endless <0x3f@0x3f3f3f3f.com>
2025-02-12 09:01:41 +0800

    cf1713B

--- main/1700-1799/1713B.go
+++ main/1700-1799/1713B.go
@@ -17,7 +17,7 @@ o:
 			Fscan(in, &a[i])
 		}
 		a = slices.Compact(a)
-		for i := 1; i < n-1; i++ {
+		for i := 1; i < len(a)-1; i++ {
 			if a[i-1] > a[i] && a[i] < a[i+1] {
 				Fprintln(out, "NO")
 				continue o
