a774492af655b9280eeb3e20d2323e438c94fb7f
Endless <0x3f@0x3f3f3f3f.com>
2025-02-15 20:21:09 +0800

    add links

---
+++ copypasta/top_tree.go
@@ -0,0 +1,20 @@
+package copypasta
+
+/*
+Top Tree 相关理论扯淡 https://www.cnblogs.com/ExplodingKonjac/p/17890636.html
+
+https://ei1333.github.io/library/graph/tree/static-top-tree.hpp.html
+https://atcoder.jp/contests/abc351/editorial/9899
+
+https://qoj.ac/contest/1434/problem/7855
+https://www.luogu.com.cn/problem/P4751
+https://codeforces.com/problemset/problem/1904/E
+https://www.luogu.com.cn/problem/P8498
+https://qoj.ac/contest/1382/problem/7563
+https://www.luogu.com.cn/problem/P5399
+https://www.luogu.com.cn/problem/P6778
+https://www.luogu.com.cn/problem/P8204
+
+https://www.luogu.com.cn/problem/P5649
+
+*/
