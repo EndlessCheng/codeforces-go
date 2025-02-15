73025e052e55280049962b48bf3bb52e79622b52
Endless <0x3f@0x3f3f3f3f.com>
2025-02-15 20:20:44 +0800

    add links

---
+++ copypasta/van_emde_boas.go
@@ -0,0 +1,10 @@
+package copypasta
+
+/*
+https://www.luogu.com.cn/blog/RuntimeErrror/ni-suo-fou-zhi-dao-di-shuo-ju-jie-gou-van-emde-boas-shu
+https://leetcode.cn/problems/online-majority-element-in-subarray/solutions/1594528/by-hqztrue-yqh1/
+https://codeforces.com/blog/entry/101663 E 题代码
+
+适用于 key 在 [0,n-1] 的范围，做到增删改查 O(loglogn)
+
+*/
