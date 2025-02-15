082888314dbc41c0369310bb320e11b4e7d0dcb5
Endless <0x3f@0x3f3f3f3f.com>
2025-02-15 20:20:03 +0800

    add links

---
+++ copypasta/linked_list.go
@@ -0,0 +1,17 @@
+package copypasta
+
+/* 链表
+https://en.wikipedia.org/wiki/Linked_list
+
+链表 + 哈希表
+https://ac.nowcoder.com/acm/contest/74362/D
+
+LC142. 环形链表 II
+- https://codeforces.com/contest/1137/problem/D 2400
+
+十字链表
+https://codeforces.com/problemset/problem/706/E
+
+数组实现的双向链表见 monotone_stack.go 中的 permLR
+https://codeforces.com/contest/1922/problem/D
+*/
