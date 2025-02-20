51bc4dd0a8e124e1d9c86b6f3562801d03dd406a
Endless <0x3f@0x3f3f3f3f.com>
2025-02-20 11:35:43 +0800

    重写题解

--- leetcode/weekly/288/d/d.go
+++ leetcode/weekly/288/d/d.go
@@ -1,41 +1,49 @@
 package main
 
-import "sort"
+import "slices"
 
-// github.com/EndlessCheng/codeforces-go
+// https://space.bilibili.com/206214
 func maximumBeauty(flowers []int, newFlowers int64, target, full, partial int) int64 {
-	sort.Ints(flowers)
 	n := len(flowers)
-	if flowers[0] >= target { // 剪枝，此时所有花园都是完善的
-		return int64(n * full)
+
+	// 如果全部种满，还剩下多少朵花？
+	leftFlowers := int(newFlowers) - target*n // 先减掉
+	for i, flower := range flowers {
+		flowers[i] = min(flower, target)
+		leftFlowers += flowers[i] // 把已有的加回来
 	}
 
-	leftFlowers := int(newFlowers) - target*n // 剩余可以种植的花
-	for i, f := range flowers {
-		flowers[i] = min(f, target)
-		leftFlowers += flowers[i]
+	// 没有种花，所有花园都已种满
+	if leftFlowers == int(newFlowers) {
+		return int64(n * full) // 答案只能是 n*full（注意不能减少花的数量）
 	}
 
-	ans := 0
-	for i, x, sumFlowers := 0, 0, 0; i <= n; i++ { // 枚举后缀长度 n-i
-		if leftFlowers >= 0 {
-			// 计算最长前缀的长度
-			for ; x < i && flowers[x]*x-sumFlowers <= leftFlowers; x++ {
-				sumFlowers += flowers[x] // 注意 x 只增不减，这部分的时间复杂度为 O(n)
-			}
-			// 计算总美丽值
-			beauty := (n - i) * full
-			if x > 0 {
-				beauty += min((leftFlowers+sumFlowers)/x, target-1) * partial
-			}
-			ans = max(ans, beauty)
+	// 可以全部种满
+	if leftFlowers >= 0 {
+		// 两种策略取最大值：留一个花园种 target-1 朵花，其余种满；或者，全部种满
+		return int64(max((target-1)*partial+(n-1)*full, n*full))
+	}
+
+	slices.Sort(flowers) // 时间复杂度的瓶颈在这，尽量写在后面
+
+	var ans, preSum, j int
+	// 枚举 i，表示后缀 [i,n-1] 种满（i=0 的情况上面已讨论）
+	for i := 1; i <= n; i++ {
+		// 撤销，flowers[i-1] 不变成 target
+		leftFlowers += target - flowers[i-1]
+		if leftFlowers < 0 { // 花不能为负数，需要继续撤销
+			continue
 		}
-		if i < n {
-			leftFlowers += target - flowers[i]
+		// 满足以下条件说明 [0,j] 都可以种 flowers[j] 朵花
+		for j < i && flowers[j]*j <= preSum+leftFlowers {
+			preSum += flowers[j]
+			j++
 		}
+		// 计算总美丽值
+		// 在前缀 [0,j-1] 中均匀种花，这样最小值最大
+		avg := (leftFlowers + preSum) / j // 由于上面特判了，这里 avg 一定小于 target
+		totalBeauty := avg*partial + (n-i)*full
+		ans = max(ans, totalBeauty)
 	}
 	return int64(ans)
 }
-
-func min(a, b int) int { if a > b { return b }; return a }
-func max(a, b int) int { if a < b { return b }; return a }
