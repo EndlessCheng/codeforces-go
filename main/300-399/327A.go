3d7c0897cca75dc0930a9e22e1d3ba4b5303e169
Endless <0x3f@0x3f3f3f3f.com>
2025-02-21 16:18:17 +0800

    cf327A

---
+++ main/300-399/327A.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf327A(in io.Reader, out io.Writer) {
+	var n, v, f0, f1 int
+	f2 := int(-1e9)
+	Fscan(in, &n)
+	for range n {
+		Fscan(in, &v)
+		f2 = max(f2, f1) + v
+		f1 = max(f1, f0) + 1 - v
+		f0 += v
+	}
+	Fprint(out, max(f1, f2))
+}
+
+//func main() { cf327A(os.Stdin, os.Stdout) }
