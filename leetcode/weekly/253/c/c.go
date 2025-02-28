00cbc197677f163ebdf189075d0824cf747575ba
Endless <0x3f@0x3f3f3f3f.com>
2025-02-28 15:33:02 +0800

    补充题解

--- leetcode/weekly/253/c/c.go
+++ leetcode/weekly/253/c/c.go
@@ -1,17 +1,14 @@
 package main
 
 // github.com/EndlessCheng/codeforces-go
-func minSwaps(s string) (ans int) {
+func minSwaps(s string) int {
 	c := 0
 	for _, b := range s {
-		if b == '[' {
+		if b == '[' || c == 0 {
 			c++
-		} else if c > 0 {
-			c--
 		} else {
-			c++ // 把最后面的 [ 和 ] 交换
-			ans++
+			c--
 		}
 	}
-	return
+	return c / 2
 }
