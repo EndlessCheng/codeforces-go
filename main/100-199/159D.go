589c2ca5acf5fda083ddff85e3d11ce41e1567c8
Endless <0x3f@0x3f3f3f3f.com>
2025-03-04 10:50:54 +0800

    O(n) 做法

--- main/100-199/159D.go
+++ main/100-199/159D.go
@@ -7,32 +7,57 @@ import (
 
 // https://github.com/EndlessCheng
 func cf159D(in io.Reader, out io.Writer) {
-	var s string
+	var s []byte
 	Fscan(in, &s)
-
 	n := len(s)
-	sum := make([]int, n)
-	for i := range 2*n - 1 {
-		l, r := i/2, (i+1)/2
-		for l >= 0 && r < n && s[l] == s[r] {
-			sum[r]++
-			l--
-			r++
+	t := append(make([]byte, 0, n*2+3), '^')
+	for _, c := range s {
+		t = append(t, '#', c)
+	}
+	t = append(t, '#', '$')
+	halfLen := make([]int, len(t)-2)
+	halfLen[1] = 1
+	boxM, boxR := 0, 0
+	for i := 2; i < len(halfLen); i++ {
+		hl := 1
+		if i < boxR {
+			hl = min(halfLen[boxM*2-i], boxR-i)
 		}
+		for t[i-hl] == t[i+hl] {
+			hl++
+			boxM, boxR = i, i+hl
+		}
+		halfLen[i] = hl
 	}
 
+	diff := make([]int, n+1)
+	for i, hl := range halfLen {
+		l, r := (i-hl)/2, (i+hl)/2-2
+		if r < l {
+			continue
+		}
+		diff[(l+r+1)/2]++
+		diff[r+1]--
+	}
+	cntEnd := diff[:n]
 	for i := 1; i < n; i++ {
-		sum[i] += sum[i-1]
+		cntEnd[i] += cntEnd[i-1]
+	}
+	for i := 1; i < n; i++ {
+		cntEnd[i] += cntEnd[i-1]
 	}
 
+	sum := make([]int, n+1)
+	for i, v := range cntEnd {
+		sum[i+1] = sum[i] + v
+	}
 	ans := 0
-	for i := range 2*n - 1 {
-		l, r := i/2, (i+1)/2
-		for l > 0 && r < n && s[l] == s[r] {
-			ans += sum[l-1]
-			l--
-			r++
+	for i, hl := range halfLen {
+		l, r := (i-hl)/2, (i+hl)/2-2
+		if r < l {
+			continue
 		}
+		ans += sum[(l+r)/2] - sum[max(l-1, 0)]
 	}
 	Fprint(out, ans)
 }
