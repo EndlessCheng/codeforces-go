a29f6c9adf90aeaea7f28830705c8a401324f988
Endless <0x3f@0x3f3f3f3f.com>
2025-03-08 13:24:15 +0800

    CF1827C

--- main/1800-1899/1827C.go
+++ main/1800-1899/1827C.go
@@ -1,54 +1,47 @@
 package main
 
 import (
-	"bufio"
 	. "fmt"
 	"io"
 )
 
 // https://space.bilibili.com/206214
-func CF1827C(_r io.Reader, _w io.Writer) {
-	in := bufio.NewReader(_r)
-	out := bufio.NewWriter(_w)
-	defer out.Flush()
-	min := func(a, b int) int {
-		if a > b {
-			return b
-		}
-		return a
-	}
-
+func CF1827C(in io.Reader, out io.Writer) {
 	var T, n int
 	var s string
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &n, &s)
-		s = "^" + s + "$"
-		n += 2
-		h := make([]int, n)
-		f := make([]int, n)
-		t := []int{}
-		S := int64(0)
-		for i, m := 1, 0; i+1 < n; i++ {
-			if h[m]+m > i {
-				h[i] = min(h[m]+m-i, h[m*2-i])
+		var ans, boxM, boxR int
+		halfLen := make([]int, n-1)
+		f := make([]int, n+1)
+		st := []int{}
+		for i := range halfLen {
+			hl := 0
+			if i < boxR {
+				hl = min(halfLen[boxM*2-i], boxR-i)
 			}
-			for s[i+h[i]+1] == s[i-h[i]] {
-				h[i]++
-				m = i
+			for i >= hl && i+hl+1 < n && s[i-hl] == s[i+hl+1] {
+				hl++
+				boxM, boxR = i, i+hl
 			}
-			t = append(t, i-1)
-			for len(t) > 0 {
-				m := t[len(t)-1]
-				if h[m]+m >= i {
-					f[i] = f[m*2-i] + 1
-					break
+			r := i + 1
+			for len(st) > 0 && st[len(st)-1]+halfLen[st[len(st)-1]] < r {
+				st = st[:len(st)-1]
+			}
+			if hl > 0 {
+				for len(st) > 0 && halfLen[st[len(st)-1]] <= hl {
+					st = st[:len(st)-1]
 				}
-				t = t[:len(t)-1]
+				st = append(st, i)
+				halfLen[i] = hl
 			}
-			S += int64(f[i])
+			if len(st) > 0 {
+				f[r+1] = f[st[len(st)-1]*2+1-r] + 1
+			}
+			ans += f[r+1]
 		}
-		Fprintln(out, S)
+		Fprintln(out, ans)
 	}
 }
 
-//func main() { CF1827C(os.Stdin, os.Stdout) }
+//func main() { CF1827C(bufio.NewReader(os.Stdin), os.Stdout) }
