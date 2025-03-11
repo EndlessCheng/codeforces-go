7afaf80f48f677d49e82a339fbba9e9efdb3a21d
Endless <0x3f@0x3f3f3f3f.com>
2025-03-11 10:37:03 +0800

    p3373

--- misc/luogu/p3373.go
+++ misc/luogu/p3373.go
@@ -4,21 +4,59 @@ import (
 	"bufio"
 	. "fmt"
 	"io"
+	"math/bits"
+	"os"
 )
 
-// github.com/EndlessCheng/codeforces-go
-const mod3373 int = 571373
+// https://space.bilibili.com/206214
+const mod3373 = 571373
 
-type seg3373 []struct{ l, r, mulTodo, addTodo, s int }
+type pair3373 struct{ k, c int }
+
+type seg3373 []struct {
+	l, r int
+	sum  int
+	todo pair3373
+}
+
+func (seg3373) mergeInfo(a, b int) int {
+	return (a + b) % mod3373
+}
+
+var todoInit3373 = pair3373{1, 0}
+
+func (seg3373) mergeTodo(f, old pair3373) pair3373 {
+	return pair3373{f.k * old.k % mod3373, (f.k*old.c + f.c) % mod3373}
+}
+
+func (t seg3373) apply(o int, f pair3373) {
+	cur := &t[o]
+
+	sz := cur.r - cur.l + 1
+	cur.sum = (f.k*cur.sum + f.c*sz) % mod3373
+
+	cur.todo = t.mergeTodo(f, cur.todo)
+}
 
 func (t seg3373) maintain(o int) {
-	t[o].s = (t[o<<1].s + t[o<<1|1].s) % mod3373
+	t[o].sum = t.mergeInfo(t[o<<1].sum, t[o<<1|1].sum)
+}
+
+func (t seg3373) spread(o int) {
+	f := t[o].todo
+	if f == todoInit3373 {
+		return
+	}
+	t.apply(o<<1, f)
+	t.apply(o<<1|1, f)
+	t[o].todo = todoInit3373
 }
 
 func (t seg3373) build(a []int, o, l, r int) {
-	t[o].l, t[o].r, t[o].mulTodo = l, r, 1
+	t[o].l, t[o].r = l, r
+	t[o].todo = todoInit3373
 	if l == r {
-		t[o].s = a[l-1]
+		t[o].sum = a[l]
 		return
 	}
 	m := (l + r) >> 1
@@ -27,53 +65,25 @@ func (t seg3373) build(a []int, o, l, r int) {
 	t.maintain(o)
 }
 
-func (t seg3373) doMul(o, v int) {
-	to := &t[o]
-	to.mulTodo = to.mulTodo * v % mod3373
-	to.addTodo = to.addTodo * v % mod3373
-	to.s = to.s * v % mod3373
-}
-func (t seg3373) doAdd(o, v int) {
-	to := &t[o]
-	to.addTodo = (to.addTodo + v) % mod3373
-	to.s = (to.s + (to.r-to.l+1)*v) % mod3373
-}
-func (t seg3373) spread(o int) {
-	if v := t[o].mulTodo; v != 1 {
-		t.doMul(o<<1, v)
-		t.doMul(o<<1|1, v)
-		t[o].mulTodo = 1
-	}
-	if v := t[o].addTodo; v != 0 {
-		t.doAdd(o<<1, v)
-		t.doAdd(o<<1|1, v)
-		t[o].addTodo = 0
-	}
-}
-
-func (t seg3373) update(o, l, r, v int, mul bool) {
+func (t seg3373) update(o, l, r int, f pair3373) {
 	if l <= t[o].l && t[o].r <= r {
-		if mul {
-			t.doMul(o, v)
-		} else {
-			t.doAdd(o, v)
-		}
+		t.apply(o, f)
 		return
 	}
 	t.spread(o)
 	m := (t[o].l + t[o].r) >> 1
 	if l <= m {
-		t.update(o<<1, l, r, v, mul)
+		t.update(o<<1, l, r, f)
 	}
 	if m < r {
-		t.update(o<<1|1, l, r, v, mul)
+		t.update(o<<1|1, l, r, f)
 	}
 	t.maintain(o)
 }
 
 func (t seg3373) query(o, l, r int) int {
 	if l <= t[o].l && t[o].r <= r {
-		return t[o].s
+		return t[o].sum
 	}
 	t.spread(o)
 	m := (t[o].l + t[o].r) >> 1
@@ -83,30 +93,34 @@ func (t seg3373) query(o, l, r int) int {
 	if l > m {
 		return t.query(o<<1|1, l, r)
 	}
-	return (t.query(o<<1, l, r) + t.query(o<<1|1, l, r)) % mod3373
+	return t.mergeInfo(t.query(o<<1, l, r), t.query(o<<1|1, l, r))
 }
 
-func p3373(_r io.Reader, _w io.Writer) {
-	in := bufio.NewReader(_r)
+func p3373(in io.Reader, _w io.Writer) {
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
-
-	var ignore, n, q, op, l, r, v int
-	Fscan(in, &n, &q, &ignore)
+	var n, q, op, l, r, k int
+	Fscan(in, &n, &q, &l)
 	a := make([]int, n)
 	for i := range a {
 		Fscan(in, &a[i])
 	}
-	t := make(seg3373, 4*n)
-	t.build(a, 1, 1, n)
+	t := make(seg3373, 2<<bits.Len(uint(n-1)))
+	t.build(a, 1, 0, n-1)
 	for ; q > 0; q-- {
-		if Fscan(in, &op, &l, &r); op < 3 {
-			Fscan(in, &v)
-			t.update(1, l, r, v, op == 1)
+		Fscan(in, &op, &l, &r)
+		l--
+		r--
+		if op == 1 {
+			Fscan(in, &k)
+			t.update(1, l, r, pair3373{k, 0})
+		} else if op == 2 {
+			Fscan(in, &k)
+			t.update(1, l, r, pair3373{1, k})
 		} else {
 			Fprintln(out, t.query(1, l, r))
 		}
 	}
 }
 
-//func main() { p3373(os.Stdin, os.Stdout) }
+func main() { p3373(bufio.NewReader(os.Stdin), os.Stdout) }
