53ce5149852d1e73a2555b05e394a81e2218534f
Endless <0x3f@0x3f3f3f3f.com>
2025-03-12 10:43:38 +0800

    upd

--- copypasta/link_cut_tree.go
+++ copypasta/link_cut_tree.go
@@ -20,8 +20,8 @@ TIPS: 对于卡常的题目，用 findRoot 判断连通性的逻辑可以用并�
 模板题 https://www.luogu.com.cn/problem/P3690
 魔法森林（维护最大边权）https://www.luogu.com.cn/problem/P2387 AC 代码 https://www.luogu.com.cn/record/46975435 去掉 link 和 cut 的多余判断后 https://www.luogu.com.cn/record/46977629
 最小差值生成树 https://www.luogu.com.cn/problem/P4234 https://codeforces.com/edu/course/2/lesson/7/2/practice/contest/289391/problem/F
-todo https://ac.nowcoder.com/acm/contest/4643/F 题解 https://ac.nowcoder.com/discuss/387703
- 动态修改 + 任意两点 LCA 的权值的期望 https://codeforces.com/problemset/problem/482/E
+https://codeforces.com/problemset/problem/482/E 3200 动态修改 + 任意两点 LCA 的权值的期望
+https://ac.nowcoder.com/acm/contest/4643/F 题解 https://ac.nowcoder.com/discuss/387703
 */
 
 type lctNode struct {
