b4d44b10ff208f6b3221311dec6e3652e198ffab
Endless <0x3f@0x3f3f3f3f.com>
2025-03-16 22:31:29 +0800

    添加题解

--- leetcode/weekly/241/a/a.go
+++ leetcode/weekly/241/a/a.go
@@ -1,27 +1,10 @@
 package main
 
-import "math/bits"
-
 // github.com/EndlessCheng/codeforces-go
-func subsetXORSum(a []int) (ans int) {
-	for sub := uint(0); sub < 1<<len(a); sub++ {
-		xor := 0
-		for s := sub; s > 0; s &= s - 1 {
-			xor ^= a[bits.TrailingZeros(s)]
-		}
-		ans += xor
+func subsetXORSum(nums []int) int {
+	or := 0
+	for _, x := range nums {
+		or |= x
 	}
-	return
-}
-
-// github.com/EndlessCheng/codeforces-go
-func subsetXORSum2(a []int) (ans int) {
-	sum := make([]int, 1<<len(a))
-	for p, v := range a {
-		for s := 0; s < 1<<p; s++ {
-			sum[1<<p|s] = sum[s] ^ v
-			ans += sum[1<<p|s]
-		}
-	}
-	return
+	return or << (len(nums) - 1)
 }
