d94850ec8d6661844c335f1f38a4079e51f010f0
Endless <0x3f@0x3f3f3f3f.com>
2025-03-17 09:46:48 +0800

    重写题解

--- leetcode/weekly/276/c/c.go
+++ leetcode/weekly/276/c/c.go
@@ -1,18 +1,31 @@
 package main
 
+import "slices"
+
 // github.com/EndlessCheng/codeforces-go
 func mostPoints(questions [][]int) int64 {
 	n := len(questions)
 	f := make([]int, n+1)
-	for i, q := range questions {
-		f[i+1] = max(f[i+1], f[i])
-		j := i + q[1] + 1
-		if j > n {
-			j = n
-		}
-		f[j] = max(f[j], f[i]+q[0])
+	for i, q := range slices.Backward(questions) {
+		j := min(i+q[1]+1, n)
+		f[i] = max(f[i+1], f[j]+q[0])
 	}
-	return int64(f[n])
+	return int64(f[0])
 }
 
-func max(a, b int) int { if b > a { return b }; return a }
+func mostPoints1(questions [][]int) int64 {
+	n := len(questions)
+	memo := make([]int, n)
+	var dfs func(int) int
+	dfs = func(i int) int {
+		if i >= n {
+			return 0
+		}
+		p := &memo[i]
+		if *p == 0 {
+			*p = max(dfs(i+1), dfs(i+questions[i][1]+1)+questions[i][0])
+		}
+		return *p
+	}
+	return int64(dfs(0))
+}
