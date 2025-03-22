cb810e26b204232a096973a66a94eaf05b37e5c4
Endless <0x3f@0x3f3f3f3f.com>
2025-03-22 15:40:35 +0800

    p2152

---
+++ misc/luogu/p2152.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+	"math/big"
+)
+
+// https://space.bilibili.com/206214
+func p2152(in io.Reader, out io.Writer) {
+	a, b := new(big.Int), new(big.Int)
+	Fscan(in, a, b)
+	Fprint(out, a.GCD(nil, nil, a, b))
+}
+
+//func main() { p2152(bufio.NewReader(os.Stdin), os.Stdout) }
