60037c610bb9f2d49d0f09890afd8e11731c18a4
Endless <0x3f@0x3f3f3f3f.com>
2025-03-24 15:07:31 +0800

    abc398_d

---
+++ misc/atcoder/abc398/d/d.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func run(in io.Reader, out io.Writer) {
+	dir4 := []struct{ x, y int }{'W': {1, 0}, 'E': {-1, 0}, 'S': {0, -1}, 'N': {0, 1}}
+	type pair struct{ x, y int }
+	var man, fire pair
+	var s []byte
+	Fscan(in, &s, &man.y, &man.x, &s)
+	has := map[pair]bool{fire: true}
+	for i, b := range s {
+		d := dir4[b]
+		man.x += d.x
+		man.y += d.y
+		fire.x += d.x
+		fire.y += d.y
+		has[fire] = true
+		if has[man] {
+			s[i] = '1'
+		} else {
+			s[i] = '0'
+		}
+	}
+	Fprintf(out, "%s", s)
+}
+
+//func main() { run(bufio.NewReader(os.Stdin), os.Stdout) }
