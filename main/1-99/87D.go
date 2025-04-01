ed95b2f8976ac1a3ef9326e23966c9bac12bfef8
Endless <0x3f@0x3f3f3f3f.com>
2025-04-01 11:17:09 +0800

    cf87D fix2

--- main/1-99/87D.go
+++ main/1-99/87D.go
@@ -1,25 +1,51 @@
 package main
 
 import (
+	"bufio"
+	"cmp"
 	. "fmt"
 	"io"
 	"slices"
 )
 
 // https://github.com/EndlessCheng
-func cf87D(in io.Reader, out io.Writer) {
+func cf87D(in io.Reader, _w io.Writer) {
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
 	var n int
 	Fscan(in, &n)
-	type edge struct{ v, w, wt, i int }
-	g := make([]edge, n-1)
-	for i := range g {
-		Fscan(in, &g[i].v, &g[i].w, &g[i].wt)
-		g[i].i = i + 1
+	type edge struct{ v, w, wt, dep, i int }
+	es := make([]edge, n-1)
+	g := make([][]int, n)
+	for i := range es {
+		var v, w, wt int
+		Fscan(in, &v, &w, &wt)
+		v--
+		w--
+		es[i] = edge{v, w, wt, 0, i + 1}
+		g[v] = append(g[v], w)
+		g[w] = append(g[w], v)
 	}
-	slices.SortFunc(g, func(a, b edge) int { return a.wt - b.wt })
 
-	fa := make([]int, n+1)
-	sz := make([]int, n+1)
+	dep := make([]int, n)
+	var dfs func(int, int)
+	dfs = func(v, fa int) {
+		for _, w := range g[v] {
+			if w != fa {
+				dep[w] = dep[v] + 1
+				dfs(w, v)
+			}
+		}
+	}
+	dfs(0, -1)
+
+	for i, e := range es {
+		es[i].dep = min(dep[e.v], dep[e.w])
+	}
+	slices.SortFunc(es, func(a, b edge) int { return cmp.Or(a.wt-b.wt, b.dep-a.dep) })
+
+	fa := make([]int, n)
+	sz := make([]int, n)
 	for i := range fa {
 		fa[i] = i
 		sz[i] = 1
@@ -36,21 +62,38 @@ func cf87D(in io.Reader, out io.Writer) {
 	}
 
 	mx := 0
-	ans := []any{}
-	for _, e := range g {
-		v, w := find(e.v), find(e.w)
-		s := sz[v] * sz[w]
-		if s > mx {
-			mx = s
-			ans = []any{e.i}
-		} else if s == mx {
-			ans = append(ans, e.i)
+	ans := []int{}
+	tmp := make([]int, n)
+	for i := 0; i < n-1; {
+		st := i
+		wt := es[st].wt
+		for ; i < n-1 && es[i].wt == wt; i++ {
+			v, w := es[i].v, es[i].w
+			if dep[v] > dep[w] {
+				v, w = w, v
+			}
+			tmp[i] = sz[w]
+			fv := find(v)
+			fa[w] = fv
+			sz[fv] += sz[w]
 		}
-		fa[v] = w
-		sz[w] += sz[v]
+		for ; st < i; st++ {
+			e := es[st]
+			fv := find(e.v)
+			s := tmp[st] * (sz[fv] - tmp[st])
+			if s > mx {
+				mx = s
+				ans = []int{e.i}
+			} else if s == mx {
+				ans = append(ans, e.i)
+			}
+		}
 	}
 	Fprintln(out, mx*2, len(ans))
-	Fprintln(out, ans...)
+	slices.Sort(ans)
+	for _, v := range ans {
+		Fprint(out, v, " ")
+	}
 }
 
 //func main() { cf87D(bufio.NewReader(os.Stdin), os.Stdout) }
