8564ff78fef8ed161d72801693cdfeca5fd92fb5
Endless <0x3f@0x3f3f3f3f.com>
2025-04-03 15:22:03 +0800

    abc391_g DP 套 DP

--- misc/atcoder/abc391/g/g.go
+++ misc/atcoder/abc391/g/g.go
@@ -13,29 +13,35 @@ func run(in io.Reader, out io.Writer) {
 	var n, m int
 	var s []byte
 	Fscan(in, &n, &m, &s)
-	f := make([][]int, m+1)
-	for i := range f {
-		f[i] = make([]int, 1<<n)
-	}
-	f[0][0] = 1
-	for i := range m {
-		for diff, fv := range f[i] {
-			for c := byte('a'); c <= 'z'; c++ {
-				var lcs, newLcs, newDiff int
-				for j, b := range s {
-					one := diff >> j & 1
-					pre := newLcs
-					newLcs = max(newLcs, lcs+(one|b2i(b == c)))
-					newDiff |= (newLcs - pre) << j
-					lcs += one
-				}
-				f[i+1][newDiff] = (f[i+1][newDiff] + fv) % mod
+	nxt := make([][26]int, 1<<n)
+	for diff := range nxt {
+		for c := byte('a'); c <= 'z'; c++ {
+			var lcs, newLcs, newDiff int
+			for j, b := range s {
+				one := diff >> j & 1
+				pre := newLcs
+				newLcs = max(newLcs, lcs+(one|b2i(b == c)))
+				newDiff |= (newLcs - pre) << j
+				lcs += one
 			}
+			nxt[diff][c-'a'] = newDiff
 		}
 	}
 
+	f := make([]int, 1<<n)
+	f[0] = 1
+	for i := 0; i < m; i++ {
+		nf := make([]int, 1<<n)
+		for diff, fv := range f {
+			for _, newDiff := range nxt[diff] {
+				nf[newDiff] = (nf[newDiff] + fv) % mod
+			}
+		}
+		f = nf
+	}
+
 	ans := make([]int, n+1)
-	for diff, fv := range f[m] {
+	for diff, fv := range f {
 		ans[bits.OnesCount(uint(diff))] += fv
 	}
 	for _, v := range ans {
