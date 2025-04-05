ddca519c9aad479798e48a24e4a17f8ab6a9fba1
Endless <0x3f@0x3f3f3f3f.com>
2025-04-05 15:06:12 +0800

    cf319C

--- main/300-399/319C.go
+++ main/300-399/319C.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"bufio"
 	. "fmt"
 	"io"
+	"math/big"
 )
 
-// github.com/EndlessCheng/codeforces-go
-func CF319C(_r io.Reader, out io.Writer) {
-	in := bufio.NewReader(_r)
+// https://github.com/EndlessCheng
+type vec19 struct{ x, y int }
+
+func (a vec19) sub(b vec19) vec19 { return vec19{a.x - b.x, a.y - b.y} }
+func (a vec19) dot(b vec19) int   { return a.x*b.x + a.y*b.y }
+func (a vec19) det(b vec19) bool {
+	v := new(big.Int).Mul(big.NewInt(int64(a.x)), big.NewInt(int64(b.y)))
+	w := new(big.Int).Mul(big.NewInt(int64(a.y)), big.NewInt(int64(b.x)))
+	return v.Cmp(w) <= 0
+}
+
+func cf319C(in io.Reader, out io.Writer) {
 	var n int
 	Fscan(in, &n)
 	a := make([]int, n)
@@ -19,20 +28,21 @@ func CF319C(_r io.Reader, out io.Writer) {
 	for i := range b {
 		Fscan(in, &b[i])
 	}
-	dp := make([]int64, n)
-	slope := func(i, j int) float64 { return float64(dp[j]-dp[i]) / float64(b[i]-b[j]) }
-	q := []int{0}
+
+	q := []vec19{{-b[0], 0}}
 	for i := 1; i < n; i++ {
-		for len(q) > 1 && slope(q[0], q[1]) < float64(a[i]) {
+		p := vec19{-a[i], 1}
+		for len(q) > 1 && p.dot(q[0]) >= p.dot(q[1]) {
 			q = q[1:]
 		}
-		dp[i] = dp[q[0]] + int64(b[q[0]])*int64(a[i])
-		for len(q) > 1 && slope(q[len(q)-1], i) < slope(q[len(q)-2], q[len(q)-1]) {
+		f := p.dot(q[0])
+		v := vec19{-b[i], f}
+		for len(q) > 1 && q[len(q)-1].sub(q[len(q)-2]).det(v.sub(q[len(q)-1])) {
 			q = q[:len(q)-1]
 		}
-		q = append(q, i)
+		q = append(q, v)
 	}
-	Fprint(out, dp[n-1])
+	Fprint(out, q[len(q)-1].y)
 }
 
-//func main() { CF319C(os.Stdin, os.Stdout) }
+//func main() { cf319C(bufio.NewReader(os.Stdin), os.Stdout) }
