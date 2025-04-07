aa8eaaeaa1679d5b9680412fe1d2c7f19bcd88d7
Endless <0x3f@0x3f3f3f3f.com>
2025-04-07 17:04:12 +0800

    upd

--- leetcode/weekly/444/c/c.go
+++ leetcode/weekly/444/c/c.go
@@ -2,6 +2,56 @@ package main
 
 // https://space.bilibili.com/206214
 func maxProduct(nums []int, k, limit int) int {
+	total := 0
+	for _, x := range nums {
+		total += x
+	}
+	if total < abs(k) { // |k| 太大
+		return -1
+	}
+
+	ans := -1
+	type args struct {
+		i, s, m    int
+		odd, empty bool
+	}
+	vis := map[args]bool{}
+	var dfs func(int, int, int, bool, bool)
+	dfs = func(i, s, m int, odd, empty bool) {
+		if ans == limit {
+			return
+		}
+
+		if i == len(nums) {
+			if !empty && s == k && m <= limit {
+				ans = max(ans, m)
+			}
+			return
+		}
+
+		t := args{i, s, m, odd, empty}
+		if vis[t] {
+			return
+		}
+		vis[t] = true
+
+		// 不选 x
+		dfs(i+1, s, m, odd, empty)
+
+		// 选 x
+		x := nums[i]
+		if odd {
+			s -= x
+		} else {
+			s += x
+		}
+		dfs(i+1, s, min(m*x, limit+1), !odd, false)
+	}
+	dfs(0, 0, 1, false, true)
+	return ans
+}
+
+func maxProduct2(nums []int, k, limit int) int {
 	// 如果数组和小于 |k|，则返回 -1
 	total := 0
 	for _, x := range nums {
@@ -92,53 +142,3 @@ func maxProduct(nums []int, k, limit int) int {
 }
 
 func abs(x int) int { if x < 0 { return -x }; return x }
-
-func maxProduct1(nums []int, k, limit int) int {
-	total := 0
-	for _, x := range nums {
-		total += x
-	}
-	if total < abs(k) { // |k| 太大
-		return -1
-	}
-
-	ans := -1
-	type args struct {
-		i, s, m    int
-		odd, empty bool
-	}
-	vis := map[args]bool{}
-	var dfs func(int, int, int, bool, bool)
-	dfs = func(i, s, m int, odd, empty bool) {
-		if ans == limit {
-			return
-		}
-
-		if i == len(nums) {
-			if !empty && s == k && m <= limit {
-				ans = max(ans, m)
-			}
-			return
-		}
-
-		t := args{i, s, m, odd, empty}
-		if vis[t] {
-			return
-		}
-		vis[t] = true
-
-		// 不选 x
-		dfs(i+1, s, m, odd, empty)
-
-		// 选 x
-		x := nums[i]
-		if odd {
-			s -= x
-		} else {
-			s += x
-		}
-		dfs(i+1, s, min(m*x, limit+1), !odd, false)
-	}
-	dfs(0, 0, 1, false, true)
-	return ans
-}
