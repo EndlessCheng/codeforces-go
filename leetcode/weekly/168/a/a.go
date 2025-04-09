d492e2f06e866fc5b52af32519c943eeba29d80f
Endless <0x3f@0x3f3f3f3f.com>
2025-04-09 13:17:00 +0800

    添加题解

--- leetcode/weekly/168/a/a.go
+++ leetcode/weekly/168/a/a.go
@@ -2,11 +2,22 @@ package main
 
 import "strconv"
 
-func findNumbers(nums []int) (cnt int) {
-	for _, v := range nums {
-		if len(strconv.Itoa(v))%2 == 0 {
-			cnt++
+func findNumbers(nums []int) (ans int) {
+	for _, x := range nums {
+		for x >= 100 {
+			x /= 100
+		}
+		if x >= 10 {
+			ans++
 		}
 	}
-	return cnt
+	return
+}
+
+func findNumbers1(nums []int) int {
+	ans := len(nums)
+	for _, x := range nums {
+		ans -= len(strconv.Itoa(x)) % 2
+	}
+	return ans
 }
