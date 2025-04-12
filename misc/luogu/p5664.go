26a77740eacf1f5d299af10f0e9a7c91b251ca00
Endless <0x3f@0x3f3f3f3f.com>
2025-04-12 09:22:41 +0800

    p5664 计数 DP + 状态设计

---
+++ misc/luogu/p5664.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://space.bilibili.com/206214
+func p5664(in io.Reader, out io.Writer) {
+	const mod = 998244353
+	var n, m int
+	Fscan(in, &n, &m)
+	a := make([][]int, n)
+	s := make([]int, n)
+	ans := 1
+	for i := range a {
+		a[i] = make([]int, m)
+		for j := range a[i] {
+			Fscan(in, &a[i][j])
+			s[i] += a[i][j]
+		}
+		s[i] %= mod
+		ans = ans * (s[i] + 1) % mod
+	}
+	ans--
+	for j := 0; j < m; j++ {
+		f := make([]int, n*2+3)
+		f[n+1] = 1
+		for i, r := range a {
+			v := r[j]
+			nf := make([]int, n*2+3)
+			for d := 0; d <= n*2; d++ {
+				nf[d+1] = (f[d+1] + f[d]*v + f[d+2]*(s[i]-v)) % mod
+			}
+			f = nf
+		}
+		for _, v := range f[n+2:] {
+			ans -= v
+		}
+	}
+	Fprint(out, (ans%mod+mod)%mod)
+}
+
+//func main() { p5664(bufio.NewReader(os.Stdin), os.Stdout) }
