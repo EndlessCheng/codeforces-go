f656aae25c3d363faccc5f117a9ee04046a5bd1c
Endless <0x3f@0x3f3f3f3f.com>
2025-04-15 16:32:17 +0800

    p1365

--- misc/luogu/p1365.go
+++ misc/luogu/p1365.go
@@ -9,19 +9,19 @@ import (
 func p1365(in io.Reader, out io.Writer) {
 	var s string
 	Fscan(in, &s, &s)
-	ans, l := 0., 0.
+	f, l := 0., 0.
 	for _, b := range s {
 		if b == 'o' {
-			ans += l*2 + 1
+			f += l*2 + 1
 			l++
 		} else if b == 'x' {
 			l = 0
 		} else {
-			ans += l + 0.5
+			f += l + 0.5
 			l = l/2 + 0.5
 		}
 	}
-	Fprintf(out, "%.4f", ans)
+	Fprintf(out, "%.4f", f)
 }
 
 //func main() { p1365(bufio.NewReader(os.Stdin), os.Stdout) }
