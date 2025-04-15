884719e25b956b87a9d1f800bf2b00aae14249f6
Endless <0x3f@0x3f3f3f3f.com>
2025-04-15 20:02:03 +0800

    p1297

--- misc/luogu/p1297.go
+++ misc/luogu/p1297.go
@@ -13,13 +13,10 @@ func p1297(in io.Reader, out io.Writer) {
 	ans := 0.
 	for i := 1; i < n; i++ {
 		nxt := (v*a + b) % 100000001
-		v = v%c + 1
-		w := nxt%c + 1
-		ans += 1 / float64(max(v, w))
+		ans += 1 / float64(max(v%c, nxt%c)+1)
 		v = nxt
 	}
-	v = v%c + 1
-	ans += 1 / float64(max(v, a0))
+	ans += 1 / float64(max(v%c+1, a0))
 	Fprintf(out, "%.3f", ans)
 }
 
