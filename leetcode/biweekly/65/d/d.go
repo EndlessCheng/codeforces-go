690551034183d0c0813a5fb5bcd635974108d85a
Endless <0x3f@0x3f3f3f3f.com>
2025-04-19 11:30:12 +0800

    添加题解

--- leetcode/biweekly/65/d/d.go
+++ leetcode/biweekly/65/d/d.go
@@ -1,39 +1,43 @@
 package main
 
-import "sort"
+import (
+	"slices"
+	"sort"
+)
 
 // github.com/EndlessCheng/codeforces-go
 func maxTaskAssign(tasks, workers []int, pills, strength int) int {
-	sort.Ints(tasks)
-	sort.Ints(workers)
+	slices.Sort(tasks)
+	slices.Sort(workers)
 	m := len(workers)
-	return sort.Search(min(len(tasks), m), func(k int) bool {
+	ans := sort.Search(min(len(tasks), m), func(k int) bool {
 		k++
-		i, p, validTasks := 0, pills, []int{}
-		for _, w := range workers[m-k:] {
+		// 贪心：用最强的 k 名工人，完成最简单的 k 个任务
+		i, p := 0, pills
+		validTasks := []int{}
+		for _, w := range workers[m-k:] { // 枚举工人
+			// 在吃药的情况下，把能完成的任务记录到 validTasks 中
 			for ; i < k && tasks[i] <= w+strength; i++ {
-				validTasks = append(validTasks, tasks[i]) // 保证吃药时去掉的是 validTasks 的最后一个元素
+				validTasks = append(validTasks, tasks[i])
 			}
+			// 即使吃药也无法完成任务
 			if len(validTasks) == 0 {
 				return true
 			}
-			if validTasks[0] <= w {
-				validTasks = validTasks[1:] // 不吃药时去掉最小的元素
-			} else {
-				if p == 0 {
-					return true
-				}
-				p--
-				validTasks = validTasks[:len(validTasks)-1]
+			// 无需吃药就能完成（最简单的）任务
+			if w >= validTasks[0] {
+				validTasks = validTasks[1:]
+				continue
 			}
+			// 必须吃药
+			if p == 0 { // 没药了
+				return true
+			}
+			p--
+			// 完成（能完成的）最难的任务
+			validTasks = validTasks[:len(validTasks)-1]
 		}
 		return false
 	})
-}
-
-func min(a, b int) int {
-	if a > b {
-		return b
-	}
-	return a
+	return ans
 }
