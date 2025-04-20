10e22b993e0f39b48b1536f471bc8ac5d1cab8f8
Endless <0x3f@0x3f3f3f3f.com>
2025-04-20 21:27:53 +0800

    abc353_d

---
+++ misc/atcoder/abc353/d/d.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+// https://github.com/EndlessCheng
+func run(in io.Reader, out io.Writer) {
+	const mod = 998244353
+	var n, ans, sum int
+	var s string
+	Fscan(in, &n)
+	for i := 0; i < n; i++ {
+		Fscan(in, &s)
+		v, _ := strconv.Atoi(s)
+		ans = (ans + sum*int(math.Pow10(len(s))) + v*i) % mod
+		sum = (sum + v) % mod
+	}
+	Fprint(out, ans)
+}
+
+func main() { run(bufio.NewReader(os.Stdin), os.Stdout) }
