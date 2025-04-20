d4f262e9493a4a47583a3394d0107dd04f989203
Endless <0x3f@0x3f3f3f3f.com>
2025-04-20 21:30:11 +0800

    abc353_d fix

--- misc/atcoder/abc353/d/d.go
+++ misc/atcoder/abc353/d/d.go
@@ -18,7 +18,7 @@ func run(in io.Reader, out io.Writer) {
 	for i := 0; i < n; i++ {
 		Fscan(in, &s)
 		v, _ := strconv.Atoi(s)
-		ans = (ans + sum*int(math.Pow10(len(s))) + v*i) % mod
+		ans = (ans + int(math.Pow10(len(s)))%mod*sum + v*i) % mod
 		sum = (sum + v) % mod
 	}
 	Fprint(out, ans)
