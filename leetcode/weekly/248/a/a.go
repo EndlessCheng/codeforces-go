08c2bb5a16eab20fd9462ace59f09bcbb3faca40
Endless <0x3f@0x3f3f3f3f.com>
2025-04-24 18:02:52 +0800

    添加题解

--- leetcode/weekly/248/a/a.go
+++ leetcode/weekly/248/a/a.go
@@ -1,10 +1,29 @@
 package main
 
 // github.com/EndlessCheng/codeforces-go
-func buildArray(a []int) []int {
-	ans := make([]int, len(a))
-	for i, v := range a {
-		ans[i] = a[v]
+func buildArray(nums []int) []int {
+	for i, x := range nums {
+		if x < 0 { // 已标记，说明之前计算过
+			continue
+		}
+		cur := i
+		for nums[cur] != i {
+			nxt := nums[cur]
+			nums[cur] = ^nums[nxt] // 把下一个数搬过来，同时做标记（取反）
+			cur = nxt
+		}
+		nums[cur] = ^x // 这一圈的最后一个数，它的下一个数是起点 nums[i]
+	}
+	for i, x := range nums {
+		nums[i] = ^x // 复原
+	}
+	return nums
+}
+
+func buildArray1(nums []int) []int {
+	ans := make([]int, len(nums))
+	for i, x := range nums {
+		ans[i] = nums[x]
 	}
 	return ans
 }
