1e33be5f04e635db5f4fc543ed1d5f0dce49b250
Endless <0x3f@0x3f3f3f3f.com>
2025-04-26 20:09:37 +0800

    add deque.search

--- copypasta/deque.go
+++ copypasta/deque.go
@@ -1,5 +1,7 @@
 package copypasta
 
+import "sort"
+
 // 双端队列
 // 用两个 slice 头对头拼在一起实现
 // 在知道数据量的情况下，也可以直接创建个两倍数据量大小的 slice，然后用两个下标表示头尾，初始化在 slice 正中
@@ -7,25 +9,25 @@ package copypasta
 // https://codeforces.com/problemset/problem/1584/E 2300 单调双端队列
 
 // l-1,...1,0,0,1...,r-1
-type Deque struct{ l, r []any }
+type deque struct{ l, r []int }
 
-func (q Deque) Empty() bool {
+func (q deque) empty() bool {
 	return len(q.l) == 0 && len(q.r) == 0
 }
 
-func (q Deque) Len() int {
+func (q deque) size() int {
 	return len(q.l) + len(q.r)
 }
 
-func (q *Deque) PushFront(v any) {
+func (q *deque) pushFront(v int) {
 	q.l = append(q.l, v)
 }
 
-func (q *Deque) PushBack(v any) {
+func (q *deque) pushBack(v int) {
 	q.r = append(q.r, v)
 }
 
-func (q *Deque) PopFront() (v any) {
+func (q *deque) popFront() (v int) {
 	if len(q.l) > 0 {
 		q.l, v = q.l[:len(q.l)-1], q.l[len(q.l)-1]
 	} else {
@@ -34,7 +36,7 @@ func (q *Deque) PopFront() (v any) {
 	return
 }
 
-func (q *Deque) PopBack() (v any) {
+func (q *deque) popBack() (v any) {
 	if len(q.r) > 0 {
 		q.r, v = q.r[:len(q.r)-1], q.r[len(q.r)-1]
 	} else {
@@ -43,24 +45,34 @@ func (q *Deque) PopBack() (v any) {
 	return
 }
 
-func (q Deque) Front() any {
+func (q deque) front() int {
 	if len(q.l) > 0 {
 		return q.l[len(q.l)-1]
 	}
 	return q.r[0]
 }
 
-func (q Deque) Back() any {
+func (q deque) back() int {
 	if len(q.r) > 0 {
 		return q.r[len(q.r)-1]
 	}
 	return q.l[0]
 }
 
-// 0 <= i < q.Size()
-func (q Deque) Get(i int) any {
+// 0 <= i < q.size()
+func (q deque) get(i int) int {
 	if i < len(q.l) {
 		return q.l[len(q.l)-1-i]
 	}
 	return q.r[i-len(q.l)]
+}
+
+// 假设 q 是有序的，二分找 >= v 的第一个数的下标
+func (q deque) search(v int) int {
+	if len(q.l) > 0 && v <= q.l[0] {
+		// q.l 是递减的 
+		i := sort.Search(len(q.l), func(i int) bool { return q.l[i] < v })
+		return len(q.l) - i
+	}
+	return len(q.l) + sort.SearchInts(q.r, v)
 }
