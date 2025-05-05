85a2d6170314f9630aca80c6d917a0f84b190667
Endless <0x3f@0x3f3f3f3f.com>
2025-05-05 21:19:35 +0800

    位运算写法

--- main/2000-2099/2093D.go
+++ main/2000-2099/2093D.go
@@ -20,27 +20,17 @@ func cf2093D(in io.Reader, _w io.Writer) {
 			if op[0] == '-' {
 				Fscan(in, &y)
 				y--
-				ans := 1
-				for b := 1; b < 1<<n; b <<= 1 {
-					if x&b > 0 && y&b > 0 {
-						ans += b * b
-					} else if x&b > 0 {
-						ans += b * b * 2
-					} else if y&b > 0 {
-						ans += b * b * 3
-					}
+				ans := 0
+				for i := range n {
+					ans |= (y>>i&1*3 ^ x>>i&1<<1) << (i * 2)
 				}
-				Fprintln(out, ans)
+				Fprintln(out, ans+1)
 			} else {
 				r, c := 0, 0
 				for i := range n {
-					if x%4 == 1 || x%4 == 2 {
-						r |= 1 << i
-					}
-					if x%2 > 0 {
-						c |= 1 << i
-					}
-					x /= 4
+					v := x >> (i * 2) & 3
+					r |= (v>>1 ^ v&1) << i
+					c |= v & 1 << i
 				}
 				Fprintln(out, r+1, c+1)
 			}
