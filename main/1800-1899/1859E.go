4aab5fa76112c531c2d7e6b3c1e94e3ad5310a5e
Endless <0x3f@0x3f3f3f3f.com>
2025-05-10 11:41:36 +0800

    cf1859E

---
+++ main/1800-1899/1859E.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf1859E(in io.Reader, out io.Writer) {
+	var T, n, m int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n, &m)
+		a := make([]struct{ x, y int }, n+1)
+		for i := 1; i <= n; i++ {
+			Fscan(in, &a[i].x)
+		}
+		for i := 1; i <= n; i++ {
+			Fscan(in, &a[i].y)
+		}
+		f := make([]int, m+1)
+		mx := make([][4]int, n+1)
+		for i := range mx {
+			for j := range mx[i] {
+				mx[i][j] = -1e18
+			}
+		}
+		for i := 1; i <= n; i++ {
+			x, y := a[i].x, a[i].y
+			for j := min(i, m); j > 0; j-- {
+				mx[i-j][0] = max(mx[i-j][0], f[j-1]-y-x)
+				mx[i-j][1] = max(mx[i-j][1], f[j-1]+y-x)
+				mx[i-j][2] = max(mx[i-j][2], f[j-1]-y+x)
+				mx[i-j][3] = max(mx[i-j][3], f[j-1]+y+x)
+				f[j] = max(f[j],
+					mx[i-j][0]+x+y,
+					mx[i-j][1]-x+y,
+					mx[i-j][2]+x-y,
+					mx[i-j][3]-x-y)
+			}
+		}
+		Fprintln(out, f[m])
+	}
+}
+
+//func main() { cf1859E(bufio.NewReader(os.Stdin), os.Stdout) }
