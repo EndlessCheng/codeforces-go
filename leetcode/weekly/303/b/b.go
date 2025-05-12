b63def7203a6cb2733088536042a42e2bf1e6373
Endless <0x3f@0x3f3f3f3f.com>
2025-05-12 21:39:43 +0800

    简化代码

--- leetcode/weekly/303/b/b.go
+++ leetcode/weekly/303/b/b.go
@@ -3,15 +3,12 @@ package main
 // https://space.bilibili.com/206214/dynamic
 func equalPairs(grid [][]int) (ans int) {
 	cnt := map[[200]int]int{}
+	a := [200]int{}
 	for _, row := range grid {
-		a := [200]int{}
-		for j, v := range row {
-			a[j] = v
-		}
+		copy(a[:], row)
 		cnt[a]++
 	}
 	for j := range grid[0] {
-		a := [200]int{}
 		for i, row := range grid {
 			a[i] = row[j]
 		}
