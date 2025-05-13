c92e969f29d5ffff44e1669b85d527e3a360b1fa
Endless <0x3f@0x3f3f3f3f.com>
2025-05-13 17:25:39 +0800

    cf645D 写法二

--- main/600-699/645D.go
+++ main/600-699/645D.go
@@ -3,62 +3,47 @@ package main
 import (
 	. "fmt"
 	"io"
-	"sort"
 )
 
 // https://github.com/EndlessCheng
 func cf645D(in io.Reader, out io.Writer) {
-	var n, m int
+	var n, m, ans int
 	Fscan(in, &n, &m)
 	type edge struct{ to, i int }
 	g := make([][]edge, n)
+	deg := make([]int, n)
 	for i := range m {
 		var v, w int
 		Fscan(in, &v, &w)
-		g[v-1] = append(g[v-1], edge{w - 1, i})
+		v--
+		w--
+		g[v] = append(g[v], edge{w, i})
+		deg[w]++
 	}
 
-	deg := make([]int, n)
-	l, r := n-1, m+1
-	ans := l + sort.Search(r-l, func(m int) bool {
-		m += l
-		clear(deg)
-		for _, es := range g {
-			for _, e := range es {
-				if e.i < m {
-					deg[e.to]++
-				}
-			}
+	q := []int{}
+	for i, d := range deg {
+		if d == 0 {
+			q = append(q, i)
 		}
-		q := []int{}
-		for i, d := range deg {
-			if d == 0 {
-				q = append(q, i)
-			}
-		}
-		for len(q) > 0 {
-			if len(q) > 1 {
-				return false
-			}
-			v := q[0]
-			q = q[1:]
-			for _, e := range g[v] {
-				if e.i >= m {
-					continue
-				}
-				w := e.to
-				deg[w]--
-				if deg[w] == 0 {
-					q = append(q, w)
-				}
-			}
-		}
-		return true
-	})
-	if ans > m {
-		ans = -1
 	}
-	Fprint(out, ans)
+	for len(q) > 0 {
+		if len(q) > 1 {
+			Fprint(out, -1)
+			return
+		}
+		v := q[0]
+		q = q[1:]
+		for _, e := range g[v] {
+			w := e.to
+			deg[w]--
+			if deg[w] == 0 {
+				ans = max(ans, e.i)
+				q = append(q, w)
+			}
+		}
+	}
+	Fprint(out, ans+1)
 }
 
 //func main() { cf645D(bufio.NewReader(os.Stdin), os.Stdout) }
