1b360489ed353a2700f6014156072bace7495a5c
Endless <0x3f@0x3f3f3f3f.com>
2025-05-21 21:37:00 +0800

    add links

---
+++ copypasta/math_gf.go
@@ -0,0 +1,13 @@
+package copypasta
+
+/*
+生成函数相关
+
+https://codeforces.com/blog/entry/133991?#comment-1198693 卡特兰数推广
+
+https://codeforces.com/problemset/problem/2077/C 2300
+https://codeforces.com/problemset/problem/632/E 2400
+https://codeforces.com/problemset/problem/1096/G 2400
+https://codeforces.com/problemset/problem/1548/C 2500 https://chatgpt.com/c/682db19e-19a4-8011-908b-50e7a797d1df
+
+*/
