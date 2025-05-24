0920cd339ac207ed67425f89c6257473f694b51e
Endless <0x3f@0x3f3f3f3f.com>
2025-05-24 13:47:42 +0800

    add link

--- copypasta/math_gf.go
+++ copypasta/math_gf.go
@@ -9,5 +9,6 @@ https://codeforces.com/problemset/problem/2077/C 2300
 https://codeforces.com/problemset/problem/632/E 2400
 https://codeforces.com/problemset/problem/1096/G 2400
 https://codeforces.com/problemset/problem/1548/C 2500 https://chatgpt.com/c/682db19e-19a4-8011-908b-50e7a797d1df
+https://codeforces.com/problemset/problem/1542/E2 2700 https://www.luogu.com.cn/article/k9ipc3ow
 
 */
