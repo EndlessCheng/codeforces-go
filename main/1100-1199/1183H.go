df152771250ea19e74aabaf8d93538cef8b9fe1c
Endless <0x3f@0x3f3f3f3f.com>
2025-06-01 09:05:17 +0800

    简化代码

--- main/1100-1199/1183H.go
+++ main/1100-1199/1183H.go
@@ -19,14 +19,13 @@ func cf1183H(in io.Reader, out io.Writer) {
 	for i, b := range s {
 		b -= 'a'
 		for sz := i + 1; sz > 0; sz-- {
-			others := sumF[sz] - f[b][sz]
+			old := f[b][sz]
 			if sz == 1 {
 				f[b][sz] = 1
 			} else {
-				f[b][sz] = sumF[sz-1]
+				f[b][sz] = min(sumF[sz-1], k)
 			}
-			f[b][sz] = min(f[b][sz], k)
-			sumF[sz] = others + f[b][sz]
+			sumF[sz] += f[b][sz] - old
 		}
 	}
 	sumF[0] = 1
