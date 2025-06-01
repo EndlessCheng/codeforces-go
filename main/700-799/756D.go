8c1625b7e9eec5ffd7c5f2fbeeedabe1c6e30acf
Endless <0x3f@0x3f3f3f3f.com>
2025-06-01 09:31:28 +0800

    cf756D

--- main/700-799/756D.go
+++ main/700-799/756D.go
@@ -3,69 +3,32 @@ package main
 import (
 	. "fmt"
 	"io"
-	"os"
-	"slices"
 )
 
 // https://github.com/EndlessCheng
-const mod = 1_000_000_007
-
-func pow(x, n int) (res int) {
-	res = 1
-	for ; n > 0; n /= 2 {
-		if n%2 > 0 {
-			res = res * x % mod
-		}
-		x = x * x % mod
-	}
-	return
-}
-
-type comb struct{ _f, _invF []int }
-
-func newComb(mx int) *comb {
-	c := &comb{[]int{1}, []int{1}}
-	c._grow(mx)
-	return c
-}
-
-func (c *comb) _grow(mx int) {
-	n := len(c._f)
-	c._f = slices.Grow(c._f, mx+1)[:mx+1]
-	for i := n; i <= mx; i++ {
-		c._f[i] = c._f[i-1] * i % mod
-	}
-	c._invF = slices.Grow(c._invF, mx+1)[:mx+1]
-	c._invF[mx] = pow(c._f[mx], mod-2)
-	for i := mx; i > n; i-- {
-		c._invF[i-1] = c._invF[i] * i % mod
-	}
-}
-
-func (c *comb) f(n int) int {
-	if n >= len(c._f) {
-		c._grow(n * 2)
-	}
-	return c._f[n]
-}
-
-func (c *comb) invF(n int) int {
-	if n >= len(c._f) {
-		c._grow(n * 2)
-	}
-	return c._invF[n]
-}
-
-func (c *comb) c(n, k int) int {
-	if k < 0 || k > n {
-		return 0
-	}
-	return c.f(n) * c.invF(k) % mod * c.invF(n-k) % mod
-}
-
-var cm = newComb(0)
-
 func cf756D(in io.Reader, out io.Writer) {
+	const M = 1_000_000_007
+	pow := func(x, n int) int {
+		res := 1
+		for ; n > 0; n /= 2 {
+			if n%2 > 0 {
+				res = res * x % M
+			}
+			x = x * x % M
+		}
+		return res
+	}
+	const mx = 5000
+	F := [mx + 1]int{1}
+	for i := 1; i <= mx; i++ {
+		F[i] = F[i-1] * i % M
+	}
+	invF := [...]int{mx: pow(F[mx], M-2)}
+	for i := mx; i > 0; i-- {
+		invF[i-1] = invF[i] * i % M
+	}
+	C := func(n, k int) int { return F[n] * invF[k] % M * invF[n-k] % M }
+
 	var n, ans int
 	var s string
 	Fscan(in, &n, &s)
@@ -79,18 +42,18 @@ func cf756D(in io.Reader, out io.Writer) {
 		for sz := i + 1; sz > 0; sz-- {
 			old := f[b][sz]
 			if sz > 1 {
-				f[b][sz] = (sumF[sz-1] - f[b][sz-1]) % mod
+				f[b][sz] = (sumF[sz-1] - f[b][sz-1]) % M
 			} else {
 				f[b][sz] = 1
 			}
-			sumF[sz] = (sumF[sz] + f[b][sz] - old) % mod
+			sumF[sz] = (sumF[sz] + f[b][sz] - old) % M
 		}
 	}
 
 	for sz := 1; sz <= n; sz++ {
-		ans = (ans + sumF[sz]*cm.c(n-1, sz-1)) % mod
+		ans = (ans + sumF[sz]*C(n-1, sz-1)) % M
 	}
-	Fprint(out, (ans+mod)%mod)
+	Fprint(out, (ans+M)%M)
 }
 
-func main() { cf756D(os.Stdin, os.Stdout) }
+//func main() { cf756D(os.Stdin, os.Stdout) }
