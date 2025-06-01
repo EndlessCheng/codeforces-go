148ce214e49d88185d75c6e4a835fbe3eedaf833
Endless <0x3f@0x3f3f3f3f.com>
2025-06-01 08:55:27 +0800

    添加题解

--- leetcode/weekly/266/c/c.go
+++ leetcode/weekly/266/c/c.go
@@ -1,16 +1,42 @@
 package main
 
-import "sort"
+import (
+	"slices"
+	"sort"
+)
 
 // 二分答案
 
 // github.com/EndlessCheng/codeforces-go
-func minimizedMaximum(n int, quantities []int) int {
-	return sort.Search(1e5, func(max int) bool {
+func minimizedMaximum2(n int, quantities []int) int {
+	left, right := 1, slices.Max(quantities)
+	return left + sort.Search(right-left, func(mx int) bool {
+		mx += left
 		cnt := 0
 		for _, q := range quantities {
-			cnt += (q + max) / (max + 1)
+			cnt += (q-1)/mx + 1
 		}
 		return cnt <= n
-	}) + 1
+	})
+}
+
+func minimizedMaximum(n int, quantities []int) int {
+	check := func(mx int) bool {
+		cnt := 0
+		for _, q := range quantities {
+			cnt += (q + mx - 1) / mx
+		}
+		return cnt <= n
+	}
+
+	left, right := 0, slices.Max(quantities)
+	for left+1 < right {
+		mid := left + (right-left)/2
+		if check(mid) {
+			right = mid
+		} else {
+			left = mid
+		}
+	}
+	return right
 }
