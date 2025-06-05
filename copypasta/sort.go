c7261edb3321145fd72bf3e2518addb66d66b3cb
Endless <0x3f@0x3f3f3f3f.com>
2025-06-05 14:46:22 +0800

    add link

--- copypasta/sort.go
+++ copypasta/sort.go
@@ -46,6 +46,7 @@ https://codeforces.com/problemset/problem/1208/B 1500
 https://codeforces.com/problemset/problem/1665/C 1600
 https://codeforces.com/problemset/problem/1843/E 1600
 https://codeforces.com/problemset/problem/1118/D2 1700
+https://codeforces.com/problemset/problem/2110/D 1700 也有 O(n+m) 做法
 https://codeforces.com/problemset/problem/645/D 1800 也有 O(n) 做法
 https://codeforces.com/problemset/problem/2037/F 2100
 
