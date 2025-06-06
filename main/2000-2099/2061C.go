6e249b55baadadfd7d727bc7ec3ff11f6a8206fd
Endless <0x3f@0x3f3f3f3f.com>
2025-06-06 18:10:43 +0800

    cf2061C 写法二

--- main/2000-2099/2061C.go
+++ main/2000-2099/2061C.go
@@ -11,19 +11,19 @@ func cf2061C(in io.Reader, out io.Writer) {
 	var T, n int
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &n)
-		a := make([]int, n+1)
-		f := make([]int, n+1)
-		f[0] = 1
-		for i := 1; i <= n; i++ {
+		a := make([]int, n+2)
+		f := make([]int, n+2)
+		f[1] = 1
+		for i := 2; i < n+2; i++ {
 			Fscan(in, &a[i])
 			if a[i] == a[i-1] {
 				f[i] = f[i-1]
 			}
-			if i > 1 && a[i]-a[i-2] == 1 {
+			if a[i]-a[i-2] == 1 {
 				f[i] = (f[i] + f[i-2]) % mod
 			}
 		}
-		Fprintln(out, (f[n-1]+f[n])%mod)
+		Fprintln(out, (f[n]+f[n+1])%mod)
 	}
 }
 
