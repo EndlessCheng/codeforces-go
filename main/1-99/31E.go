67baf6c1c3caa6da1d04f53c7db04a2a0d24004b
Endless <0x3f@0x3f3f3f3f.com>
2025-06-07 16:53:38 +0800

    cf31E 写法二 多维 DP

--- main/1-99/31E.go
+++ main/1-99/31E.go
@@ -8,47 +8,36 @@ import (
 
 // https://github.com/EndlessCheng
 func cf31E(in io.Reader, out io.Writer) {
-	var n, mx, mask, mask2 int
-	var s []byte
+	var n int
+	var s string
 	Fscan(in, &n, &s)
-	type pair struct{ s, mask int }
-	f := func(s []byte, mul bool) []pair {
-		g := make([]pair, n+1)
-		for i := range 1 << n {
-			var a, b, c int
-			for j, v := range s {
-				if i>>j&1 > 0 {
-					a = a*10 + int(v-'0')
-					c++
-				} else {
-					b = b*10 + int(v-'0')
-				}
-			}
-			res := a + b
-			if mul {
-				res = a*int(math.Pow10(n-c)) + b*int(math.Pow10(c))
-			}
-			if res >= g[c].s {
-				g[c] = pair{res, i}
-			}
-		}
-		return g
+	f := make([][]int, n*2+1)
+	for i := range f {
+		f[i] = make([]int, n+1)
 	}
-	a := f(s[:n], true)
-	b := f(s[n:], false)
+	for i, b := range s {
+		for j, fv := range f[i] {
+			res := 0
+			if j < n {
+				res = f[i][j+1] + int(b-'0')*int(math.Pow10(j))
+			}
+			if j > n-1-i {
+				res = max(res, fv+int(b-'0')*int(math.Pow10(n*2-1-i-j)))
+			}
+			f[i+1][j] = res
+		}
+	}
 
-	for i, p := range a {
-		sum := p.s + b[n-i].s
-		if sum >= mx {
-			mx, mask, mask2 = sum, p.mask, b[n-i].mask
+	ans := make([]byte, n*2)
+	for i, j := n*2-1, 0; i >= 0; i-- {
+		if j < n && f[i+1][j] == f[i][j+1]+int(s[i]-'0')*int(math.Pow10(j)) {
+			ans[i] = 'M'
+			j++
+		} else {
+			ans[i] = 'H'
 		}
 	}
-	for i := range n {
-		Fprintf(out, "%c", "MH"[mask>>i&1])
-	}
-	for i := range n {
-		Fprintf(out, "%c", "MH"[mask2>>i&1])
-	}
+	Fprintf(out, "%s", ans)
 }
 
 //func main() { cf31E(os.Stdin, os.Stdout) }
