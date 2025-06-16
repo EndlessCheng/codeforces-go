780f03f5ff8dab651874bb59494eeeeac24f1750
Endless <0x3f@0x3f3f3f3f.com>
2025-06-16 13:49:34 +0800

    添加题解

--- leetcode/biweekly/67/a/a.go
+++ leetcode/biweekly/67/a/a.go
@@ -1,20 +1,23 @@
 package main
 
-import "sort"
-
-// 两次排序
+import "slices"
 
 // github.com/EndlessCheng/codeforces-go
 func maxSubsequence(nums []int, k int) []int {
-	id := make([]int, len(nums))
-	for i := range id {
-		id[i] = i
+	// 创建下标数组，对下标数组排序
+	idx := make([]int, len(nums))
+	for i := range idx {
+		idx[i] = i
 	}
-	sort.Slice(id, func(i, j int) bool { return nums[id[i]] > nums[id[j]] }) // 按元素值从大到小排序
-	sort.Ints(id[:k]) // 对下标从小到大排序
-	ans := make([]int, k)
-	for i, j := range id[:k] {
-		ans[i] = nums[j]
+	slices.SortFunc(idx, func(i, j int) int { return nums[j] - nums[i] })
+
+	// 取前 k 大元素的下标，排序
+	idx = idx[:k]
+	slices.Sort(idx)
+
+	// 取出 nums 的子序列
+	for i, j := range idx {
+		idx[i] = nums[j]
 	}
-	return ans
+	return idx
 }
