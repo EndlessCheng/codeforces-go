3285b0d8f09e8f5e5b5871ae63aae53e1713ef16
Endless <0x3f@0x3f3f3f3f.com>
2025-06-16 09:24:53 +0800

    添加题解

--- leetcode/weekly/195/c/c.go
+++ leetcode/weekly/195/c/c.go
@@ -1,29 +1,30 @@
 package main
 
-import "sort"
+import "slices"
 
-const mod int = 1e9 + 7
+const mod = 1_000_000_007
 
-var pow2 = [1e5 + 5]int{1}
+var pow2 = [100_000]int{1}
 
 func init() {
-	for i := 1; i <= 1e5+4; i++ {
-		pow2[i] = pow2[i-1] << 1 % mod
+	for i := 1; i < len(pow2); i++ {
+		pow2[i] = pow2[i-1] * 2 % mod
 	}
 }
 
-func numSubseq(a []int, target int) (ans int) {
-	sort.Ints(a)
-	for r, v := range a {
-		l := sort.SearchInts(a[:r], target-v+1)
-		if a[l]+a[r] > target {
-			l--
-		}
-		if l == r {
-			ans = (ans + pow2[l]) % mod
-		} else if l >= 0 {
-			ans = (ans + pow2[r-l-1]*(pow2[l+1]-1)) % mod
+func numSubseq(nums []int, target int) (ans int) {
+	slices.Sort(nums)
+	left, right := 0, len(nums)-1
+	for left <= right {
+		if nums[left]+nums[right] <= target {
+			// nums[left] 可以作为子序列的最小值
+			// 其余下标在 [left+1,right] 中的数选或不选都可以
+			ans += pow2[right-left]
+			left++
+		} else {
+			// nums[right] 太大了，即使与剩余元素的最小值 nums[left] 相加也不满足要求
+			right--
 		}
 	}
-	return
+	return ans % mod
 }
