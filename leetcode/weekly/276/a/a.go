1e00393bf2a0d34a8a06afabd3a6c675e86b2b96
Endless <0x3f@0x3f3f3f3f.com>
2025-06-16 12:20:55 +0800

    添加题解

--- leetcode/weekly/276/a/a.go
+++ leetcode/weekly/276/a/a.go
@@ -2,12 +2,10 @@ package main
 
 import "strings"
 
-// Go 模拟
-
 // github.com/EndlessCheng/codeforces-go
 func divideString(s string, k int, fill byte) []string {
 	n := len(s)
-	ans := make([]string, 0, (n+k-1)/k)
+	ans := make([]string, 0, (n+k-1)/k) // 预分配空间
 	for i := 0; i < n; i += k {
 		if i+k <= n {
 			ans = append(ans, s[i:i+k])
