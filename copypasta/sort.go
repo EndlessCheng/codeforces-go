87c79c0752b51c5ce7a0c056f2f9cc16942bd042
Endless <0x3f@0x3f3f3f3f.com>
2025-06-27 16:08:33 +0800

    add links

--- copypasta/sort.go
+++ copypasta/sort.go
@@ -83,11 +83,11 @@ https://www.luogu.com.cn/problem/P2680
 ### 最大化最小值（二分最小值 mn+1，如果满足要求，例如所有元素最后都 >= mn+1 则返回 false，否则返回 true，为什么要这样返回请看下面的【sort.Search 的使用技巧·其一】）
 https://codeforces.com/problemset/problem/1623/C 1600
 https://codeforces.com/problemset/problem/460/C 1700
-https://atcoder.jp/contests/abc374/tasks/abc374_e 1504=CF1849
 https://codeforces.com/problemset/problem/2046/C 2100
-https://atcoder.jp/contests/abc215/tasks/abc215_f 1853=CF2113
 https://codeforces.com/problemset/problem/1550/E 2500
 https://codeforces.com/problemset/problem/627/D 2600
+https://atcoder.jp/contests/abc374/tasks/abc374_e 1504=CF1849
+https://atcoder.jp/contests/abc215/tasks/abc215_f 1853=CF2113
 https://www.luogu.com.cn/problem/P3017
 
 ### 最小化中位数
