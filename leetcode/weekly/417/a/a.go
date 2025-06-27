f134c0547d0eee47bd30ff2a986f0d0b01f9a017
Endless <0x3f@0x3f3f3f3f.com>
2025-06-27 11:41:23 +0800

    upd

--- leetcode/weekly/417/a/a.go
+++ leetcode/weekly/417/a/a.go
@@ -4,5 +4,5 @@ import "math/bits"
 
 // https://space.bilibili.com/206214
 func kthCharacter(k int) byte {
-	return byte('a' + bits.OnesCount(uint(k-1)))
+	return 'a' + byte(bits.OnesCount(uint(k-1)))
 }
