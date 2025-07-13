a908c503f602555a925b7b00ddcee7e453d5504f
Endless <0x3f@0x3f3f3f3f.com>
2025-07-13 08:49:56 +0800

    cf2039C2

---
+++ main/2000-2099/2039C2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+	"math/bits"
+)
+
+// https://github.com/EndlessCheng
+func cf2039C2(in io.Reader, out io.Writer) {
+	var T, x, m int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &x, &m)
+		ans := 0
+		l := bits.Len(uint(x))
+		mask := 1<<l - 1
+		for y := 1; y <= min(mask, m); y++ {
+			if (x^y)%x == 0 || (x^y)%y == 0 {
+				ans++
+			}
+		}
+		if m > mask {
+			for k := (m | mask) / x; ; k-- {
+				y := k*x ^ x
+				if y>>l < m>>l {
+					ans += k - 1
+					break
+				}
+				if y <= m {
+					ans++
+				}
+			}
+		}
+		Fprintln(out, ans)
+	}
+}
+
+//func main() { cf2039C2(bufio.NewReader(os.Stdin), os.Stdout) }
