571ce943ef080a57e3d11fe575f34560f744175e
Endless <0x3f@0x3f3f3f3f.com>
2025-07-18 13:10:25 +0800

    rename

--- copypasta/orderedset.go
+++ copypasta/orderedset.go
@@ -5,7 +5,7 @@ import "container/list"
 // 哈希表：支持插入和删除，并能够按照插入顺序遍历哈希表中的元素
 // 做 https://codeforces.com/contest/962/problem/D 时有感而发
 type OrderedSet struct {
-	set map[interface{}]*list.Element
+	set map[any]*list.Element
 	lst *list.List
 }
 
@@ -14,7 +14,7 @@ func NewOrderedSet() OrderedSet {
 }
 
 func NewOrderedSetWithSpace(space int) OrderedSet {
-	return OrderedSet{make(map[interface{}]*list.Element, space), list.New()}
+	return OrderedSet{make(map[any]*list.Element, space), list.New()}
 }
 
 func (s *OrderedSet) Len() int {
@@ -22,25 +22,25 @@ func (s *OrderedSet) Len() int {
 }
 
 // 添加元素 v
-func (s *OrderedSet) Store(v interface{}) {
+func (s *OrderedSet) Store(v any) {
 	s.set[v] = s.lst.PushBack(v)
 }
 
 // 删除元素 v
-func (s *OrderedSet) Delete(v interface{}) {
+func (s *OrderedSet) Delete(v any) {
 	s.lst.Remove(s.set[v])
 	delete(s.set, v)
 }
 
 // 判断 v 是否存在
-func (s *OrderedSet) Contains(v interface{}) bool {
+func (s *OrderedSet) Contains(v any) bool {
 	_, ok := s.set[v]
 	return ok
 }
 
 // 按照插入顺序遍历哈希表中的元素
 // 当 f 返回 false 时停止遍历
-func (s *OrderedSet) Range(f func(key interface{}) bool) {
+func (s *OrderedSet) Range(f func(key any) bool) {
 	for node := s.lst.Front(); node != nil; node = node.Next() {
 		if !f(node.Value) {
 			break
