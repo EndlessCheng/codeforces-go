827205349fef357e29e334488c63d851fb582067
Endless <0x3f@0x3f3f3f3f.com>
2025-07-18 22:01:09 +0800

    添加题解

--- leetcode/weekly/251/d/d.go
+++ leetcode/weekly/251/d/d.go
@@ -1,86 +1,80 @@
 package main
 
 import (
-	"sort"
+	"slices"
 	"strings"
 )
 
-/* 字典树+哈希表
-
-我们用一棵字典树去表示整个文件系统，然后 DFS 这棵字典树，并用括号表达式按字典序拼接所有子树。
-
-以示例 $4$ 为例，文件夹 $\texttt{a}$ 的子树的括号表达式为 $\texttt{(x(y))(z)}$。
-
-![](https://assets.leetcode.com/uploads/2021/07/19/lc-dupfolder4_.jpg)
-
-这一表达方式包含了子树的节点名称和父子关系，因此可用于判断两个文件夹是否为相同文件夹。
-
-代码实现时，可以用一个哈希表去存储括号表达式及其对应的文件夹节点列表。在 DFS 结束后，遍历哈希表，若一个括号表达式对应的节点个数大于 $1$，则将该括号表达式对应的所有节点标记为待删除。然后再次 DFS 这棵字典树，仅访问未被删除的节点，并将路径记录到答案中。
-
-*/
-
 // github.com/EndlessCheng/codeforces-go
-type folder struct {
-	son map[string]*folder
-	val string // 文件夹名称
-	del bool   // 删除标记
+type trieNode struct {
+	son     map[string]*trieNode
+	name    string // 文件夹名称
+	deleted bool   // 删除标记
 }
 
 func deleteDuplicateFolder(paths [][]string) (ans [][]string) {
-	root := &folder{}
+	root := &trieNode{}
 	for _, path := range paths {
-		// 将 path 加入字典树
-		f := root
+		// 把 path 插到字典树中，见 208. 实现 Trie
+		cur := root
 		for _, s := range path {
-			if f.son == nil {
-				f.son = map[string]*folder{}
+			if cur.son == nil {
+				cur.son = map[string]*trieNode{}
 			}
-			if f.son[s] == nil {
-				f.son[s] = &folder{}
+			if cur.son[s] == nil {
+				cur.son[s] = &trieNode{}
 			}
-			f = f.son[s]
-			f.val = s
+			cur = cur.son[s]
+			cur.name = s
 		}
 	}
 
-	fMap := map[string]*folder{} // 存储括号表达式及其对应的文件夹节点
-	var dfs func(*folder) string
-	dfs = func(f *folder) string {
-		if f.son == nil {
-			return "(" + f.val + ")"
+	exprToNode := map[string]*trieNode{} // 子树括号表达式 -> 子树根节点
+	var genExpr func(*trieNode) string
+	genExpr = func(node *trieNode) string {
+		if node.son == nil { // 叶子
+			return node.name // 表达式就是文件夹名
 		}
-		expr := make([]string, 0, len(f.son))
-		for _, son := range f.son {
-			expr = append(expr, dfs(son))
+
+		expr := make([]string, 0, len(node.son)) // 预分配空间
+		for _, son := range node.son {
+			// 每个子树的表达式外面套一层括号
+			expr = append(expr, "("+genExpr(son)+")")
 		}
-		sort.Strings(expr)
-		subTreeExpr := strings.Join(expr, "") // 按字典序拼接所有子树
-		if o := fMap[subTreeExpr]; o != nil {
-			o.del = true
-			f.del = true
+		slices.Sort(expr)
+
+		subTreeExpr := strings.Join(expr, "") // 按字典序拼接所有子树的表达式
+		n := exprToNode[subTreeExpr]
+		if n != nil { // 哈希表中有 subTreeExpr，说明有重复的文件夹
+			n.deleted = true    // 哈希表中记录的节点标记为删除
+			node.deleted = true // 当前节点标记为删除
 		} else {
-			fMap[subTreeExpr] = f
+			exprToNode[subTreeExpr] = node
 		}
-		return "(" + f.val + subTreeExpr + ")"
-	}
-	dfs(root)
 
-	// 再次 DFS 这棵字典树，仅访问未被删除的节点，并将路径记录到答案中
-	path := []string{}
-	var dfs2 func(*folder)
-	dfs2 = func(f *folder) {
-		if f.del {
-			return
-		}
-		path = append(path, f.val)
-		ans = append(ans, append([]string(nil), path...))
-		for _, son := range f.son {
-			dfs2(son)
-		}
-		path = path[:len(path)-1]
+		return node.name + subTreeExpr
 	}
 	for _, son := range root.son {
-		dfs2(son)
+		genExpr(son)
+	}
+
+	// 在字典树上回溯，仅访问未被删除的节点，并将路径记录到答案中
+	// 类似 257. 二叉树的所有路径
+	path := []string{}
+	var dfs func(*trieNode)
+	dfs = func(node *trieNode) {
+		if node.deleted {
+			return
+		}
+		path = append(path, node.name)
+		ans = append(ans, slices.Clone(path))
+		for _, son := range node.son {
+			dfs(son)
+		}
+		path = path[:len(path)-1] // 恢复现场
+	}
+	for _, son := range root.son {
+		dfs(son)
 	}
 	return
 }
