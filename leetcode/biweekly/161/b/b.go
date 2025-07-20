434c3e80047b31c6c48179c8ab7f7bb9966b5ecc
Endless <0x3f@0x3f3f3f3f.com>
2025-07-20 20:38:37 +0800

    upd

--- leetcode/biweekly/161/b/b.go
+++ leetcode/biweekly/161/b/b.go
@@ -4,6 +4,7 @@ package main
 func countIslands(grid [][]int, k int) (ans int) {
 	dirs := []struct{ x, y int }{{0, -1}, {0, 1}, {-1, 0}, {1, 0}}
 	m, n := len(grid), len(grid[0])
+
 	var dfs func(int, int) int
 	dfs = func(i, j int) int {
 		res := grid[i][j]
@@ -16,6 +17,7 @@ func countIslands(grid [][]int, k int) (ans int) {
 		}
 		return res
 	}
+
 	for i, row := range grid {
 		for j, x := range row {
 			if x > 0 && dfs(i, j)%k == 0 {
