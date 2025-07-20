0dcffdc79bdd3a3dc847e933955c8f6701eea166
Endless <0x3f@0x3f3f3f3f.com>
2025-07-20 09:20:04 +0800

    cf383E

---
+++ main/300-399/383E.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf383E(in io.Reader, out io.Writer) {
+	const mx = 24
+	f := [1 << mx]uint16{}
+	n, s := uint16(0), ""
+	Fscan(in, &n)
+	for range n {
+		Fscan(in, &s)
+		f[1<<(s[0]-'a')|1<<(s[1]-'a')|1<<(s[2]-'a')] = 1
+	}
+
+	for i := range mx {
+		for s := 0; s < 1<<mx; s++ {
+			s |= 1 << i
+			f[s] += f[s^1<<i]
+		}
+	}
+
+	ans := 0
+	for _, v := range f {
+		x := int(n - v)
+		ans ^= x * x
+	}
+	Fprint(out, ans)
+}
+
+//func main() { cf383E(bufio.NewReader(os.Stdin), os.Stdout) }
