a7be84b590c305d62b9292d5fb106552ea07aa60
Endless <0x3f@0x3f3f3f3f.com>
2025-07-24 08:08:09 +0800

    添加题解

--- leetcode/weekly/312/b/b.go
+++ leetcode/weekly/312/b/b.go
@@ -2,14 +2,18 @@ package main
 
 // https://space.bilibili.com/206214
 func longestSubarray(nums []int) (ans int) {
-	max, cnt := 0, 0
+	mx, cnt := 0, 0
 	for _, x := range nums {
-		if x > max {
-			max, ans, cnt = x, 1, 1
-		} else if x < max {
+		if x > mx {
+			// 重新统计所有内容
+			mx = x
+			cnt = 1
+			ans = 1
+		} else if x == mx {
+			cnt++
+			ans = max(ans, cnt)
+		} else {
 			cnt = 0
-		} else if cnt++; cnt > ans {
-			ans = cnt
 		}
 	}
 	return
