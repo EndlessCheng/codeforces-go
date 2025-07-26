0b27aba532a943f0a5d7372ad15296b21a2bf2a9
Endless <0x3f@0x3f3f3f3f.com>
2025-07-26 21:03:02 +0800

    cf2053C

---
+++ main/2000-2099/2053C.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf2053C(in io.Reader, _w io.Writer) {
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+	var T, n, k int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n, &k)
+		m := n + 1
+		ans := 0
+		if n&1 > 0 {
+			ans = m / 2
+		}
+		n /= 2
+		for c := 0; n >= k; n /= 2 {
+			if n&1 > 0 {
+				ans += m << c
+			}
+			c++
+		}
+		Fprintln(out, ans)
+	}
+}
+
+//func main() { cf2053C(bufio.NewReader(os.Stdin), os.Stdout) }
