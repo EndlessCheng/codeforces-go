7a8307c519379837caa7840cae9293a77c9668b3
Endless <0x3f@0x3f3f3f3f.com>
2025-07-29 07:45:53 +0800

    upd

--- leetcode/weekly/263/c/c.go
+++ leetcode/weekly/263/c/c.go
@@ -2,11 +2,12 @@ package main
 
 // github.com/EndlessCheng/codeforces-go
 func countMaxOrSubsets(nums []int) (ans int) {
+	n := len(nums)
 	totalOr := 0
 	for _, x := range nums {
 		totalOr |= x
 	}
-	n := len(nums)
+
 	var dfs func(int, int)
 	dfs = func(i, subsetOr int) {
 		if subsetOr == totalOr {
@@ -19,6 +20,7 @@ func countMaxOrSubsets(nums []int) (ans int) {
 		dfs(i+1, subsetOr)         // 不选 nums[i]
 		dfs(i+1, subsetOr|nums[i]) // 选 nums[i]
 	}
+
 	dfs(0, 0)
 	return
 }
