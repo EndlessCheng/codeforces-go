c11a05b88ef24d440ac0e1efb4e910a8d60dc8cd
Endless <0x3f@0x3f3f3f3f.com>
2025-07-29 09:11:03 +0800

    cf1101G

---
+++ main/1100-1199/1101G.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf1101G(in io.Reader, out io.Writer) {
+	var n, v, xor, num int
+	b := [30]int{}
+	Fscan(in, &n)
+	for range n {
+		Fscan(in, &v)
+		xor ^= v
+		for i := len(b) - 1; i >= 0; i-- {
+			if v>>i == 0 {
+				continue
+			}
+			if b[i] == 0 {
+				b[i] = v
+				num++
+				break
+			}
+			v ^= b[i]
+		}
+	}
+	if xor == 0 {
+		num = -1
+	}
+	Fprint(out, num)
+}
+
+//func main() { cf1101G(bufio.NewReader(os.Stdin), os.Stdout) }
