ba7f0a91489787b4afea036506f74c030e53556c
Endless <0x3f@0x3f3f3f3f.com>
2025-07-30 16:18:27 +0800

    rename

--- main/1600-1699/1612G.go
+++ main/1600-1699/1612G.go
@@ -23,16 +23,16 @@ func cf1612G(in io.Reader, out io.Writer) {
 
 	ans := 0
 	ways := 1
-	for i := mx; i > 1; i-- {
-		k := cc[i]
+	for c := mx; c > 1; c-- {
+		k := cc[c]
 		if k == 0 {
 			continue
 		}
-		// k 种元素，每种选两个一左一右，剩余的放入 cc[i-2]
-		ans = (ans + (tot-k)%mod*k%mod*(i-1)) % mod
-		ways = ways * fac[k] % mod * fac[k] % mod
+		// k 种元素，每种选两个一左一右，剩余的放入 cc[c-2]
+		ans = (ans + (tot-k)%mod*k%mod*(c-1)) % mod
+		ways = ways * fac[k] % mod * fac[k] % mod // 左右独立全排列
 		tot -= k * 2
-		cc[i-2] += k
+		cc[c-2] += k
 	}
 	ways = ways * fac[cc[1]] % mod // 1 个数无贡献，只有全排列
 	Fprint(out, (ans+mod)%mod, ways)
