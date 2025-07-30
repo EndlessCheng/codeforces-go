b10bf2e661271a9d71c36d17df106f9fac57cbd9
Endless <0x3f@0x3f3f3f3f.com>
2025-07-30 14:47:35 +0800

    add link

--- copypasta/math_gf.go
+++ copypasta/math_gf.go
@@ -8,6 +8,7 @@ https://codeforces.com/blog/entry/133991?#comment-1198693 卡特兰数推广
 https://codeforces.com/problemset/problem/2077/C 2300
 https://codeforces.com/problemset/problem/632/E 2400
 https://codeforces.com/problemset/problem/1096/G 2400
+https://codeforces.com/problemset/problem/1251/F 2500
 https://codeforces.com/problemset/problem/1548/C 2500 https://chatgpt.com/c/682db19e-19a4-8011-908b-50e7a797d1df
 https://codeforces.com/problemset/problem/1542/E2 2700 https://www.luogu.com.cn/article/k9ipc3ow
 https://codeforces.com/problemset/problem/1845/F 2800
