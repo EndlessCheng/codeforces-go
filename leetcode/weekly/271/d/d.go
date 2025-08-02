ae95ffc87da88ba9172227b6f61e61bad344a649
Endless <0x3f@0x3f3f3f3f.com>
2025-08-03 06:59:09 +0800

    重写题解

--- leetcode/weekly/271/d/d.go
+++ leetcode/weekly/271/d/d.go
@@ -3,46 +3,46 @@ package main
 import "sort"
 
 // github.com/EndlessCheng/codeforces-go
-func maxTotalFruits(fruits [][]int, startPos int, k int) (ans int) {
+func maxTotalFruits1(fruits [][]int, startPos, k int) int {
 	n := len(fruits)
-	sum := make([]int, n+1)
-	for i, f := range fruits {
-		sum[i+1] = sum[i] + f[1]
+	// 向左最远能到 fruits[left][0]
+	left := sort.Search(n, func(i int) bool { return fruits[i][0] >= startPos-k })
+
+	right, s := left, 0
+	// 从 fruits[left][0] 到 startPos 的水果数
+	for ; right < n && fruits[right][0] <= startPos; right++ {
+		s += fruits[right][1]
 	}
 
-	// 先向右再向左
-	mid := sort.Search(n, func(i int) bool { return fruits[i][0] >= startPos })
-	for i := mid; i < n; i++ {
-		d := fruits[i][0] - startPos
-		if d > k {
-			break
+	ans := s
+	// 枚举最右走到 fruits[right][0]
+	for ; right < n && fruits[right][0] <= startPos+k; right++ {
+		s += fruits[right][1] // 枚举最右位置为 fruits[right][0]
+		for fruits[right][0]*2-fruits[left][0]-startPos > k &&
+			fruits[right][0]-fruits[left][0]*2+startPos > k {
+			s -= fruits[left][1] // fruits[left][0] 太远了
+			left++
 		}
-		cnt := sum[i+1] - sum[mid]
-		d *= 2
-		if d < k {
-			// 往左最远能到达的水果下标
-			left := sort.Search(n, func(i int) bool { return fruits[i][0] >= startPos-k+d })
-			cnt += sum[mid] - sum[left]
-		}
-		ans = max(ans, cnt)
+		ans = max(ans, s) // 更新答案最大值
 	}
+	return ans
+}
 
-	// 先向左再向右
-	for i := mid - 1; i >= 0; i-- {
-		d := startPos - fruits[i][0]
-		if d > k {
-			break
+func maxTotalFruits(fruits [][]int, startPos, k int) (ans int) {
+	n := len(fruits)
+	// 向左最远能到 fruits[left][0]
+	left := sort.Search(n, func(i int) bool { return fruits[i][0] >= startPos-k })
+
+	s := 0
+	// 枚举最右走到 fruits[right][0]
+	for right := left; right < n && fruits[right][0] <= startPos+k; right++ {
+		s += fruits[right][1]
+		for fruits[right][0]*2-fruits[left][0]-startPos > k &&
+			fruits[right][0]-fruits[left][0]*2+startPos > k {
+			s -= fruits[left][1] // fruits[left][0] 太远了
+			left++
 		}
-		cnt := sum[mid] - sum[i]
-		d *= 2
-		if d < k {
-			// 往右最远能到达的水果下标+1
-			right := sort.Search(n, func(i int) bool { return fruits[i][0] > startPos+k-d })
-			cnt += sum[right] - sum[mid]
-		}
-		ans = max(ans, cnt)
+		ans = max(ans, s) // 更新答案最大值
 	}
 	return
 }
-
-func max(a, b int) int { if b > a { return b }; return a }
