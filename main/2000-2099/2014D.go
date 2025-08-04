9fa052081251b23726ba71bdff83e674ec11cd61
Endless <0x3f@0x3f3f3f3f.com>
2025-08-04 16:16:05 +0800

    cf2014D

---
+++ main/2000-2099/2014D.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf2014D(in io.Reader, out io.Writer) {
+	var T, n, d, k, l, r int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n, &d, &k)
+		diff := make([]int, n+d+1)
+		for range k {
+			Fscan(in, &l, &r)
+			diff[l]++
+			diff[r+d]--
+		}
+
+		mx, mxI := -1, 0
+		mn, mnI := k+1, 0
+		s := 0
+		for i := 1; i <= n; i++ {
+			s += diff[i]
+			l := i + 1 - d
+			if l <= 0 {
+				continue
+			}
+			if s > mx {
+				mx, mxI = s, l
+			}
+			if s < mn {
+				mn, mnI = s, l
+			}
+		}
+		Fprintln(out, mxI, mnI)
+	}
+}
+
+//func main() { cf2014D(bufio.NewReader(os.Stdin), os.Stdout) }
