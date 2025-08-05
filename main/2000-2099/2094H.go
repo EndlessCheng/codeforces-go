84826b6306aaa5f5f6d36f8d0b0568c7053b0ba0
Endless <0x3f@0x3f3f3f3f.com>
2025-08-05 08:41:11 +0800

    cf2094H

---
+++ main/2000-2099/2094H.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"sort"
+)
+
+// https://github.com/EndlessCheng
+func cf2094H(in io.Reader, _w io.Writer) {
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+	const mx = 100_001
+	divisors := [mx][]int{}
+	for i := 2; i < mx; i++ {
+		for j := i; j < mx; j += i {
+			divisors[j] = append(divisors[j], i)
+		}
+	}
+
+	var T, n, q, v, l, r int
+	pos := [mx][]int{}
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n, &q)
+		a := make([]int, n)
+		for i := range a {
+			Fscan(in, &a[i])
+			pos[a[i]] = append(pos[a[i]], i)
+		}
+		for range q {
+			Fscan(in, &v, &l, &r)
+			l--
+			ans := 0
+			for {
+				mnI := n
+				for _, d := range divisors[v] {
+					ps := pos[d]
+					i := sort.SearchInts(ps, l)
+					if i < len(ps) && ps[i] < r {
+						mnI = min(mnI, ps[i])
+					}
+				}
+				if mnI == n {
+					ans += v * (r - l)
+					break
+				}
+				ans += v * (mnI - l)
+				l = mnI
+				d := a[l]
+				for v /= d; v%d == 0; v /= d {
+				}
+			}
+			Fprintln(out, ans)
+		}
+		for _, v := range a {
+			pos[v] = pos[v][:0]
+		}
+	}
+}
+
+//func main() { cf2094H(bufio.NewReader(os.Stdin), os.Stdout) }
