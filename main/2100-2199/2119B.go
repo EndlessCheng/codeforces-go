f23237fdadda97bf683570de326addfa831ffb56
Endless <0x3f@0x3f3f3f3f.com>
2025-08-06 10:09:59 +0800

    cf2119B

---
+++ main/2100-2199/2119B.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf2119B(in io.Reader, out io.Writer) {
+	var T, n, x, y, tx, ty int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n, &x, &y, &tx, &ty)
+		d := (x-tx)*(x-tx) + (y-ty)*(y-ty)
+		s, mx := 0, 0
+		for range n {
+			Fscan(in, &x)
+			s += x
+			mx = max(mx, x)
+		}
+		if d > s*s || d < (mx*2-s)*(mx*2-s) {
+			Fprintln(out, "No")
+		} else {
+			Fprintln(out, "Yes")
+		}
+	}
+}
+
+//func main() { cf2119B(bufio.NewReader(os.Stdin), os.Stdout) }
