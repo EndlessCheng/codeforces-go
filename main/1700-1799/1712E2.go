e8e710732219ec0be88cb57ca800cd8ad6a3da7e
Endless <0x3f@0x3f3f3f3f.com>
2025-08-09 10:06:11 +0800

    cf1712E2 写法二

--- main/1700-1799/1712E2.go
+++ main/1700-1799/1712E2.go
@@ -4,19 +4,18 @@ import (
 	"bufio"
 	. "fmt"
 	"io"
-	"slices"
 )
 
 // https://github.com/EndlessCheng
-type fenwick []int
+type fenwick12 []int
 
-func (f fenwick) update(i, v int) {
+func (f fenwick12) update(i, v int) {
 	for ; i < len(f); i += i & -i {
 		f[i] += v
 	}
 }
 
-func (f fenwick) pre(i int) (s int) {
+func (f fenwick12) pre(i int) (s int) {
 	for ; i > 0; i &= i - 1 {
 		s += f[i]
 	}
@@ -26,31 +25,28 @@ func (f fenwick) pre(i int) (s int) {
 func cf1712E2(in io.Reader, _w io.Writer) {
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
-	var m int
+	var m, l, r int
 	Fscan(in, &m)
-	type query struct{ l, r, i int }
-	qs := make([]query, m)
-	for i := range qs {
-		Fscan(in, &qs[i].l, &qs[i].r)
-		qs[i].i = i
+	const mx int = 2e5 + 1
+	type pair struct{ r, i int }
+	qs := [mx][]pair{}
+	for i := range m {
+		Fscan(in, &l, &r)
+		qs[l] = append(qs[l], pair{r, i})
 	}
-	slices.SortFunc(qs, func(a, b query) int { return a.l - b.l })
 
 	ans := make([]int, m)
-	const mx int = 2e5 + 1
 	divisors := [mx]int{}
-	t := make(fenwick, mx)
-	curL := mx
-	for i := m - 1; i >= 0; i-- {
-		l, r := qs[i].l, qs[i].r
-		for curL > l {
-			curL--
-			for j := curL * 2; j < mx; j += curL {
-				t.update(j, divisors[j])
-				divisors[j]++
-			}
+	t := make(fenwick12, mx)
+	for l := mx - 1; l > 0; l-- {
+		for j := l * 2; j < mx; j += l {
+			t.update(j, divisors[j])
+			divisors[j]++
 		}
-		ans[qs[i].i] = (r-l+1)*(r-l)*(r-l-1)/6 - t.pre(r) - max(r/6-(l+2)/3+1, 0) - max(r/15-(l+5)/6+1, 0)
+		for _, p := range qs[l] {
+			r := p.r
+			ans[p.i] = (r-l+1)*(r-l)*(r-l-1)/6 - t.pre(r) - max(r/6-(l+2)/3+1, 0) - max(r/15-(l+5)/6+1, 0)
+		}
 	}
 	for _, v := range ans {
 		Fprintln(out, v)
