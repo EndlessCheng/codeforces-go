47f4f666177a42f3f33617cec661b20523f63196
Endless <0x3f@0x3f3f3f3f.com>
2025-08-09 09:33:23 +0800

    添加题解

--- leetcode/weekly/275/b/b.go
+++ leetcode/weekly/275/b/b.go
@@ -2,20 +2,27 @@ package main
 
 // github.com/EndlessCheng/codeforces-go
 func minSwaps(nums []int) int {
-	tot1 := 0
-	for _, num := range nums {
-		tot1 += num
+	// 统计 1 的个数
+	k := 0
+	for _, x := range nums {
+		k += x
 	}
-	cnt1, maxCnt1 := 0, 0
-	nums = append(nums, nums...) // 断环成链
-	for i, num := range nums {
-		cnt1 += num
-		if i >= tot1 { // 滑窗
-			cnt1 -= nums[i-tot1]
-			if cnt1 > maxCnt1 {
-				maxCnt1 = cnt1
-			}
+	if k == 0 { // 没有 1，无需交换
+		return 0
+	}
+
+	n := len(nums)
+	max1, cnt1 := 0, 0
+	for i := range n + k - 1 {
+		// 1. 进入窗口
+		cnt1 += nums[i%n]
+		if i < k-1 { // 窗口大小不足 k
+			continue
 		}
+		// 2. 更新答案
+		max1 = max(max1, cnt1)
+		// 3. 离开窗口，为下一个循环做准备
+		cnt1 -= nums[i-k+1] // 由于我们保证 i < n+k-1，所以 i-k+1 < n
 	}
-	return tot1 - maxCnt1
+	return k - max1
 }
