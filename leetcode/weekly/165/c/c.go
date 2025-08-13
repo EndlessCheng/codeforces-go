253976cab9cb7ba3feee6bee2e05aceb2afee1ef
Endless <0x3f@0x3f3f3f3f.com>
2025-08-13 12:30:25 +0800

    添加题解

--- leetcode/weekly/165/c/c.go
+++ leetcode/weekly/165/c/c.go
@@ -1,9 +1,9 @@
 package main
 
-func countSquares(mat [][]int) (ans int) {
-	sum := make([][]int, len(mat)+1)
-	sum[0] = make([]int, len(mat[0])+1)
-	for i, mi := range mat {
+func countSquares1(matrix [][]int) (ans int) {
+	sum := make([][]int, len(matrix)+1)
+	sum[0] = make([]int, len(matrix[0])+1)
+	for i, mi := range matrix {
 		sum[i+1] = make([]int, len(mi)+1)
 		for j, mij := range mi {
 			sum[i+1][j+1] = sum[i+1][j] + sum[i][j+1] - sum[i][j] + mij
@@ -12,12 +12,24 @@ func countSquares(mat [][]int) (ans int) {
 	query := func(r1, c1, r2, c2 int) int {
 		return sum[r2][c2] - sum[r2][c1] - sum[r1][c2] + sum[r1][c1]
 	}
-	for i, mi := range mat {
+	for i, mi := range matrix {
 		for j := range mi {
-			for sz := 1; i+sz <= len(mat) && j+sz <= len(mi) && query(i, j, i+sz, j+sz) == sz*sz; sz++ {
+			for sz := 1; i+sz <= len(matrix) && j+sz <= len(mi) && query(i, j, i+sz, j+sz) == sz*sz; sz++ {
 				ans++
 			}
 		}
 	}
 	return ans
+}
+
+func countSquares(matrix [][]int) (ans int) {
+	for i, row := range matrix {
+		for j, x := range row {
+			if x > 0 && i > 0 && j > 0 {
+				row[j] += min(matrix[i-1][j], matrix[i-1][j-1], row[j-1])
+			}
+			ans += row[j]
+		}
+	}
+	return
 }
