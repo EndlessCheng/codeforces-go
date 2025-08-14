b7c0f3b55c50fccbb981c49c2ec10f3547277068
Endless <0x3f@0x3f3f3f3f.com>
2025-08-14 12:07:01 +0800

    cf2125D 写法二

--- main/2100-2199/2125D.go
+++ main/2100-2199/2125D.go
@@ -22,26 +22,20 @@ func cf2125D(in io.Reader, out io.Writer) {
 	Fscan(in, &n, &m)
 	type pair struct{ l, p int }
 	g := make([][]pair, m+1)
-	notP := make([]int, m+1)
-	for i := range notP {
-		notP[i] = 1
-	}
+	notP := 1
 	for range n {
 		Fscan(in, &l, &r, &p, &q)
 		p = p * pow(q, mod-2) % mod
 		g[r] = append(g[r], pair{l, p})
-		notP[r] = notP[r] * (1 - p) % mod
-	}
-	for i := 2; i <= m; i++ {
-		notP[i] = notP[i] * notP[i-1] % mod
+		notP = notP * (1 - p) % mod
 	}
 
 	f := make([]int, m+1)
-	f[0] = 1
+	f[0] = notP
 	for i, ps := range g {
 		for _, p := range ps {
 			// [p.l, i] 只能有这一条线段，所以其余右端点在 [p.l, i] 中的区间都不能留
-			f[i] = (f[i] + f[p.l-1]*p.p%mod*notP[i]%mod*pow(notP[p.l-1]*(1-p.p)%mod, mod-2)) % mod
+			f[i] = (f[i] + f[p.l-1]*p.p%mod*pow(1-p.p, mod-2)) % mod
 		}
 	}
 	Fprint(out, (f[m]+mod)%mod)
