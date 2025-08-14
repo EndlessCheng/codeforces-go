8b5f4bd689c136affbbd5741cce9b5e9ba91756d
Endless <0x3f@0x3f3f3f3f.com>
2025-08-14 11:59:34 +0800

    cf2125D

---
+++ main/2100-2199/2125D.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf2125D(in io.Reader, out io.Writer) {
+	const mod = 998244353
+	pow := func(x, n int) int {
+		res := 1
+		for ; n > 0; n /= 2 {
+			if n%2 > 0 {
+				res = res * x % mod
+			}
+			x = x * x % mod
+		}
+		return res
+	}
+	var n, m, l, r, p, q int
+	Fscan(in, &n, &m)
+	type pair struct{ l, p int }
+	g := make([][]pair, m+1)
+	notP := make([]int, m+1)
+	for i := range notP {
+		notP[i] = 1
+	}
+	for range n {
+		Fscan(in, &l, &r, &p, &q)
+		p = p * pow(q, mod-2) % mod
+		g[r] = append(g[r], pair{l, p})
+		notP[r] = notP[r] * (1 - p) % mod
+	}
+	for i := 2; i <= m; i++ {
+		notP[i] = notP[i] * notP[i-1] % mod
+	}
+
+	f := make([]int, m+1)
+	f[0] = 1
+	for i, ps := range g {
+		for _, p := range ps {
+			// [p.l, i] 只能有这一条线段，所以其余右端点在 [p.l, i] 中的区间都不能留
+			f[i] = (f[i] + f[p.l-1]*p.p%mod*notP[i]%mod*pow(notP[p.l-1]*(1-p.p)%mod, mod-2)) % mod
+		}
+	}
+	Fprint(out, (f[m]+mod)%mod)
+}
+
+//func main() { cf2125D(bufio.NewReader(os.Stdin), os.Stdout) }
