5c84d3e28fef7d60177d01dbb337bb11bd8670d6
Endless <0x3f@0x3f3f3f3f.com>
2025-08-14 08:20:39 +0800

    添加题解

--- leetcode/biweekly/47/b/b.go
+++ leetcode/biweekly/47/b/b.go
@@ -1,13 +1,11 @@
 package main
 
 // github.com/EndlessCheng/codeforces-go
-
-// 另一种方法是判断三进制是否包含 2
-
-func f(s, p int) bool {
-	return s == 0 || p < 1e7 && (f(s, p*3) || f(s-p, p*3))
-}
-
 func checkPowersOfThree(n int) bool {
-	return f(n, 1)
+	for ; n > 0; n /= 3 {
+		if n%3 == 2 {
+			return false
+		}
+	}
+	return true
 }
