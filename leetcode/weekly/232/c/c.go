e6b6d68533e67b4e4473c09cd2a5f77c8f1542f9
Endless <0x3f@0x3f3f3f3f.com>
2025-08-16 09:36:03 +0800

    添加题解

--- leetcode/weekly/232/c/c.go
+++ leetcode/weekly/232/c/c.go
@@ -3,28 +3,35 @@ package main
 import "container/heap"
 
 // github.com/EndlessCheng/codeforces-go
-type hp [][]int
+func maxAverageRatio(classes [][]int, extraStudents int) float64 {
+	n := len(classes)
+	h := make(hp, n)
+	for i, c := range classes {
+		h[i] = pair{c[0], c[1]}
+	}
+	heap.Init(&h)
+
+	for range extraStudents {
+		h[0].a++
+		h[0].b++
+		heap.Fix(&h, 0)
+	}
+
+	sum := 0.0
+	for _, t := range h {
+		sum += float64(t.a) / float64(t.b)
+	}
+	return sum / float64(n)
+}
+
+type pair struct{ a, b int }
+type hp []pair
 
 func (h hp) Len() int { return len(h) }
 func (h hp) Less(i, j int) bool {
 	a, b := h[i], h[j]
-	return (a[1]-a[0])*b[1]*(b[1]+1) > (b[1]-b[0])*a[1]*(a[1]+1)
+	return (a.b-a.a)*b.b*(b.b+1) > (b.b-b.a)*a.b*(a.b+1)
 }
-func (h hp) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (hp) Push(interface{})     {}
-func (hp) Pop() (_ interface{}) { return }
-
-func maxAverageRatio(classes [][]int, ex int) float64 {
-	h := hp(classes)
-	heap.Init(&h)
-	for ; ex > 0; ex-- {
-		h[0][0]++
-		h[0][1]++
-		heap.Fix(&h, 0)
-	}
-	ans := .0
-	for _, c := range h {
-		ans += float64(c[0]) / float64(c[1])
-	}
-	return ans / float64(len(classes))
-}
+func (h hp) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+func (hp) Push(any)        {}
+func (hp) Pop() (_ any)    { return }
