5699d5ebe642e00d62801b3254874b50195fa680
Endless <0x3f@0x3f3f3f3f.com>
2025-08-19 07:44:28 +0800

    重写题解

--- leetcode/biweekly/83/b/b.go
+++ leetcode/biweekly/83/b/b.go
@@ -1,14 +1,26 @@
 package main
 
 // https://space.bilibili.com/206214/dynamic
-func zeroFilledSubarray(nums []int) (ans int64) {
-	c := 0
-	for _, num := range nums {
-		if num == 0 {
-			c++
-			ans += int64(c)
+func zeroFilledSubarray1(nums []int) (ans int64) {
+	last := -1
+	for i, x := range nums {
+		if x != 0 {
+			last = i // 记录上一个非 0 元素的位置
 		} else {
-			c = 0
+			ans += int64(i - last)
+		}
+	}
+	return
+}
+
+func zeroFilledSubarray(nums []int) (ans int64) {
+	cnt0 := 0
+	for _, x := range nums {
+		if x != 0 {
+			cnt0 = 0
+		} else {
+			cnt0++ // 右端点为 i 的全 0 子数组比右端点为 i-1 的全 0 子数组多一个
+			ans += int64(cnt0)
 		}
 	}
 	return
