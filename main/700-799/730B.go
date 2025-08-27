58c5aad475cc6349fd01087b39d74d75ef89db06
Endless <0x3f@0x3f3f3f3f.com>
2025-08-27 10:26:45 +0800

    写法二

--- main/700-799/730B.go
+++ main/700-799/730B.go
@@ -3,30 +3,29 @@ package main
 import . "fmt"
 
 // https://github.com/EndlessCheng
-func less30(i, j int) bool {
-	Println("?", i, j)
-	c := ""
-	Scan(&c)
-	return c == "<"
-}
-
 func cf730B() {
+	less := func(i, j int) bool {
+		Println("?", i, j)
+		c := ""
+		Scan(&c)
+		return c == "<"
+	}
 	var T, n int
 	for Scan(&T); T > 0; T-- {
 		Scan(&n)
 		mn, mx := 1, 2-n%2
-		if n%2 == 0 && less30(2, 1) {
+		if n%2 == 0 && less(2, 1) {
 			mn, mx = 2, 1
 		}
 		for i := 3 - n%2; i <= n; i += 2 {
 			x, y := i, i+1
-			if less30(i+1, i) {
+			if less(i+1, i) {
 				x, y = i+1, i
 			}
-			if less30(x, mn) {
+			if less(x, mn) {
 				mn = x
 			}
-			if less30(mx, y) {
+			if less(mx, y) {
 				mx = y
 			}
 		}
