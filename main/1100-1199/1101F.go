a8a54e9230d1b5c5b25fdf68f855204bbbee4fa8
Endless <0x3f@0x3f3f3f3f.com>
2025-08-28 10:28:15 +0800

    cf1101F 决策单调性优化 DP

---
+++ main/1100-1199/1101F.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf1101F(in io.Reader, out io.Writer) {
+	var n, m, ans int
+	Fscan(in, &n, &m)
+	a := make([]int, n)
+	for i := range a {
+		Fscan(in, &a[i])
+	}
+
+	type query struct{ r, c, k int }
+	g := make([][]query, n)
+	for range m {
+		var l, r, c, k int
+		Fscan(in, &l, &r, &c, &k)
+		g[l-1] = append(g[l-1], query{r - 1, c, min(k+1, r-l)})
+	}
+
+	f := make([][]int, n)
+	for i := range f {
+		f[i] = make([]int, n)
+	}
+	for l, qs := range g {
+		f[0][l] = 0
+		for r := l + 1; r < n; r++ {
+			f[0][r] = 1e9
+		}
+		for k := 1; k < n-l; k++ {
+			opt := l
+			for r := l + 1; r < n; r++ {
+				for a[r]-a[opt+1] > f[k-1][opt+1] {
+					opt++
+				}
+				// V 型曲线谷底的两个数
+				f[k][r] = min(a[r]-a[opt], f[k-1][opt+1])
+			}
+		}
+		for _, q := range qs {
+			ans = max(ans, f[q.k][q.r]*q.c)
+		}
+	}
+	Fprint(out, ans)
+}
+
+//func main() { cf1101F(bufio.NewReader(os.Stdin), os.Stdout) }
