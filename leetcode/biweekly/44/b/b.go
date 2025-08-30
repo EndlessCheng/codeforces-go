63cff52d492efc1a4f20b726bd7ffcb02b1aa96b
Endless <0x3f@0x3f3f3f3f.com>
2025-08-30 08:56:31 +0800

    添加题解

--- leetcode/biweekly/44/b/b.go
+++ leetcode/biweekly/44/b/b.go
@@ -1,50 +1,82 @@
 package main
 
+import "slices"
+
 // github.com/EndlessCheng/codeforces-go
-func minimumTeachings(n int, languages, friendships [][]int) (ans int) {
-	ls := make([]map[int]bool, len(languages))
-	for i, l := range languages {
-		ls[i] = map[int]bool{}
-		for _, v := range l {
-			ls[i][v] = true
+func minimumTeachings1(n int, languages [][]int, friendships [][]int) int {
+	m := len(languages)
+	learned := make([][]bool, m)
+	for i, list := range languages {
+		learned[i] = make([]bool, n+1)
+		for _, x := range list {
+			learned[i][x] = true
 		}
 	}
 
-	ok := make([]bool, len(friendships))
-	for i, e := range friendships {
-		e[0]--
-		e[1]--
-		for v := range ls[e[0]] {
-			if ls[e[1]][v] {
-				ok[i] = true
-				break
+	todoList := [][2]int{}
+next:
+	for _, f := range friendships {
+		u, v := f[0]-1, f[1]-1 // 减一，下标从 0 开始
+		for _, x := range languages[u] {
+			if learned[v][x] { // 两人可以相互沟通，无需学习语言
+				continue next
 			}
 		}
+		todoList = append(todoList, [2]int{u, v})
 	}
 
-	ans = 1e9
-	for l := 1; l <= n; l++ {
-		taught := map[int]bool{}
-		for i, e := range friendships {
-			if ok[i] {
-				continue
+	ans := m
+	for k := 1; k <= n; k++ { // 枚举需要教的语言 k
+		set := map[int]struct{}{}
+		for _, f := range todoList {
+			u, v := f[0], f[1]
+			if !learned[u][k] { // u 需要学习语言 k
+				set[u] = struct{}{}
 			}
-			v, w := e[0], e[1]
-			if !ls[v][l] && !taught[v] {
-				taught[v] = true
-			}
-			if !ls[w][l] && !taught[w] {
-				taught[w] = true
+			if !learned[v][k] { // v 需要学习语言 k
+				set[v] = struct{}{}
 			}
 		}
-		ans = min(ans, len(taught))
+		ans = min(ans, len(set)) // len(set) 是需要学习语言 k 的人数
 	}
-	return
+	return ans
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
+func minimumTeachings(n int, languages [][]int, friendships [][]int) int {
+	m := len(languages)
+	learned := make([][]bool, m)
+	for i, list := range languages {
+		learned[i] = make([]bool, n+1)
+		for _, x := range list {
+			learned[i][x] = true
+		}
 	}
-	return b
+
+	vis := make([]bool, m)
+	cnt := make([]int, n+1)
+	total := 0
+	add := func(u int) {
+		if vis[u] {
+			return
+		}
+		total++
+		vis[u] = true // 避免重复统计
+		for _, x := range languages[u] {
+			cnt[x]++
+		}
+	}
+
+next:
+	for _, f := range friendships {
+		u, v := f[0]-1, f[1]-1 // 减一，下标从 0 开始
+		for _, x := range languages[u] {
+			if learned[v][x] { // 两人可以相互沟通，无需学习语言
+				continue next
+			}
+		}
+		add(u)
+		add(v)
+	}
+
+	return total - slices.Max(cnt)
 }
