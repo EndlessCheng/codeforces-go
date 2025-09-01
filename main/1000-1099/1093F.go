4e7e548a4c15a23d48b83d7c38b6d54ea6aaa744
Endless <0x3f@0x3f3f3f3f.com>
2025-09-01 19:00:57 +0800

    cf1093F DP 容斥型

---
+++ main/1000-1099/1093F.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf1093F(in io.Reader, out io.Writer) {
+	const mod = 998244353
+	var n, k, l, v int
+	Fscan(in, &n, &k, &l)
+	f := make([][]int, n+1)
+	f[0] = make([]int, k+1)
+	sumF := make([]int, n+1)
+	sumF[0] = 1
+	cnt := make([]int, k+1)
+	for i := 1; i <= n; i++ {
+		Fscan(in, &v)
+		f[i] = make([]int, k+1)
+		for x := 1; x <= k; x++ {
+			if v > 0 && x != v {
+				cnt[x] = 0
+				continue
+			}
+			cnt[x]++
+			f[i][x] = sumF[i-1]
+			if cnt[x] >= l {
+				f[i][x] = (f[i][x] - sumF[i-l] + f[i-l][x]) % mod
+			}
+			sumF[i] += f[i][x]
+		}
+		sumF[i] %= mod
+	}
+	Fprint(out, (sumF[n]+mod)%mod)
+}
+
+//func main() { cf1093F(bufio.NewReader(os.Stdin), os.Stdout) }
