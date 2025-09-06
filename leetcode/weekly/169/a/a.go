cf5a5004df1a3c0eac0d67b06461f3b158dc09e9
Endless <0x3f@0x3f3f3f3f.com>
2025-09-07 07:53:30 +0800

    添加方法二

--- leetcode/weekly/169/a/a.go
+++ leetcode/weekly/169/a/a.go
@@ -1,11 +1,20 @@
 package main
 
-func sumZero(n int) []int {
+func sumZero1(n int) []int {
 	a := make([]int, n)
 	m := n / 2
 	for i := range m {
 		a[i] = i + 1
 		a[i+m] = -i - 1
+	}
+	return a
+}
+
+func sumZero(n int) []int {
+	a := make([]int, n)
+	a[0] = -n * (n - 1) / 2
+	for i := 1; i < n; i++ {
+		a[i] = i
 	}
 	return a
 }
