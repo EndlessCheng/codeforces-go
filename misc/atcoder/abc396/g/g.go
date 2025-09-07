40de78a136639ee8c9b1da1d81b961cc0eef6313
Endless <0x3f@0x3f3f3f3f.com>
2025-09-07 08:14:12 +0800

    abc396_g

--- misc/atcoder/abc396/g/g.go
+++ misc/atcoder/abc396/g/g.go
@@ -10,23 +10,21 @@ import (
 // https://github.com/EndlessCheng
 func run(in io.Reader, out io.Writer) {
 	var n, m int
-	Fscan(in, &n, &m)
-	a := make([]string, n)
-	for i := range a {
-		Fscan(in, &a[i])
-	}
-
+	var s string
+	Fscan(in, &m, &n)
 	f := make([][]int, n+1)
 	for i := range f {
 		f[i] = make([]int, 1<<n)
 	}
-	for j := 0; j < m; j++ {
+	for i := 0; i < m; i++ {
+		Fscan(in, &s)
 		v := 0
-		for i, s := range a {
-			v |= int(s[j]-'0') << i
+		for j, c := range s {
+			v |= int(c-'0') << j
 		}
 		f[0][v]++
 	}
+
 	for i := 0; i < n; i++ {
 		for j := n; j > 0; j-- {
 			for k := range f[j] {
