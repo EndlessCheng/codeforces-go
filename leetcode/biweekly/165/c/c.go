45b008db617e2dfd8aecdacff69806d3b806ef86
Endless <0x3f@0x3f3f3f3f.com>
2025-09-14 12:31:31 +0800

    添加构造方法二

--- leetcode/biweekly/165/c/c.go
+++ leetcode/biweekly/165/c/c.go
@@ -8,17 +8,6 @@ func generateSchedule(n int) [][]int {
 
 	ans := make([][]int, 0, n*(n-1)) // 预分配空间
 
-	// 单独处理 d=1
-	for i := 0; i < n; i += 2 {
-		ans = append(ans, []int{i, (i + 1) % n})
-	}
-	for i := 1; i < n; i += 2 {
-		ans = append(ans, []int{i, (i + 1) % n})
-	}
-	if n%2 == 0 { // 保证 d=1 的最后一场比赛与 d=2 的第一场比赛无冲突
-		ans[len(ans)-1], ans[len(ans)-2] = ans[len(ans)-2], ans[len(ans)-1]
-	}
-
 	// 处理 d=2,3,...,n-2
 	for d := 2; d < n-1; d++ {
 		for i := range n {
@@ -26,15 +15,9 @@ func generateSchedule(n int) [][]int {
 		}
 	}
 
-	// 单独处理 d=n-1
-	for i := 1; i < n; i += 2 {
-		ans = append(ans, []int{i, (i + n - 1) % n})
-	}
-	if n%2 == 0 { // 保证 a 为奇数时的最后一场比赛与 a 为偶数时的第一场比赛无冲突
-		ans[len(ans)-1], ans[len(ans)-2] = ans[len(ans)-2], ans[len(ans)-1]
-	}
-	for i := 0; i < n; i += 2 {
-		ans = append(ans, []int{i, (i + n - 1) % n})
+	// 交错排列 d=1 与 d=n-1（或者说 d=-1）
+	for i := range n {
+		ans = append(ans, []int{i, (i + 1) % n}, []int{(i + n - 1) % n, (i + n - 2) % n})
 	}
 
 	return ans
