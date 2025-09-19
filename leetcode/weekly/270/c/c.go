9a94dd72f114f4e3fe91f269038ccef34c5b0bba
Endless <0x3f@0x3f3f3f3f.com>
2025-09-19 10:05:43 +0800

    重写题解

--- leetcode/weekly/270/c/c.go
+++ leetcode/weekly/270/c/c.go
@@ -5,61 +5,84 @@ import (
 	"strings"
 )
 
-/* 一题双解：BFS / 最近公共祖先
-
-#### 解法一：DFS + BFS
-
-我们可以从起点出发，通过 BFS 找到终点，同时记录每个点的来源节点和方向，在找到终点后，顺着来源节点往回走，同时记录答案。
-
-由于要往父节点方向走，我们需要先通过一次 DFS 记录每个节点的父节点，这样就可以在 BFS 中往父节点和左右节点三个方向前进了。
-
-DFS 的过程中也可以顺带找到起点。
-
-#### 解法二：最近公共祖先
-
-按照 [236. 二叉树的最近公共祖先](https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/) 给出的方法，我们可以从起点出发，找到到起点和终点的路径，然后去掉前缀相同的部分。剩下即为从起点和终点的最近公共祖先出发，到起点和终点的路径，分别记作 $\textit{pathToStart}$ 和 $\textit{pathToDest}$。
-
-我们要找的最短路径即为：起点 => 起点和终点的最近公共祖先 => 终点。
-
-对于起点到最近公共祖先这一段，可以看成长度为 $\textit{pathToStart}$ 的向父节点走的路径；对于最近公共祖先到终点这一段就是 $\textit{pathToDest}$。将这两段路径拼起来即为答案。
-
-*/
-
 // github.com/EndlessCheng/codeforces-go
 func getDirections(root *TreeNode, startValue, destValue int) string {
-	var path []byte
+	var pathToStart, pathToDest string
+	path := []byte{}
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+
+		if node.Val == startValue {
+			pathToStart = string(path)
+		} else if node.Val == destValue {
+			pathToDest = string(path)
+		}
+
+		// 往左找
+		path = append(path, 'L')
+		dfs(node.Left)
+
+		// 往右找
+		path[len(path)-1] = 'R' // 直接覆盖，无需恢复现场
+		dfs(node.Right)
+		path = path[:len(path)-1] // 恢复现场
+	}
+	dfs(root)
+
+	i := 0
+	for i < min(len(pathToStart), len(pathToDest)) && pathToStart[i] == pathToDest[i] {
+		i++
+	}
+
+	return strings.Repeat("U", len(pathToStart)-i) + pathToDest[i:]
+}
+
+func getDirections1(root *TreeNode, startValue, destValue int) string {
+	path := []byte{}
+	// dfs 返回是否找到了 target
 	var dfs func(*TreeNode, int) bool
 	dfs = func(node *TreeNode, target int) bool {
 		if node == nil {
 			return false
 		}
+
+		// 找到 target
 		if node.Val == target {
 			return true
 		}
+
+		// 往左找 target
 		path = append(path, 'L')
 		if dfs(node.Left, target) {
 			return true
 		}
-		path[len(path)-1] = 'R'
+
+		// 往右找 target
+		path[len(path)-1] = 'R' // 直接覆盖，无需恢复现场
 		if dfs(node.Right, target) {
 			return true
 		}
-		path = path[:len(path)-1]
+
+		path = path[:len(path)-1] // 恢复现场
 		return false
 	}
+
 	dfs(root, startValue)
 	pathToStart := path
 
-	path = nil
+	path = []byte{}
 	dfs(root, destValue)
 	pathToDest := path
 
-	for len(pathToStart) > 0 && len(pathToDest) > 0 && pathToStart[0] == pathToDest[0] {
-		pathToStart = pathToStart[1:]
-		pathToDest = pathToDest[1:]
+	i := 0
+	for i < min(len(pathToStart), len(pathToDest)) && pathToStart[i] == pathToDest[i] {
+		i++
 	}
 
-	return strings.Repeat("U", len(pathToStart)) + string(pathToDest)
+	return strings.Repeat("U", len(pathToStart)-i) + string(pathToDest[i:])
 }
 
 func getDirections2(root *TreeNode, startValue, destValue int) string {
