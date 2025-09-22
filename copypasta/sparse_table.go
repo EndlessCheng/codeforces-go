9abde227ae8988cf5045d95c1349cae55cecc6fd
Endless <0x3f@0x3f3f3f3f.com>
2025-09-22 16:27:52 +0800

    add links

--- copypasta/sparse_table.go
+++ copypasta/sparse_table.go
@@ -107,3 +107,12 @@ func (st sparseTableWithIndex) query(l, r int) int {
 	}
 	return b.i
 }
+
+// 二维 ST 表
+// 其一 · 保证询问的是正方形区域
+// https://blog.csdn.net/weixin_41162823/article/details/98471161
+// https://www.luogu.com.cn/problem/P2216
+
+// 其二 · 询问任意长宽
+// https://blog.nowcoder.net/n/3eccd1386a8846398d3bee62b485309b
+// https://codeforces.com/problemset/problem/1301/E 2500
