958a0aca5699e0bb3de27509d0328fdcec2b37b8
Endless <0x3f@0x3f3f3f3f.com>
2025-09-23 14:25:22 +0800

    添加题解

--- leetcode/weekly/198/a/a.go
+++ leetcode/weekly/198/a/a.go
@@ -1,9 +1,13 @@
 package main
 
-func numWaterBottles(n, e int) (ans int) {
-	for ; n >= e; n -= e - 1 {
-		ans += e
+func numWaterBottles1(numBottles, numExchange int) (ans int) {
+	for numBottles >= numExchange {
+		ans += numExchange
+		numBottles -= numExchange - 1
 	}
-	ans += n
-	return
+	return ans + numBottles
+}
+
+func numWaterBottles(numBottles, numExchange int) int {
+	return numBottles + (numBottles-1)/(numExchange-1)
 }
