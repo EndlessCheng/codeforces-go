4ed741a8fc9b4d7db591de2aac80fd9df95bab60
Endless <0x3f@0x3f3f3f3f.com>
2025-10-02 13:32:29 +0800

    lc1488

--- leetcode/weekly/194/c/c.go
+++ leetcode/weekly/194/c/c.go
@@ -1,113 +1,71 @@
 package main
 
-type node struct {
-	lr       [2]*node
-	priority uint
-	key      int
-}
+import "github.com/emirpasic/gods/v2/trees/redblacktree"
 
-func (o *node) cmp(b int) int8 {
-	switch {
-	case b < o.key:
-		return 0
-	case b > o.key:
-		return 1
-	default:
-		return -1
-	}
-}
-
-func (o *node) rotate(d int8) *node {
-	x := o.lr[d^1]
-	o.lr[d^1] = x.lr[d]
-	x.lr[d] = o
-	return x
-}
-
-type treap struct {
-	rd   uint
-	root *node
-}
-
-func (t *treap) fastRand() uint {
-	t.rd ^= t.rd << 13
-	t.rd ^= t.rd >> 17
-	t.rd ^= t.rd << 5
-	return t.rd
-}
-
-func (t *treap) _put(o *node, key int) *node {
-	if o == nil {
-		return &node{priority: t.fastRand(), key: key}
-	}
-	if d := o.cmp(key); d >= 0 {
-		o.lr[d] = t._put(o.lr[d], key)
-		if o.lr[d].priority > o.priority {
-			o = o.rotate(d ^ 1)
-		}
-	}
-	return o
-}
-
-func (t *treap) put(key int) { t.root = t._put(t.root, key) }
-
-func (t *treap) _delete(o *node, key int) *node {
-	if o == nil {
-		return nil
-	}
-	if d := o.cmp(key); d >= 0 {
-		o.lr[d] = t._delete(o.lr[d], key)
-	} else {
-		if o.lr[1] == nil {
-			return o.lr[0]
-		}
-		if o.lr[0] == nil {
-			return o.lr[1]
-		}
-		d = 0
-		if o.lr[0].priority > o.lr[1].priority {
-			d = 1
-		}
-		o = o.rotate(d)
-		o.lr[d] = t._delete(o.lr[d], key)
-	}
-	return o
-}
-
-func (t *treap) delete(key int) { t.root = t._delete(t.root, key) }
-
-func (t *treap) next(key int) (next *node) {
-	for o := t.root; o != nil; {
-		if o.cmp(key) == 0 {
-			next = o
-			o = o.lr[0]
-		} else {
-			o = o.lr[1]
-		}
-	}
-	return
-}
-
-func avoidFlood(a []int) (ans []int) {
-	ans = make([]int, len(a))
-	lpos := map[int]int{}
-	zpos := &treap{rd: 1}
-	for i, lake := range a {
+func avoidFlood1(rains []int) []int {
+	ans := make([]int, len(rains))
+	fullDay := map[int]int{}                    // lake -> 装满日
+	dryDay := redblacktree.New[int, struct{}]() // 未被使用的抽水日
+	for i, lake := range rains {
 		if lake == 0 {
-			ans[i] = 1
-			zpos.put(i)
+			ans[i] = 1                // 先随便选一个湖抽干
+			dryDay.Put(i, struct{}{}) // 保存抽水日
 			continue
 		}
-		ans[i] = -1
-		if l, ok := lpos[lake]; ok {
-			p := zpos.next(l)
-			if p == nil {
-				return []int{}
+		if j, ok := fullDay[lake]; ok {
+			// 必须在 j 之后，i 之前把 lake 抽干
+			// 选一个最早的未被使用的抽水日，如果选晚的，可能会导致其他湖没有可用的抽水日
+			node, _ := dryDay.Ceiling(j)
+			if node == nil {
+				return nil // 无法阻止洪水
 			}
-			ans[p.key] = lake
-			zpos.delete(p.key)
+			ans[node.Key] = lake
+			dryDay.Remove(node.Key) // node.Key 已使用，移除
 		}
-		lpos[lake] = i
+		ans[i] = -1
+		fullDay[lake] = i // 插入或更新装满日
 	}
-	return
+	return ans
+}
+
+func avoidFlood(rains []int) []int {
+	n := len(rains)
+	// 非递归并查集
+	fa := make([]int, n+1)
+	for i := range fa {
+		fa[i] = i
+	}
+	find := func(x int) int {
+		rt := x
+		for fa[rt] != rt {
+			rt = fa[rt]
+		}
+		for fa[x] != rt {
+			fa[x], x = rt, fa[x]
+		}
+		return rt
+	}
+
+	ans := make([]int, n)
+	fullDay := map[int]int{} // lake -> 装满日
+	for i, lake := range rains {
+		if lake == 0 {
+			ans[i] = 1 // 先随便选一个湖抽干
+			continue
+		}
+		if j, ok := fullDay[lake]; ok {
+			// 必须在 j 之后，i 之前把 lake 抽干
+			// 选一个最早的未被使用的抽水日，如果选晚的，可能会导致其他湖没有可用的抽水日
+			dryDay := find(j + 1)
+			if dryDay >= i {
+				return nil // 无法阻止洪水
+			}
+			ans[dryDay] = lake
+			fa[dryDay] = find(dryDay + 1) // 删除 dryDay
+		}
+		ans[i] = -1
+		fa[i] = i + 1 // 删除 i
+		fullDay[lake] = i // 插入或更新装满日
+	}
+	return ans
 }
