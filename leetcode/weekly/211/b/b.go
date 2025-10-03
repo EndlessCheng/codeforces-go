d8a362447ef9df98866cda4efbef833f051e7d97
Endless <0x3f@0x3f3f3f3f.com>
2025-10-03 10:38:24 +0800

    add lc1625

--- leetcode/weekly/211/b/b.go
+++ leetcode/weekly/211/b/b.go
@@ -1,33 +1,41 @@
 package main
 
-// github.com/EndlessCheng/codeforces-go
-func findLexSmallestString(s string, A int, shift int) (ans string) {
-	add := func(b, inc byte) byte { return '0' + (b&15+inc)%10 }
+import "bytes"
 
-	n, a, up := len(s), byte(A), byte(shift&1*9)
-	ans = s
-	for range s {
-		for i := byte(0); i < 10; i++ { // 奇
-			for i2 := byte(0); i2 <= up; i2++ { // 偶
-				t := []byte(s)
-				for j, b := range t {
-					if j&1 > 0 {
-						t[j] = add(b, i*a)
-					} else {
-						t[j] = add(b, i2*a)
-					}
-				}
-				ans = min(ans, string(t))
+// github.com/EndlessCheng/codeforces-go
+func findLexSmallestString(s string, a int, b int) string {
+	n := len(s)
+	step := gcd(b, n)
+	g := gcd(a, 10)
+	var ans []byte
+
+	for i := 0; i < n; i += step {
+		t := []byte(s[i:] + s[:i])
+		modify := func(start int) {
+			ch := t[start] - '0' // 最靠前的数字，越小越好
+			// ch 可以变成的最小值为 ch%g
+			// 例如 ch=5，g=2，那么 ch+2+2+2（模 10）后变成 1，不可能变得更小
+			// 从 ch 到 ch%g，需要增加 inc，其中 +10 保证 inc 非负（循环中会 %10 保证结果在 [0,9] 中）
+			inc := ch%byte(g) + 10 - ch
+			for j := start; j < n; j += 2 {
+				t[j] = '0' + (t[j]-'0'+inc)%10
 			}
 		}
-		s = s[n-shift:] + s[:n-shift]
+		modify(1) // 累加操作（所有奇数下标）
+		if step%2 > 0 { // 轮转一次后，原来的偶数下标变成奇数下标，所以能对偶数下标执行累加操作
+			modify(0) // 累加操作（所有偶数下标）
+		}
+		if ans == nil || bytes.Compare(t, ans) < 0 {
+			ans = t
+		}
 	}
-	return
+
+	return string(ans)
 }
 
-func min(a, b string) string {
-	if a < b {
-		return a
+func gcd(a, b int) int {
+	for a != 0 {
+		a, b = b%a, a
 	}
 	return b
 }
