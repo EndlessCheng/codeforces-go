72ed3747a5d35ae2de931038b76aa8d47da93ecd
Endless <0x3f@0x3f3f3f3f.com>
2025-10-04 20:28:11 +0800

    最小表示法添加注释

--- copypasta/strings.go
+++ copypasta/strings.go
@@ -493,8 +493,10 @@ func _() {
 	// 找到位置 i，从这个位置输出即得到字典序最小的串
 	// https://oi-wiki.org/string/minimal-string/
 	// 其他方法 https://codeforces.com/blog/entry/90035
+	// 时间复杂度：O(n)
+	// 看代码，如果 s[i+k] < s[j+k]，那么我们排除
 	//
-	// 模板题 https://www.luogu.com.cn/problem/P1368 http://poj.org/problem?id=1509 https://codeforces.com/gym/103585/problem/K
+	// 模板题 https://www.luogu.com.cn/problem/P13270 https://www.luogu.com.cn/problem/P1368 http://poj.org/problem?id=1509 https://codeforces.com/gym/103585/problem/K
 	// https://codeforces.com/problemset/problem/496/B 非暴力做法
 	// 应用：判断两个字符串是否循环同构
 	// - 都转成最小表示，看是否一样
@@ -506,6 +508,8 @@ func _() {
 		// 如果要返回一个和原串不同的字符串，初始化 i=1, j=2
 		i := 0
 		for j := 1; j < n; {
+			// 暴力比较：是 i 开头的字典序小，还是 j 开头的字典序小？
+			// 相同就继续往后比，至多循环 n 次（如果循环 n 次，说明所有字母都相同，不用再比了）
 			k := 0
 			for k < n && s[i+k] == s[j+k] {
 				k++
@@ -513,13 +517,29 @@ func _() {
 			if k >= n {
 				break
 			}
+
 			if s[i+k] < s[j+k] { // 改成 > 则返回字典序最大的
-				// j 到 j+k 都不会是最小串的开头位置
+				// 从 i 开始比从 j 开始更小，排除 j
+				// 此外：
+				// 从 i+1 开始比从 j+1 开始更小，所以从 j+1 开始不可能是答案，排除
+				// 从 i+2 开始比从 j+2 开始更小，所以从 j+2 开始不可能是答案，排除
+				// ……
+				// 从 i+k 开始比从 j+k 开始更小，所以从 j+k 开始不可能是答案，排除
+				// 所以下一个「可能是答案」的开始位置是 j+k+1
 				j += k + 1
 			} else {
-				// i 到 i+k 都不会是最小串的开头位置
+				// 从 j 开始比从 i 开始更小，更新 i=j
+				// 此外：
+				// 从 j+1 开始比从 i+1 开始更小，所以从 i+1 开始不可能是答案，排除
+				// 从 j+2 开始比从 i+2 开始更小，所以从 i+2 开始不可能是答案，排除
+				// ……
+				// 从 j+k 开始比从 i+k 开始更小，所以从 i+k 开始不可能是答案，排除
+				// 所以把 j 跳到 i+k+1，不过这可能比 j+1 小，所以与 j+1 取 max
+				// 综上所述，下一个「可能是答案」的开始位置是 max(j+1,i+k+1)
 				i, j = j, max(j, i+k)+1
 			}
+			// 每次要么排除 k+1 个与 i 相关的位置（这样的位置至多 n 个），要么排除 k+1 个与 j 相关的位置（这样的位置至多 n 个）
+			// 所以上面关于 k 的循环，∑k <= 2n，所以二重循环的总循环次数是 O(n) 的
 		}
 		return s[i : i+n]
 	}
