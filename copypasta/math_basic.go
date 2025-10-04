6f46a27b20b8080ac1ba18efaf98e9b1ed9246c2
Endless <0x3f@0x3f3f3f3f.com>
2025-10-04 16:51:21 +0800

    add link

--- copypasta/math_basic.go
+++ copypasta/math_basic.go
@@ -8,4 +8,7 @@ https://codeforces.com/problemset/problem/11/A 900 上取整
 https://codeforces.com/problemset/problem/485/A 1400
 https://codeforces.com/problemset/problem/1759/D 1400
 https://codeforces.com/problemset/problem/1950/F 1700
+
+式子变形
+https://codeforces.com/problemset/problem/293/C 2400
 */
