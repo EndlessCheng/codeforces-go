50e1b83611676c19af53c117fe6fbbc7a0f9cca0
Endless <0x3f@0x3f3f3f3f.com>
2025-10-18 20:14:12 +0800

    cf2072E

--- main/2000-2099/2072E.go
+++ main/2000-2099/2072E.go
@@ -11,21 +11,20 @@ import (
 func cf2072E(in io.Reader, _w io.Writer) {
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
-	var T, k, x, y int
+	var T, k, y int
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &k)
-		a := [][2]int{}
+		a := []int{}
 		for ; k > 0; y++ {
 			m := (int(math.Sqrt(float64(k*8+1))) + 1) / 2
 			for range m {
-				a = append(a, [2]int{x, y})
-				x++
+				a = append(a, y)
 			}
 			k -= m * (m - 1) / 2
 		}
 		Fprintln(out, len(a))
-		for _, p := range a {
-			Fprintln(out, p[0], p[1])
+		for x, y := range a {
+			Fprintln(out, x, y)
 		}
 	}
 }
