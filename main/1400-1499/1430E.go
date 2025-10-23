7604441f400062015cf60b3aaae45157cb289e1d
Endless <0x3f@0x3f3f3f3f.com>
2025-10-23 14:14:16 +0800

    CF1430E

--- main/1400-1499/1430E.go
+++ main/1400-1499/1430E.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"bufio"
 	. "fmt"
 	"io"
 )
 
 // github.com/EndlessCheng/codeforces-go
-func CF1430E(_r io.Reader, out io.Writer) {
+func CF1430E(in io.Reader, out io.Writer) {
 	var n int
 	var s []byte
-	Fscan(bufio.NewReader(_r), &n, &s)
+	Fscan(in, &n, &s)
 	tree := make([]int, n+1)
 	add := func(i int) {
 		for ; i <= n; i += i & -i {
@@ -25,21 +24,20 @@ func CF1430E(_r io.Reader, out io.Writer) {
 	}
 	query := func(l, r int) int { return sum(r) - sum(l-1) }
 
-	posS := [26][]int{}
-	t := make([]byte, n)
+	pos := [26][]int{}
 	for i, b := range s {
 		b -= 'a'
-		t[n-1-i] = b
-		posS[b] = append(posS[b], i)
+		pos[b] = append(pos[b], i)
 	}
 	ans := int64(0)
-	for i, b := range t {
-		p := posS[b][0]
-		posS[b] = posS[b][1:]
+	for i := range n {
+		b := s[n-1-i] - 'a'
+		p := pos[b][0]
+		pos[b] = pos[b][1:]
 		ans += int64(p + query(p+1, n) - i)
 		add(p + 1)
 	}
 	Fprint(out, ans)
 }
 
-//func main() { CF1430E(os.Stdin, os.Stdout) }
+//func main() { CF1430E(bufio.NewReader(os.Stdin), os.Stdout) }
