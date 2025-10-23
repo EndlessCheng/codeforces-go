49e89bef022662aa078bc196cdf5f5e4b5904dde
Endless <0x3f@0x3f3f3f3f.com>
2025-10-23 15:02:33 +0800

    cf1857E

---
+++ main/1800-1899/1857E.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"slices"
+)
+
+// https://github.com/EndlessCheng
+func cf1857E(in io.Reader, _w io.Writer) {
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+	var T, n int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n)
+		type pair struct{ x, i int }
+		a := make([]pair, n)
+		for i := range a {
+			Fscan(in, &a[i].x)
+			a[i].i = i
+		}
+		slices.SortFunc(a, func(a, b pair) int { return a.x - b.x })
+
+		s := n
+		for _, p := range a {
+			s += p.x - a[0].x
+		}
+
+		ans := make([]any, n)
+		ans[a[0].i] = s
+		for i := 1; i < n; i++ {
+			s -= (n - i*2) * (a[i].x - a[i-1].x)
+			ans[a[i].i] = s
+		}
+		Fprintln(out, ans...)
+	}
+}
+
+//func main() { cf1857E(bufio.NewReader(os.Stdin), os.Stdout) }
