8227e670ad077d9faaa807a1126ad718da3fbd7e
Endless <0x3f@0x3f3f3f3f.com>
2025-10-24 17:42:18 +0800

    添加题解

--- leetcode/weekly/280/a/a.go
+++ leetcode/weekly/280/a/a.go
@@ -1,12 +1,10 @@
 package main
 
-// 在辗转相除法的基础上稍作修改，累加两数相除时的商，即为答案。
-
 // github.com/EndlessCheng/codeforces-go
-func countOperations(num1, num2 int) (ans int) {
-	for num1 > 0 {
-		ans += num2 / num1
-		num1, num2 = num2%num1, num1
+func countOperations(x, y int) (ans int) {
+	for y > 0 {
+		ans += x / y // x 变成 x%y
+		x, y = y, x%y
 	}
 	return
 }
