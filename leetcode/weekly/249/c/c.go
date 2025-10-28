ce4e52b4a03efef8fc0c2ca336444345bc9cb12a
Endless <0x3f@0x3f3f3f3f.com>
2025-10-28 11:34:11 +0800

    添加方法四：Berlekamp-Massey 算法 + Kitamasa 算法

--- leetcode/weekly/249/c/c.go
+++ leetcode/weekly/249/c/c.go
@@ -1,7 +1,9 @@
 package main
 
+import "slices"
+
 // github.com/EndlessCheng/codeforces-go
-func colorTheGrid(m, n int) int {
+func colorTheGrid1(m, n int) int {
 	const mod = 1_000_000_007
 	pow3 := make([]int, m)
 	pow3[0] = 1
@@ -55,4 +57,91 @@ next:
 		ans += fv
 	}
 	return ans % mod
+}
+
+const mod = 1_000_000_007
+
+func pow(x, n int) int {
+	res := 1
+	for ; n > 0; n /= 2 {
+		if n%2 > 0 {
+			res = res * x % mod
+		}
+		x = x * x % mod
+	}
+	return res
+}
+
+// 给定常系数齐次线性递推式 f(n) = coef[k-1] * f(n-1) + coef[k-2] * f(n-2) + ... + coef[0] * f(n-k)
+// 以及初始值 f(i) = a[i] (0 <= i < k)
+// 返回 f(n) % mod，其中参数 n 从 0 开始
+// 注意 coef 的顺序
+// 时间复杂度 O(k^2 log n)，其中 k 是 coef 的长度
+func kitamasa(coef, a []int, n int) (ans int) {
+	if n < len(a) {
+		return a[n] % mod
+	}
+
+	k := len(coef)
+	if k == 1 {
+		return a[0] * pow(coef[0], n) % mod
+	}
+
+	// 已知 f(n) 的各项系数为 a，f(m) 的各项系数为 b
+	// 计算并返回 f(n+m) 的各项系数 c
+	compose := func(a, b []int) []int {
+		c := make([]int, k)
+		for _, v := range a {
+			for j, w := range b {
+				c[j] = (c[j] + v*w) % mod
+			}
+			// 原地计算下一组系数，比如已知 f(4) 的各项系数，现在要计算 f(5) 的各项系数
+			// 倒序遍历，避免提前覆盖旧值
+			bk1 := b[k-1]
+			for i := k - 1; i > 0; i-- {
+				b[i] = (b[i-1] + bk1*coef[i]) % mod
+			}
+			b[0] = bk1 * coef[0] % mod
+		}
+		return c
+	}
+
+	// 计算 resC，以表出 f(n) = resC[k-1] * a[k-1] + resC[k-2] * a[k-2] + ... + resC[0] * a[0]
+	resC := make([]int, k)
+	resC[0] = 1
+	c := make([]int, k)
+	c[1] = 1
+	for ; n > 0; n /= 2 {
+		if n%2 > 0 {
+			resC = compose(c, resC)
+		}
+		// 由于会修改 compose 的第二个参数，这里把 c 复制一份再传入
+		c = compose(c, slices.Clone(c))
+	}
+
+	for i, c := range resC {
+		ans = (ans + c*a[i]) % mod
+	}
+
+	return (ans + mod) % mod // 保证结果非负
+}
+
+// 注意 kitamasa 入参的顺序
+var coef = [][]int{
+	{2},
+	{3},
+	{-2, 5},
+	{6, -15, 9},
+	{8, -48, 92, -65, 16},
+}
+var a = [][]int{
+	{3},
+	{6},
+	{12, 54},
+	{24, 162, 1122},
+	{48, 486, 5118, 54450, 580986},
+}
+
+func colorTheGrid(m, n int) int {
+	return kitamasa(coef[m-1], a[m-1], n-1)
 }
