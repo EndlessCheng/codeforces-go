2a8ef208ffa8c11aaee5e477bdc4d29336538c40
Endless <0x3f@0x3f3f3f3f.com>
2025-11-02 08:31:37 +0800

    cf2149E 简化代码

--- main/2100-2199/2149E.go
+++ main/2100-2199/2149E.go
@@ -7,19 +7,19 @@ import (
 
 // https://github.com/EndlessCheng
 func cf2149E(in io.Reader, out io.Writer) {
-	var T, n, k, l, r int
+	var T, n, k, L, R int
 	for Fscan(in, &T); T > 0; T-- {
-		Fscan(in, &n, &k, &l, &r)
+		Fscan(in, &n, &k, &L, &R)
 		a := make([]int, n)
 		for i := range a {
 			Fscan(in, &a[i])
 		}
-		f := func(mxL, mxK int) (res int) {
+		f := func(mx int) (res int) {
 			cnt := map[int]int{}
 			l := 0
 			for i, v := range a {
 				cnt[v]++
-				for len(cnt) > mxK || i-l+1 > mxL {
+				for len(cnt) > mx {
 					w := a[l]
 					cnt[w]--
 					if cnt[w] == 0 {
@@ -27,11 +27,11 @@ func cf2149E(in io.Reader, out io.Writer) {
 					}
 					l++
 				}
-				res += i - l + 1
+				res += max(min(i-l+1, R)-L+1, 0)
 			}
 			return
 		}
-		Fprintln(out, f(r, k)-f(r, k-1)-f(l-1, k)+f(l-1, k-1))
+		Fprintln(out, f(k)-f(k-1))
 	}
 }
 
