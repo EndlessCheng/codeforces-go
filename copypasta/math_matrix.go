6e57254c1ee767c962a8731d4fc930397c8c750b
Endless <0x3f@0x3f3f3f3f.com>
2025-11-08 19:51:45 +0800

    add link

--- copypasta/math_matrix.go
+++ copypasta/math_matrix.go
@@ -49,13 +49,14 @@ https://codeforces.com/problemset/problem/107/D 2400
 https://codeforces.com/problemset/problem/226/C 2400 https://www.luogu.com.cn/problem/P1306
 https://codeforces.com/problemset/problem/593/E 2400 分段
 https://codeforces.com/problemset/problem/93/D 2500 前 n 项之和
+https://codeforces.com/problemset/problem/351/C 2500 min +
 https://codeforces.com/problemset/problem/60/E 2600
 https://codeforces.com/problemset/problem/1895/F 2600 如何容斥（正难则反）
 https://codeforces.com/problemset/problem/498/E 2700 分段 DP套DP
 https://codeforces.com/problemset/problem/575/A 2700 分段 倍增
 https://codeforces.com/problemset/problem/1511/F 2700 字典树 todo 什么情况下系数矩阵阶数最大？
 https://codeforces.com/problemset/problem/497/E 2900 数位 DP
-https://codeforces.com/problemset/problem/917/C 2900 分段
+https://codeforces.com/problemset/problem/917/C 2900 分段 min +
 https://codeforces.com/problemset/problem/1152/F2 3000 状态设计
 https://codeforces.com/problemset/problem/618/G 3300 期望 DP
 https://atcoder.jp/contests/abc232/tasks/abc232_e
