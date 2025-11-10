3a8da068791f9149b6716039d6e5f7615d8c71e4
Endless <0x3f@0x3f3f3f3f.com>
2025-11-10 13:27:32 +0800

    upd

--- copypasta/hack.go
+++ copypasta/hack.go
@@ -12,7 +12,7 @@ import (
 https://github.com/LeetCode-Feedback/LeetCode-Feedback/issues?q=is%3Aissue+author%3AEndlessCheng+is%3Aclosed
 
 hack 乘法溢出
-https://leetcode.cn/problems/frog-position-after-t-seconds/solutions/2281408/dfs-ji-yi-ci-you-qu-de-hack-by-endlessch-jtsr/
+限定质因子最大值 https://leetcode.cn/problems/frog-position-after-t-seconds/solutions/2281408/dfs-ji-yi-ci-you-qu-de-hack-by-endlessch-jtsr/
 
 用所有的偶数 hack，O(U^logU)
 https://leetcode.cn/problems/maximum-total-reward-using-operations-i/solutions/2805422/0-1-bei-bao-by-endlesscheng-702p/comments/2435993
