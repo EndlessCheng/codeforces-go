8a5ae6c6a8ed37d81e799b3faeefd19f30ea18ac
Endless <0x3f@0x3f3f3f3f.com>
2025-11-13 14:27:27 +0800

    add lc1929

--- leetcode/weekly/249/a/a.go
+++ leetcode/weekly/249/a/a.go
@@ -1,6 +1,16 @@
 package main
 
 // github.com/EndlessCheng/codeforces-go
-func getConcatenation(a []int) []int {
-	return append(a, a...)
+func getConcatenation1(nums []int) []int {
+	return append(nums, nums...)
+}
+
+func getConcatenation(nums []int) []int {
+	n := len(nums)
+	ans := make([]int, n*2)
+	for i, x := range nums {
+		ans[i] = x
+		ans[i+n] = x
+	}
+	return ans
 }
