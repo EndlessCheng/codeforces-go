72a8cf5ab8d8fbce0f2275db67134ad00f1ae101
Endless <0x3f@0x3f3f3f3f.com>
2025-11-14 16:54:30 +0800

    add lc1475

--- leetcode/biweekly/28/a/a.go
+++ leetcode/biweekly/28/a/a.go
@@ -1,16 +1,30 @@
 package main
 
 // github.com/EndlessCheng/codeforces-go
-func finalPrices(prices []int) (ans []int) {
-o:
-	for i, v := range prices {
-		for _, w := range prices[i+1:] {
-			if w <= v {
-				ans = append(ans, v-w)
-				continue o
-			}
+func finalPrices1(prices []int) []int {
+	n := len(prices)
+	st := []int{0} // 哨兵，作为没有折扣时的栈顶值
+	ans := make([]int, n)
+	for i := n - 1; i >= 0; i-- {
+		p := prices[i]
+		for st[len(st)-1] > p {
+			st = st[:len(st)-1] // p 比栈顶小，比栈顶更能成为左侧元素的折扣值
 		}
-		ans = append(ans, v)
+		// 循环结束后，栈顶的价格 <= p，作为折扣值
+		ans[i] = p - st[len(st)-1]
+		st = append(st, p)
 	}
-	return
+	return ans
+}
+
+func finalPrices(prices []int) []int {
+	st := []int{} // todolist
+	for i, p := range prices {
+		for len(st) > 0 && prices[st[len(st)-1]] >= p {
+			prices[st[len(st)-1]] -= p // p 是栈顶的折扣值
+			st = st[:len(st)-1]
+		}
+		st = append(st, i)
+	}
+	return prices
 }
