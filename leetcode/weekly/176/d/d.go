129122903f72c584d299860dd5f70e5339846ff5
Endless <0x3f@0x3f3f3f3f.com>
2025-11-15 12:35:29 +0800

    add lc1354 题解

--- leetcode/weekly/176/d/d.go
+++ leetcode/weekly/176/d/d.go
@@ -5,32 +5,37 @@ import (
 	"sort"
 )
 
-// github.com/EndlessCheng/codeforces-go
-type hp struct{ sort.IntSlice }
-
-func (h hp) Less(i, j int) bool    { return h.IntSlice[i] > h.IntSlice[j] }
-func (h *hp) Push(interface{})     {}
-func (h *hp) Pop() (_ interface{}) { return }
-
-func isPossible(a []int) bool {
+// github.com/EndlessCheng
+func isPossible(target []int) bool {
 	sum := 0
-	for _, v := range a {
-		sum += v
+	for _, x := range target {
+		sum += x
 	}
-	h := hp{a}
+
+	h := hp{target}
 	heap.Init(&h)
-	for {
-		max := h.IntSlice[0]
-		sum -= max
-		if max == 1 || sum == 1 {
-			return true
-		}
-		if max < sum || sum == 0 || max%sum == 0 {
+
+	// 如果最大值等于 1，说明所有数都等于 1
+	for h.IntSlice[0] > 1 {
+		x := h.IntSlice[0]
+		sum -= x // 减去 x 后，sum 为其余元素之和
+		// sum 不能是 0，这意味着 target 只有一个数且这个数大于 1
+		// x 减去 sum 后必须是正数
+		if sum == 0 || x <= sum {
 			return false
 		}
-		max %= sum
-		sum += max
-		h.IntSlice[0] = max
+		// 把 x 多次减去 sum，直到 x <= sum 为止
+		// 也就是计算 x%sum，但如果 x%sum == 0 则调整为 sum
+		x = (x-1)%sum + 1
+		sum += x
+		h.IntSlice[0] = x
 		heap.Fix(&h, 0)
 	}
+	return true
 }
+
+type hp struct{ sort.IntSlice }
+
+func (h hp) Less(i, j int) bool { return h.IntSlice[i] > h.IntSlice[j] }
+func (hp) Push(any)             {} // 没用到，无需实现
+func (hp) Pop() (_ any)         { return }
