943ed3bbd2090a14af0c3a9ad373584bb3d51fdc
Endless <0x3f@0x3f3f3f3f.com>
2025-11-22 13:53:01 +0800

    cf498A

--- main/400-499/498A.go
+++ main/400-499/498A.go
@@ -9,11 +9,10 @@ import (
 // https://github.com/EndlessCheng
 func cf498A(in io.Reader, out io.Writer) {
 	var x1, y1, x2, y2, n, a, b, c, ans int
-	f := func(x, y int) int { return a*x + b*y + c }
 	Fscan(in, &x1, &y1, &x2, &y2, &n)
 	for range n {
 		Fscan(in, &a, &b, &c)
-		if cmp.Compare(f(x1, y1), 0)*cmp.Compare(f(x2, y2), 0) < 0 {
+		if cmp.Compare(a*x1+b*y1+c, 0)*cmp.Compare(a*x2+b*y2+c, 0) < 0 {
 			ans++
 		}
 	}
