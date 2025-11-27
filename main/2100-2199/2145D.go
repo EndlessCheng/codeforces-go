db9449ff0dc6d60c7f479ef424a44588d56d8311
Endless <0x3f@0x3f3f3f3f.com>
2025-11-27 08:36:27 +0800

    cf2145D

---
+++ main/2100-2199/2145D.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf2145D(in io.Reader, out io.Writer) {
+	const mx = 30
+	f := [mx + 1][mx*(mx+1)/2 + 1]bool{0: {true}}
+	for i := 1; i <= mx; i++ {
+		for sz := 1; sz <= i; sz++ {
+			for v, ok := range f[i-sz] {
+				if ok {
+					f[i][v+sz*(sz+1)/2] = true
+				}
+			}
+		}
+	}
+
+	var T, n, k int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n, &k)
+		k = n*(n+1)/2 - k
+		if !f[n][k] {
+			Fprintln(out, 0)
+			continue
+		}
+		for n > 0 {
+			for sz := 1; sz <= n; sz++ {
+				if f[n-sz][k-sz*(sz+1)/2] {
+					for i := n - sz + 1; i <= n; i++ {
+						Fprint(out, i, " ")
+					}
+					n -= sz
+					k -= sz * (sz + 1) / 2
+					break
+				}
+			}
+		}
+		Fprintln(out)
+	}
+}
+
+//func main() { cf2145D(os.Stdin, os.Stdout) }
