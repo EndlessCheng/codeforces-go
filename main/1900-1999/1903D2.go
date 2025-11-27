5cdac33b2643f7590264d9338ceb93a85b0fcc68
Endless <0x3f@0x3f3f3f3f.com>
2025-11-27 20:43:27 +0800

    cf1903D2

---
+++ main/1900-1999/1903D2.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"math/bits"
+)
+
+// https://github.com/EndlessCheng
+func cf1903D2(in io.Reader, _w io.Writer) {
+	// todo 快读
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+	var n, q, v int
+	Fscan(in, &n, &q)
+	const w = 20
+	const u = 1 << w
+	low := n << w
+	f := [u][w][2]int{}
+	for range n {
+		Fscan(in, &v)
+		low -= v
+		for j := uint32(u - 1 ^ v); j > 0; j &= j - 1 {
+			i := bits.TrailingZeros32(j)
+			f[v][i][0]++ // 满足 v 但 i 位是 0 的数的个数
+			f[v][i][1] += v & (1<<i - 1)
+		}
+	}
+
+	for i := range w {
+		for s := 0; s < u; s++ {
+			s |= 1 << i
+			for j := range w { // todo 改成 f[s][:] 看看？
+				f[s^1<<i][j][0] += f[s][j][0]
+				f[s^1<<i][j][1] += f[s][j][1]
+			}
+		}
+	}
+
+	for range q {
+		Fscan(in, &v)
+		if v >= low {
+			Fprintln(out, u+(v-low)/n)
+			continue
+		}
+		ans, cnt := 0, 0
+		for i := w - 1; i >= 0; i-- {
+			p := f[ans][i] // 满足 ans 但 i 位是 0 的数的个数
+			c := (cnt+p[0])<<i - p[1] // 减掉已经有的
+			if c <= v {
+				v -= c
+				cnt += p[0]
+				ans |= 1 << i
+			}
+		}
+		Fprintln(out, ans)
+	}
+}
+
+//func main() { cf1903D2(bufio.NewReader(os.Stdin), os.Stdout) }
