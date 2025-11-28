6b43d3bf082719a8c12582bcb9f9ca2e13cdd6c7
Endless <0x3f@0x3f3f3f3f.com>
2025-11-28 08:27:17 +0800

    补充注释

--- main/1900-1999/1903D2.go
+++ main/1900-1999/1903D2.go
@@ -9,7 +9,6 @@ import (
 
 // https://github.com/EndlessCheng
 func cf1903D2(in io.Reader, _w io.Writer) {
-	// todo 快读
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
 	var n, q, v int
@@ -23,15 +22,16 @@ func cf1903D2(in io.Reader, _w io.Writer) {
 		low -= v
 		for j := uint32(u - 1 ^ v); j > 0; j &= j - 1 {
 			i := bits.TrailingZeros32(j)
-			f[v][i][0]++ // 满足 v 但 i 位是 0 的数的个数
+			f[v][i][0]++ // 恰好满足 v 但 i 位是 0 的数的个数
 			f[v][i][1] += v & (1<<i - 1)
 		}
 	}
 
+	// 算完 SOS DP 后，「恰好满足」就变成「至少满足 s 中的比特位是 1（其余比特位可能还有 1）」
 	for i := range w {
 		for s := 0; s < u; s++ {
 			s |= 1 << i
-			for j := range w { // todo 改成 f[s][:] 看看？
+			for j := range w {
 				f[s^1<<i][j][0] += f[s][j][0]
 				f[s^1<<i][j][1] += f[s][j][1]
 			}
@@ -46,7 +46,7 @@ func cf1903D2(in io.Reader, _w io.Writer) {
 		}
 		ans, cnt := 0, 0
 		for i := w - 1; i >= 0; i-- {
-			p := f[ans][i] // 满足 ans 但 i 位是 0 的数的个数
+			p := f[ans][i] // 至少满足 ans 但 i 位是 0 的数的个数
 			c := (cnt+p[0])<<i - p[1] // 减掉已经有的
 			if c <= v {
 				v -= c
