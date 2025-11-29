b85da90970053b9d829d51a91afc04af0e1fe389
Endless <0x3f@0x3f3f3f3f.com>
2025-11-30 06:23:11 +0800

    lc1590

--- leetcode/biweekly/35/c/c.go
+++ leetcode/biweekly/35/c/c.go
@@ -1,34 +1,29 @@
 package main
 
 // github.com/EndlessCheng/codeforces-go
-func minSubarray(a []int, p int) int {
-	n := len(a)
-	ans := n
-	sum := make([]int, n+1)
-	for i, v := range a {
-		sum[i+1] = sum[i] + v
+func minSubarray(nums []int, p int) int {
+	x := 0
+	for _, v := range nums {
+		x += v
 	}
-	pos := map[int][]int{}
-	for i, v := range sum {
-		v %= p
-		pos[v] = append(pos[v], i)
+	x %= p
+	if x == 0 {
+		return 0 // 移除空子数组（这个 if 可以不要）
 	}
-	for i, v := range sum {
-		v = (p - (sum[n]-v)%p) % p
-		for len(pos[v]) > 0 && pos[v][0] <= i {
-			ans = min(ans, i-pos[v][0])
-			pos[v] = pos[v][1:]
+
+	n := len(nums)
+	ans, s := n, 0
+	// 由于下面 i 是从 0 开始的，前缀和下标就要从 -1 开始了
+	last := map[int]int{s: -1}
+	for i, v := range nums {
+		s += v
+		last[s%p] = i
+		if j, ok := last[(s-x+p)%p]; ok {
+			ans = min(ans, i-j)
 		}
 	}
 	if ans < n {
 		return ans
 	}
 	return -1
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
 }
