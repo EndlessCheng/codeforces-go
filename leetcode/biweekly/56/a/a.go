68f6700c20b284f49ab3a8168501f31dd1ef3fe1
Endless <0x3f@0x3f3f3f3f.com>
2025-11-29 11:51:09 +0800

    add lc1925

--- leetcode/biweekly/56/a/a.go
+++ leetcode/biweekly/56/a/a.go
@@ -1,23 +1,36 @@
 package main
 
+import "math"
+
 // github.com/EndlessCheng/codeforces-go
-const mx = 250
-
-var c2 [mx*mx + 1]bool
-
-func init() {
-	for i := 1; i <= mx; i++ {
-		c2[i*i] = true
-	}
-}
-
-func countTriples(n int) (ans int) {
-	for i := 1; i <= n; i++ {
-		for j := 1; j < i; j++ {
-			if s := i*i + j*j; s <= n*n && c2[s] {
-				ans += 2
+func countTriples1(n int) (ans int) {
+	for a := 1; a < n; a++ {
+		for b := 1; b < a && a*a+b*b <= n*n; b++ {
+			c2 := a*a + b*b
+			rt := int(math.Sqrt(float64(c2)))
+			if rt*rt == c2 {
+				ans++
 			}
 		}
 	}
-	return
+	return ans * 2 // (a,b,c) 和 (b,a,c) 各算一次
+}
+
+func countTriples(n int) (ans int) {
+	for u := 3; u*u < n*2; u += 2 {
+		for v := 1; v < u && (u*u+v*v)/2 <= n; v += 2 {
+			if gcd(u, v) == 1 {
+				c0 := (u*u + v*v) / 2
+				ans += n / c0
+			}
+		}
+	}
+	return ans * 2
+}
+
+func gcd(a, b int) int {
+	for a != 0 {
+		a, b = b%a, a
+	}
+	return b
 }
