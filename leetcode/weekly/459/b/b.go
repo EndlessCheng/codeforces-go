523aa39a1a7bf5445ceca659c1f0933496bf8883
Endless <0x3f@0x3f3f3f3f.com>
2025-12-02 07:17:01 +0800

    优化

--- leetcode/weekly/459/b/b.go
+++ leetcode/weekly/459/b/b.go
@@ -3,7 +3,7 @@ package main
 // https://space.bilibili.com/206214
 func countTrapezoids(points [][]int) (ans int) {
 	const mod = 1_000_000_007
-	cnt := map[int]int{}
+	cnt := make(map[int]int, len(points)) // 预分配空间
 	for _, p := range points {
 		cnt[p[1]]++ // 统计每一行（水平线）有多少个点
 	}
