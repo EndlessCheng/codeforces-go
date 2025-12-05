16d74d5dcf43ca077eeefe1ca55eb818df2ded49
Endless <0x3f@0x3f3f3f3f.com>
2025-12-05 09:48:09 +0800

    cf311E

---
+++ main/300-399/311E.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf311E(in io.Reader, out io.Writer) {
+	var n, m, fg, k, tar, w, id, ans int
+	Fscan(in, &n, &m, &fg)
+
+	st := n + m
+	end := st + 1
+	type nb struct{ to, rid, cap int }
+	g := make([][]nb, end+1)
+	addEdge := func(from, to, cap int) {
+		g[from] = append(g[from], nb{to, len(g[to]), cap})
+		g[to] = append(g[to], nb{from, len(g[from]) - 1, 0})
+	}
+
+	sex := make([]bool, n)
+	for i := range sex {
+		Fscan(in, &sex[i])
+	}
+	for i, s := range sex {
+		Fscan(in, &k)
+		if !s {
+			addEdge(st, i, k)
+		} else {
+			addEdge(i, end, k)
+		}
+	}
+	for i := range m {
+		Fscan(in, &tar, &w, &k)
+		for range k {
+			Fscan(in, &id)
+			id--
+			if tar == 0 {
+				addEdge(n+i, id, 1e18)
+			} else {
+				addEdge(id, n+i, 1e18)
+			}
+		}
+		ans += w
+		Fscan(in, &k)
+		if tar == 0 {
+			addEdge(st, n+i, w+k*fg)
+		} else {
+			addEdge(n+i, end, w+k*fg)
+		}
+	}
+
+	dis := make([]int, len(g))
+	bfs := func() bool {
+		clear(dis)
+		dis[st] = 1
+		q := []int{st}
+		for len(q) > 0 {
+			v := q[0]
+			q = q[1:]
+			for _, e := range g[v] {
+				if w := e.to; e.cap > 0 && dis[w] == 0 {
+					dis[w] = dis[v] + 1
+					q = append(q, w)
+				}
+			}
+		}
+		return dis[end] > 0
+	}
+	iter := make([]int, len(g))
+	var dfs func(int, int) int
+	dfs = func(v, totalFlow int) (curFlow int) {
+		if v == end {
+			return totalFlow
+		}
+		for ; iter[v] < len(g[v]); iter[v]++ {
+			e := &g[v][iter[v]]
+			if w := e.to; e.cap > 0 && dis[w] > dis[v] {
+				f := dfs(w, min(totalFlow-curFlow, e.cap))
+				if f == 0 {
+					continue
+				}
+				e.cap -= f
+				g[w][e.rid].cap += f
+				curFlow += f
+				if curFlow == totalFlow {
+					break
+				}
+			}
+		}
+		return
+	}
+	maxFlow := 0
+	for bfs() {
+		clear(iter)
+		maxFlow += dfs(st, 1e18)
+	}
+	Fprint(out, ans-maxFlow)
+}
+
+//func main() { cf311E(bufio.NewReader(os.Stdin), os.Stdout) }
