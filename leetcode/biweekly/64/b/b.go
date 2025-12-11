9104199d58e6da1aa8fa532fe8408ad539154a8d
Endless <0x3f@0x3f3f3f3f.com>
2025-12-11 14:53:48 +0800

    lc2054

--- leetcode/biweekly/64/b/b.go
+++ leetcode/biweekly/64/b/b.go
@@ -1,37 +1,46 @@
 package main
 
 import (
-	"container/heap"
+	"slices"
 	"sort"
 )
 
 // github.com/EndlessCheng/codeforces-go
 func maxTwoEvents(events [][]int) (ans int) {
-	sort.Slice(events, func(i, j int) bool { return events[i][0] < events[j][0] }) // 按开始时间排序
-	h := hp{}
-	maxVal := 0 // 另一个活动的最大价值
+	// 按照结束时间排序
+	slices.SortFunc(events, func(a, b []int) int { return a[1] - b[1] })
+
+	// 从栈底到栈顶，结束时间递增，价值递增
+	type pair struct{ endTime, value int }
+	st := []pair{{}} // 栈底哨兵
 	for _, e := range events {
-		start, end, val := e[0], e[1], e[2]
-		for len(h) > 0 && h[0].end < start { // 如果结束时间早于当前活动开始时间
-			maxVal = max(maxVal, heap.Pop(&h).(pair).val) // 更新另一个活动的最大价值
+		startTime, value := e[0], e[2]
+		// 二分查找最后一个结束时间 < startTime 的活动
+		i := sort.Search(len(st), func(i int) bool { return st[i].endTime >= startTime }) - 1
+		ans = max(ans, st[i].value+value)
+		// 遇到比栈顶更大的价值，入栈
+		if value > st[len(st)-1].value {
+			st = append(st, pair{e[1], value})
 		}
-		ans = max(ans, maxVal+val) // 至多参加两个活动
-		heap.Push(&h, pair{end, val})
 	}
 	return
 }
 
-type pair struct{ end, val int }
-type hp []pair
-func (h hp) Len() int            { return len(h) }
-func (h hp) Less(i, j int) bool  { return h[i].end < h[j].end }
-func (h hp) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
-func (h *hp) Push(v interface{}) { *h = append(*h, v.(pair)) }
-func (h *hp) Pop() interface{}   { a := *h; v := a[len(a)-1]; *h = a[:len(a)-1]; return v }
+func maxTwoEvents(events [][]int) (ans int) {
+	slices.SortFunc(events, func(a, b []int) int { return a[1] - b[1] })
 
-func max(a, b int) int {
-	if b > a {
-		return b
+	st := events[:0] // 把 events 当作栈
+	for _, e := range events {
+		startTime, value := e[0], e[2]
+		i := sort.Search(len(st), func(i int) bool { return st[i][1] >= startTime }) - 1
+		if i >= 0 {
+			ans = max(ans, value+events[i][2])
+		} else {
+			ans = max(ans, value)
+		}
+		if len(st) == 0 || value > st[len(st)-1][2] {
+			st = append(st, e)
+		}
 	}
-	return a
+	return
 }
