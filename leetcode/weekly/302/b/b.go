e66b86a478780887d44fd1f1afc85dc3482fb059
Endless <0x3f@0x3f3f3f3f.com>
2025-12-17 18:59:37 +0800

    upd

--- leetcode/weekly/302/b/b.go
+++ leetcode/weekly/302/b/b.go
@@ -1,20 +1,21 @@
 package main
 
-// https://space.bilibili.com/206214/dynamic
+import "math"
+
+// https://space.bilibili.com/206214/
 func maximumSum(nums []int) int {
 	ans := -1
-	mx := map[int]int{}
-	for _, v := range nums {
-		s := 0
-		for x := v; x > 0; x /= 10 {
+	mx := [82]int{} // 至多 9 个 9 相加
+	for i := range mx {
+		mx[i] = math.MinInt
+	}
+	for _, num := range nums { // 枚举 num = nums[j]
+		s := 0 // num 的数位和
+		for x := num; x > 0; x /= 10 { // 枚举 num 的每个数位
 			s += x % 10
 		}
-		if mx[s] > 0 {
-			ans = max(ans, mx[s] + v)
-		}
-		mx[s] = max(mx[s], v)
+		ans = max(ans, mx[s]+num) // 左边找一个数位和也为 s 的最大的 nums[i]
+		mx[s] = max(mx[s], num)   // 维护数位和等于 s 的最大元素
 	}
 	return ans
 }
-
-func max(a, b int) int { if b > a { return b }; return a }
