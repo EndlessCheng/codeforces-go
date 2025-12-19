cb570d959088524079340c262d3a0d61320e2881
Endless <0x3f@0x3f3f3f3f.com>
2025-12-19 19:41:36 +0800

    lc1351

--- leetcode/weekly/176/a/a.go
+++ leetcode/weekly/176/a/a.go
@@ -1,12 +1,14 @@
 package main
 
-// 二分亦可。
 func countNegatives(grid [][]int) (ans int) {
-	for _, row := range grid {
-		for _, v := range row {
-			if v < 0 {
-				ans++
-			}
+	m, n := len(grid), len(grid[0])
+	i, j := 0, n-1 // 从右上角开始
+	for i < m && j >= 0 { // 还有剩余元素
+		if grid[i][j] < 0 {
+			ans += m - i // 这一列剩余元素都是负数
+			j--
+		} else {
+			i++ // 这一行剩余元素全都非负，排除
 		}
 	}
 	return
