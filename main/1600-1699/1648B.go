06ccae490119ce9b6e65dd72d8abac293f8bbb67
Endless <0x3f@0x3f3f3f3f.com>
2025-12-24 14:31:57 +0800

    cf1648B

--- main/1600-1699/1648B.go
+++ main/1600-1699/1648B.go
@@ -16,19 +16,19 @@ o:
 			Fscan(in, &v)
 			s[v] = 1
 		}
-		for i := 2; i <= mx; i++ {
-			s[i] += s[i-1]
-		}
 		if s[1] == 0 {
 			Fprintln(out, "No")
 			continue
+		}
+		for i := 2; i <= mx; i++ {
+			s[i] += s[i-1]
 		}
 		for i := 2; i <= mx/2; i++ {
 			if s[i] == s[i-1] {
 				continue
 			}
 			for j := i; j <= mx; j += i {
-				if s[j-1] < s[min(j+i, mx)] && s[j/i] == s[j/i-1] {
+				if s[j-1] < s[min(j+i-1, mx)] && s[j/i] == s[j/i-1] {
 					Fprintln(out, "No")
 					continue o
 				}
