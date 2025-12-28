c6334ccd8359d733a0f4f2ef25e25fdf0c8b978a
Endless <0x3f@0x3f3f3f3f.com>
2025-12-28 19:47:55 +0800

    lc1975

--- leetcode/biweekly/59/b/b.go
+++ leetcode/biweekly/59/b/b.go
@@ -1,22 +1,23 @@
 package main
 
+import "math"
+
 // github.com/EndlessCheng/codeforces-go
-func maxMatrixSum(a [][]int) int64 {
-	sum, min, neg := 0, int(1e9), false
-	for _, r := range a {
-		for _, v := range r {
-			if v < 0 {
-				neg = !neg
-				v = -v
+func maxMatrixSum(matrix [][]int) int64 {
+	total, negCnt, mn := 0, 0, math.MaxInt
+	for _, row := range matrix {
+		for _, x := range row {
+			if x < 0 {
+				negCnt++
+				x = -x // 先把负数都变成正数
 			}
-			if v < min {
-				min = v
-			}
-			sum += v
+			mn = min(mn, x)
+			total += x
 		}
 	}
-	if neg {
-		sum -= min * 2
+
+	if negCnt%2 > 0 { // 必须有一个负数
+		total -= mn * 2 // 最小的数添加负号
 	}
-	return int64(sum)
+	return int64(total)
 }
