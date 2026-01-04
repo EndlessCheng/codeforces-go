d88867aff3a90c23acc283e0edaa321db1cf7e48
Endless <0x3f@0x3f3f3f3f.com>
2026-01-04 19:13:57 +0800

    cf2091F 简化代码

--- main/2000-2099/2091F.go
+++ main/2000-2099/2091F.go
@@ -12,30 +12,28 @@ func cf2091F(in io.Reader, out io.Writer) {
 	var s string
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &n, &m, &d)
-		f0 := make([]int, m+1)
-		f1 := make([]int, m+1)
+		f := make([]int, m+1)
+		g := make([]int, m+1)
 		for i := range n {
 			Fscan(in, &s)
-			nf0 := make([]int, m+1)
 			for j, b := range s {
 				if b == '#' {
-					nf0[j+1] = nf0[j]
+					f[j+1] = f[j]
 				} else if i == 0 {
-					nf0[j+1] = nf0[j] + 1
+					f[j+1] = f[j] + 1
 				} else {
-					nf0[j+1] = (nf0[j] + f0[min(j+d, m)] - f0[max(j-d+1, 0)] + f1[min(j+d, m)] - f1[max(j-d+1, 0)]) % mod
+					f[j+1] = (f[j] + g[min(j+d, m)] - g[max(j-d+1, 0)]) % mod
 				}
 			}
-			f0 = nf0
 			for j, b := range s {
 				if b == '#' {
-					f1[j+1] = f1[j]
+					g[j+1] = g[j]
 				} else {
-					f1[j+1] = (f1[j] + f0[min(j+d+1, m)] - f0[j+1] + f0[j] - f0[max(j-d, 0)]) % mod
+					g[j+1] = (g[j] + f[min(j+d+1, m)] - f[max(j-d, 0)]) % mod
 				}
 			}
 		}
-		Fprintln(out, (f0[m]+f1[m]+mod*2)%mod)
+		Fprintln(out, (g[m]+mod)%mod)
 	}
 }
 
