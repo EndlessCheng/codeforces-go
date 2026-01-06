d7d95123706670e5339fde57f7b43d44fae0588b
Endless <0x3f@0x3f3f3f3f.com>
2026-01-06 09:34:38 +0800

    lc1292

--- leetcode/weekly/167/c/c.go
+++ leetcode/weekly/167/c/c.go
@@ -1,39 +1,28 @@
 package main
 
-func maxSideLength(mat [][]int, threshold int) int {
-	n, m := len(mat), len(mat[0])
-	var sum [][]int
-	initS := func(mat [][]int) {
-		sum = make([][]int, len(mat)+1)
-		sum[0] = make([]int, len(mat[0])+1)
-		for i, mi := range mat {
-			sum[i+1] = make([]int, len(mi)+1)
-			for j, mij := range mi {
-				sum[i+1][j+1] = sum[i+1][j] + sum[i][j+1] - sum[i][j] + mij
-			}
+func maxSideLength(mat [][]int, threshold int) (ans int) {
+	m, n := len(mat), len(mat[0])
+	sum := make([][]int, m+1)
+	sum[0] = make([]int, n+1)
+	for i, row := range mat {
+		sum[i+1] = make([]int, n+1)
+		for j, x := range row {
+			sum[i+1][j+1] = sum[i+1][j] + sum[i][j+1] - sum[i][j] + x
 		}
-	}
-	// r1<=r<=r2 && c1<=c<=c2
-	query := func(r1, c1, r2, c2 int) int {
-		r2++
-		c2++
-		return sum[r2][c2] - sum[r2][c1] - sum[r1][c2] + sum[r1][c1]
 	}
 
-	initS(mat)
-	ans := 0
-	for i, mi := range mat {
-		for j := range mi {
-			for sz := 0; i+sz < n && j+sz < m; sz++ {
-				if query(i, j, i+sz, j+sz) <= threshold {
-					if sz+1 > ans {
-						ans = sz + 1
-					}
-				} else {
-					break
-				}
+	// 返回左上角在 (r1, c1)，右下角在 (r2, c2) 的子矩阵元素和
+	query := func(r1, c1, r2, c2 int) int {
+		return sum[r2+1][c2+1] - sum[r2+1][c1] - sum[r1][c2+1] + sum[r1][c1]
+	}
+
+	for i := range m {
+		for j := range n {
+			// 边长为 ans+1 的正方形，左上角在 (i, j)，右下角在 (i+ans, j+ans)
+			for i+ans < m && j+ans < n && query(i, j, i+ans, j+ans) <= threshold {
+				ans++
 			}
 		}
 	}
-	return ans
+	return
 }
