20d1eca21b67c96a761000700941fa42d2c4dce7
Endless <0x3f@0x3f3f3f3f.com>
2026-01-24 11:01:55 +0800

    cf1835C

--- main/1800-1899/1835C.go
+++ main/1800-1899/1835C.go
@@ -14,33 +14,33 @@ func cf1835C(in io.Reader, _w io.Writer) {
 	var T, k int
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &k)
-		m := 1 << k
-		s := make([]int, m*2+1)
-		pos := make([]int, m)
+		half := 1 << k // 值域的一半
+		s := make([]int, half*2+1)
+		pos := make([]int, half)
 		for i := range pos {
 			pos[i] = -1
 		}
 		pos[0] = 0
-		pos2 := make([][2]int, m)
+		pos2 := make([][2]int, half)
 		for i := 1; ; i++ {
 			Fscan(in, &s[i])
 			s[i] ^= s[i-1]
 			si := s[i]
-			j := pos[si&(m-1)]
+			j := pos[si&(half-1)]
 			if j >= 0 {
 				s2 := (si ^ s[j]) >> k
 				if pos2[s2][1] > 0 {
 					a := append(pos2[s2][:], j)
 					slices.Sort(a)
 					Fprintln(out, a[0]+1, a[1], a[2]+1, i)
-					for range len(s) - 1 - i {
+					for range half*2 - i {
 						Fscan(in, &k)
 					}
 					break
 				}
 				pos2[s2] = [2]int{j, i}
 			}
-			pos[si&(m-1)] = i
+			pos[si&(half-1)] = i
 		}
 	}
 }
