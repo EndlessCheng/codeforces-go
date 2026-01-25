2cb983fcf66593ff49c84c31b9664888b06794a8
Endless <0x3f@0x3f3f3f3f.com>
2026-01-25 09:01:14 +0800

    cf963C

---
+++ main/900-999/963C.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf963C(in io.Reader, out io.Writer) {
+	gcd := func(a, b int) int {
+		for a != 0 {
+			a, b = b%a, a
+		}
+		return b
+	}
+	var n, w, h, c, g int
+	Fscan(in, &n)
+	cntW := map[int]int{}
+	st := map[int]bool{}
+	for range n {
+		Fscan(in, &w, &h, &c)
+		g = gcd(g, c)
+		cntW[h]++
+		st[w] = true
+	}
+
+	if g == 1 {
+		Fprint(out, 0)
+		return
+	}
+
+	for _, c := range cntW {
+		if c != len(st) {
+			Fprint(out, 0)
+			return
+		}
+	}
+
+	ans := 0
+	for i := 1; i*i <= g; i++ {
+		if g%i == 0 {
+			ans++
+			if i*i != g {
+				ans++
+			}
+		}
+	}
+	Fprintln(out, ans)
+}
+
+//func main() { cf963C(bufio.NewReader(os.Stdin), os.Stdout) }
