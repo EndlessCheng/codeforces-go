d3d7e22de000416d217d21578e80bab4a9481208
Endless <0x3f@0x3f3f3f3f.com>
2026-02-07 08:24:22 +0800

    upd

--- leetcode/biweekly/39/b/b.go
+++ leetcode/biweekly/39/b/b.go
@@ -1,20 +1,27 @@
 package main
 
+import "strings"
+
 // github.com/EndlessCheng/codeforces-go
-func minimumDeletions(s string) (ans int) {
-	n := len(s)
-	sum := make([]int, n+1)
-	for i, b := range s {
-		sum[i+1] = sum[i]
-		if b == 'a' {
-			sum[i+1]++
+func minimumDeletions1(s string) int {
+	del := strings.Count(s, "a")
+	ans := del
+	for _, c := range s {
+		// 'a' -> -1    'b' -> 1
+		del += int((c-'a')*2 - 1)
+		ans = min(ans, del)
+	}
+	return ans
+}
+
+func minimumDeletions(s string) int {
+	f, cntB := 0, 0
+	for _, c := range s {
+		if c == 'b' { // f 值不变
+			cntB++
+		} else {
+			f = min(f+1, cntB)
 		}
 	}
-	ans = n
-	for i, s := range sum {
-		if del := sum[n] + i - 2*s; del < ans {
-			ans = del
-		}
-	}
-	return
+	return f
 }
