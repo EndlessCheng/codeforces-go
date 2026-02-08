f1038361877cc45769af6a2c16603bd6ab679712
Endless <0x3f@0x3f3f3f3f.com>
2026-02-08 21:20:01 +0800

    cf1728D

---
+++ main/1700-1799/1728D.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf1728D(in io.Reader, out io.Writer) {
+	var T int
+	var s string
+o:
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &s)
+		n := len(s)
+		for i := range n / 2 {
+			if s[i] == s[n-1-i] {
+				continue
+			}
+			for j := i; j < n-i; j += 2 {
+				if s[j] != s[j+1] {
+					Fprintln(out, "Alice")
+					continue o
+				}
+			}
+		}
+		Fprintln(out, "Draw")
+	}
+}
+
+//func main() { cf1728D(bufio.NewReader(os.Stdin), os.Stdout) }
