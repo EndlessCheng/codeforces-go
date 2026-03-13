3cab233a3d19b92a02cae03181a9c72d39c8e9c4
Endless <0x3f@0x3f3f3f3f.com>
2026-03-13 09:24:48 +0800

    lc1594

--- leetcode/weekly/207/c/c.go
+++ leetcode/weekly/207/c/c.go
@@ -1,50 +1,125 @@
 package main
 
+import "math"
+
 // github.com/EndlessCheng/codeforces-go
-func maxProductPath(g [][]int) (ans int) {
-	const mod int = 1e9 + 7
-	min := func(a, b int) int {
-		if a < b {
-			return a
+func maxProductPath1(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	memo := make([][][2]int, m)
+	for i := range memo {
+		memo[i] = make([][2]int, n)
+		for j := range memo[i] {
+			memo[i][j] = [2]int{math.MinInt, math.MinInt}
 		}
-		return b
-	}
-	max := func(a, b int) int {
-		if a > b {
-			return a
-		}
-		return b
-	}
-	do := func(v [2]int, w int) [2]int {
-		return [2]int{min(v[0]*w, v[1]*w), max(v[0]*w, v[1]*w)}
 	}
 
-	n, m := len(g), len(g[0])
-	dp := make([][][2]int, n) // min, max
-	for i := range dp {
-		dp[i] = make([][2]int, m)
-	}
-	for i, v := range g[0] {
+	var dfs func(int, int) (int, int)
+	dfs = func(i, j int) (int, int) {
+		x := grid[i][j]
+		if i == 0 && j == 0 {
+			return x, x
+		}
+
+		p := &memo[i][j]
+		if p[0] != math.MinInt {
+			return p[0], p[1]
+		}
+
+		resMin := math.MaxInt
+		resMax := math.MinInt
 		if i > 0 {
-			v *= dp[0][i-1][0]
+			mn, mx := dfs(i-1, j)
+			resMin = min(mn*x, mx*x)
+			resMax = max(mn*x, mx*x)
 		}
-		dp[0][i] = [2]int{v, v}
-	}
-	for i := 1; i < n; i++ {
-		for j, v := range g[i] {
-			dp[i][j] = do(dp[i-1][j], v)
-			if j > 0 {
-				p := do(dp[i][j-1], v)
-				dp[i][j][0] = min(dp[i][j][0], p[0])
-				dp[i][j][1] = max(dp[i][j][1], p[1])
-			}
+		if j > 0 {
+			mn, mx := dfs(i, j-1)
+			resMin = min(resMin, mn*x, mx*x)
+			resMax = max(resMax, mn*x, mx*x)
 		}
+
+		p[0], p[1] = resMin, resMax
+		return resMin, resMax
 	}
 
-	ans = dp[n-1][m-1][1]
+	_, ans := dfs(m-1, n-1)
 	if ans < 0 {
 		return -1
 	}
-	ans %= mod
-	return
+	return ans % 1_000_000_007
+}
+
+func maxProductPath2(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	f := make([][][2]int, m)
+	for i := range f {
+		f[i] = make([][2]int, n)
+	}
+
+	for i, row := range grid {
+		for j, x := range row {
+			if i == 0 && j == 0 {
+				f[0][0] = [2]int{x, x}
+				continue
+			}
+
+			resMin := math.MaxInt
+			resMax := math.MinInt
+			if i > 0 {
+				mn, mx := f[i-1][j][0], f[i-1][j][1]
+				resMin = min(mn*x, mx*x)
+				resMax = max(mn*x, mx*x)
+			}
+			if j > 0 {
+				mn, mx := f[i][j-1][0], f[i][j-1][1]
+				resMin = min(resMin, mn*x, mx*x)
+				resMax = max(resMax, mn*x, mx*x)
+			}
+
+			f[i][j] = [2]int{resMin, resMax}
+		}
+	}
+
+	ans := f[m-1][n-1][1]
+	if ans < 0 {
+		return -1
+	}
+	return ans % 1_000_000_007
+}
+
+func maxProductPath(grid [][]int) int {
+	n := len(grid[0])
+	fMin := make([]int, n)
+	fMax := make([]int, n)
+
+	for i, row := range grid {
+		for j, x := range row {
+			if i == 0 && j == 0 {
+				fMin[0], fMax[0] = x, x
+				continue
+			}
+
+			resMin := math.MaxInt
+			resMax := math.MinInt
+			if i > 0 {
+				mn, mx := fMin[j], fMax[j]
+				resMin = min(mn*x, mx*x)
+				resMax = max(mn*x, mx*x)
+			}
+			if j > 0 {
+				mn, mx := fMin[j-1], fMax[j-1]
+				resMin = min(resMin, mn*x, mx*x)
+				resMax = max(resMax, mn*x, mx*x)
+			}
+
+			fMin[j] = resMin
+			fMax[j] = resMax
+		}
+	}
+
+	ans := fMax[n-1]
+	if ans < 0 {
+		return -1
+	}
+	return ans % 1_000_000_007
 }
