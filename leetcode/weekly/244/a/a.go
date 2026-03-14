a21cf38b20df7c5999c6fae2c2fce62766d25251
Endless <0x3f@0x3f3f3f3f.com>
2026-03-14 08:01:46 +0800

    lc1886

--- leetcode/weekly/244/a/a.go
+++ leetcode/weekly/244/a/a.go
@@ -1,37 +1,50 @@
 package main
 
+import "slices"
+
 // github.com/EndlessCheng/codeforces-go
-func findRotation(a, b [][]int) bool {
-	for k := 0; k < 4; k++ {
-		a = rotate(a)
-		if equalMatrix(a, b) {
+// 48. 旋转图像
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i, row := range matrix {
+		for j := i + 1; j < n; j++ { // 遍历对角线上方元素，做转置
+			row[j], matrix[j][i] = matrix[j][i], row[j]
+		}
+		slices.Reverse(row) // 行翻转
+	}
+}
+
+func findRotation1(mat, target [][]int) bool {
+	for range 4 {
+		if slices.EqualFunc(mat, target, slices.Equal[[]int]) {
 			return true
 		}
+		rotate(mat)
 	}
 	return false
 }
 
-func rotate(a [][]int) [][]int {
-	n := len(a)
-	b := make([][]int, n)
-	for i := range b {
-		b[i] = make([]int, n)
-	}
-	for i, r := range a {
-		for j, v := range r {
-			b[j][n-1-i] = v
-		}
-	}
-	return b
-}
-
-func equalMatrix(a, b [][]int) bool {
-	for i, r := range a {
-		for j, v := range r {
-			if v != b[i][j] {
+func findRotation(mat, target [][]int) bool {
+	n := len(mat)
+	ok := 1<<4 - 1 // ok := []bool{true, true, true, true}
+	for i, row := range mat {
+		for j, x := range row {
+			if x != target[i][j] {
+				ok &^= 1 << 0 // ok[0] = false
+			}
+			if x != target[j][n-1-i] {
+				ok &^= 1 << 1 // ok[1] = false
+			}
+			if x != target[n-1-i][n-1-j] {
+				ok &^= 1 << 2 // ok[2] = false
+			}
+			if x != target[n-1-j][i] {
+				ok &^= 1 << 3 // ok[3] = false
+			}
+			if ok == 0 { // 所有的 ok[i] 都是 false
 				return false
 			}
 		}
 	}
-	return true
+	return true // 至少有一个 ok[i] 是 true
 }
