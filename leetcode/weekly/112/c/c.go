b09bafd33c20ee4a49e427f45bb354ed38113679
Endless <0x3f@0x3f3f3f3f.com>
2026-03-20 18:43:40 +0800

    lc947

---
+++ leetcode/weekly/112/c/c.go
@@ -0,0 +1,31 @@
+package main
+
+// https://space.bilibili.com/206214
+func removeStones(stones [][]int) int {
+	// 哈希表并查集
+	fa := map[int]int{}
+	var find func(int) int
+	find = func(x int) int {
+		fx, ok := fa[x]
+		if !ok {
+			return x
+		}
+		if fx != x {
+			fa[x] = find(fx)
+			return fa[x]
+		}
+		return x
+	}
+
+	const offset int = 3e4
+	for _, p := range stones {
+		fa[find(p[0])] = find(p[1] + offset)
+	}
+
+	// 记录不同的代表元
+	set := map[int]struct{}{}
+	for _, p := range stones {
+		set[find(p[0])] = struct{}{}
+	}
+	return len(stones) - len(set)
+}
