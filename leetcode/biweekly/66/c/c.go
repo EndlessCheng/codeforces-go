59577f39d47d4a606cb7524a94da5ca5be1502ec
Endless <0x3f@0x3f3f3f3f.com>
2026-03-21 10:53:59 +0800

    lc2087

--- leetcode/biweekly/66/c/c.go
+++ leetcode/biweekly/66/c/c.go
@@ -2,11 +2,19 @@ package main
 
 // github.com/EndlessCheng/codeforces-go
 func minCost(startPos, homePos, rowCosts, colCosts []int) int {
-	x0, y0, x1, y1 := startPos[0], startPos[1], homePos[0], homePos[1]
-	ans := -rowCosts[x0] - colCosts[y0] // 初始的行列无需计算
-	if x0 > x1 { x0, x1 = x1, x0 } // 交换位置，保证 x0 <= x1
-	if y0 > y1 { y0, y1 = y1, y0 } // 交换位置，保证 y0 <= y1
-	for _, cost := range rowCosts[x0 : x1+1] { ans += cost } // 统计答案
-	for _, cost := range colCosts[y0 : y1+1] { ans += cost } // 统计答案
+	x0, y0 := startPos[0], startPos[1]
+	x1, y1 := homePos[0], homePos[1]
+
+	// 起点的代价不计入，先减去
+	ans := -rowCosts[x0] - colCosts[y0]
+
+	// 累加代价（包含起点）
+	for _, cost := range rowCosts[min(x0, x1) : max(x0, x1)+1] {
+		ans += cost
+	}
+	for _, cost := range colCosts[min(y0, y1) : max(y0, y1)+1] {
+		ans += cost
+	}
+
 	return ans
 }
