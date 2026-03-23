9340e1e2d759319f6b5dc02594c0a0d0df5152ec
Endless <0x3f@0x3f3f3f3f.com>
2026-03-23 09:37:44 +0800

    cf1977E

---
+++ main/1900-1999/1977E.go
@@ -0,0 +1,46 @@
+package main
+
+import . "fmt"
+
+// https://github.com/EndlessCheng
+func q(i, j int) bool {
+	Println("?", i+1, j+1)
+	s := ""
+	Scan(&s)
+	return s[0] == 'Y'
+}
+
+func cf1977E() {
+	var T, n int
+	for Scan(&T); T > 0; T-- {
+		Scan(&n)
+		c := make([]int, n)
+		for i, j := 0, 0; i+1 < n; i = j {
+			j = i + 1
+			for j < n && q(j-1, j) {
+				c[j] = c[i]
+				j++
+			}
+			if j == n {
+				break
+			}
+			if i == 0 || q(i-1, j) {
+				c[j] = c[i] ^ 1
+				continue
+			}
+			c[j] = c[i]
+			c[j-1] ^= 1
+			for k := j - 2; k > i && !q(k, j); k-- {
+				c[k] ^= 1
+			}
+		}
+
+		Print("!")
+		for _, v := range c {
+			Print(" ", v)
+		}
+		Println()
+	}
+}
+
+//func main() { cf1977E() }
