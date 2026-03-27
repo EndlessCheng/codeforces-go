0fb948e311c393ce12f398bd4b6f750190e1d0d6
Endless <0x3f@0x3f3f3f3f.com>
2026-03-27 13:33:51 +0800

    cf578D

---
+++ main/500-599/578D.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf578D(in io.Reader, out io.Writer) {
+	var n, m, cnt int
+	var s string
+	Fscan(in, &n, &m, &s)
+	s = "$$" + s
+	ans := 1
+	for i := 1; i <= n; i++ {
+		if s[i+1] == s[i-1] {
+			cnt++
+		} else {
+			cnt = 0
+		}
+		if s[i+1] != s[i] {
+			ans += n*(m-1) - cnt - 1
+		}
+	}
+	Fprint(out, ans)
+}
+
+//func main() { cf578D(bufio.NewReader(os.Stdin), os.Stdout) }
