baad08541ed49e9ecab95dd46b8ae52424c7918f
Endless <0x3f@0x3f3f3f3f.com>
2026-03-31 14:48:45 +0800

    lc2069

--- leetcode/biweekly/65/b/b.go
+++ leetcode/biweekly/65/b/b.go
@@ -1,32 +1,40 @@
 package main
 
 // github.com/EndlessCheng/codeforces-go
-type Robot struct{}
-var w, h, step int
-
-func Constructor(width, height int) (_ Robot) {
-	w, h, step = width, height, 0
-	return
+type Robot struct {
+	w, h, step int
 }
 
-func (Robot) Move(num int) {
+func Constructor(width, height int) Robot {
+	return Robot{width, height, 0}
+}
+
+func (r *Robot) Step(num int) {
 	// 由于机器人只能走外圈，那么走 (w+h-2)*2 步后会回到起点
-	// 同时，将 step 取模固定在 [1,(w+h-2)*2] 范围内，这样不需要特判处于原点时的方向
-	step = (step+num-1)%((w+h-2)*2) + 1
+	// 把 step 取模调整到 [1, (w+h-2)*2]，这样不需要特判处于 step == 0 时的方向
+	r.step = (r.step+num-1)%((r.w+r.h-2)*2) + 1
 }
 
-func get() (x, y int, dir string) {
+func (r *Robot) getState() (x, y int, dir string) {
+	w, h, step := r.w, r.h, r.step
 	switch {
 	case step < w:
 		return step, 0, "East"
 	case step < w+h-1:
 		return w - 1, step - w + 1, "North"
 	case step < w*2+h-2:
-		return w*2 + h - 3 - step, h - 1, "West"
+		return w*2 + h - step - 3, h - 1, "West"
 	default:
-		return 0, (w+h-2)*2 - step, "South"
+		return 0, (w+h)*2 - step - 4, "South"
 	}
 }
 
-func (Robot) GetPos() []int  { x, y, _ := get(); return []int{x, y} }
-func (Robot) GetDir() string { _, _, d := get(); return d }
+func (r *Robot) GetPos() []int {
+	x, y, _ := r.getState()
+	return []int{x, y}
+}
+
+func (r *Robot) GetDir() string {
+	_, _, d := r.getState()
+	return d
+}
