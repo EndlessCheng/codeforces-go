ba934441df7f0f2fc2b43ab3db72ddaf1fca7029
Endless <0x3f@0x3f3f3f3f.com>
2026-04-02 22:08:11 +0800

    CF1788F

--- main/1700-1799/1788F.go
+++ main/1700-1799/1788F.go
@@ -7,12 +7,10 @@ import (
 )
 
 // https://space.bilibili.com/206214
-func CF1788F(_r io.Reader, _w io.Writer) {
-	in := bufio.NewReader(_r)
+func CF1788F(in io.Reader, _w io.Writer) {
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
-
-	var n, q, v, w, d, xorA int
+	var n, q, v, w, d int
 	Fscan(in, &n, &q)
 	es := make([]struct{ x, y int }, n-1)
 	deg := make([]byte, n)
@@ -32,42 +30,44 @@ func CF1788F(_r io.Reader, _w io.Writer) {
 	var find func(int) int
 	find = func(x int) int {
 		if fa[x] != x {
-			ffx := find(fa[x])
+			rt := find(fa[x])
 			dis[x] ^= dis[fa[x]]
-			fa[x] = ffx
+			fa[x] = rt
 		}
 		return fa[x]
 	}
 	merge := func(from, to, d int) bool {
-		if fFrom, fTo := find(from), find(to); fFrom != fTo {
-			dis[fFrom] = d ^ dis[to] ^ dis[from]
-			fa[fFrom] = fTo
-			return true
+		x, y := find(from), find(to)
+		if x == y {
+			return dis[to]^dis[from] == d
 		}
-		return dis[to]^dis[from] == d
+		dis[x] = d ^ dis[to] ^ dis[from]
+		fa[x] = y
+		return true
 	}
-	for ; q > 0; q-- {
+
+	for range q {
 		Fscan(in, &v, &w, &d)
-		if !merge(v-1, w-1, d) { // 边权转点权
+		if !merge(v-1, w-1, d) {
 			Fprint(out, "No")
 			return
 		}
 	}
 
 	ccOddDegCnt := make([]byte, n)
+	xor := 0
 	for i, d := range deg {
-		find(i)
-		if d != 0 { // 每个点权的计算次数是它在原图上的度数，想要影响所有点权的异或值，度数必须是奇数
-			ccOddDegCnt[fa[i]] ^= 1 // 连通块中的奇度数点的个数
-			xorA ^= dis[i]
+		if d > 0 { // 每个点权的计算次数是它在原图上的度数，想要影响所有点权的异或值，度数必须是奇数
+			ccOddDegCnt[find(i)] ^= 1 // 连通块中的奇度数点的个数
+			xor ^= dis[i]
 		}
 	}
 
 	for rt, c := range ccOddDegCnt {
-		if c != 0 { // 连通块里有奇数个奇度数点，xorA 会影响答案奇数次，才能真正地影响答案
+		if c > 0 { // 连通块里有奇数个奇度数点，dis[i] ^= xor 会影响答案奇数次，才能真正地影响答案
 			for i, f := range fa {
 				if f == rt {
-					dis[i] ^= xorA
+					dis[i] ^= xor
 				}
 			}
 			break
@@ -80,4 +80,4 @@ func CF1788F(_r io.Reader, _w io.Writer) {
 	}
 }
 
-//func main() { CF1788F(os.Stdin, os.Stdout) }
+//func main() { CF1788F(bufio.NewReader(os.Stdin), os.Stdout) }
