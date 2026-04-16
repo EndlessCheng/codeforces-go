236bd22e61f8099c44547e557d39b6bfd5902326
Endless <0x3f@0x3f3f3f3f.com>
2026-04-16 08:37:48 +0800

    lc1559

--- leetcode/biweekly/33/d/d.go
+++ leetcode/biweekly/33/d/d.go
@@ -1,41 +1,36 @@
 package main
 
 // github.com/EndlessCheng/codeforces-go
-type pair struct{ x, y int }
-var dir4 = []pair{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+var dirs = []struct{ x, y int }{{0, -1}, {0, 1}, {-1, 0}, {1, 0}} // 左右上下
 
-func containsCycle(g [][]byte) (ans bool) {
-	n, m := len(g), len(g[0])
-	vis := make([][]bool, n)
+func containsCycle(grid [][]byte) bool {
+	m, n := len(grid), len(grid[0])
+	vis := make([][]bool, m)
 	for i := range vis {
-		vis[i] = make([]bool, m)
+		vis[i] = make([]bool, n)
 	}
-	var cur byte
-	var f func(x, y, px, py int) bool
-	f = func(x, y, px, py int) bool {
+
+	var dfs func(int, int, int, int) bool
+	dfs = func(x, y, px, py int) bool {
 		vis[x][y] = true
-		for _, d := range dir4 {
-			if xx, yy := x+d.x, y+d.y; 0 <= xx && xx < n && 0 <= yy && yy < m && g[xx][yy] == cur {
-				if vis[xx][yy] {
-					if xx != px || yy != py {
-						return true
-					}
-				} else if f(xx, yy, x, y) {
-					return true
-				}
+		for _, d := range dirs { // 枚举移动方向
+			i, j := x+d.x, y+d.y
+			if (i != px || j != py) && // (i, j) 不是上一步的格子 (px, py)
+				0 <= i && i < m && 0 <= j && j < n && // (i, j) 没有出界
+				grid[i][j] == grid[x][y] && // (i, j) 和 (x, y) 的格子值相同
+				(vis[i][j] || dfs(i, j, x, y)) { // 如果之前访问过 (i, j)，那么找到了环，否则继续递归找
+				return true
 			}
 		}
 		return false
 	}
-	for i, row := range g {
-		for j := range row {
-			if !vis[i][j] {
-				cur = g[i][j]
-				if f(i, j, -1, -1) {
-					return true
-				}
+
+	for i, row := range vis {
+		for j, b := range row {
+			if !b && dfs(i, j, -1, -1) {
+				return true
 			}
 		}
 	}
-	return
+	return false
 }
