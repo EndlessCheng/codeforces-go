cbfe2d24f93d5f1839598af792b74eb49fa65b39
Endless <0x3f@0x3f3f3f3f.com>
2026-04-21 19:19:07 +0800

    cf2049D

---
+++ main/2000-2099/2049D.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+)
+
+// https://github.com/EndlessCheng
+func cf2049D(in io.Reader, out io.Writer) {
+	var T, n, m, k int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n, &m, &k)
+		f := make([]int, m)
+		for i := 1; i < m; i++ {
+			f[i] = 1e18
+		}
+		a := make([]int, m)
+		for range n {
+			for i := range a {
+				Fscan(in, &a[i])
+			}
+
+			nf := make([]int, m)
+			for i := range nf {
+				nf[i] = 1e18
+			}
+			for shift := range m {
+				s := int(1e18)
+				for x := range m {
+					s = min(s, f[x]) + a[(x+shift)%m]
+					nf[x] = min(nf[x], s+shift*k)
+				}
+			}
+			f = nf
+		}
+		Fprintln(out, f[m-1])
+	}
+}
+
+//func main() { cf2049D(bufio.NewReader(os.Stdin), os.Stdout) }
