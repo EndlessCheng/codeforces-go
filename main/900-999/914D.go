a0adb83cae057f27f653849347a1e7b80e0979b1
Endless <0x3f@0x3f3f3f3f.com>
2026-04-27 11:45:40 +0800

    cf914D

--- main/900-999/914D.go
+++ main/900-999/914D.go
@@ -4,90 +4,84 @@ import (
 	"bufio"
 	. "fmt"
 	"io"
+	"math/bits"
 )
 
-// github.com/EndlessCheng/codeforces-go
-type seg914 []struct{ l, r, g int }
+// https://github.com/EndlessCheng
+type seg14 []struct{ l, r, gcd int }
 
-func (seg914) gcd(a, b int) int {
-	for b != 0 {
-		a, b = b, a%b
-	}
-	return a
+func (t seg14) maintain(o int) {
+	t[o].gcd = gcd14(t[o<<1].gcd, t[o<<1|1].gcd)
 }
 
-func (t seg914) _pushUp(o int) { t[o].g = t.gcd(t[o<<1].g, t[o<<1|1].g) }
-
-func (t seg914) _build(a []int, o, l, r int) {
+func (t seg14) build(in io.Reader, o, l, r int) {
 	t[o].l, t[o].r = l, r
 	if l == r {
-		t[o].g = a[l-1]
+		Fscan(in, &t[o].gcd)
 		return
 	}
 	m := (l + r) >> 1
-	t._build(a, o<<1, l, m)
-	t._build(a, o<<1|1, m+1, r)
-	t._pushUp(o)
+	t.build(in, o<<1, l, m)
+	t.build(in, o<<1|1, m+1, r)
+	t.maintain(o)
 }
 
-func (t seg914) _update(o, i, v int) {
-	if t[o].l == t[o].r {
-		t[o].g = v
+func (t seg14) update(o, i, v int) {
+	cur := &t[o]
+	if cur.l == cur.r {
+		cur.gcd = v
 		return
 	}
-	if i <= (t[o].l+t[o].r)>>1 {
-		t._update(o<<1, i, v)
+	m := (cur.l + cur.r) >> 1
+	if i <= m {
+		t.update(o<<1, i, v)
 	} else {
-		t._update(o<<1|1, i, v)
+		t.update(o<<1|1, i, v)
 	}
-	t._pushUp(o)
+	t.maintain(o)
 }
 
-func (t seg914) _query(o, l, r, v int) (c int8) {
-	if t[o].g%v == 0 {
-		return
+func (t seg14) query(o, l, r, v int) int {
+	if t[o].gcd%v == 0 {
+		return 0
 	}
 	if t[o].l == t[o].r {
 		return 1
 	}
 	m := (t[o].l + t[o].r) >> 1
+	cnt := 0
 	if l <= m {
-		c += t._query(o<<1, l, r, v)
+		cnt += t.query(o<<1, l, r, v)
 	}
-	if c <= 1 && r > m {
-		c += t._query(o<<1|1, l, r, v)
+	if cnt < 2 && r > m {
+		cnt += t.query(o<<1|1, l, r, v)
 	}
-	return
+	return cnt
 }
 
-func (t seg914) init(a []int)           { t._build(a, 1, 1, len(a)) }
-func (t seg914) update(i, v int)        { t._update(1, i, v) }
-func (t seg914) query(l, r, v int) bool { return t._query(1, l, r, v) <= 1 }
-
-func CF914D(_r io.Reader, _w io.Writer) {
-	in := bufio.NewReader(_r)
+func cf914D(in io.Reader, _w io.Writer) {
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
-
 	var n, q, op, l, r, v int
 	Fscan(in, &n)
-	a := make([]int, n)
-	for i := range a {
-		Fscan(in, &a[i])
-	}
-	t := make(seg914, 4*n)
-	t.init(a)
-	for Fscan(in, &q); q > 0; q-- {
-		if Fscan(in, &op, &l, &r); op == 1 {
-			if Fscan(in, &v); t.query(l, r, v) {
-				Fprintln(out, "YES")
-			} else {
-				Fprintln(out, "NO")
-			}
+	t := make(seg14, 2<<bits.Len(uint(n-1)))
+	t.build(in, 1, 1, n)
+
+	Fscan(in, &q)
+	for range q {
+		Fscan(in, &op, &l, &r)
+		if op == 2 {
+			t.update(1, l, r)
+			continue
+		}
+		Fscan(in, &v)
+		if t.query(1, l, r, v) < 2 {
+			Fprintln(out, "YES")
 		} else {
-			t.update(l, r)
+			Fprintln(out, "NO")
 		}
 	}
 }
 
-//func main() { CF914D(os.Stdin, os.Stdout) }
+//func main() { cf914D(bufio.NewReader(os.Stdin), os.Stdout) }
+func gcd14(a, b int) int { for a != 0 { a, b = b%a, a }; return b }
