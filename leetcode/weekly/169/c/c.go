6ef629773e942c80fe2f73bba01498c8361660e7
Endless <0x3f@0x3f3f3f3f.com>
2026-04-29 10:04:02 +0800

    lc1306

--- leetcode/weekly/169/c/c.go
+++ leetcode/weekly/169/c/c.go
@@ -2,20 +2,19 @@ package main
 
 func canReach(arr []int, start int) bool {
 	n := len(arr)
-    vis := make([]bool,n )
-    var f func(i int) bool
-    f = func(i int) bool {
-    	if i<0 || i>=n {
+	vis := make([]bool, n)
+
+	var dfs func(int) bool
+	dfs = func(i int) bool {
+		if i < 0 || i >= n || vis[i] { // 出界，或者之前访问过（没找到）
 			return false
 		}
-		if arr[i] == 0 {
+		if arr[i] == 0 { // 找到了
 			return true
 		}
-		if vis[i]{
-			return false
-		}
-		vis[i]=true
-		return f(i+arr[i]) || f(i-arr[i])
+		vis[i] = true // 避免重复访问
+		return dfs(i+arr[i]) || dfs(i-arr[i])
 	}
-	return f(start)
+
+	return dfs(start)
 }
