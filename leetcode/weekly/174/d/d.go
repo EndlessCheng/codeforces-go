96a8706b044393490053a8b775b75e8ab70cf8aa
Endless <0x3f@0x3f3f3f3f.com>
2026-04-30 09:20:56 +0800

    upd

--- leetcode/weekly/174/d/d.go
+++ leetcode/weekly/174/d/d.go
@@ -1,39 +1,31 @@
 package main
 
-// 注：可以用单调队列 DP 优化到 O(n)
-// https://leetcode-cn.com/circle/article/W7083y/
-func maxJumps(a []int, d int) (ans int) {
-	max := func(a, b int) int {
-		if a > b {
-			return a
-		}
-		return b
-	}
-	n := len(a)
+func maxJumps(arr []int, d int) (ans int) {
+	n := len(arr)
 
-	dp := make([]int, n)
-	var f func(p int) int
-	f = func(p int) (_ans int) {
-		if dp[p] > 0 {
-			return dp[p]
+	memo := make([]int, n)
+	var dfs func(int) int
+	dfs = func(i int) (res int) {
+		if memo[i] > 0 { // 之前计算过
+			return memo[i]
 		}
-		defer func() { dp[p] = _ans }()
-		for i := p + 1; i <= p+d; i++ {
-			if i == n || a[i] >= a[p] {
-				break
-			}
-			_ans = max(_ans, f(i))
+		defer func() { memo[i] = res }()
+
+		// 往左跳
+		for j := i - 1; j >= max(i-d, 0) && arr[j] < arr[i]; j-- {
+			res = max(res, dfs(j))
 		}
-		for i := p - 1; i >= p-d; i-- {
-			if i == -1 || a[i] >= a[p] {
-				break
-			}
-			_ans = max(_ans, f(i))
+
+		// 往右跳
+		for j := i + 1; j <= min(i+d, n-1) && arr[j] < arr[i]; j++ {
+			res = max(res, dfs(j))
 		}
-		return _ans + 1
+
+		return res + 1 // +1 提到循环外面
 	}
-	for i := range a {
-		ans = max(ans, f(i))
+
+	for i := range arr {
+		ans = max(ans, dfs(i))
 	}
 	return
 }
