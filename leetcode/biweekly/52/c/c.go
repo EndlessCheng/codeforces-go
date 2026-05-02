cf20140ccdd8f580ac939f09da27187a20c87b0d
Endless <0x3f@0x3f3f3f3f.com>
2026-05-02 21:28:49 +0800

    lc1861

--- leetcode/biweekly/52/c/c.go
+++ leetcode/biweekly/52/c/c.go
@@ -1,31 +1,55 @@
 package main
 
+import "bytes"
+
 // github.com/EndlessCheng/codeforces-go
-func rotateTheBox(a [][]byte) [][]byte {
-	n, m := len(a), len(a[0])
-	ans := make([][]byte, m)
+func rotateTheBox1(boxGrid [][]byte) [][]byte {
+	m, n := len(boxGrid), len(boxGrid[0])
+	ans := make([][]byte, n)
 	for i := range ans {
-		ans[i] = make([]byte, n)
-		for j := range ans[i] {
-			ans[i][j] = '.'
-		}
+		ans[i] = make([]byte, m)
 	}
-	for i, r := range a {
-		for j := 0; j < m; j++ {
-			c := 0
-			for ; j < m && r[j] != '*'; j++ {
-				if r[j] == '#' {
-					c++
+
+	for i, row := range boxGrid {
+		stone := 0
+		for j, ch := range row {
+			if ch == '#' { // 石头
+				stone++
+				ch = '.' // 先把石头清空，下面循环会填入石头
+			}
+			ans[j][m-1-i] = ch
+			if j == n-1 || row[j+1] == '*' { // 下一个格子是障碍物
+				// 石头垂直掉落后，从 j 往前 stone 个格子都是石头
+				for k := j; k > j-stone; k-- {
+					ans[k][m-1-i] = '#'
 				}
-			}
-			if j < m {
-				ans[j][n-1-i] = '*'
-			}
-			for k := j - 1; c > 0; k-- {
-				ans[k][n-1-i] = '#'
-				c--
+				stone = 0 // 重置计数器
 			}
 		}
 	}
+
+	return ans
+}
+
+func rotateTheBox(boxGrid [][]byte) [][]byte {
+	m, n := len(boxGrid), len(boxGrid[0])
+	ans := make([][]byte, n)
+	for i := range ans {
+		ans[i] = bytes.Repeat([]byte{'.'}, m)
+	}
+
+	for i, row := range boxGrid {
+		stone := n - 1
+		for j := n - 1; j >= 0; j-- {
+			if row[j] == '*' { // 障碍物
+				ans[j][m-1-i] = '*'
+				stone = j - 1 // 障碍物左边最近的石头，在旋转后掉落到 j-1
+			} else if row[j] == '#' { // 石头
+				ans[stone][m-1-i] = '#' // 旋转后，石头掉落到 stone
+				stone--
+			}
+		}
+	}
+
 	return ans
 }
