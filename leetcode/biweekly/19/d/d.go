b3424af006f58e060f0ce4418e02be2228a55628
Endless <0x3f@0x3f3f3f3f.com>
2026-05-06 08:35:02 +0800

    lc1345

--- leetcode/biweekly/19/d/d.go
+++ leetcode/biweekly/19/d/d.go
@@ -1,40 +1,48 @@
 package main
 
 // github.com/EndlessCheng/codeforces-go
-func minJumps(a []int) int {
-	pos := map[int][]int{}
-	for i, v := range a {
-		pos[v] = append(pos[v], i)
+func minJumps(arr []int) (ans int) {
+	pos := map[int][]int{} // 元素值 -> [下标]
+	for i, x := range arr {
+		pos[x] = append(pos[x], i)
 	}
-	n := len(a)
+
+	n := len(arr)
 	vis := make([]bool, n)
 	vis[0] = true
-	type pair struct{ v, d int }
-	q := []pair{{0, 0}}
-	for {
-		p := q[0]
-		q = q[1:]
-		v, d := p.v, p.d
-		if v == n-1 {
-			return d
-		}
-		if v > 0 && !vis[v-1] {
-			vis[v-1] = true
-			q = append(q, pair{v - 1, d + 1})
-		}
-		if !vis[v+1] {
-			vis[v+1] = true
-			q = append(q, pair{v + 1, d + 1})
-		}
-		if pos[a[v]] == nil {
-			continue
-		}
-		for _, w := range pos[a[v]] {
-			if !vis[w] {
-				vis[w] = true
-				q = append(q, pair{w, d + 1})
+	q := []int{0} // 起点
+	for ; ; ans++ {
+		tmp := q
+		q = nil
+		for _, i := range tmp {
+			if i == n-1 { // 到达终点
+				return
 			}
+
+			// 往右
+			if !vis[i+1] {
+				vis[i+1] = true
+				q = append(q, i+1)
+			}
+
+			// 往左
+			if i > 0 && !vis[i-1] {
+				vis[i-1] = true
+				q = append(q, i-1)
+			}
+
+			// 访问所有元素值为 arr[i] 的点（下标）
+			x := arr[i]
+			if pos[x] == nil { // 之前访问过
+				continue
+			}
+			for _, j := range pos[x] {
+				if !vis[j] {
+					vis[j] = true
+					q = append(q, j)
+				}
+			}
+			delete(pos, x) // 避免重复访问
 		}
-		delete(pos, a[v])
 	}
 }
