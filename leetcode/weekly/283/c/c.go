3ef0cc60d0731ce041acf0e863c010a0d1b912ee
Endless <0x3f@0x3f3f3f3f.com>
2026-05-26 10:09:01 +0800

    lc2196

--- leetcode/weekly/283/c/c.go
+++ leetcode/weekly/283/c/c.go
@@ -3,9 +3,12 @@ package main
 import . "github.com/EndlessCheng/codeforces-go/leetcode/testutil"
 
 // github.com/EndlessCheng/codeforces-go
-func createBinaryTree(descriptions [][]int) *TreeNode {
-	nodes := map[int]*TreeNode{}
-	in := map[int]bool{}
+func createBinaryTree1(descriptions [][]int) *TreeNode {
+	n := len(descriptions)
+	nodes := make(map[int]*TreeNode, n+1) // 预分配空间
+	children := make(map[int]bool, n)
+
+	// 建树
 	for _, d := range descriptions {
 		x, y := d[0], d[1]
 		if nodes[x] == nil {
@@ -19,13 +22,40 @@ func createBinaryTree(descriptions [][]int) *TreeNode {
 		} else {
 			nodes[x].Right = nodes[y]
 		}
-		in[y] = true
+		children[y] = true // y 不是根节点
 	}
 
-	for i, node := range nodes {
-		if !in[i] { // 入度为 0 则为根
+	for x, node := range nodes {
+		if !children[x] { // node 是根节点
 			return node
 		}
 	}
-	return nil
+
+	// 测试用例保证可以构造出有效的二叉树
+	panic("不是有效的二叉树")
+}
+
+func createBinaryTree(descriptions [][]int) *TreeNode {
+	nodes := make(map[int]*TreeNode, len(descriptions)+1) // 预分配空间
+	root := 0
+
+	for _, d := range descriptions {
+		x, y := d[0], d[1]
+		if nodes[x] == nil {
+			nodes[x] = &TreeNode{Val: x}
+			root ^= x
+		}
+		if nodes[y] == nil {
+			nodes[y] = &TreeNode{Val: y}
+			root ^= y
+		}
+		if d[2] == 1 {
+			nodes[x].Left = nodes[y]
+		} else {
+			nodes[x].Right = nodes[y]
+		}
+		root ^= y
+	}
+
+	return nodes[root]
 }
