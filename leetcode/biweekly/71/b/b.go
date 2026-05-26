a63cc8560b2c66c2fb29a28d537a31357f4286f0
Endless <0x3f@0x3f3f3f3f.com>
2026-05-26 11:16:34 +0800

    lc2161

--- leetcode/biweekly/71/b/b.go
+++ leetcode/biweekly/71/b/b.go
@@ -1,18 +1,27 @@
 package main
 
-// 模拟
+import "cmp"
 
 // github.com/EndlessCheng/codeforces-go
 func pivotArray(nums []int, pivot int) []int {
 	var a, b, c []int
-	for _, v := range nums {
-		if v < pivot {
-			a = append(a, v)
-		} else if v == pivot {
-			b = append(b, v)
+	for _, x := range nums {
+		if x < pivot {
+			a = append(a, x)
+		} else if x == pivot {
+			b = append(b, x)
 		} else {
-			c = append(c, v)
+			c = append(c, x)
 		}
 	}
 	return append(append(a, b...), c...)
+}
+
+func pivotArray2(nums []int, pivot int) []int {
+	a := [3][]int{}
+	for _, x := range nums {
+		cmp := cmp.Compare(x, pivot)
+		a[cmp+1] = append(a[cmp+1], x)
+	}
+	return append(append(a[0], a[1]...), a[2]...)
 }
