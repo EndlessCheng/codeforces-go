e57c889e06a9cad23f7a951479d402567adfd40b
Endless <0x3f@0x3f3f3f3f.com>
2026-05-28 06:11:17 +0800

    abc431_f

---
+++ misc/atcoder/abc431/f/f.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"os"
+	"slices"
+	"sort"
+)
+
+// https://github.com/EndlessCheng
+func run(in io.Reader, out io.Writer) {
+	const mod = 998244353
+	pow := func(x, n int) int {
+		res := 1
+		for ; n > 0; n /= 2 {
+			if n%2 > 0 {
+				res = res * x % mod
+			}
+			x = x * x % mod
+		}
+		return res
+	}
+
+	var n, d int
+	Fscan(in, &n, &d)
+	a := make([]int, n)
+	cnt := [1e6 + 1]int{}
+	dup := 1
+	for i := range a {
+		Fscan(in, &a[i])
+		cnt[a[i]]++
+		dup = dup * cnt[a[i]] % mod
+	}
+	slices.Sort(a)
+
+	ans := pow(dup, mod-2)
+	for i, v := range a {
+		j := sort.SearchInts(a, v-d)
+		ans = ans * (i - j + 1) % mod
+	}
+	Fprint(out, ans)
+}
+
+func main() { run(bufio.NewReader(os.Stdin), os.Stdout) }
