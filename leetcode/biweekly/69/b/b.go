5e53fdb903b68fb4ce692de78ad677c915176944
Endless <0x3f@0x3f3f3f3f.com>
2026-05-27 09:16:27 +0800

    lc2130

--- leetcode/biweekly/69/b/b.go
+++ leetcode/biweekly/69/b/b.go
@@ -3,15 +3,56 @@ package main
 import . "github.com/EndlessCheng/codeforces-go/leetcode/testutil"
 
 // github.com/EndlessCheng/codeforces-go
-func pairSum(head *ListNode) (ans int) {
-	a := []int{}
-	for node := head; node != nil; node = node.Next {
-		a = append(a, node.Val)
+func pairSum1(head *ListNode) (ans int) {
+	left := head
+
+	var dfs func(*ListNode)
+	dfs = func(right *ListNode) {
+		// 「递」，先把 right 移到链表末尾
+		if right.Next != nil {
+			dfs(right.Next)
+		}
+		// 「归」的过程就是在从右到左遍历链表
+		ans = max(ans, left.Val+right.Val)
+		left = left.Next // left 往右走
+		// 归，right 会往左走
 	}
-	for i, n := 0, len(a); i < n/2; i++ {
-		ans = max(ans, a[i]+a[n-1-i])
-	}
+
+	dfs(head)
 	return
 }
 
-func max(a, b int) int { if b > a { return b }; return a }
+//
+
+// 876. 链表的中间结点
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
+
+// 206. 反转链表
+func reverseList(head *ListNode) *ListNode {
+	var pre, cur *ListNode = nil, head
+	for cur != nil {
+		nxt := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = nxt
+	}
+	return pre
+}
+
+func pairSum(head *ListNode) (ans int) {
+	mid := middleNode(head)
+	head2 := reverseList(mid)
+	for head2 != nil {
+		ans = max(ans, head.Val+head2.Val)
+		head = head.Next
+		head2 = head2.Next
+	}
+	return
+}
