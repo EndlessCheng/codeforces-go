73e2d6fe02026511705ca03ab006854d0d499356
Endless <0x3f@0x3f3f3f3f.com>
2026-05-31 09:35:44 +0800

    abc408_f

--- misc/atcoder/abc408/f/f.go
+++ misc/atcoder/abc408/f/f.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"math/bits"
 	"os"
-	"slices"
 )
 
 // https://github.com/EndlessCheng
@@ -17,27 +16,27 @@ func (t seg) update(o, l, r, i, v int) {
 		t[o] = v
 		return
 	}
-	m := (l + r) / 2
+	m := (l + r) >> 1
 	if i <= m {
-		t.update(o*2, l, m, i, v)
+		t.update(o<<1, l, m, i, v)
 	} else {
-		t.update(o*2+1, m+1, r, i, v)
+		t.update(o<<1|1, m+1, r, i, v)
 	}
-	t[o] = max(t[o*2], t[o*2+1])
+	t[o] = max(t[o<<1], t[o<<1|1])
 }
 
 func (t seg) query(o, l, r, ql, qr int) int {
 	if ql <= l && r <= qr {
 		return t[o]
 	}
-	m := (l + r) / 2
+	m := (l + r) >> 1
 	if qr <= m {
-		return t.query(o*2, l, m, ql, qr)
+		return t.query(o<<1, l, m, ql, qr)
 	}
 	if ql > m {
-		return t.query(o*2+1, m+1, r, ql, qr)
+		return t.query(o<<1|1, m+1, r, ql, qr)
 	}
-	return max(t.query(o*2, l, m, ql, qr), t.query(o*2+1, m+1, r, ql, qr))
+	return max(t.query(o<<1, l, m, ql, qr), t.query(o<<1|1, m+1, r, ql, qr))
 }
 
 func run(in io.Reader, out io.Writer) {
@@ -54,12 +53,14 @@ func run(in io.Reader, out io.Writer) {
 		t[i] = -1e9
 	}
 	f := make([]int, n)
+	ans := 0
 	for x := d + 1; x <= n; x++ {
 		t.update(1, 0, n-1, pos[x-d], f[pos[x-d]])
 		i := pos[x]
 		f[i] = max(t.query(1, 0, n-1, max(i-r, 0), min(i+r, n-1))+1, 0)
+		ans = max(ans, f[i])
 	}
-	Fprint(out, slices.Max(f))
+	Fprint(out, ans)
 }
 
 func main() { run(bufio.NewReader(os.Stdin), os.Stdout) }
