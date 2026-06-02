84c3de62dfcfcdf6231113c0fbe2010b9f1f957f
Endless <0x3f@0x3f3f3f3f.com>
2026-06-02 10:27:00 +0800

    lc2095

--- leetcode/weekly/270/b/b.go
+++ leetcode/weekly/270/b/b.go
@@ -2,24 +2,19 @@ package main
 
 import . "github.com/EndlessCheng/codeforces-go/leetcode/testutil"
 
-/* 快慢指针
-
-这题其实就是把 [876. 链表的中间结点](https://leetcode-cn.com/problems/middle-of-the-linked-list/) 和删除链表结点结合起来
-
-我们只需要在 876 题的基础上，额外记录 $\textit{slow}$ 结点的上一个结点即可。
-
-*/
-
 // github.com/EndlessCheng/codeforces-go
 func deleteMiddle(head *ListNode) *ListNode {
-	dummyHead := &ListNode{Next: head}
-	pre, slow, fast := dummyHead, head, head
+	if head.Next == nil { // 只有一个节点
+		return nil
+	}
+	// 876. 链表的中间结点
+	// 本题先让兔子走两步，这样乌龟少走一步，刚好落在中间节点的前一个节点
+	slow := head
+	fast := head.Next.Next
 	for fast != nil && fast.Next != nil {
-		pre = slow // pre 记录了 slow 的上一个结点
 		slow = slow.Next
 		fast = fast.Next.Next
 	}
-	// 循环结束后，slow 为待删除结点
-	pre.Next = slow.Next // 删除 slow
-	return dummyHead.Next
+	slow.Next = slow.Next.Next // 删除 slow 的下一个节点
+	return head
 }
