2b0f659639d3db1b35c4b64c3af06136e107654a
Endless <0x3f@0x3f3f3f3f.com>
2026-06-05 13:26:25 +0800

    lc1344

--- leetcode/biweekly/19/c/c.go
+++ leetcode/biweekly/19/c/c.go
@@ -3,7 +3,19 @@ package main
 import "math"
 
 // github.com/EndlessCheng/codeforces-go
+func angleClock1(hour, minutes int) float64 {
+	d := math.Abs(float64(hour*30) - float64(minutes)*5.5)
+	return min(d, 360-d)
+}
+
 func angleClock(hour, minutes int) float64 {
-	d := math.Abs((float64(hour%12)+float64(minutes)/60)*30 - float64(minutes*6))
-	return math.Min(d, 360-d)
+	d := abs(hour*60 - minutes*11)
+	return float64(min(d, 720-d)) / 2
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
