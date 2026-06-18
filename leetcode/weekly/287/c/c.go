93813ecceb241825a63c93a88b59abfd8d88dc18
Endless <0x3f@0x3f3f3f3f.com>
2026-06-18 11:59:25 +0800

    lc 加了数据，之前一些不明显的优化现在明显了

--- leetcode/weekly/287/c/c.go
+++ leetcode/weekly/287/c/c.go
@@ -4,18 +4,21 @@ import "sort"
 
 // github.com/EndlessCheng/codeforces-go
 func maximumCandies(candies []int, k int64) int {
-	mx, sum := 0, 0
+	mx, total := 0, 0
 	for _, c := range candies {
 		mx = max(mx, c)
-		sum += c
+		total += c
 	}
 	// 二分最大的不满足要求的 low+1，那么答案就是 low
-	return sort.Search(min(mx, sum/int(k)), func(low int) bool {
+	return sort.Search(min(mx, total/int(k)), func(low int) bool {
 		low++
 		sum := 0
 		for _, candy := range candies {
 			sum += candy / low
+			if sum >= int(k) {
+				return false
+			}
 		}
-		return sum < int(k)
+		return true
 	})
 }
