4161be19257d983fba77689a3f7d5056de391b38
Endless <0x3f@0x3f3f3f3f.com>
2026-06-21 06:31:46 +0800

    lc1833

--- leetcode/weekly/237/b/b.go
+++ leetcode/weekly/237/b/b.go
@@ -1,15 +1,35 @@
 package main
 
-import "sort"
+import "slices"
 
 // github.com/EndlessCheng/codeforces-go
-func maxIceCream(a []int, coins int) int {
-	sort.Ints(a)
-	for i, v := range a {
-		if coins < v {
-			return i
+func maxIceCream(costs []int, coins int) int {
+	slices.Sort(costs)
+
+	// 按照价格从低到高买
+	for i, cost := range costs {
+		if coins < cost { // 钱不够
+			return i // 买 [0, i-1] 一共 i 根雪糕
 		}
-		coins -= v
+		coins -= cost
 	}
-	return len(a)
+
+	// 可以买所有雪糕
+	return len(costs)
+}
+
+func maxIceCream2(costs []int, coins int) (ans int) {
+	mx := slices.Max(costs)
+	cnt := make([]int, mx+1)
+	for _, cost := range costs {
+		cnt[cost]++
+	}
+
+	// 按照价格从低到高买
+	for cost := 1; cost <= mx && cost <= coins; cost++ {
+		num := min(cnt[cost], coins/cost)
+		coins -= cost * num // 买 num 根雪糕
+		ans += num
+	}
+	return
 }
