d046b0a9936f2c8e205d14a3d7c2da85445a60e3
Endless <0x3f@0x3f3f3f3f.com>
2026-06-22 07:38:59 +0800

    lc2287

--- leetcode/weekly/295/a/a.go
+++ leetcode/weekly/295/a/a.go
@@ -1,17 +1,22 @@
 package main
 
+import "math"
+
 // https://space.bilibili.com/206214/dynamic
 func rearrangeCharacters(s, target string) int {
 	var cntS, cntT [26]int
-	for _, ch := range s { cntS[ch-'a']++ }
-	for _, ch := range target { cntT[ch-'a']++ }
-	ans := len(s)
-	for i, c := range cntS {
-		if cntT[i] > 0 {
-			ans = min(ans, c/cntT[i])
+	for _, ch := range s {
+		cntS[ch-'a']++
+	}
+	for _, ch := range target {
+		cntT[ch-'a']++
+	}
+
+	ans := math.MaxInt
+	for i, ct := range cntT {
+		if ct > 0 {
+			ans = min(ans, cntS[i]/ct)
 		}
 	}
 	return ans
 }
-
-func min(a, b int) int { if a > b { return b }; return a }
