1ee7035735ebd555e1396e3608c7acd36447a3b5
Endless <0x3f@0x3f3f3f3f.com>
2026-06-29 10:32:29 +0800

    upd

--- leetcode/weekly/508/d/d.go
+++ leetcode/weekly/508/d/d.go
@@ -6,8 +6,7 @@ import (
 )
 
 // https://space.bilibili.com/206214
-// todo 错在哪？
-func minTimeMaxPower1(n int, edges [][]int, power int, cost []int, source int, target int) []int64 {
+func minTimeMaxPower(n int, edges [][]int, power int, cost []int, source int, target int) []int64 {
 	type edge struct{ to, t int }
 	g := make([][]edge, n)
 	for _, e := range edges {
@@ -15,29 +14,32 @@ func minTimeMaxPower1(n int, edges [][]int, power int, cost []int, source int, t
 		g[x] = append(g[x], edge{y, t})
 	}
 
-	dis := make([]pair, n)
+	dis := make([][]int, n)
 	for i := range dis {
-		dis[i].d = math.MaxInt
+		dis[i] = make([]int, power+1)
+		for j := range dis[i] {
+			dis[i][j] = math.MaxInt
+		}
 	}
-	dis[source] = pair{0, power}
-	h := hp{{dis[source], source}}
+	dis[source][power] = 0
+	h := hp{{0, source, power}}
 
 	for len(h) > 0 {
 		top := heap.Pop(&h).(tuple)
-		d, rem, x := top.d, top.rem, top.x
+		d, x, rem := top.dis, top.x, top.rem
 		if x == target {
 			return []int64{int64(d), int64(rem)}
 		}
-		if d > dis[x].d || d == dis[x].d && rem < dis[x].rem || rem < cost[x] {
+		if d > dis[x][rem] || rem < cost[x] {
 			continue
 		}
 		rem -= cost[x]
 		for _, e := range g[x] {
 			y := e.to
-			newD := pair{d + e.t, rem}
-			if less(newD, dis[y]) {
-				dis[y] = newD
-				heap.Push(&h, tuple{newD, rem})
+			newD := d + e.t
+			if newD < dis[y][rem] {
+				dis[y][rem] = newD
+				heap.Push(&h, tuple{newD, y, rem})
 			}
 		}
 	}
@@ -45,28 +47,22 @@ func minTimeMaxPower1(n int, edges [][]int, power int, cost []int, source int, t
 	return []int64{-1, -1}
 }
 
-type pair struct{ d, rem int }
-
-func less(a, b pair) bool {
-	return a.d < b.d || a.d == b.d && a.rem > b.rem
-}
-
-// 最短路长度, 剩余电量, 节点编号
-type tuple struct {
-	pair
-	x int
-}
+// 最短路长度, 节点编号, 剩余电量
+type tuple struct{ dis, x, rem int }
 type hp []tuple
 
-func (h hp) Len() int           { return len(h) }
-func (h hp) Less(i, j int) bool { return less(h[i].pair, h[j].pair) }
-func (h hp) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *hp) Push(v any)        { *h = append(*h, v.(tuple)) }
-func (h *hp) Pop() (v any)      { a := *h; *h, v = a[:len(a)-1], a[len(a)-1]; return }
+func (h hp) Len() int { return len(h) }
+func (h hp) Less(i, j int) bool {
+	a, b := h[i], h[j]
+	return a.dis < b.dis || a.dis == b.dis && a.rem > b.rem
+}
+func (h hp) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+func (h *hp) Push(v any)   { *h = append(*h, v.(tuple)) }
+func (h *hp) Pop() (v any) { a := *h; *h, v = a[:len(a)-1], a[len(a)-1]; return }
 
 //
 
-func minTimeMaxPower(n int, edges [][]int, power int, cost []int, source int, target int) []int64 {
+func minTimeMaxPower2(n int, edges [][]int, power int, cost []int, source int, target int) []int64 {
 	type edge struct{ to, t int }
 	g := make([][]edge, n)
 	for _, e := range edges {
