40017663a6496bf255360a338a6bc39d9aaec006
Endless <0x3f@0x3f3f3f3f.com>
2026-06-30 08:39:32 +0800

    lc1291

--- leetcode/weekly/167/b/b.go
+++ leetcode/weekly/167/b/b.go
@@ -1,23 +1,39 @@
 package main
 
-import "strconv"
+import (
+	"strconv"
+)
 
-func sequentialDigits(low int, high int) (ans []int) {
-	all := []int{}
-	for sz := 2; sz <= 9; sz++ {
-		for st := 1; st+sz-1 <= 9; st++ {
-			s := []byte{}
-			for i := 0; i < sz; i++ {
-				s = append(s, byte('0'+st+i))
+func sequentialDigits1(low, high int) (ans []int) {
+	const digits = "123456789"
+	minLen := len(strconv.Itoa(low))
+	maxLen := len(strconv.Itoa(high))
+	// 枚举 digits 的子串（先枚举子串长度，再枚举子串位置）
+	for length := minLen; length <= maxLen; length++ {
+		for r := length; r <= len(digits); r++ {
+			x, _ := strconv.Atoi(digits[r-length : r])
+			if low <= x && x <= high {
+				ans = append(ans, x)
 			}
-			v, _ := strconv.Atoi(string(s))
-			all = append(all, v)
 		}
 	}
-	for _, v := range all {
-		if v >= low && v <= high {
-			ans = append(ans, v)
+	return
+}
+
+func sequentialDigits(low, high int) (ans []int) {
+	x0 := 12 // 第一个窗口
+	pow10 := 10
+	for length := 2; x0 <= high; length++ {
+		pow10 *= 10
+		x := x0
+		for i := length; i <= 9 && x <= high; i++ {
+			if x >= low {
+				ans = append(ans, x)
+			}
+			// 窗口向右滑动，i+1 进入窗口，i+1-length 离开窗口
+			x = x*10 + i + 1 - (i+1-length)*pow10
 		}
+		x0 = x0*10 + length + 1
 	}
 	return
 }
