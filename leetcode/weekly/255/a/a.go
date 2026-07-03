1dd055cf4d5b33681c3df5dbd3fd97999925a695
Endless <0x3f@0x3f3f3f3f.com>
2026-07-03 09:43:57 +0800

    lc1979

--- leetcode/weekly/255/a/a.go
+++ leetcode/weekly/255/a/a.go
@@ -1,16 +1,37 @@
 package main
 
+import "slices"
+
 // github.com/EndlessCheng/codeforces-go
-func findGCD(a []int) int {
-	min, max := a[0], a[0]
-	for _, v := range a[1:] {
-		if v < min {
-			min = v
-		} else if v > max {
-			max = v
+func findGCD1(nums []int) int {
+	return gcd(slices.Min(nums), slices.Max(nums))
+}
+
+func findGCD(nums []int) int {
+	n := len(nums)
+	mn, mx := nums[0], nums[0]
+	if n%2 == 0 {
+		if nums[0] < nums[1] {
+			mx = nums[1]
+		} else {
+			mn = nums[1]
 		}
 	}
-	return gcd(min, max)
+
+	for i := 2 - n%2; i < n; i += 2 {
+		x, y := nums[i], nums[i+1]
+		if x > y {
+			x, y = y, x
+		}
+		if x < mn {
+			mn = x
+		}
+		if y > mx {
+			mx = y
+		}
+	}
+
+	return gcd(mn, mx)
 }
 
 func gcd(a, b int) int {
