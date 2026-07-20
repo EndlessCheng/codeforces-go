962343a1c9c3ded661936614fc3617e3d81442ac
Endless <0x3f@0x3f3f3f3f.com>
2026-07-20 08:22:16 +0800

    lc1260

--- leetcode/weekly/163/a/a.go
+++ leetcode/weekly/163/a/a.go
@@ -1,17 +1,40 @@
 package main
 
-func shiftGrid(grid [][]int, k int) [][]int {
-	n, m := len(grid), len(grid[0])
-	g := make([][]int, n)
-	for i := range g {
-		g[i] = make([]int, m)
+func shiftGrid1(grid [][]int, k int) [][]int {
+	m, n := len(grid), len(grid[0])
+	ans := make([][]int, m)
+	for i := range ans {
+		ans[i] = make([]int, n)
 	}
-	for i, gi := range grid {
-		for j, gij := range gi {
-			newPos := i*m + j + k
-			ni, nj := newPos/m%n, newPos%m
-			g[ni][nj] = gij
+
+	size := m * n
+	for i, row := range grid {
+		for j, x := range row {
+			p := (i*n + j + k) % size
+			ans[p/n][p%n] = x
 		}
 	}
-	return g
+	return ans
+}
+
+func shiftGrid(grid [][]int, k int) [][]int {
+	m, n := len(grid), len(grid[0])
+
+	reverse := func(l, r int) {
+		for l < r {
+			x1, y1 := l/n, l%n
+			x2, y2 := r/n, r%n
+			grid[x1][y1], grid[x2][y2] = grid[x2][y2], grid[x1][y1]
+			l++
+			r--
+		}
+	}
+
+	// 189. 轮转数组
+	size := m * n
+	k %= size // 轮转 k 次等于轮转 k % size 次
+	reverse(0, size-1)
+	reverse(0, k-1)
+	reverse(k, size-1)
+	return grid
 }
