439eb61eb05758bee8f8ec9392f247e018b47030
Endless <0x3f@0x3f3f3f3f.com>
2026-07-27 08:27:14 +0800

    lc1464

--- leetcode/weekly/191/a/a.go
+++ leetcode/weekly/191/a/a.go
@@ -1,19 +1,10 @@
 package main
 
-func maxProduct(a []int) (ans int) {
-	max := func(a, b int) int {
-		if a > b {
-			return a
-		}
-		return b
-	}
-	for i, v := range a {
-		for j, w := range a {
-			if j == i {
-				continue
-			}
-			ans = max(ans, (v-1)*(w-1))
-		}
+func maxProduct(nums []int) (ans int) {
+	mx := 0
+	for _, x := range nums {
+		ans = max(ans, (mx-1)*(x-1))
+		mx = max(mx, x)
 	}
 	return
 }
