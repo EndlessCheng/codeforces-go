b05777a322389caed4a5e0683a57660404b0af28
Endless <0x3f@0x3f3f3f3f.com>
2026-07-28 09:27:16 +0800

    cf1608F

--- main/1600-1699/1608F.go
+++ main/1600-1699/1608F.go
@@ -8,71 +8,86 @@ import (
 // https://github.com/EndlessCheng
 func cf1608F(in io.Reader, out io.Writer) {
 	const mod = 998244353
-	var n, K int
-	Fscan(in, &n, &K)
-	b := make([]int, n+1)
+	var n, k int
+	Fscan(in, &n, &k)
+	a := make([]int, n+1)
 	for i := 1; i <= n; i++ {
-		Fscan(in, &b[i])
+		Fscan(in, &a[i])
+		a[i] -= k
 	}
-	f := [2][][]int{}
+
+	C := make([][]int, n+1)
+	for i := range C {
+		C[i] = make([]int, n+1)
+	}
+	C[0][0] = 1
+	for i := 1; i <= n; i++ {
+		C[i][0] = 1
+		for j := 1; j <= i; j++ {
+			C[i][j] = (C[i-1][j-1] + C[i-1][j]) % mod
+		}
+	}
+
+	f := make([][][]int, 2)
 	for i := range f {
-		f[i] = make([][]int, n+2)
+		f[i] = make([][]int, 2*k+1)
 		for j := range f[i] {
 			f[i][j] = make([]int, n+2)
 		}
 	}
 
-	cur := 0
-	ans := 0
-	l, r := 0, 0
+	g := make([]int, n+2)
 	f[0][0][0] = 1
-
-	add := func(x int) int {
-		if x < 0 {
-			return x + mod
-		}
-		return x
-	}
-
 	for i := 1; i <= n; i++ {
-		r = min(i, b[i]+K)
-		for j := 0; j <= n; j++ {
-			for k := 0; k <= n; k++ {
-				f[cur^1][j][k] = 0
+		for j := 0; j <= 2*k; j++ {
+			for p := 0; p <= i; p++ {
+				f[i&1][j][p] = 0
 			}
 		}
-		for j := l + 1; j <= r; j++ {
-			for k := 0; k <= i-j; k++ {
-				f[cur^1][j][k] = f[cur^1][j-1][k+1] * (k + 1) % mod
-				if j-1 <= b[i-1]+K {
-					f[cur^1][j][k] = add(f[cur^1][j][k] + f[cur][j-1][k] - mod)
+		for p := 0; p <= i; p++ {
+			g[p] = 0
+		}
+		for j := 0; j <= 2*k && a[i-1]+j < a[i]-1; j++ {
+			for p := 0; p+a[i]-2-(a[i-1]+j) <= i; p++ {
+				g[p] = (g[p] + f[i&1^1][j][p+a[i]-2-(a[i-1]+j)]) % mod
+			}
+		}
+		for j := 0; j <= 2*k; j++ {
+			if a[i]+j < 0 {
+				continue
+			}
+			for p := 0; p <= i; p++ {
+				g[p] = g[p+1]
+			}
+			cur := a[i] + j - a[i-1]
+			if cur-1 >= 0 && cur-1 <= 2*k {
+				for p := 0; p <= i; p++ {
+					g[p] = (g[p] + f[i&1^1][cur-1][p]) % mod
+				}
+			}
+			for p := 0; p <= i; p++ {
+				f[i&1][j][p] = g[p]
+			}
+			if cur >= 0 && cur <= 2*k {
+				for p := 0; p <= i; p++ {
+					f[i&1][j][p] = (f[i&1][j][p] + f[i&1^1][cur][p]*(a[i]+j+p)) % mod
+					if p > 0 {
+						f[i&1][j][p] = (f[i&1][j][p] + f[i&1^1][cur][p-1]*p) % mod
+					}
 				}
 			}
 		}
-		l = max(l, b[i]-K)
-		r = min(r, min(b[i-1]+K, i-1))
-		for j := l; j <= r; j++ {
-			for k := 0; k <= i-j; k++ {
-				if k < i-j {
-					f[cur^1][j][k] = (f[cur^1][j][k] + f[cur][j][k]*(j+k)) % mod
-				}
-				if k > 0 {
-					f[cur^1][j][k] = add(f[cur^1][j][k] + f[cur][j][k-1] - mod)
-				}
-			}
-		}
-		cur ^= 1
 	}
 
-	r = min(n, b[n]+K)
-	for i := l; i <= r; i++ {
-		s := 1
-		for j := 0; j <= n-i; j++ {
-			ans = (ans + f[cur][i][j]*s) % mod
-			s = s * (n - i - j) % mod
+	ans := 0
+	for j := 0; j <= 2*k; j++ {
+		if a[n]+j >= 0 {
+			for p := 0; p <= n-(a[n]+j); p++ {
+				ans = (ans + C[n-(a[n]+j)][p]*f[n&1][j][p]) % mod
+			}
 		}
 	}
-	Fprint(out, ans)
+	Fprintln(out, ans)
 }
 
 //func main() { cf1608F(bufio.NewReader(os.Stdin), os.Stdout) }
