857980eb8379e5f6a323a56a5605f61a27fe7561
Endless <0x3f@0x3f3f3f3f.com>
2026-07-30 15:32:32 +0800

    写法二

--- main/2200-2299/2236G.go
+++ main/2200-2299/2236G.go
@@ -5,6 +5,7 @@ import (
 	. "fmt"
 	"io"
 	"math/bits"
+	"os"
 )
 
 // https://github.com/EndlessCheng
@@ -12,64 +13,58 @@ func cf2236G(in io.Reader, _w io.Writer) {
 	out := bufio.NewWriter(_w)
 	defer out.Flush()
 	var T, n, q, v, w, ans, t int
+	mem := [21]int{}
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &n, &q)
-		a := make([]int, n)
-		for i := range a {
+		a := make([]int, n+1)
+		for i := 1; i <= n; i++ {
 			Fscan(in, &a[i])
 		}
-		g := make([][]int, n)
+		g := make([][]int, n+1)
 		for range n - 1 {
 			Fscan(in, &v, &w)
-			v--
-			w--
 			g[v] = append(g[v], w)
 			g[w] = append(g[w], v)
 		}
 
 		mx := bits.Len(uint(n))
-		dep := make([]int, n)
-		pa := make([][17]int, n)
-		type pair struct{ topDep, cnt int }
-		data := make([][17]pair, n)
-		lastNZ := make([]int, n)
+		pa := make([][17]int, n+1)
+		dep := make([]int, n+1)
+		topDep := make([]int, n+1)
+		sum := make([]int, n+1)
+		lastNZ := make([]int, n+1)
 		st := []int{}
 		var dfs func(int, int, int, int, int)
-		dfs = func(v, fa, or, top, last int) {
+		dfs = func(v, fa, or, topD, nz int) {
+			pa[v][0] = fa
+			for i := range mx - 1 {
+				pa[v][i+1] = pa[pa[v][i]][i]
+			}
+
 			dep[v] = len(st)
 			st = append(st, v)
 			for or&a[v] > 0 {
-				or &^= a[st[top]]
-				top++
+				or &^= a[st[topD]]
+				topD++
 			}
 			or |= a[v]
+			topDep[v] = topD
+			sum[v] = sum[fa] + len(st) - topD
 
-			pa[v][0] = fa
-			data[v][0] = pair{top, len(st) - top}
-			for i := range mx - 1 {
-				p := pa[v][i]
-				if p != -1 {
-					pa[v][i+1] = pa[p][i]
-					d := data[p][i]
-					data[v][i+1] = pair{d.topDep, data[v][i].cnt + d.cnt}
-				} else {
-					pa[v][i+1] = -1
-				}
-			}
-
-			lastNZ[v] = last
+			lastNZ[v] = nz
 			if a[v] > 0 {
-				last = v
+				nz = v
 			}
 
 			for _, w := range g[v] {
 				if w != fa {
-					dfs(w, v, or, top, last)
+					dfs(w, v, or, topD, nz)
 				}
 			}
 			st = st[:len(st)-1]
 		}
-		dfs(0, -1, 0, 0, -1)
+		dfs(1, 0, 0, 0, 0)
+
 		uptoDep := func(v, d int) int {
 			for k := uint32(dep[v] - d); k > 0; k &= k - 1 {
 				v = pa[v][bits.TrailingZeros32(k)]
@@ -90,23 +85,22 @@ func cf2236G(in io.Reader, _w io.Writer) {
 			return pa[v][0]
 		}
 		up := func(v, dLca int) (int, int) {
-			tot := 0
+			x := v
 			for i := mx - 1; i >= 0; i-- {
-				w := pa[v][i]
-				d := data[v][i]
-				if w >= 0 && d.topDep > dLca {
-					tot += d.cnt
-					v = w
+				p := pa[x][i]
+				if topDep[p] > dLca {
+					x = p
 				}
 			}
-			sz := dep[v] - dLca + 1
-			return v, tot + sz*(sz+1)/2
+			if topDep[x] > dLca {
+				x = pa[x][0]
+			}
+			sz := dep[x] - dLca + 1
+			return x, sum[v] - sum[x] + sz*(sz+1)/2
 		}
 
 		for range q {
 			Fscan(in, &v, &w)
-			v--
-			w--
 			if dep[v] < dep[w] {
 				v, w = w, v
 			}
@@ -120,13 +114,13 @@ func cf2236G(in io.Reader, _w io.Writer) {
 			w, t = up(w, dLca)
 			ans += t - 1
 
-			nodes := []int{}
-			for x := w; x >= 0 && dep[x] > dLca; x = lastNZ[x] {
+			nodes := mem[:0]
+			for x := w; dep[x] > dLca; x = lastNZ[x] {
 				nodes = append(nodes, x)
 			}
 
 			or := a[lca]
-			for x := v; x >= 0 && dep[x] > dLca; x = lastNZ[x] {
+			for x := v; dep[x] > dLca; x = lastNZ[x] {
 				or |= a[x]
 			}
 
@@ -136,7 +130,7 @@ func cf2236G(in io.Reader, _w io.Writer) {
 				w := nodes[i]
 				ans += (depV - dLca) * (dep[w] - depW)
 				depW = dep[w]
-				for v >= 0 && dep[v] > dLca && or&a[w] > 0 {
+				for dep[v] > dLca && or&a[w] > 0 {
 					or &^= a[v]
 					depV = dep[v] - 1
 					v = lastNZ[v]
@@ -151,4 +145,4 @@ func cf2236G(in io.Reader, _w io.Writer) {
 	}
 }
 
-//func main() { cf2236G(bufio.NewReader(os.Stdin), os.Stdout) }
+func main() { cf2236G(bufio.NewReader(os.Stdin), os.Stdout) }
