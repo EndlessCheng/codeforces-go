ef6f697ddc49d70884d4292577a5a739dc4dcc3c
Endless <0x3f@0x3f3f3f3f.com>
2026-08-01 09:41:58 +0800

    lc1510

--- leetcode/biweekly/30/d/d.go
+++ leetcode/biweekly/30/d/d.go
@@ -1,19 +1,57 @@
 package main
 
 // github.com/EndlessCheng/codeforces-go
-var win [1e5 + 1]bool
+// https://oeis.org/A030193
+func winnerSquareGame1(n int) bool {
+	memo := make([]int8, n+1)
+	for i := range memo {
+		memo[i] = -1
+	}
 
-func init() {
-	for i := 1; i <= 1e5; i++ {
-		for j := 1; j*j <= i; j++ {
-			if !win[i-j*j] {
-				win[i] = true
+	var dfs func(int) bool
+	dfs = func(i int) bool {
+		if i == 0 {
+			return false
+		}
+		p := &memo[i]
+		if *p != -1 {
+			return *p == 1
+		}
+		for x := 1; x*x <= i; x++ {
+			if !dfs(i - x*x) {
+				*p = 1
+				return true
+			}
+		}
+		*p = 0
+		return false
+	}
+
+	return dfs(n)
+}
+
+func winnerSquareGame2(n int) bool {
+	f := make([]bool, n+1)
+	for i := 1; i <= n; i++ {
+		for x := 1; x*x <= i; x++ {
+			if !f[i-x*x] {
+				f[i] = true
 				break
 			}
 		}
 	}
+	return f[n]
 }
 
 func winnerSquareGame(n int) bool {
-	return win[n]
+	f := make([]bool, n+1)
+	for i := range n {
+		if f[i] {
+			continue
+		}
+		for x := 1; x*x <= n-i; x++ {
+			f[i+x*x] = true
+		}
+	}
+	return f[n]
 }
