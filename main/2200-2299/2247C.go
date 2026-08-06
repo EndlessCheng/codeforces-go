7082eb4df5b82ce0521cf1b6268c54201fabd1a4
Endless <0x3f@0x3f3f3f3f.com>
2026-08-06 21:32:49 +0800

    cf2247C

--- main/2200-2299/2247C.go
+++ main/2200-2299/2247C.go
@@ -15,16 +15,18 @@ func cf2247C(in io.Reader, out io.Writer) {
 			Fscan(in, &a[i])
 		}
 		cnt := [2]int{}
+		c1 := 0
 		for _, v := range a {
+			c1 += v
 			Fscan(in, &w)
 			if v != w {
 				cnt[v]++
 			}
 		}
-		if cnt[0] > 0 && cnt[1] == 0 {
-			Fprintln(out, -1)
-		} else if cnt[0] == 0 && cnt[1] == 0 {
+		if cnt[0] == 0 && cnt[1] == 0 {
 			Fprintln(out, 0)
+		} else if c1 == 0 && cnt[0] > 0 || cnt[1] == 0 && n-c1 == cnt[0] {
+			Fprintln(out, -1)
 		} else {
 			Fprintln(out, 2-cnt[1]%2)
 		}
