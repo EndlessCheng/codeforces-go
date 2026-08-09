665f2acceed5154a8d00dd38c8c2a41f779c5eb3
Endless <0x3f@0x3f3f3f3f.com>
2026-08-09 17:45:32 +0800

    添加写法二

--- leetcode/weekly/270/b/b.go
+++ leetcode/weekly/270/b/b.go
@@ -4,17 +4,16 @@ import . "github.com/EndlessCheng/codeforces-go/leetcode/testutil"
 
 // github.com/EndlessCheng/codeforces-go
 func deleteMiddle(head *ListNode) *ListNode {
-	if head.Next == nil { // 只有一个节点
-		return nil
-	}
+	dummy := ListNode{Next: head}
+
 	// 876. 链表的中间结点
-	// 本题先让兔子走两步，这样乌龟少走一步，刚好落在中间节点的前一个节点
-	slow := head
-	fast := head.Next.Next
+	slow := &dummy
+	fast := head
 	for fast != nil && fast.Next != nil {
 		slow = slow.Next
 		fast = fast.Next.Next
 	}
+
 	slow.Next = slow.Next.Next // 删除 slow 的下一个节点
-	return head
+	return dummy.Next
 }
