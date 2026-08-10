270a20d763e22bbce8e53a9a9c85636c3674fc26
Endless <0x3f@0x3f3f3f3f.com>
2026-08-10 15:52:39 +0800

    lc1927

--- leetcode/biweekly/56/c/c.go
+++ leetcode/biweekly/56/c/c.go
@@ -1,33 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
 // 原题 https://codeforces.com/problemset/problem/1215/D
 
 // github.com/EndlessCheng/codeforces-go
-func sumGame(s string) (ans bool) {
-	var sl, cl, sr, cr int
-	n := len(s)
-	for _, b := range s[:n/2] {
-		if b != '?' {
-			sl += int(b & 15)
+func sumGame(num string) bool {
+	m := len(num) / 2
+	d := 0
+	for i, ch := range num {
+		x := 9
+		if ch != '?' {
+			x = int(ch-'0') * 2
+		}
+		if i < m {
+			d += x
 		} else {
-			cl++
+			d -= x
 		}
 	}
-	for _, b := range s[n/2:] {
-		if b != '?' {
-			sr += int(b & 15)
-		} else {
-			cr++
+	return d != 0
+}
+
+func main() {
+	bobWin := 0
+	const n = 300
+	const t = 1000000
+	s := make([]byte, n)
+	for range t {
+		for i := range s {
+			v := rand.Intn(11)
+			if v == 10 {
+				s[i] = '?'
+			} else {
+				s[i] = '0' + byte(v)
+			}
+		}
+		if !sumGame(string(s)) {
+			bobWin++
 		}
 	}
-	if (cl+cr)&1 > 0 {
-		return true
-	}
-	if sl == sr {
-		return cl != cr
-	}
-	if cl > cr {
-		sl, cl, sr, cr = sr, cr, sl, cl
-	}
-	return sl != sr+(cr-cl)/2*9
+	fmt.Println(float64(bobWin) / t)
+	fmt.Println(1/ math.Sqrt(float64(n * 60) * math.Pi))
 }
