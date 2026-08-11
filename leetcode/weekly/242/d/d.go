f1c522ecf5b1884ad209a9293bedc96869d8008b
Endless <0x3f@0x3f3f3f3f.com>
2026-08-11 11:39:34 +0800

    lc1872

--- leetcode/weekly/242/d/d.go
+++ leetcode/weekly/242/d/d.go
@@ -1,22 +1,61 @@
 package main
 
+import "math"
+
 // github.com/EndlessCheng/codeforces-go
-func stoneGameVIII(a []int) int {
-	sum := 0
-	for _, v := range a {
-		sum += v
+func stoneGameVIII1(stones []int) int {
+	n := len(stones)
+	sum := make([]int, n)
+	sum[0] = stones[0]
+	for i := 1; i < n; i++ {
+		sum[i] = sum[i-1] + stones[i]
 	}
+	memo := make([]int, n-1)
+	for i := range memo {
+		memo[i] = math.MaxInt
+	}
+
+	var dfs func(int) int
+	dfs = func(i int) int {
+		if i == n-1 {
+			return sum[n-1]
+		}
+		p := &memo[i]
+		if *p == math.MaxInt {
+			*p = max(dfs(i+1), sum[i]-dfs(i+1))
+		}
+		return *p
+	}
+
+	return dfs(1)
+}
+
+func stoneGameVIII2(stones []int) int {
+	n := len(stones)
+	sum := make([]int, n)
+	sum[0] = stones[0]
+	for i := 1; i < n; i++ {
+		sum[i] = sum[i-1] + stones[i]
+	}
+
+	f := make([]int, n)
+	f[n-1] = sum[n-1]
+	for i := n - 2; i > 0; i-- {
+		f[i] = max(f[i+1], sum[i]-f[i+1])
+	}
+	return f[1]
+}
+
+func stoneGameVIII(stones []int) int {
+	sum := 0
+	for _, x := range stones {
+		sum += x
+	}
+
 	f := sum
-	for i := len(a) - 1; i > 1; i-- {
-		sum -= a[i]
+	for i := len(stones) - 2; i > 0; i-- {
+		sum -= stones[i+1]
 		f = max(f, sum-f)
 	}
 	return f
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
