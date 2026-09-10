7e4f89895c7c2bebe84ca571f299bf62737f7ca3
Endless <0x3f@0x3f3f3f3f.com>
2026-09-10 09:38:08 +0800

    cf1673F

---
+++ main/1600-1699/1673F.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	. "fmt"
+	"math/bits"
+)
+
+// https://github.com/EndlessCheng
+func cf1673F() {
+	var n, m int
+	Scan(&n, &m)
+	for range n {
+		for j := 1; j < n; j++ {
+			t := bits.TrailingZeros(uint(j))
+			Print(1<<(t*2+1), " ")
+		}
+		Println()
+	}
+	for i := 1; i < n; i++ {
+		for range n {
+			t := bits.TrailingZeros(uint(i))
+			Print(1<<(t*2), " ")
+		}
+		Println()
+	}
+	var x, y int
+	for range m {
+		var w int
+		Scan(&w)
+		for j := 4; j >= 0; j-- {
+			if w>>(j*2)&1 != 0 {
+				x ^= (1 << (j + 1)) - 1
+			}
+			if w>>(j*2+1)&1 != 0 {
+				y ^= (1 << (j + 1)) - 1
+			}
+		}
+		Println(x+1, y+1)
+	}
+}
+
+//func main() { cf1673F() }
