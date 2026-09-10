9b647225f7fff84db7e5cb276991406923cf2a29
Endless <0x3f@0x3f3f3f3f.com>
2026-09-10 18:45:38 +0800

    cf2200D

---
+++ main/2200-2299/2200D.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"io"
+	"slices"
+)
+
+// https://github.com/EndlessCheng
+func cf2200D(in io.Reader, _w io.Writer) {
+	out := bufio.NewWriter(_w)
+	defer out.Flush()
+	var T, n, x, y int
+	for Fscan(in, &T); T > 0; T-- {
+		Fscan(in, &n, &x, &y)
+		a := make([]int, n)
+		for i := range a {
+			Fscan(in, &a[i])
+		}
+
+		b := a[x:y]
+		i := slices.Index(b, slices.Min(b))
+		b = append(slices.Clone(b[i:]), b[:i]...)
+
+		a = append(a[:x], a[y:]...)
+		i = 0
+		for i < len(a) && a[i] < b[0] {
+			i++
+		}
+		a = append(append(slices.Clone(a[:i]), b...), a[i:]...)
+
+		for _, v := range a {
+			Fprint(out, v, " ")
+		}
+		Fprintln(out)
+	}
+}
+
+//func main() { cf2200D(bufio.NewReader(os.Stdin), os.Stdout) }
