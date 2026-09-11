7882ce2886ce297bf48666fc829e1f8418946750
Endless <0x3f@0x3f3f3f3f.com>
2026-09-12 06:38:35 +0800

    lc1422

--- leetcode/weekly/186/a/a.go
+++ leetcode/weekly/186/a/a.go
@@ -3,14 +3,31 @@ package main
 import "strings"
 
 func maxScore(s string) (ans int) {
-	max := func(a, b int) int {
-		if a > b {
-			return a
+	score := strings.Count(s, "1")
+	for _, c := range s[:len(s)-1] {
+		if c == '0' {
+			score++
+		} else {
+			score--
 		}
-		return b
-	}
-	for i := 1; i < len(s); i++ {
-		ans = max(ans, strings.Count(s[:i], "0")+strings.Count(s[i:], "1"))
+		ans = max(ans, score)
 	}
 	return
+}
+
+func maxScore2(s string) int {
+	n := len(s)
+	total1, delta := 0, 0
+	maxDelta := -1
+	for _, c := range s[:n-1] {
+		if c == '0' {
+			delta++
+		} else {
+			total1++
+			delta--
+		}
+		maxDelta = max(maxDelta, delta)
+	}
+	total1 += int(s[n-1] - '0')
+	return total1 + maxDelta
 }
