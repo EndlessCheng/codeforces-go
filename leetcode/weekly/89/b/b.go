edd893a0619dbdb6091d9b3d6300f6e0101a6453
Endless <0x3f@0x3f3f3f3f.com>
2026-09-12 15:58:35 +0800

    upd

--- leetcode/weekly/89/b/b.go
+++ leetcode/weekly/89/b/b.go
@@ -11,6 +11,25 @@ func carFleet1(target int, position, speed []int) int {
 	}
 	slices.SortFunc(a, func(a, b pair) int { return a.p - b.p })
 
+	st := []float64{}
+	for _, p := range a {
+		t := float64(target-p.p) / float64(p.v)
+		for len(st) > 0 && st[len(st)-1] <= t {
+			st = st[:len(st)-1]
+		}
+		st = append(st, t)
+	}
+	return len(st)
+}
+
+func carFleet2(target int, position, speed []int) int {
+	type pair struct{ p, v int }
+	a := make([]pair, len(position))
+	for i, p := range position {
+		a[i] = pair{p, speed[i]}
+	}
+	slices.SortFunc(a, func(a, b pair) int { return a.p - b.p })
+
 	st := []pair{}
 	for _, p := range a {
 		for len(st) > 0 {
@@ -25,23 +44,23 @@ func carFleet1(target int, position, speed []int) int {
 	return len(st)
 }
 
-func carFleet(target int, position, speed []int) int {
+func carFleet(target int, position, speed []int) (ans int) {
 	n := len(position)
 	type pair struct{ p, v int }
 	a := make([]pair, n)
 	for i, p := range position {
 		a[i] = pair{p, speed[i]}
 	}
-	slices.SortFunc(a, func(a, b pair) int { return a.p - b.p })
+	// 降序排序，这样下面可以正序遍历
+	slices.SortFunc(a, func(a, b pair) int { return b.p - a.p })
 
-	mx := a[n-1]
-	ans := 1
-	for i := n - 2; i >= 0; i-- {
-		p := a[i]
-		if (target-p.p)*mx.v > (target-mx.p)*p.v {
-			mx = p
+	maxT := 0.0
+	for _, p := range a {
+		t := float64(target-p.p) / float64(p.v)
+		if t > maxT {
+			maxT = t
 			ans++
 		}
 	}
-	return ans
+	return
 }
