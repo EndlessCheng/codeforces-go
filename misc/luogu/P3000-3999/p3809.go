52e75fb9e497edf04a1b45845021da8fb0d4182c
Endless <0x3f@0x3f3f3f3f.com>
2026-09-14 10:12:29 +0800

    upd

--- misc/luogu/P3000-3999/p3809.go
+++ misc/luogu/P3000-3999/p3809.go
@@ -12,13 +12,13 @@ import (
 // https://space.bilibili.com/206214
 func init() { debug.SetGCPercent(-1) }
 
-var t [6888896]byte
+var t809 [6888896]byte
 
 func p3809(in io.Reader, _w io.Writer) {
-	n, _ := syscall.Read(syscall.Stdin, t[:])
-	sa := t[:0]
+	n, _ := syscall.Read(syscall.Stdin, t809[:])
+	sa := t809[:0]
 	tmp := [7]byte{}
-	for _, v := range (*struct{_[]byte;sa[]int32})(unsafe.Pointer(suffixarray.New(bytes.TrimSpace(t[:n])))).sa {
+	for _, v := range (*struct{_[]byte;sa[]int32})(unsafe.Pointer(suffixarray.New(bytes.TrimSpace(t809[:n])))).sa {
 		p := len(tmp)
 		for v++; v > 0; v /= 10 {
 			p--
