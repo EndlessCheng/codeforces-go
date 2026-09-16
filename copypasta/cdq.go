2f81ff222782a0f161ac0d775b6b4522e1ba4557
Endless <0x3f@0x3f3f3f3f.com>
2026-09-16 08:50:13 +0800

    move

--- copypasta/cdq.go
+++ copypasta/cdq.go
@@ -2,7 +2,8 @@ package copypasta
 
 import "slices"
 
-/* CDQ 分治
+/*
+CDQ 分治
 https://oi-wiki.org/misc/cdq-divide/
 https://www.luogu.com.cn/blog/ljc20020730/cdq-fen-zhi-xue-xi-bi-ji
 https://codeforces.com/blog/entry/150934
@@ -21,6 +22,12 @@ https://codeforces.com/problemset/problem/848/C  2600 转化成带修二维数�
 https://codeforces.com/problemset/problem/1175/G 3000 斜率优化
 https://www.luogu.com.cn/problem/P2487 CDQ 优化 DP
 https://acm.hdu.edu.cn/showproblem.php?pid=5126 四维偏序
+
+
+值域分治
+LC4055 https://leetcode.cn/problems/count-shadow-pairs-ii/
+https://www.luogu.com.cn/problem/P13508
+
 */
 
 // 动态逆序对
@@ -98,4 +105,58 @@ func dynamicInversion(nums, del []int) (res []int) {
 		inv -= p.inv
 	}
 	return
+}
+
+/* 整体二分 Parallel Binary Search
+https://oi-wiki.org/misc/parallel-binsearch/
+https://codeforces.com/blog/entry/45578
+todo 整体二分解决静态区间第 k 小的优化 https://www.luogu.com/article/gbzqyzwn
+模板题 https://www.luogu.com.cn/problem/P3527
+https://www.luogu.com.cn/problem/P2617
+https://atcoder.jp/contests/agc002/tasks/agc002_d
+https://www.hackerrank.com/contests/hourrank-23/challenges/selective-additions/problem
+https://www.codechef.com/problems/MCO16504
+*/
+func parallelBinarySearch(n int, qs []struct{ l, r, v int }) []int {
+	// 读入询问时可以处理成左闭右开的形式
+	ans := make([]int, n)
+	tar := make([]int, n)
+	for i := range tar {
+		tar[i] = i
+	}
+	var f func([]int, int, int)
+	f = func(tar []int, ql, qr int) {
+		if len(tar) == 0 {
+			return
+		}
+		if ql+1 == qr {
+			for _, c := range tar {
+				ans[c] = ql // qr
+			}
+			return
+		}
+		qm := (ql + qr) / 2
+		for _, q := range qs[ql:qm] {
+			_ = q
+			// apply(q)
+
+		}
+
+		// 根据此刻查询的结果将 tar 分成左右两部分
+		var left, right []int
+		for _, who := range tar {
+			_ = who
+
+		}
+
+		for _, q := range qs[ql:qm] {
+			_ = q
+			// rollback(q)
+
+		}
+		f(left, ql, qm)
+		f(right, qm, qr)
+	}
+	f(tar, 0, len(qs)+1) // 这样可以将无法满足要求的 ans[i] 赋值为 len(qs)
+	return ans
 }
--- copypasta/sort.go
+++ copypasta/sort.go
@@ -685,58 +685,8 @@ func sortCollections() {
 	// WQS 二分
 	// 见 dp.go
 
-	// 整体二分 Parallel Binary Search
-	// https://oi-wiki.org/misc/parallel-binsearch/
-	// https://codeforces.com/blog/entry/45578
-	// todo 整体二分解决静态区间第 k 小的优化 https://www.luogu.com.cn/blog/2-6-5-3-5/zheng-ti-er-fen-xie-jue-jing-tai-ou-jian-di-k-xiao-di-you-hua
-	// 模板题 https://www.luogu.com.cn/problem/P3527
-	// todo https://atcoder.jp/contests/agc002/tasks/agc002_d
-	//  https://www.hackerrank.com/contests/hourrank-23/challenges/selective-additions/problem
-	//  https://www.codechef.com/problems/MCO16504
-	parallelBinarySearch := func(n int, qs []struct{ l, r, v int }) []int {
-		// 读入询问时可以处理成左闭右开的形式
-
-		ans := make([]int, n)
-		tar := make([]int, n)
-		for i := range tar {
-			tar[i] = i
-		}
-		var f func([]int, int, int)
-		f = func(tar []int, ql, qr int) {
-			if len(tar) == 0 {
-				return
-			}
-			if ql+1 == qr {
-				for _, c := range tar {
-					ans[c] = ql // qr
-				}
-				return
-			}
-			qm := (ql + qr) / 2
-			for _, q := range qs[ql:qm] {
-				_ = q
-				// apply(q)
-
-			}
-
-			// 根据此刻查询的结果将 tar 分成左右两部分
-			var left, right []int
-			for _, who := range tar {
-				_ = who
-
-			}
-
-			for _, q := range qs[ql:qm] {
-				_ = q
-				// rollback(q)
-
-			}
-			f(left, ql, qm)
-			f(right, qm, qr)
-		}
-		f(tar, 0, len(qs)+1) // 这样可以将无法满足要求的 ans[i] 赋值为 len(qs)
-		return ans
-	}
+	// 整体二分
+	// 见 cdq.go
 
 	// 倍增
 	// https://www.acwing.com/problem/content/description/111/
@@ -768,7 +718,6 @@ func sortCollections() {
 		kthSmallest, kthSmallestRangeSum, kthSubsetSum,
 		binarySearchF, ternarySearchF, ternarySearchInt, ternarySearchInt2,
 		search01,
-		parallelBinarySearch,
 		binaryLifting,
 	}
 }
