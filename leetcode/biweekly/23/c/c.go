dc64bbd02d820fc219100904c03b41d9b51dc023
Endless <0x3f@0x3f3f3f3f.com>
2026-09-19 07:35:25 +0800

    lc1401

--- leetcode/biweekly/23/c/c.go
+++ leetcode/biweekly/23/c/c.go
@@ -3,10 +3,19 @@ package main
 import "math"
 
 // github.com/EndlessCheng/codeforces-go
-func checkOverlap(r, ox, oy, x1, y1, x2, y2 int) (ans bool) {
+func checkOverlap1(r, ox, oy, x1, y1, x2, y2 int) (ans bool) {
 	rx, ry := float64(x1+x2)/2, float64(y1+y2)/2
 	hx, hy := float64(x2-x1)/2, float64(y2-y1)/2
 	x, y := math.Abs(float64(ox)-rx), math.Abs(float64(oy)-ry)
 	x, y = math.Max(x-hx, 0), math.Max(y-hy, 0)
 	return x*x+y*y < float64(r*r)+1e-8
+}
+
+func checkOverlap(radius, xCenter, yCenter, x1, y1, x2, y2 int) bool {
+	// 找到在矩形中的到圆心 (xCenter, yCenter) 最近的点 (x, y)
+	x := max(x1, min(xCenter, x2))
+	y := max(y1, min(yCenter, y2))
+
+	// 判断 (x, y) 是否在圆中
+	return (x-xCenter)*(x-xCenter)+(y-yCenter)*(y-yCenter) <= radius*radius
 }
