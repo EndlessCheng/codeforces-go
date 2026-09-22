e721321baf1f564f591150746617e8d9e818a273
Endless <0x3f@0x3f3f3f3f.com>
2026-09-22 08:40:06 +0800

    cf2048G

--- main/2000-2099/2048G.go
+++ main/2000-2099/2048G.go
@@ -25,28 +25,30 @@ func cf2048G(in io.Reader, _w io.Writer) {
 	var T, n, m, v int
 	for Fscan(in, &T); T > 0; T-- {
 		Fscan(in, &n, &m, &v)
-		ans := pow(v, n*m)
-		F := func(x, y int) int {
+		f := func(x, y int) int {
 			sum, C := 0, 1
 			for i := range n + 1 {
-				term := pow(v, n-i) * pow(x-1, i) % mod
-				term = (term - pow(v-y, n-i)*pow(x-y-1, i)%mod + mod) % mod
-				term = term * C % mod
+				x1 := pow(v, n-i) * pow(x-1, i) % mod
+				x2 := pow(v-y, n-i) * pow(x-y-1, i) % mod
+				x1 = (x1 - x2 + mod) % mod
+				x1 = pow(x1, m)
+				x1 = x1 * C % mod
 				if i&1 != 0 {
-					sum = (sum - term + mod) % mod
+					sum = (sum - x1 + mod) % mod
 				} else {
-					sum = (sum + term) % mod
+					sum = (sum + x1) % mod
 				}
 				C = C * (n - i) % mod * pow(i+1, mod-2) % mod
 			}
-			return sum * m % mod
+			return sum
 		}
 
+		ans := pow(v, n*m)
 		for i := 1; i < v; i++ {
-			ans = (ans - F(i+1, i) + mod) % mod
+			ans = (ans - f(i+1, i) + mod) % mod
 		}
 		for i := 2; i < v; i++ {
-			ans = (ans + F(i+1, i-1)) % mod
+			ans = (ans + f(i+1, i-1)) % mod
 		}
 		Fprintln(out, ans)
 	}
